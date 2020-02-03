2677f629321ca13e8e617352f6045ce3a49d77e1
Parth Shah <pshah@chanzuckerberg.com>
2020-02-03 12:05:06 -0800

    setup database secrets in secrets manager with config class

---
+++ infra/modules/backend/ledger/secrets.tf
@@ -0,0 +1,12 @@
+resource "aws_secretsmanager_secret" "ledger_database_secrets" {
+  name = "dcp/backend/ledger/${var.deployment_stage}/database"
+}
+
+resource "aws_secretsmanager_secret_version" "ledger_database_secrets" {
+  secret_id = aws_secretsmanager_secret.ledger_database_secrets.id
+  secret_string = <<SECRETS_JSON
+{
+  "database_uri": "postgresql://${aws_rds_cluster.ledger.master_username}:${aws_rds_cluster.ledger.master_password}@${aws_rds_cluster.ledger.endpoint}/${aws_rds_cluster.ledger.database_name}"
+}
+SECRETS_JSON
+}
