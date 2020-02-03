b0235f3c8ffeb7e846cd5c027acc0871373764b1
Parth Shah <pshah@chanzuckerberg.com>
2020-02-03 12:08:52 -0800

    fix up linting

--- dcp-prototype/backend/ledger/code/config/ledger_config.py
+++ dcp-prototype/backend/ledger/code/config/ledger_config.py
@@ -3,4 +3,6 @@ from dcplib.config import Config
 
 class LedgerDbConfig(Config):
     def __init__(self, *args, **kwargs):
-        super().__init__(component_name='backend/ledger', secret_name='database', **kwargs)
+        super().__init__(
+            component_name="backend/ledger", secret_name="database", **kwargs
+        )
