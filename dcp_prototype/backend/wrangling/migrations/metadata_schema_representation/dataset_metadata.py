f4559398db5a8193187b3a2e445392f574205651
maniarathi <arathi.mani@chanzuckerberg.com>
2020-02-03 17:15:25 -0800

    Hack to make linter and black happy

--- dcp_prototype/backend/wrangling/migrations/metadata_schema_representation/dataset_metadata.py
+++ dcp_prototype/backend/wrangling/migrations/metadata_schema_representation/dataset_metadata.py
@@ -67,9 +67,8 @@ class DatasetMetadata:
                 sheet_name,
                 entity_entries,
             ) in self.saved_full_metadata_structure.items():
-                entity_transformation_to_dictionary_format = \
-                    self._transform_metadata_entities_to_data_frame(
-                        entity_entries
+                entity_transformation_to_dictionary_format = self._transform_metadata_entities_to_data_frame(  # noqa
+                    entity_entries
                 )
                 entity_transformation_to_data_frame_format = DataFrame(
                     entity_transformation_to_dictionary_format,
