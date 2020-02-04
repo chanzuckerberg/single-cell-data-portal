1420acae640394b17e54ad5d7c19759ce617e5e4
Parth Shah <pshah@chanzuckerberg.com>
2020-02-03 16:28:42 -0800

    setup database secrets in secrets manager with config class (#47)
    * setup database secrets in secrets manager with config class
    
    * fix up linting
    
    * fix requirement file for flake8-black

---
+++ dcp-prototype/backend/ledger/code/config/ledger_config.py
@@ -0,0 +1,8 @@
+from dcplib.config import Config
+
+
+class LedgerDbConfig(Config):
+    def __init__(self, *args, **kwargs):
+        super().__init__(
+            component_name="backend/ledger", secret_name="database", **kwargs
+        )
