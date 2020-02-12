d6e184baeda30af8b2daf251869d0b41b70bb5b4
Trent Smith <trent.smith@chanzuckerberg.com>
2020-02-12 12:40:27 -0800

    wip

--- dcp-prototype/backend/browser/main.tf
+++ dcp-prototype/backend/browser/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = "=0.12.20"
 
   backend "s3" {
-    key     = "terraform/envs/dev/browser.tfstate"
+    key     = "terraform/envs/dev/browser_backend.tfstate"
     encrypt = true
     region  = "us-east-1"
     profile = "czi-hca-dev"
