27d2df3646fc202c32c6cdd547aaec24ff6065eb
Trent Smith <trent.smith@chanzuckerberg.com>
2020-02-14 09:48:03 -0800

    make it black

--- dcp_prototype/backend/browser/app.py
+++ dcp_prototype/backend/browser/app.py
@@ -6,7 +6,9 @@ from chalice import Chalice
 app = Chalice(app_name="browser")
 logger = logging.getLogger(__name__)
 
-swagger_spec_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), f'{os.environ["APP_NAME"]}-api.yml')
+swagger_spec_path = os.path.join(
+    os.path.abspath(os.path.dirname(__file__)), f'{os.environ["APP_NAME"]}-api.yml'
+)
 
 
 @app.route("/")
@@ -18,16 +20,16 @@ def index():
     return {"hello": "world"}
 
 
-@app.route("/projects",  methods=['GET'])
+@app.route("/projects", methods=["GET"])
 def projects():
     return 200
 
 
-@app.route("/projects/{project_uuid}",  methods=['GET'])
+@app.route("/projects/{project_uuid}", methods=["GET"])
 def get_project(project_uuid):
     return 200
 
 
-@app.route("/files/{file_uuid}", methods=['POST'])
+@app.route("/files/{file_uuid}", methods=["POST"])
 def post_file(file_uuid):
     return 200
