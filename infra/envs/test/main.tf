835be09f55b8b4e154184993a0ff672523ce1905
Parth Shah <pshah@chanzuckerberg.com>
2020-02-13 21:16:12 -0800

    test terraform env (#83)

---
+++ infra/envs/test/main.tf
@@ -0,0 +1,34 @@
+data "aws_caller_identity" "current" {}
+
+output "account_id" {
+  value = "${data.aws_caller_identity.current.account_id}"
+}
+
+terraform {
+  required_version = "=0.12.20"
+
+  backend "s3" {
+    key     = "terraform/envs/test/state.tfstate"
+    encrypt = true
+    region  = "us-east-1"
+    profile = "czi-hca-dev"
+  }
+}
+
+provider "aws" {
+  version = "~> 2.0"
+  region  = "us-east-1"
+  profile = "czi-hca-dev"
+}
+
+module "ledger" {
+  source = "../../modules/backend/ledger"
+
+  deployment_stage = "${var.deployment_stage}"
+
+  // Database
+  db_username = "${var.db_username}"
+  db_password = "${var.db_password}"
+  db_instance_count = "${var.db_instance_count}"
+  preferred_maintenance_window = "${var.preferred_maintenance_window}"
+}
