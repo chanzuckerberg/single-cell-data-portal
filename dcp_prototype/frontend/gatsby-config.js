e072e32e3928c0ec2b880cf86a225879d7ac7382
Colin Megill <colinmegill@gmail.com>
2020-02-19 07:48:46 -0800

    config image

--- dcp_prototype/frontend/gatsby-config.js
+++ dcp_prototype/frontend/gatsby-config.js
@@ -23,7 +23,7 @@ module.exports = {
         short_name: `starter`,
         start_url: `/`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        // icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
