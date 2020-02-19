af20a6ca462b99e325d3cfeb6deffe5b5925fadd
Madison Dunitz <madison.dunitz@chanzuckerberg.com>
2020-02-18 16:26:51 -0800

    script to copy data files between buckets

--- dcp_prototype/backend/wrangling/migrations/scripts/copy_data_files.py
+++ dcp_prototype/backend/wrangling/migrations/scripts/copy_data_files.py
@@ -1,18 +1,45 @@
-# import concurrent
+import concurrent.futures
 import json
 import os
+import typing
 
 import boto3
 from argparse import ArgumentParser
 
 
-def copy_between_s3_buckets(file_name, project):
+class FileMetadata:
+    FORMAT = "format"
+    CREATOR_UID = "creator_uid"
+    VERSION = "version"
+    CONTENT_TYPE = "content-type"
+    SIZE = "size"
+    CRC32C = "crc32c"
+    S3_ETAG = "s3_etag"
+    SHA1 = "sha1"
+    SHA256 = "sha256"
+
+
+def compose_blob_key(file_info: typing.Dict[str, str]) -> str:
+    """
+    Create the key for a blob, given the file metadata.
+    :param file_info: This can either be an object that contains the four keys (SHA256, SHA1, S3_ETAG, and CRC32C) in
+                      the key_class.
+    """
+    return "blobs/" + ".".join((
+        file_info[FileMetadata.SHA256],
+        file_info[FileMetadata.SHA1],
+        file_info[FileMetadata.S3_ETAG],
+        file_info[FileMetadata.CRC32C]
+    ))
+
+
+def copy_between_s3_buckets(key, project):
     s3 = boto3.resource('s3')
     copy_source = {
         'Bucket': 'org-hca-dss-prod',
-        'Key': f'files/{file_name}'
+        'Key': key
     }
-    s3.meta.client.copy(copy_source, 'dunitz-prod-copy', f'{project}/{file_name}')
+    s3.meta.client.copy(copy_source, 'dunitz-prod-copy', f'{project}/{key}')
 
 
 def list_data_files_for_project(input_directory, project_name):
@@ -23,13 +50,36 @@ def list_data_files_for_project(input_directory, project_name):
         if file.startswith("."):
             continue
         file_path = os.path.join(target_directory_path, file)
-        print(file_path)
         with open(file_path, "rb") as file_reader:
             contents = file_reader.read()
             files = json.loads(contents)
             for file_info in files['files']:
-                if file_info["content-type"].find("'application/json; dcp-type=\"metadata*\"'")
-    return []
+                if "metadata" in file_info["content-type"]:
+                    continue
+                else:
+                    project_data_files.append(file_info)
+    return project_data_files
+
+
+def move_data_files(project_data_files, project):
+    files_moved = 0
+    dispatch_executor_class = concurrent.futures.ThreadPoolExecutor
+    with dispatch_executor_class(max_workers=1) as executor:
+        print(f"{len(project_data_files)} files to copy over for {project}")
+        futures = []
+        for file_info in project_data_files:
+            blob_name = compose_blob_key(file_info)
+            f = executor.submit(copy_between_s3_buckets, blob_name, project)
+            futures.append(f)
+
+        for future in concurrent.futures.as_completed(futures):
+            try:
+                extract_result = future.result()
+                files_moved += 1
+                if files_moved % 50 == 0:
+                    print(f"{files_moved} files moved (out of {len(project_data_files)} for project {project}")
+            except Exception as e:
+                print(f"Something went wrong: {e}")
 
 
 if __name__ == "__main__":
@@ -40,27 +90,12 @@ if __name__ == "__main__":
         nargs="+",
         required=True,
         help="A data directory containing bundles with files that contain both the "
-        "metadata and the data of a single project.",
+             "metadata and the data of a single project.",
     )
     arguments = parser.parse_args()
 
     if arguments.input_directory:
         input_directory = arguments.input_directory[0]
     for project in os.listdir(input_directory):
-        project_file_list = list_data_files_for_project(input_directory, project)
-    # dispatch_executor_class = concurrent.futures.ThreadPoolExecutor
-    projects = []
-    # with dispatch_executor_class(max_workers=50) as executor:
-    #     for project in projects:
-    #         futures = []
-    #         f = executor.submit(list_data_files_for_project, project)
-    #         futures.append(f)
-    #
-    #         for future in concurrent.futures.as_completed(futures):
-    #             try:
-    #                 extract_result = future.result()
-    #                 for file in extract_result:
-    #                     file_name = f"{file['uuid']}.{file['version']}"
-    #                     copy_between_s3_buckets(file_name, project)
-    #             except Exception as e:
-    #                 print(f"Something went wrong: {e}")
+        project_data_files = list_data_files_for_project(input_directory, project)
+        move_data_files(project_data_files, project)
