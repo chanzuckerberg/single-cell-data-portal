d3529ffe32d8c57dd4e76f34894e231c84af11b9
Calvin Nhieu <cnhieu@chanzuckerberg.com>
2020-02-20 14:54:48 -0800

    Implement Browser API (#119)
    * implement mvp rds-backed browser api
    
    * fix CORS issue
    
    * fix chalice Makefile
    
    * include chalice vendor.in
    
    * fix lint
    
    * remove unnecessary vendor packages
    
    * refactor browser code dirs
    
    * move browser requirements-dev.txt to top level
    
    * move db session to global namespace
    
    * refactor rds dir to common
    
    * fix api Makefile
    
    * create browser db_utils module
    
    * fix lint

--- dcp_prototype/backend/browser/scripts/create_db.py
+++ dcp_prototype/backend/browser/scripts/create_db.py
@@ -6,8 +6,8 @@ from sqlalchemy import create_engine
 pkg_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))  # noqa
 sys.path.insert(0, pkg_root)  # noqa
 
-from browser.config.db_config import BrowserDbConfig
-from browser.rds import browser_orm as orm
+from browser.code.config.db_config import BrowserDbConfig
+from browser.code.common import browser_orm as orm
 
 
 if __name__ == "__main__":
