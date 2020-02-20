a8552805e5e2253dcd44d6a7d0bc1f0dedc3ebd7
Calvin Nhieu <calvin.nhieu@gmail.com>
2020-02-20 13:29:37 -0800

    create browser db_utils module

--- dcp_prototype/backend/browser/api/app.py
+++ dcp_prototype/backend/browser/api/app.py
@@ -7,11 +7,10 @@ from chalice import Chalice
 pkg_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "chalicelib"))  # noqa
 sys.path.insert(0, pkg_root)  # noqa
 
-from browser.code.common.browser_orm import (DBSessionMaker,
-                                             Project, File,
-                                             LibraryPrepProtocol, Tissue, Species,
-                                             LibraryPrepProtocolJoinProject, TissueJoinProject,
-                                             SpeciesJoinProject)
+from browser.code.common.browser_orm import DBSessionMaker, Project, File
+from browser.code.common.db_utils import (_get_project_assays,
+                                          _get_project_tissues,
+                                          _get_project_species)
 
 app = Chalice(app_name='browser-api')
 session = DBSessionMaker().session()
@@ -114,35 +113,3 @@ def get_file(file_id):
                             body=response_body)
 
 
-def _get_project_assays(project_id):
-    assays = []
-    for result in session.query(
-        LibraryPrepProtocolJoinProject, LibraryPrepProtocol
-    ).filter(
-        (LibraryPrepProtocolJoinProject.library_prep_protocol_id ==
-         LibraryPrepProtocol.id),
-        LibraryPrepProtocolJoinProject.project_id == project_id
-    ).all():
-        assays.append(result.LibraryPrepProtocol.construction_method_ontology)
-
-    return assays
-
-
-def _get_project_tissues(project_id):
-    tissues = []
-    for result in session.query(TissueJoinProject, Tissue).filter(
-            TissueJoinProject.tissue_id == Tissue.id,
-            TissueJoinProject.project_id == project_id).all():
-        tissues.append(result.Tissue.tissue_ontology)
-
-    return tissues
-
-
-def _get_project_species(project_id):
-    species = []
-    for result in session.query(SpeciesJoinProject, Species).filter(
-            SpeciesJoinProject.species_id == Species.id,
-            SpeciesJoinProject.project_id == project_id).all():
-        species.append(result.Species.species_ontology)
-
-    return species
