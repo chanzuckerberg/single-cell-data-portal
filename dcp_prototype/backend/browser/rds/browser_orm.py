ca5b28ab0aa4f3bf1eff98a12657b5647dc2a15d
Calvin Nhieu <cnhieu@chanzuckerberg.com>
2020-02-19 16:34:50 -0800

    Define browser db schema and populate from a sample artifact (#108)
    * store browser db credentials secretsmanager
    
    * fix rds mysql port
    
    * create browser db and schema
    
    * load browser db via sample artifact
    
    * address PR comments
    create browser/scripts
    create browser/config
    create create_db.py
    
    * fix lint
    
    * fix comment
    
    * add created_at and updated_at fields to all browser db tables

---
+++ dcp_prototype/backend/browser/rds/browser_orm.py
@@ -0,0 +1,202 @@
+import os
+import sys
+
+from sqlalchemy import (
+    create_engine,
+    Column,
+    DateTime,
+    ForeignKey,
+    Integer,
+    String,
+    text,
+)
+from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+pkg_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))  # noqa
+sys.path.insert(0, pkg_root)  # noqa
+
+from browser.config.db_config import BrowserDbConfig
+
+
+Base = declarative_base()
+
+
+class DBSessionMaker:
+    def __init__(self):
+        engine = create_engine(BrowserDbConfig().database_uri)
+        Base.metadata.bind = engine
+        self.session_maker = sessionmaker()
+        self.session_maker.bind = engine
+
+    def session(self, **kwargs):
+        return self.session_maker(**kwargs)
+
+
+class Project(Base):
+    __tablename__ = "project"
+
+    id = Column(String(64), primary_key=True)
+    title = Column(String(255))
+    label = Column(String(100))
+    description = Column(String(3000))
+    category = Column(String(32))
+    developmental_stage = Column(String(32))
+    disease_ontology = Column(String(16))
+    sample_type = Column(String(32))
+    organ_part = Column(String(32))
+    analysis_protocol = Column(String(64))
+    cell_count = Column(Integer)
+    donor_count = Column(Integer)
+    publication_title = Column(String(128))
+    publication_doi = Column(String(32))
+    contact_name = Column(String(32))
+    contact_institution = Column(String(64))
+    contact_email = Column(String(32))
+    created_at = Column(DateTime(True), nullable=False, server_default=text("now()"))
+    updated_at = Column(DateTime(True), nullable=False, server_default=text("now()"))
+
+
+class File(Base):
+    __tablename__ = "file"
+
+    id = Column(String(64), primary_key=True)
+    project_id = Column(ForeignKey("project.id"), nullable=False)
+    filename = Column(String(80))
+    file_format = Column(String(8))
+    file_size = Column(Integer)
+    flowcell_id = Column(String(64))
+    lane_index = Column(String(8))
+    read_index = Column(String(8))
+    s3_uri = Column(String(200))
+    created_at = Column(DateTime(True), nullable=False, server_default=text("now()"))
+    updated_at = Column(DateTime(True), nullable=False, server_default=text("now()"))
+    species = Column(String(16))
+    library_construction_method_ontology = Column(String(16))
+    tissue_ontology = Column(String(16))
+    paired_end_sequencing = Column(String(16))
+    instrument_manufacturer_model_ontology = Column(String(32))
+    file_output_type = Column(String(32))
+    software = Column(String(32))
+    algorithm = Column(String(32))
+    genome_reference = Column(String(32))
+    genomic_annotation = Column(String(32))
+    genomic_annotation_biotypes = Column(String(32))
+    quantification_software = Column(String(32))
+
+
+class LibraryPrepProtocol(Base):
+    __tablename__ = "library_prep_protocol"
+
+    id = Column(Integer, primary_key=True)
+    construction_method_ontology = Column(String(16))
+    end_bias = Column(String(16))
+    nucleic_acid_source = Column(String(16))
+    created_at = Column(DateTime(True), nullable=False, server_default=text("now()"))
+    updated_at = Column(DateTime(True), nullable=False, server_default=text("now()"))
+
+
+class Tissue(Base):
+    __tablename__ = "tissue"
+
+    id = Column(Integer, primary_key=True)
+    tissue_ontology = Column(String(16))
+    created_at = Column(DateTime(True), nullable=False, server_default=text("now()"))
+    updated_at = Column(DateTime(True), nullable=False, server_default=text("now()"))
+
+
+class Species(Base):
+    __tablename__ = "species"
+
+    id = Column(Integer, primary_key=True)
+    species_ontology = Column(String(16))
+    created_at = Column(DateTime(True), nullable=False, server_default=text("now()"))
+    updated_at = Column(DateTime(True), nullable=False, server_default=text("now()"))
+
+
+class DataRepository(Base):
+    __tablename__ = "data_repository"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(32))
+    created_at = Column(DateTime(True), nullable=False, server_default=text("now()"))
+    updated_at = Column(DateTime(True), nullable=False, server_default=text("now()"))
+
+
+class Contributor(Base):
+    __tablename__ = "contributor"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(32))
+    institution = Column(String(32))
+    email = Column(String(32))
+    created_at = Column(DateTime(True), nullable=False, server_default=text("now()"))
+    updated_at = Column(DateTime(True), nullable=False, server_default=text("now()"))
+
+
+class ExternalAccession(Base):
+    __tablename__ = "external_accession"
+
+    id = Column(Integer, primary_key=True)
+    project_id = Column(ForeignKey("project.id"), nullable=False)
+    data_repository_id = Column(ForeignKey("data_repository.id"), nullable=False)
+    accession = Column(String(32))
+    created_at = Column(DateTime(True), nullable=False, server_default=text("now()"))
+    updated_at = Column(DateTime(True), nullable=False, server_default=text("now()"))
+
+    project = relationship("Project")
+    data_repository = relationship("DataRepository")
+
+
+class LibraryPrepProtocolJoinProject(Base):
+    __tablename__ = "library_prep_protocol_join_project"
+
+    id = Column(Integer, primary_key=True)
+    library_prep_protocol_id = Column(
+        ForeignKey("library_prep_protocol.id"), nullable=False
+    )
+    project_id = Column(ForeignKey("project.id"), nullable=False)
+    created_at = Column(DateTime(True), nullable=False, server_default=text("now()"))
+    updated_at = Column(DateTime(True), nullable=False, server_default=text("now()"))
+
+    library_prep_protocol = relationship("LibraryPrepProtocol")
+    project = relationship("Project")
+
+
+class TissueJoinProject(Base):
+    __tablename__ = "tissue_join_project"
+
+    id = Column(Integer, primary_key=True)
+    tissue_id = Column(ForeignKey("tissue.id"), nullable=False)
+    project_id = Column(ForeignKey("project.id"), nullable=False)
+    created_at = Column(DateTime(True), nullable=False, server_default=text("now()"))
+    updated_at = Column(DateTime(True), nullable=False, server_default=text("now()"))
+
+    tissue = relationship("Tissue")
+    project = relationship("Project")
+
+
+class SpeciesJoinProject(Base):
+    __tablename__ = "species_join_project"
+
+    id = Column(Integer, primary_key=True)
+    species_id = Column(ForeignKey("species.id"), nullable=False)
+    project_id = Column(ForeignKey("project.id"), nullable=False)
+    created_at = Column(DateTime(True), nullable=False, server_default=text("now()"))
+    updated_at = Column(DateTime(True), nullable=False, server_default=text("now()"))
+
+    species = relationship("Species")
+    project = relationship("Project")
+
+
+class ContributorJoinProject(Base):
+    __tablename__ = "contributor_join_project"
+
+    id = Column(Integer, primary_key=True)
+    contributor_id = Column(ForeignKey("contributor.id"), nullable=False)
+    project_id = Column(ForeignKey("project.id"), nullable=False)
+    created_at = Column(DateTime(True), nullable=False, server_default=text("now()"))
+    updated_at = Column(DateTime(True), nullable=False, server_default=text("now()"))
+
+    contributor = relationship("Contributor")
+    project = relationship("Project")
---
+++ dcp_prototype/backend/browser/scripts/load_artifact.py
@@ -0,0 +1,168 @@
+"""
+(2/19/20) Temporary script used to load a preliminary artifact CSV sample.
+          To be succeeded by ETL job(s) from a JSON artifact.
+"""
+
+import os
+import pandas
+import sys
+from math import ceil
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+pkg_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))  # noqa
+sys.path.insert(0, pkg_root)  # noqa
+
+from browser.config.db_config import BrowserDbConfig
+
+stage = os.environ["DEPLOYMENT_STAGE"]
+db_name = f"browser_{stage}"
+
+Base = declarative_base()
+engine = create_engine(BrowserDbConfig().database_uri)
+conn = engine.connect()
+engine.execute(f"USE {db_name}")
+
+# project
+projects_df = pandas.read_csv("projects.csv")
+for index, row in projects_df.iterrows():
+    project_id = row["project_id"]
+    title = row["project_title"]
+    label = "Single cell transcriptome analysis of human pancreas"
+    description = (
+        "As organisms age, cells accumulate genetic and "
+        "epigenetic changes that eventually lead to "
+        "impaired organ function or catastrophic failure "
+        "such as cancer. Here we describe a single-cell "
+        "transcriptome analysis of 2544 human pancreas "
+        "cells from donors, spanning six decades of life. "
+        "We find that islet cells from older donors have "
+        "increased levels of disorder as measured both "
+        "by noise in the transcriptome and by the number "
+        "of cells which display inappropriate hormone "
+        "expression, revealing a transcriptional instability "
+        "associated with aging. By analyzing the spectrum "
+        "of somatic mutations in single cells from "
+        "previously-healthy donors, we find a specific "
+        "age-dependent mutational signature characterized "
+        "by C to A and C to G transversions, indicators "
+        "of oxidative stress, which is absent in single "
+        "cells from human brain tissue or in a tumor cell "
+        "line. Cells carrying a high load of such mutations "
+        "also express higher levels of stress and senescence "
+        "markers, including FOS, JUN, and the cytoplasmic "
+        "superoxide dismutase SOD1, markers previously linked "
+        "to pancreatic diseases with substantial age-dependent "
+        "risk, such as type 2 diabetes mellitus and adenocarcinoma. "
+        "Thus, our single-cell approach unveils gene expression "
+        "changes and somatic mutations acquired in aging human "
+        "tissue, and identifies molecular pathways induced by "
+        "these genetic changes that could influence human "
+        "disease. Also, our results demonstrate the feasibility "
+        "of using single-cell RNA-seq data from primary "
+        "cells to derive meaningful insights into the "
+        "genetic processes that operate on aging human "
+        "tissue and to determine which molecular mechanisms "
+        "are coordinated with these processes. Examination "
+        "of single cells from primary human pancreas tissue"
+    )
+    category = row["category"]
+    developmental_stage = row["developmental_stage"]
+    disease_ontology = row["disease_ontology"]
+    sample_type = "specimens"
+    organ_part = "islet of Langerhans"
+    analysis_protocol = "smartseq2_v2.3.0,smartseq2_v2.4.0"
+    cell_count = 2544
+    donor_count = 8
+    publication_title = row["publication_title"]
+    publication_doi = row["publication_doi"]
+    contact_name = row["contributor_name"]
+    contact_institution = row["contributor_lab"]
+    contact_email = row["contributor_email"]
+
+    organ_ontology = row["organ_ontology"]
+    species = row["species"]
+    external_accessions = row["external_accessions"]
+    library_construction_method_ontology = row["library_construction_method_ontology"]
+    nucleic_acid_source = row["nucleic_acid_source"]
+    end_bias = row["end_bias"]
+
+    engine.execute(
+        f"INSERT INTO project VALUES ("
+        f"'{project_id}', '{title}', '{label}', '{description}', '{category}', "
+        f"'{developmental_stage}', '{disease_ontology}', '{sample_type}', "
+        f"'{organ_part}', '{analysis_protocol}', {cell_count}, {donor_count}, "
+        f"'{publication_title}', '{publication_doi}', '{contact_name}', "
+        f"'{contact_institution}', '{contact_email}', DEFAULT, DEFAULT)"
+    )
+
+    # library prep protocol
+    engine.execute(
+        f"INSERT INTO library_prep_protocol VALUES ("
+        f"NULL, '{library_construction_method_ontology}', "
+        f"'{end_bias}', '{nucleic_acid_source}', DEFAULT, DEFAULT)"
+    )
+
+    # tissue
+    engine.execute(
+        f"INSERT INTO tissue VALUES (NULL, '{organ_ontology}', DEFAULT, DEFAULT)"
+    )
+
+    # species
+    engine.execute(f"INSERT INTO species VALUES (NULL, '{species}', DEFAULT, DEFAULT)")
+
+    # data repository
+
+    # contributor
+
+    # external accession
+
+    # lpp x project
+    engine.execute(
+        f"INSERT INTO library_prep_protocol_join_project VALUES "
+        f"(NULL, 1, '{project_id}', DEFAULT, DEFAULT)"
+    )
+
+    # tissue x project
+    engine.execute(
+        f"INSERT INTO tissue_join_project VALUES "
+        f"(NULL, 1, '{project_id}', DEFAULT, DEFAULT)"
+    )
+
+    # species x project
+    engine.execute(
+        f"INSERT INTO species_join_project VALUES "
+        f"(NULL, 1, '{project_id}', DEFAULT, DEFAULT)"
+    )
+
+    # contributor x project
+
+
+# file
+files_df = pandas.read_csv("files.csv")
+files_df["file_size"].fillna(0, inplace=True)
+files_df.fillna("", inplace=True)
+files_df.insert(
+    1,
+    "project_id",
+    ["HCA-Project-cddab57b-6868-4be4-806f-395ed9dd635a"] * files_df.shape[0],
+)
+
+items = []
+for index, row in files_df.iterrows():
+    items.append(
+        ", ".join([f"'{str(v)}'" if isinstance(v, str) else str(v) for v in row.array])
+    )
+
+chunk_size = 100
+num_chunks = ceil(len(items) / chunk_size)
+for i in range(num_chunks):
+    chunk = items[
+        i * chunk_size : len(items)
+        if len(items) < i * chunk_size + chunk_size
+        else i * chunk_size + chunk_size
+    ]
+    values = ", ".join([f"({item})" for item in chunk])
+    engine.execute(f"INSERT INTO file VALUES {values}")
+    print(i * chunk_size)
