ce70a6b28dc53c817b2b459df591113d0cc63a27
Brian McCandless <bmccandless@chanzuckerberg.com>
2020-02-20 15:35:57 -0800

    Improve speedup of migration script (#85)
    I experimented with speeding up the access to S3 objects.
    I found that using the lower level Client.get_object() interface
    is a bit faster than the Resources.Object() interface.
    In the pancreas dataset the speedup was 30% from this change (using 20 threads)
    
    Also having a thread local client is better practice than
    using a shared client, although it did not have much impact on performance.

--- dcp_prototype/backend/wrangling/migrations/scripts/migrate_dcp_one_to_dcp_two.py
+++ dcp_prototype/backend/wrangling/migrations/scripts/migrate_dcp_one_to_dcp_two.py
@@ -80,6 +80,11 @@ def gather_group_file_list(file_list):
 
 
 def consume_file(prefix, bucket, filequeue, dataset_metadata):
+
+    # thread local session and client
+    session = boto3.session.Session()
+    s3_client = session.client("s3")
+
     while True:
         filename = filequeue.get()
 
@@ -96,8 +101,8 @@ def consume_file(prefix, bucket, filequeue, dataset_metadata):
         qsize = filequeue.qsize()
         if qsize % 1000 == 0 and qsize > 0:
             print(f"Working with JSON input file: {file_prefix}, queued size={qsize}")
-        object = S3_RESOURCE.Object(bucket, file_prefix)
-        object_body = object.get()["Body"].read()
+        object = s3_client.get_object(Bucket=bucket, Key=file_prefix)
+        object_body = object["Body"].read()
 
         tsv_generated_data_frame = json_normalize(flatten(json.loads(object_body), separator="."))
         entity_type = get_entity_type(filename)
