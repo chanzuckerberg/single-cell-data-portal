01948cfdf886d78365a1713aff2cf7449f3b0045
Calvin Nhieu <calvin.nhieu@gmail.com>
2020-03-07 23:19:22 -0500

    create basic matrix download flow

---
+++ dcp_prototype/frontend/src/components/exploreData.js
@@ -0,0 +1,29 @@
+import React from "react"
+import { Flex, Box, Button, Heading, Text } from "theme-ui"
+
+import { api_prefix } from "../globals"
+
+const ExploreData = ({ project, files }) => {
+  const matrix_files = files.filter(file => (
+    file.file_format === "loom"
+  ))
+
+  return (
+    <Box>
+      <Heading as="h3" sx={{ mb: 4 }}>
+        Explore data
+      </Heading>
+      <Flex>
+        <Button onClick={() => fetch(`${api_prefix}/files/${matrix_files[0].id}`)
+            .then(response => response.json()) // parse JSON from request
+            .then(resultData => {
+              console.log(resultData)
+              window.open(resultData.url)
+            })}
+        >Download matrix</Button>
+        <Button>Open in cellxgene</Button>
+      </Flex>
+    </Box>
+  )
+}
+export default ExploreData
