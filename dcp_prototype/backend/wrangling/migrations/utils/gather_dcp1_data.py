451588f5d4ae541c2a038d9a56e031a1865cad44
Brian McCandless <bmccandless@chanzuckerberg.com>
2020-03-09 15:51:34 -0700

    Remove dead code and lint

--- dcp_prototype/backend/wrangling/migrations/utils/gather_dcp1_data.py
+++ dcp_prototype/backend/wrangling/migrations/utils/gather_dcp1_data.py
@@ -2,13 +2,9 @@ import sys
 
 sys.path.insert(0, "")  # noqa
 import boto3
-from argparse import ArgumentParser
 from urllib.parse import urlparse
 import os.path
 import json
-
-from dcp_prototype.backend.wrangling.migrations.utils.migration_utils import DatasetMetadata
-
 import queue
 import threading
 import tarfile
@@ -214,114 +210,3 @@ def generate_metadata_structure_from_targz(input_source, num_threads, dataset_me
                 print("processed ", index)
 
     return dataset_metadata
-
-
-def main():
-    parser = ArgumentParser()
-    parser.add_argument(
-        "-i",
-        "--input-source",
-        required=True,
-        help="input_source contains all metadata files that were part of a single DCP 1.0 project."
-             " It may be one of the following:  An s3 bucket containing json files."
-             " A directory containing json files."
-             " An s3 object in the tar.gz format containing the json files."
-             " Or a local tar.gz file containing the json files.",
-    )
-
-    parser.add_argument(
-        "-t", "--threads", required=False, default=1, type=int, help="Number of threads to use when reading files"
-    )
-
-    parser.add_argument(
-        "-o",
-        "--output-file",
-        default=None,
-        help="If provided, the json output will be save at this file.  "
-             "If not provided, the json output will go to stdout",
-    )
-
-    parser.add_argument(
-        "-a",
-        "--append",
-        action="store_true",
-        help="If provided, the json output will be save at this file.  "
-             "If not provided, the json output will go to stdout",
-    )
-    arguments = parser.parse_args()
-
-    input_source = arguments.input_source
-    num_threads = arguments.threads
-
-    if "s3" in input_source:
-
-        bucket = urlparse(input_source).netloc
-        path = urlparse(input_source).path
-        if path.startswith("/"):
-            path = path[1:]
-
-        if input_source.endswith(".tar.gz"):
-            input_tarfile = os.path.basename(path)
-            if os.path.exists(input_tarfile):
-                print("Error, cannot overwrite {input_tarfile}")
-                sys.exit(1)
-            s3_client = boto3.client("s3")
-            s3_client.download_file(bucket, path, input_tarfile)
-            dataset_metadata = DatasetMetadata()
-            generate_metadata_structure_from_targz(input_tarfile, num_threads, dataset_metadata)
-        else:
-            if not input_source.endswith("/"):
-                input_source += "/"
-
-            dataset_metadata = DatasetMetadata()
-            generate_metadata_structure_from_s3_uri(input_source, num_threads, dataset_metadata)
-
-    else:
-        dataset_metadata = DatasetMetadata()
-        if input_source.endswith(".tar.gz"):
-            input_tarfile = input_source
-            generate_metadata_structure_from_targz(input_tarfile, num_threads, dataset_metadata)
-        else:
-            generate_metadata_structure_from_dir(input_source, num_threads, dataset_metadata)
-
-    dataset_metadata.process()
-    result_project = dataset_metadata.to_dict()
-
-    okay = dataset_metadata.validate(result_project)
-    if not okay:
-        print("result failed schema validation")
-        sys.exit(1)
-
-    for key, value in dataset_metadata.missing.items():
-        print("MISSING:", key, value)
-
-    out_dict = result_project
-    title = out_dict.get("projects")[0].get("title")
-    output_file = arguments.output_file
-    if output_file:
-        if arguments.append and os.path.exists(output_file):
-            with open(output_file) as json_file:
-                data = json.load(json_file)
-                projects = data.get("projects")
-                if projects is None:
-                    print(f"expected 'projects' in {output_file}")
-                    sys.exit(1)
-
-                do_append = True
-                for index, project in enumerate(projects):
-                    if project.get("title") == title:
-                        print(f"replaced project {title} in {output_file}")
-                        projects[index] = out_dict
-                        do_append = False
-                        break
-                if do_append:
-                    print(f"append project {title} in {output_file}")
-                    projects.append(out_dict)
-                out_dict = data
-        else:
-            print(f"write project {title} to {output_file}")
-
-        with open(arguments.output_file, "w") as json_file:
-            json_file.write(json.dumps(out_dict, indent=2))
-    else:
-        print(json.dumps(out_dict, indent=2))
