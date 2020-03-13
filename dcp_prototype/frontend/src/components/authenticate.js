7a06db66ff83d341448797faf00c6530ad636b4b
Trent Smith <trent.smith@chanzuckerberg.com>
2020-03-13 16:11:03 -0700

    handling async edge cases

--- dcp_prototype/frontend/src/components/authenticate.js
+++ dcp_prototype/frontend/src/components/authenticate.js
@@ -1,29 +1,36 @@
 import React from "react"
-import {useAuth0} from "../contexts/auth0Context"
+import { useAuth0 } from "../contexts/auth0Context"
 import { Box, Button, Image } from "theme-ui"
 
 const Authenticate = () => {
-  const {isAuthenticated, user, logout, loginWithRedirect} = useAuth0()
+  const { isAuthenticated, loading, user, logout, loginWithRedirect } = useAuth0()
 
-  return isAuthenticated? (
+  return (
     <>
-      <Button onClick={logout}>
-        Logout
-      </Button>
+      {!loading && user && (
+        <>
+          <Button onClick={logout}>
+            Logout
+          </Button>
 
-      {!user ? (<Box>...</Box>) : (
-          <Image src={user.picture}
-                 width="48"
-                 height="48"
-          />
-        )}
+          {!user ? (<Box>...</Box>) : (
+            <Image src={user.picture}
+                   width="48"
+                   height="48"
+            />
+          )}
+        </>
+      )}
+      {!loading && !isAuthenticated && (
+        <>
+          <Button onClick={loginWithRedirect}>
+            Login/Register
+          </Button>
+        </>
+      )}
+    </>
+  )
 
-    </>):
-    (<>
-      <Button onClick={loginWithRedirect}>
-        Login/Register
-      </Button>
-      </>)
 }
 
 export default Authenticate
