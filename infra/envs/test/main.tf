1f983bc746078ed8537ed36dae8a7aa9aeeaa142
Calvin Nhieu <calvin.nhieu@gmail.com>
2020-04-03 15:55:19 -0700

    fix lint

--- infra/envs/test/main.tf
+++ infra/envs/test/main.tf
@@ -48,8 +48,8 @@ module "browser_site_cert" {
   source = "github.com/chanzuckerberg/cztack//aws-acm-cert?ref=v0.29.0"
 
   # the cert domain name
-  cert_domain_name = "browser-testing.dev.single-cell.czi.technology"
-  cert_subject_alternative_names = {"www.browser-testing.dev.single-cell.czi.technology"= "Z3GU3D81Z7R7CN"}
+  cert_domain_name               = "browser-testing.dev.single-cell.czi.technology"
+  cert_subject_alternative_names = { "www.browser-testing.dev.single-cell.czi.technology" = "Z3GU3D81Z7R7CN" }
 
   # the route53 zone for validating the `cert_domain_name`
   aws_route53_zone_id = "Z3GU3D81Z7R7CN"
@@ -64,10 +64,10 @@ module "browser_site_cert" {
 module "browser_frontend" {
   source = "github.com/chanzuckerberg/cztack//aws-single-page-static-site?ref=v0.29.0"
 
-  aws_route53_zone_id            = "Z3GU3D81Z7R7CN"
-  aws_acm_cert_arn = "${module.browser_site_cert.arn}"
-  bucket_name = "dcp-static-site-${var.deployment_stage}-${data.aws_caller_identity.current.account_id}"
-  subdomain = "browser-testing"
+  aws_route53_zone_id = "Z3GU3D81Z7R7CN"
+  aws_acm_cert_arn    = "${module.browser_site_cert.arn}"
+  bucket_name         = "dcp-static-site-${var.deployment_stage}-${data.aws_caller_identity.current.account_id}"
+  subdomain           = "browser-testing"
 
   # Variables used for tagging
   env     = "${var.deployment_stage}"
