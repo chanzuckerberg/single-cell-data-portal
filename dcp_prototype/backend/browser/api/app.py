72f8aa798e229da28243a33dbd771bf3366c46ab
Trent Smith <trent.smith@chanzuckerberg.com>
2020-04-09 13:15:37 -0700

    Revert "Authorization header allowed"
    This reverts commit c2cb78648b392ef7a961576557a3562301564a6d.

--- dcp_prototype/backend/browser/api/app.py
+++ dcp_prototype/backend/browser/api/app.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 import chalice
-from chalice import Chalice, CORSConfig
+from chalice import Chalice
 
 pkg_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "chalicelib"))  # noqa
 sys.path.insert(0, pkg_root)  # noqa
@@ -12,53 +12,48 @@ from browser.code.common.s3_utils import generate_file_url
 
 app = Chalice(app_name=f"{os.environ['APP_NAME']}-{os.environ['DEPLOYMENT_STAGE']}")
 
-cors_config = CORSConfig(
-    allow_origin="*",
-    max_age=600,
-    allow_credentials=True
-)
-
 
 @app.route("/")
 def index():
     return "render documentation here"
 
 
-@app.route("/projects", cors=cors_config)
+@app.route("/projects")
 def get_projects():
     db = DbUtils()
     projects = db.query_projects()
 
     return chalice.Response(
-        status_code=200, headers={"Content-Type": "application/json"}, body=projects
+        status_code=200, headers={"Content-Type": "application/json", "Access-Control-Allow-Origin": "*"}, body=projects
     )
 
 
-@app.route("/projects/{project_id}", cors=cors_config)
+@app.route("/projects/{project_id}")
 def get_project(project_id):
     db = DbUtils()
     project = db.query_project(project_id)
 
     return chalice.Response(
         status_code=200 if project else 404,
-        headers={"Content-Type": "application/json"},
+        headers={"Content-Type": "application/json", "Access-Control-Allow-Origin": "*"},
         body=project,
     )
 
 
-@app.route("/projects/{project_id}/files", cors=cors_config)
+@app.route("/projects/{project_id}/files")
 def get_project_files(project_id):
+    print("headers: ", chalice.current_request.headers)
     db = DbUtils()
     files = db.query_downloadable_project_files(project_id)
 
     return chalice.Response(
         status_code=200 if files else 404,
-        headers={"Content-Type": "application/json"},
+        headers={"Content-Type": "application/json", "Access-Control-Allow-Origin": "*"},
         body=files,
     )
 
 
-@app.route("/files/{file_id}", cors=cors_config)
+@app.route("/files/{file_id}")
 def get_file(file_id):
     db = DbUtils()
     file = db.query_file(file_id)
@@ -71,6 +66,6 @@ def get_file(file_id):
 
     return chalice.Response(
         status_code=200 if file else 404,
-        headers={"Content-Type": "application/json"},
+        headers={"Content-Type": "application/json", "Access-Control-Allow-Origin": "*"},
         body={"url": download_url},
     )
