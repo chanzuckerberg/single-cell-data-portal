3df2138ec3867695f599ec00aa94bf84788eb8a6
Trent Smith <trent.smith@chanzuckerberg.com>
2020-04-16 16:37:59 -0700

    Adding additional test for invalid token
    - Fixing a flaky test with generating an invalid hash
    - adding tests for wrong header and wrong message

--- tests/unit/dcp-prototype/backend/browser/code/common/test_authorizer.py
+++ tests/unit/dcp-prototype/backend/browser/code/common/test_authorizer.py
@@ -30,25 +30,29 @@ class TestAuthorizer(unittest.TestCase):
 
     def test_invalid_token(self):
         token = self.get_auth_token()
+        header, msg, hash = token['access_token'].split('.')
         with self.subTest("short hash"):
             with self.assertRaises(UnauthorizedError):
                 assert_authorized({"Authorization": f"bearer {token['access_token'][:-1]}"})
 
         with self.subTest("wrong hash"):
             with self.assertRaises(UnauthorizedError):
-                bad_char = "0" if token["access_token"][-1] != "0" else "1"
-                _token = token["access_token"][:-1] + bad_char
+                _token = '.'.join([header, msg, "0" * len(hash)])
                 assert_authorized({"Authorization": f"bearer {_token}"})
 
         with self.subTest("wrong header"):
             with self.assertRaises(UnauthorizedError):
-                bad_char = "0" if token["access_token"][0] != "0" else "1"
-                _token = bad_char + token["access_token"][0:]
+                _token = '.'.join(["0" * len(header), msg, hash])
                 assert_authorized({"Authorization": f"bearer {_token}"})
 
         with self.subTest("short header"):
             with self.assertRaises(UnauthorizedError):
                 _token = token["access_token"][1:]
+                assert_authorized({"Authorization": f"bearer {_token}"})
+
+        with self.subTest("wrong message"):
+            with self.assertRaises(UnauthorizedError):
+                _token = '.'.join([header, "0" * len(msg), hash])
                 assert_authorized({"Authorization": f"bearer {_token}"})
 
     def get_auth_token(self) -> dict:
