66f2953d279de9748730a78edbf0b5b1c47cf556
Trent Smith <trent.smith@chanzuckerberg.com>
2020-05-29 09:52:00 -0700

    rename BrowserDbConfig to DataPortalDbConfig

--- browser/backend/scripts/create_db.py
+++ browser/backend/scripts/create_db.py
@@ -6,12 +6,12 @@ from sqlalchemy import create_engine
 pkg_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))  # noqa
 sys.path.insert(0, pkg_root)  # noqa
 
-from code.config.db_config import BrowserDbConfig
+from code.config.db_config import DataPortalDbConfig
 from code.common import browser_orm as orm
 
 
 if __name__ == "__main__":
-    engine = create_engine(BrowserDbConfig().database_uri)
+    engine = create_engine(DataPortalDbConfig().database_uri)
     print("Dropping tables")
     orm.Base.metadata.drop_all(engine)
     print("Recreating tables")
