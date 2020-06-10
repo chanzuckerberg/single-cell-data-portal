a07454d18638fe46ff4759d07085d9714deb2f41
Trent Smith <trent.smith@chanzuckerberg.com>
2020-06-10 15:53:08 -0700

    black

--- tests/unit/backend/corpora/common/test_authorizer.py
+++ tests/unit/backend/corpora/common/test_authorizer.py
@@ -14,7 +14,8 @@ if not os.getenv("DEPLOYMENT_STAGE"):  # noqa
 secret_name = "corpora/cicd/dev/auth0-secret"
 # using the same secret for all none production stages.
 
-@unittest.skipIf(os.getenv("DEPLOYMENT_STAGE", 'test') == "test", "DEPLOYMENT_STAGE is 'test'")
+
+@unittest.skipIf(os.getenv("DEPLOYMENT_STAGE", "test") == "test", "DEPLOYMENT_STAGE is 'test'")
 class TestAuthorizer(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
