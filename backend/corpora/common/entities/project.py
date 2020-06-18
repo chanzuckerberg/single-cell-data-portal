86d31a349436d51f443faeaed01db4d0e4d798b3
Calvin Nhieu <calvin.nhieu@gmail.com>
2020-06-18 14:25:02 -0700

    [temp] define Entity interface and Project

---
+++ backend/corpora/common/entities/project.py
@@ -0,0 +1,37 @@
+import typing
+
+from .entity import Entity
+from ..corpora_orm import DbProject, DbProjectLink, DbProjectDataset, DbDatasetContributor, DbContributor
+
+
+class Project(Entity):
+    def __init__(self, **kwargs):
+        # TODO: list and set fields
+        self.id = kwargs['id']
+        self.status = kwargs['status']
+
+    @classmethod
+    def get(cls, key: typing.Tuple[str]) -> "Project":
+        # db_project = cls.db.get(DbProject, key)
+        # TODO: query multiple tables: DbProjectLink, DbDataset, etc...
+        data = cls.db.query(
+            table_args=[DbProject, DbProjectLink],
+            filter_args=[
+                DbProject.id == key[0],
+                DbProject.status == key[1],
+                DbProject.id == DbProjectLink.project_id,
+                DbProject.status == DbProjectLink.project_status
+            ]
+        )
+        # TODO: inst Project object
+        project = Project(id=data.result.DbProject.id,
+                          status=data.result.DbProject.status,
+                          )
+        return project
+
+    @classmethod
+    def list(cls):
+        pass
+
+    def save(self):
+        pass
--- backend/corpora/common/utils/db_utils.py
+++ backend/corpora/common/utils/db_utils.py
@@ -8,7 +8,7 @@ class DbUtils:
         self.session = DBSessionMaker().session()
         self.engine = self.session.get_bind()
 
-    def _get(self, table: Base, entity_id: str) -> typing.Union[Base, None]:
+    def get(self, table: Base, entity_id: typing.Union[str, typing.Tuple[str]]) -> typing.Union[Base, None]:
         """
         Query a table row by its primary key
         :param table: SQLAlchemy Table to query
@@ -17,7 +17,7 @@ class DbUtils:
         """
         return self.session.query(table).get(entity_id)
 
-    def _query(self, table_args: typing.List[Base], filter_args: typing.List[bool] = None) -> typing.List[Base]:
+    def query(self, table_args: typing.List[Base], filter_args: typing.List[bool] = None) -> typing.List[Base]:
         """
         Query the database using the current DB session
         :param table_args: List of SQLAlchemy Tables to query/join
