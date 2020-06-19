50a11ddeae18e79f709f19d1525636d34038f278
Trent Smith <1429913+Bento007@users.noreply.github.com>
2020-06-18 17:34:12 -0700

    Removed ABC from entity (#397)

--- backend/corpora/common/entities/entity.py
+++ backend/corpora/common/entities/entity.py
@@ -1,10 +1,9 @@
 import typing
-from abc import ABC, abstractmethod
 
 from ..utils.db_utils import DbUtils
 
 
-class Entity(ABC):
+class Entity:
     """
     An abstract base class providing an interface to parse
     application-level objects to and from their database counterparts.
@@ -17,28 +16,26 @@ class Entity(ABC):
     db = DbUtils()
 
     @classmethod
-    @abstractmethod
     def get(cls, key: typing.Union[str, typing.Tuple[str, str]]):
         """
         Retrieves an entity from the database given its primary key
         :param key: Simple or composite primary key
         :return: Entity
         """
-        pass
+        raise NotImplemented()
 
     @classmethod
-    @abstractmethod
     def list(cls):
         """
         Retrieves a list of entities from the database
         :return: list of Entity
         """
-        pass
+        raise NotImplemented()
 
-    @abstractmethod
     def save(self):
         """
         Writes the current object state to the database
         :return: saved Entity
         """
-        pass
+        raise NotImplemented()
+
