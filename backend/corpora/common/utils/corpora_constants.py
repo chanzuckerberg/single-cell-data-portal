73644c391e3dcc7aad297e9cbe9e198655b0611a
maniarathi <arathi.mani@chanzuckerberg.com>
2020-06-22 11:22:07 -0700

    Adding typing to constants

--- backend/corpora/common/utils/corpora_constants.py
+++ backend/corpora/common/utils/corpora_constants.py
@@ -1,38 +1,75 @@
+import collections
+
+
 class CorporaConstants(object):
+
+    TypedMetadata = collections.namedtuple("TypedMetadata", "field_name required_type valid_alternative")
+
+    # Constants related to type checking ontology fields
+    EFO_ONTOLOGY = "efo"
+    CELL_ONTOLOGY = "cl"
+    HANCESTRO_ONTOLOGY = "hancestro"
+    HSAPDV_ONTOLOGY = "hsapdv"
+    MONDO_ONTOLOGY = "mondo"
+    NCBI_TAXON_ONTOLOGY = "ncbi_taxon"
+    UBERON_ONTOLOGY = "uberon"
+
+    # Constants related to type checking enum fields
+    SEX_VALID_ENUM_VALUES = ["male", "female", "na"]
+
     # Constants related to processing layers
     LAYER_DESCRIPTIONS = "layer_descriptions"
     X_DATA_LAYER_NAME = "X"
     RAW_DATA_LAYER_NAME = "raw.X"
 
+
     # Constants related to the metadata requirements of submitting a dataset to Corpora. Note that these metadata
     # fields are CASE-SENSITIVE.
     REQUIRED_OBSERVATION_METADATA_FIELDS = [
-        "tissue",
-        "assay",
-        "disease",
-        "cell_type",
-        "sex",
-        "ethnicity",
-        "developmental_stage",
+        TypedMetadata(field_name="tissue", required_type=str),
+        TypedMetadata(field_name="assay", required_type=str),
+        TypedMetadata(field_name="disease", required_type=str),
+        TypedMetadata(field_name="cell_type", required_type=str),
+        TypedMetadata(field_name="sex", required_type=SEX_VALID_ENUM_VALUES),
+        TypedMetadata(field_name="ethnicity", required_type=str),
+        TypedMetadata(field_name="developmental_stage", required_type=str),
     ]
     REQUIRED_OBSERVATION_ONTOLOGY_METADATA_FIELDS = [
-        "tissue_ontology",
-        "assay_ontology",
-        "disease_ontology",
-        "cell_type_ontology",
-        "ethnicity_ontology",
-        "developmental_stage_ontology",
+        TypedMetadata(field_name="tissue_ontology", required_type=UBERON_ONTOLOGY),
+        TypedMetadata(field_name="assay_ontology", required_type=EFO_ONTOLOGY),
+        TypedMetadata(field_name="disease_ontology", required_type=MONDO_ONTOLOGY, valid_alternative="PATO:0000461"),
+        TypedMetadata(field_name="cell_type_ontology", required_type=CELL_ONTOLOGY),
+        TypedMetadata(field_name="ethnicity_ontology", required_type=HANCESTRO_ONTOLOGY, valid_alternative="na"),
+        TypedMetadata(
+            field_name="developmental_stage_ontology", required_type=HSAPDV_ONTOLOGY, valid_alternative="EFO:0000399"
+        ),
     ]
-    REQUIRED_DATASET_METADATA_FIELDS = ["organism", "organism_ontology", LAYER_DESCRIPTIONS]
-    REQUIRED_DATASET_PRESENTATION_METADATA_FIELDS = ["title", "contributors", "preprint_doi", "publication_doi"]
-    REQUIRED_DATASET_PRESENTATION_HINTS_METADATA_FIELDS = ["color_map", "tags", "default_field", "default_embedding"]
+    REQUIRED_DATASET_METADATA_FIELDS = [
+        TypedMetadata(field_name="organism", required_type=str),
+        TypedMetadata(field_name="organism_ontology", required_type=NCBI_TAXON_ONTOLOGY),
+        TypedMetadata(field_name=LAYER_DESCRIPTIONS, required_type=dict),
+    ]
+    REQUIRED_DATASET_PRESENTATION_METADATA_FIELDS = [
+        TypedMetadata(field_name="title", required_type=str),
+        TypedMetadata(field_name="contributors", required_type=dict),
+        TypedMetadata(field_name="preprint_doi", required_type=str),
+        TypedMetadata(field_name="publication_doi", required_type=str),
+    ]
+
+    REQUIRED_DATASET_PRESENTATION_HINTS_METADATA_FIELDS = [
+        TypedMetadata(field_name="color_map", required_type=list),
+        TypedMetadata(field_name="tags", required_type=list),
+        TypedMetadata(field_name="default_field", required_type=str),
+        TypedMetadata(field_name="default_embedding", required_type=str),
+    ]
     OPTIONAL_PROJECT_LEVEL_METADATA_FIELDS = [
-        "project_name",
-        "project_description",
-        "project_protocol_links",
-        "project_raw_data_links",
-        "project_other_links",
+        TypedMetadata(field_name="project_name", required_type=str),
+        TypedMetadata(field_name="project_description", required_type=str),
+        TypedMetadata(field_name="project_protocol_links", required_type=list),
+        TypedMetadata(field_name="project_raw_data_links", required_type=list),
+        TypedMetadata(field_name="project_other_links", required_type=list),
     ]
+
 
     # Constants related to the dataset format of which a submission to Corpora must be.
     LOOM_FILE_TYPE = ".loom"
