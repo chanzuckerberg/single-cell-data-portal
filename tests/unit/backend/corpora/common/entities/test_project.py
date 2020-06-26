fb21edd91de8ea3015f9a8a15853aa86ece36cc7
Trent Smith <1429913+Bento007@users.noreply.github.com>
2020-06-26 16:39:48 -0700

    Moving path declaration (#405)

--- tests/unit/backend/corpora/common/entities/test_project.py
+++ tests/unit/backend/corpora/common/entities/test_project.py
@@ -1,10 +1,4 @@
-import os
-import sys
 import unittest
-
-pkg_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", "..", "..", "..", ".."))  # noqa
-sys.path.insert(0, pkg_root)  # noqa
-
 from backend.corpora.common.entities.project import Project
 from backend.corpora.common.utils.exceptions import CorporaException
 from backend.corpora.common.corpora_orm import ProjectStatus
