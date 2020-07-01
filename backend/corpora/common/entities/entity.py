c609dd522b33fa1cdccd2d431e5120863a67ea10
Trent Smith <trent.smith@chanzuckerberg.com>
2020-07-01 13:26:41 -0700

    Enhancing docstring

--- backend/corpora/common/entities/entity.py
+++ backend/corpora/common/entities/entity.py
@@ -9,6 +9,10 @@ class Entity:
     An abstract base class providing an interface to parse application-level objects to and from their
     database counterparts.
 
+    This class uses a has-a relationship with SQLAlchemy Table object and simplify the CRUD operations performed on the
+    database through these objects. Columns and relationship of the database object can be access as attributes of the of
+    Entity.
+
     Every application-level object must inherit Entity.
     Examples: Project, Dataset
     """
@@ -37,7 +41,19 @@ class Entity:
         raise NotImplementedError()
 
     @classmethod
-    def _load(cls, db_result: typing.List["Entity"]) -> "Entity":
+    def _load(cls, db_result: typing.List["Entity"]) -> typing.Union["Entity", None]:
+        """
+        Parses query result rows produced by Entity._query into an instantiated Entity object.
+        The output of this function is the return value of Entity.get.
+        SQLAlchemy query results are stored in a list in which each item contains Table objects returned by the query. If no
+        results are found, None is returned.
+
+        Example query results access:
+        row = query_results[0]
+        project_id = row.DbProject.id
+        :param query_results: list of query result rows
+        :return: Entity
+        """
         try:
             return cls(db_result[0])
         except IndexError:
@@ -59,4 +75,9 @@ class Entity:
         raise NotImplementedError()
 
     def __getattr__(self, name):
+        """
+        If the attribute is not in Entity, return the attribute in db_object.
+        :param name:
+        :return:
+        """
         return self.db_object.__getattribute__(name)
