e4bb799acbb85eeccfe92e1c3047b0e80db6c3f1
Trent Smith <trent.smith@chanzuckerberg.com>
2020-07-02 10:56:17 -0700

    Adding test cases for relationships

--- tests/unit/backend/corpora/common/entities/test_project.py
+++ tests/unit/backend/corpora/common/entities/test_project.py
@@ -1,7 +1,7 @@
 import unittest
 from backend.corpora.common.entities.project import Project
 from backend.corpora.common.utils.exceptions import CorporaException
-from backend.corpora.common.corpora_orm import ProjectStatus
+from backend.corpora.common.corpora_orm import ProjectStatus, DbDataset, DbUser
 
 
 class TestProject(unittest.TestCase):
@@ -14,7 +14,19 @@ class TestProject(unittest.TestCase):
 
         project = Project.get(key)
 
+        # Verify Columns
         self.assertEqual(project.name, "test_project")
+        self.assertEqual(project.owner, "test_user_id")
+
+        # Verify User relationship
+        self.assertIsInstance(project.user, DbUser)
+        self.assertEqual(project.user.id, "test_user_id")
+
+        # Verify Dataset relationship
+        dataset = project.datasets[0]
+        self.assertIsInstance(dataset, DbDataset)
+        self.assertEqual(dataset.id, "test_dataset_id")
+        self.assertEqual(dataset.assay, "test_assay")
 
     def test__get__does_not_exist(self):
         non_existent_key = ("non_existent_id", self.status)
