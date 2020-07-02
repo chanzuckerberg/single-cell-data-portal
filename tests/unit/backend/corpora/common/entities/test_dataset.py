3fea2c02b4bbad92dc1e1c3429f2e906508f2791
Trent Smith <trent.smith@chanzuckerberg.com>
2020-07-02 11:02:04 -0700

    Adding relationship tests in dataset

--- tests/unit/backend/corpora/common/entities/test_dataset.py
+++ tests/unit/backend/corpora/common/entities/test_dataset.py
@@ -1,6 +1,6 @@
 import unittest
 
-
+from backend.corpora.common.corpora_orm import DbContributor, DbDatasetArtifact, DbDeploymentDirectory
 from backend.corpora.common.entities.dataset import Dataset
 
 
@@ -12,8 +12,17 @@ class TestDataset(unittest.TestCase):
         dataset = Dataset.get(self.uuid)
         self.assertEqual(dataset.id, self.uuid)
         self.assertEqual(dataset.assay, "test_assay")
+
+        # Verify Artifact relationship
+        self.assertIsInstance(dataset.artifacts[0], DbDatasetArtifact)
         self.assertEqual(dataset.artifacts[0].id, "test_dataset_artifact_id")
+
+        # Verify Deployment Directory relationship
+        self.assertIsInstance(dataset.deployment_directories[0], DbDeploymentDirectory)
         self.assertEqual(dataset.deployment_directories[0].id, "test_deployment_directory_id")
+
+        # Verify Contributor Relationship
+        self.assertIsInstance(dataset.contributors[0], DbContributor)
         self.assertEqual(dataset.contributors[0].id, "test_contributor_id")
 
     def test__get__does_not_exist(self):
