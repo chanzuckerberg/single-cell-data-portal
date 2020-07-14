1749f6019aa70c44d05da6586806cad5465fe881
Trent Smith <1429913+Bento007@users.noreply.github.com>
2020-07-14 11:21:19 -0700

    Create Project and Dataset (#421)
    * Adding creation functions for new Dataset and Project entities. A unique Id is generated when using create for projects and datasets. Existing IDs can be used during joins for example with contributors and datasets.
    * adding test to verify database relationships cardinality is maintained
    * logging exception and return CorporaException when database commits fail.
    * Update backend/corpora/common/entities/entity.py
    
    Co-authored-by: maniarathi <mani.arathi@gmail.com>
    Co-authored-by: Calvin Nhieu <calvin.nhieu@gmail.com>

--- backend/corpora/common/entities/project.py
+++ backend/corpora/common/entities/project.py
@@ -1,5 +1,7 @@
+import uuid
+
 from .entity import Entity
-from ..corpora_orm import DbProject
+from ..corpora_orm import DbProject, DbProjectLink
 
 
 class Project(Entity):
@@ -7,3 +9,51 @@ class Project(Entity):
 
     def __init__(self, db_object: DbProject):
         super().__init__(db_object)
+
+    @classmethod
+    def create(
+        cls,
+        status: str,
+        name: str = "",
+        description: str = "",
+        owner: str = "",
+        s3_bucket: str = "",
+        tc_uri: str = "",
+        needs_attestation: bool = False,
+        processing_state: str = "",
+        validation_state: str = "",
+        links: list = None,
+        **kwargs,
+    ) -> "Project":
+        """
+        Create a new Project and related objects and store in the database. UUIDs are generated for all new table
+        entries.
+        """
+        primary_key = str(uuid.uuid4())
+
+        # Setting Defaults
+        links = links if links else []
+
+        #  Prevent accidentally linking an existing row to a different Project. This maintains the relationship of one
+        #  to many for links
+        [link.pop("id", None) for link in links]
+
+        new_db_object = DbProject(
+            id=primary_key,
+            status=status,
+            name=name,
+            description=description,
+            owner=owner,
+            s3_bucket=s3_bucket,
+            tc_uri=tc_uri,
+            needs_attestation=needs_attestation,
+            processing_state=processing_state,
+            validation_state=validation_state,
+            links=cls._create_sub_objects(
+                links, DbProjectLink, add_columns=dict(project_id=primary_key, project_status=status, **kwargs)
+            ),
+        )
+
+        cls.db.session.add(new_db_object)
+        cls.db.commit()
+        return cls(new_db_object)
