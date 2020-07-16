6219357503b899fc498934b9a90170fde2cd6e62
Timmy Huang <thuang@chanzuckerberg.com>
2020-07-16 11:16:53 -0700

    remove unused fileList component

--- frontend/src/common/entities.ts
+++ frontend/src/common/entities.ts
@@ -87,23 +87,6 @@ export interface DatasetAsset {
 //   journal: string;
 // }
 
-// id: "HCA-AnalysisFile-001a3fdd-c9e6-4871-9e48-a840d52ecdf9"
-// filename: "b29eeb85-53ff-4786-9101-3e241e6dc250_rsem.bam"
-// file_format: "bam"
-// file_size: 0
-// species: "homo sapiens"
-// library_construction_method_ontology: ""
-// tissue_ontology: ""
-export interface File {
-  file_format: string;
-  file_size: number;
-  filename: string;
-  id: string;
-  library_construction_method_ontology: string;
-  species: string;
-  tissue_ontology: string;
-}
-
 export interface User {
   picture: string;
 }
