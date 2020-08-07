5c62d0ece9c99035a791e300c0a2e3d4f5096742
Trent Smith <trent.smith@chanzuckerberg.com>
2020-08-07 11:01:34 -0700

    Adding more comments

--- scripts/db_access.py
+++ scripts/db_access.py
@@ -3,6 +3,8 @@ A simple script demonstrating how to access the deployed database using the corp
 
 Follow the Development instructions up to step 4 before running this script.
 https://github.com/chanzuckerberg/corpora-data-portal/tree/main/backend/chalice/api_server#development
+
+see tests cases in tests/unit/backend/corpora/entities for examples uses.
 """
 
 import os
@@ -14,8 +16,8 @@ sys.path.insert(0, pkg_root)  # noqa
 
 from backend.corpora.common.entities import Project, Dataset
 
-os.environ["DEPLOYMENT_STAGE"] = 'dev'
-os.environ["CORPORA_LOCAL_DEV"] = ''
+os.environ["DEPLOYMENT_STAGE"] = 'dev'  # This can be change to match the stage you are targeting
+os.environ["CORPORA_LOCAL_DEV"] = ''  # This is required to target the postsql db hosted on localhost:5432
 
 
 projects = Project.list()
