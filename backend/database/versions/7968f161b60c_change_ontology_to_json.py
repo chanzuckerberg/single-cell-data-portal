c5eec02c506a65b3be459029bbf5f8590d1bdc11
Marcus Kinsella <mkinsella@chanzuckerberg.com>
2020-08-20 12:00:14 -0700

    Correctly construct the new jsonb value

--- backend/database/versions/7968f161b60c_change_ontology_to_json.py
+++ backend/database/versions/7968f161b60c_change_ontology_to_json.py
@@ -18,25 +18,60 @@ depends_on = None
 
 
 def upgrade():
-    op.alter_column(table_name="dataset", column_name="organism", type_=JSONB())
+    op.alter_column(
+        table_name="dataset",
+        column_name="organism",
+        type_=JSONB(),
+        postgresql_using="json_build_object('ontology_term_id', organism_ontology, 'label', organism)",
+    )
     op.drop_column(table_name="dataset", column_name="organism_ontology")
 
-    op.alter_column(table_name="dataset", column_name="tissue", type_=JSONB())
+    op.alter_column(
+        table_name="dataset",
+        column_name="tissue",
+        type_=JSONB(),
+        postgresql_using="json_build_array(json_build_object('ontology_term_id', tissue_ontology, 'label', tissue))",
+    )
     op.drop_column(table_name="dataset", column_name="tissue_ontology")
 
-    op.alter_column(table_name="dataset", column_name="assay", type_=JSONB())
+    op.alter_column(
+        table_name="dataset",
+        column_name="assay",
+        type_=JSONB(),
+        postgresql_using="json_build_array(json_build_object('ontology_term_id', assay_ontology, 'label', assay))",
+    )
     op.drop_column(table_name="dataset", column_name="assay_ontology")
 
-    op.alter_column(table_name="dataset", column_name="disease", type_=JSONB())
+    op.alter_column(
+        table_name="dataset",
+        column_name="disease",
+        type_=JSONB(),
+        postgresql_using="json_build_array(json_build_object('ontology_term_id', disease_ontology, 'label', disease))",
+    )
     op.drop_column(table_name="dataset", column_name="disease_ontology")
 
-    op.alter_column(table_name="dataset", column_name="ethnicity", type_=JSONB())
+    op.alter_column(
+        table_name="dataset",
+        column_name="ethnicity",
+        type_=JSONB(),
+        postgresql_using=(
+            "json_build_array(json_build_object(" "'ontology_term_id', ethnicity_ontology, 'label', ethnicity))"
+        ),
+    )
     op.drop_column(table_name="dataset", column_name="ethnicity_ontology")
 
-    op.alter_column(table_name="dataset", column_name="development_stage", type_=JSONB())
+    op.alter_column(
+        table_name="dataset",
+        column_name="development_stage",
+        type_=JSONB(),
+        postgresql_using=(
+            "json_build_array(json_build_object("
+            "'ontology_term_id', development_stage_ontology, 'label', development_stage))"
+        ),
+    )
     op.drop_column(table_name="dataset", column_name="development_stage_ontology")
 
-    op.alter_column(table_name="dataset", column_name="sex", type_=JSONB())
+    op.alter_column(table_name="dataset", column_name="sex", type_=JSONB(), postgresql_using="json_build_array(sex)")
 
 
 def downgrade():
