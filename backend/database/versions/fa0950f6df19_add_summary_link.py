d97088c5a8453d6069c9cb0251c53aa2224e1526
Marcus Kinsella <mkinsella@chanzuckerberg.com>
2020-08-24 11:41:28 -0700

    Summary link type migration

---
+++ backend/database/versions/fa0950f6df19_add_summary_link.py
@@ -0,0 +1,57 @@
+"""add_summary_link
+
+Revision ID: fa0950f6df19
+Revises: 7968f161b60c
+Create Date: 2020-08-24 11:04:30.640496
+
+"""
+from alembic import op
+from sqlalchemy.dialects.postgresql import ENUM
+
+
+# revision identifiers, used by Alembic.
+revision = "fa0950f6df19"
+down_revision = "7968f161b60c"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    project_link_enum = ENUM(name="projectlink")
+    project_link_enum.drop(op.get_bind(), checkfirst=True)
+    project_link_enum = ENUM("PROTOCOL", "SUMMARY", "RAW_DATA", "OTHER", name="projectlink", create_type=False)
+    project_link_enum.create(op.get_bind(), checkfirst=True)
+
+    op.alter_column(
+        table_name="project_link",
+        column_name="link_type",
+        type_=project_link_enum,
+        nullable=True,
+        postgresql_using=(
+            "CASE WHEN link_type = 'PROTOCOL' THEN 'PROTOCOL'::projectlink "
+            "     WHEN link_type = 'RAW_DATA' THEN 'RAW_DATA'::projectlink "
+            "     WHEN link_type = 'OTHER' THEN 'OTHER'::projectlink "
+            "     END"
+        ),
+    )
+
+
+def downgrade():
+    project_link_enum = ENUM(name="projectlink")
+    project_link_enum.drop(op.get_bind(), checkfirst=True)
+    project_link_enum = ENUM("PROTOCOL", "RAW_DATA", "OTHER", name="projectlink", create_type=False)
+    project_link_enum.create(op.get_bind(), checkfirst=True)
+
+    op.alter_column(
+        table_name="project_link",
+        column_name="link_type",
+        type_=project_link_enum,
+        nullable=True,
+        postgresql_using=(
+            "CASE WHEN link_type = 'PROTOCOL' THEN 'PROTOCOL'::projectlink "
+            "     WHEN link_type = 'RAW_DATA' THEN 'RAW_DATA'::projectlink "
+            "     WHEN link_type = 'OTHER' THEN 'OTHER'::projectlink "
+            "     WHEN link_type = 'SUMMARY' THEN 'OTHER'::projectlink "
+            "     END"
+        ),
+    )
