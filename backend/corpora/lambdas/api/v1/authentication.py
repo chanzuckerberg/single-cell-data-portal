257a08eb1d9a361d123f857c77ac079ad7d52899
Brian McCandless <bmccandless@chanzuckerberg.com>
2020-08-25 21:22:13 -0700

    Added cxguserCookie to the securitySchema, which handles the cookie processing.

--- backend/corpora/lambdas/api/v1/authentication.py
+++ backend/corpora/lambdas/api/v1/authentication.py
@@ -1,8 +1,10 @@
 from flask import make_response, jsonify, current_app, request, redirect
+import json
 from authlib.integrations.flask_client import OAuth
 from urllib.parse import urlencode
 from functools import lru_cache
-from ....common.authorizer import get_userinfo
+import base64
+from ....common.authorizer import get_userinfo, assert_authorized_token
 from ....common.corpora_config import CorporaAuthConfig
 
 
@@ -17,7 +19,7 @@ def create_oauth_client(config):
         api_base_url=api_base_url,
         access_token_url=f"{api_base_url}/oauth/token",
         authorize_url=f"{api_base_url}/authorize",
-        client_kwargs={"scope": "openid profile email"},
+        client_kwargs={"scope": "openid profile email offline"},
     )
     return client
 
@@ -38,7 +40,7 @@ def logout():
     params = {"returnTo": config.callback_base_url, "client_id": config.client_id}
     response = redirect(client.api_base_url + "/v2/logout?" + urlencode(params))
     # remove the cookie
-    response.set_cookie(config["cookie_name"], "", expires=0)
+    response.set_cookie(config.cookie_name, "", expires=0)
     return response
 
 
@@ -47,16 +49,34 @@ def oauth2_callback():
     config = CorporaAuthConfig()
     client = create_oauth_client(config)
     token = client.authorize_access_token()
-    id_token = token.get("id_token")
     response = redirect(config.callback_base_url)
     # write the cookie
-    response.set_cookie(config.cookie_name, id_token, httponly=True, max_age=24 * 60 * 60 * 90)
+    value = encode_token(dict(token))
+    response.set_cookie(config.cookie_name, value, httponly=True, max_age=24 * 60 * 60 * 90)
     return response
+
+
+def encode_token(token):
+    return base64.b64encode(json.dumps(token).encode("utf-8"))
+
+
+def decode_token(value):
+    value = base64.b64decode(value)
+    return json.loads(value)
+
+
+def apikey_info_func(tokenstr, required_scopes):
+    """Function used by connexion in the securitySchemes.
+    The return dictionary must contains a "sub" key"""
+    token = decode_token(tokenstr)
+    payload = assert_authorized_token(token.get("id_token"))
+    return payload
 
 
 def userinfo():
     """api call,  retrieve the user info from the id token stored in the cookie"""
     config = CorporaAuthConfig()
-    token = request.cookies.get(config.cookie_name)
-    userinfo = get_userinfo(token)
+    tokenstr = request.cookies.get(config.cookie_name)
+    token = decode_token(tokenstr)
+    userinfo = get_userinfo(token.get("id_token"))
     return make_response(jsonify(userinfo))
