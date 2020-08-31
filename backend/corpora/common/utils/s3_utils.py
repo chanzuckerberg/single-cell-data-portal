b3802c0b2d8050a9d36b44322ed5452b11b93480
Trent Smith <trent.smith@chanzuckerberg.com>
2020-08-31 15:02:22 -0700

    feat(download): Add function to get S3 file metadata

--- backend/corpora/common/utils/s3_utils.py
+++ backend/corpora/common/utils/s3_utils.py
@@ -1,46 +1,47 @@
-import os
-
 import boto3
 from botocore.exceptions import ClientError
+import logging
 
+from backend.corpora.common.utils.exceptions import CorporaException
 
-def generate_file_url(file_prefix: str, expiration: int = 3600, s3=None) -> str:
+logger = logging.getLogger(__name__)
+
+def generate_file_url(bucket_name, file_prefix, expiration: int = 3600, s3=None) -> str:
     """
     Generate a presigned URL for a file for user download.
+    :param bucket_name: S3 name of the bucket
     :param file_prefix: S3 prefix location of the file
     :param expiration: Presigned URL expiration in seconds
     :param s3: Optional S3 client instance
     :return: Presigned URL to download the requested file
     """
-    if not s3:
-        s3 = boto3.client("s3")
-
-    bucket_name = f"corpora-data-{os.environ['DEPLOYMENT_STAGE']}"
+    s3 = s3 if s3 else boto3.client("s3")
 
     try:
         response = s3.generate_presigned_url(
             "get_object", Params={"Bucket": bucket_name, "Key": file_prefix}, ExpiresIn=expiration
         )
-    except ClientError as e:
-        print(f"Failed to generate presigned URL for {file_prefix} with error: {e}")
-        return ""
+    except ClientError:
+        logger.exception(f"Failed to generate presigned URL for {file_prefix}")
+        raise CorporaException()
 
     return response
 
-def head_file(file_prefix: str, s3=None):
+
+def head_file(bucket_name, file_prefix: str, s3=None) -> dict:
     """
-    The HEAD operation retrieves metadata from an object.
+    The HEAD operation retrieves metadata from an S3 object.
+    :param bucket_name: S3 name of the bucket
     :param file_prefix: S3 prefix location of the file
     :param s3: Optional S3 client instance
-    :return: Presigned URL to download the requested file
+    :return: S3 metadata for the file
     """
-    if not s3:
-        s3 = boto3.client("s3")
-    bucket_name = f"corpora-data-{os.environ['DEPLOYMENT_STAGE']}"
+    s3 = s3 if s3 else boto3.client("s3")
+
     try:
         response = s3.head_object(Bucket=bucket_name, Key=file_prefix)
-    except ClientError as e:
-        print(f"Failed for {file_prefix} with error: {e}")
-        return ""
+    except ClientError:
+        logger.exception(f"Failed to retrieve meta data for {file_prefix}.")
+        raise CorporaException()
 
     return response
--- tests/unit/backend/corpora/common/utils/test_s3_utils.py
+++ tests/unit/backend/corpora/common/utils/test_s3_utils.py
@@ -1,8 +1,10 @@
 import unittest
 
+import boto3
 from moto import mock_s3
 
-from backend.corpora.common.utils.s3_utils import generate_file_url
+from backend.corpora.common.utils.exceptions import CorporaException
+from backend.corpora.common.utils.s3_utils import generate_file_url, head_file
 
 
 class TestS3Utils(unittest.TestCase):
@@ -15,8 +17,33 @@ class TestS3Utils(unittest.TestCase):
 
     def test_generate_file_url(self):
         file_prefix = "test_prefix"
+        bucket_name = "mock-bucket"
 
         with self.subTest("Generate URL OK"):
-            url = generate_file_url(file_prefix)
+            url = generate_file_url(bucket_name, file_prefix)
             self.assertIn(file_prefix, url)
             self.assertIn("Expires=", url)
+
+    def test_head_file(self):
+        file_name='fake_file.txt'
+        body = "hello world!"
+        bucket = 'corpora-data-test'
+        region = 'us-west-2'
+        conn = boto3.resource('s3', region_name=region)
+        conn.create_bucket(Bucket=bucket)
+        s3 = boto3.client('s3', region_name=region)
+        s3.put_object(Bucket=bucket, Key=file_name, Body=body)
+
+        response = head_file(bucket, file_name)
+        self.assertEqual(len(body), response['ContentLength'])
+
+    def test_head_file__not_found(self):
+        file_name = 'fake_file.txt'
+        bucket = 'corpora-data-test'
+        region = 'us-west-2'
+        conn = boto3.resource('s3', region_name=region)
+        conn.create_bucket(Bucket=bucket)
+        s3 = boto3.client('s3', region_name=region)
+
+        with self.assertRaises(CorporaException):
+            head_file(bucket, file_name, s3=s3)
