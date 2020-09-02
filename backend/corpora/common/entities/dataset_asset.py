9a15b97f15ae7f4e387e5df48b28ecfdc04d9183
Trent Smith <trent.smith@chanzuckerberg.com>
2020-09-01 18:55:32 -0700

    stop passing s3 around

--- backend/corpora/common/entities/dataset_asset.py
+++ backend/corpora/common/entities/dataset_asset.py
@@ -3,20 +3,18 @@ import typing
 from backend.corpora.common.utils.s3_utils import head_file, generate_file_url
 from .entity import Entity
 from ..corpora_orm import DbDatasetArtifact
-import boto3
 
 
 class DatasetAsset(Entity):
     table = DbDatasetArtifact
-    s3 = boto3.client("s3")
 
     def __init__(self, db_object: DbDatasetArtifact):
         super().__init__(db_object)
         self.bucket_name, self.file_prefix = self.s3_uri[5:].split("/", 1)
 
     def generate_file_url(self, expiration: int = 3600) -> typing.Union[str, None]:
-        return generate_file_url(self.bucket_name, self.file_prefix, expiration, self.s3)
+        return generate_file_url(self.bucket_name, self.file_prefix, expiration)
 
     def get_file_size(self) -> typing.Union[dict, None]:
-        metadata = head_file(self.bucket_name, self.file_prefix, s3=self.s3)
+        metadata = head_file(self.bucket_name, self.file_prefix)
         return metadata["ContentLength"] if metadata else metadata
--- backend/corpora/common/utils/s3_utils.py
+++ backend/corpora/common/utils/s3_utils.py
@@ -7,16 +7,15 @@ from botocore.exceptions import ClientError
 logger = logging.getLogger(__name__)
 
 
-def generate_file_url(bucket_name: str, file_prefix: str, expiration: int = 3600, s3=None) -> typing.Union[str, None]:
+def generate_file_url(bucket_name: str, file_prefix: str, expiration: int = 3600) -> typing.Union[str, None]:
     """
     Generate a presigned URL for a file for user download.
     :param bucket_name: S3 name of the bucket
     :param file_prefix: S3 prefix location of the file
     :param expiration: Presigned URL expiration in seconds
-    :param s3: Optional S3 client instance
     :return: Presigned URL to download the requested file
     """
-    s3 = s3 if s3 else boto3.client("s3")
+    s3 = boto3.client("s3")
 
     try:
         response = s3.generate_presigned_url(
@@ -29,15 +28,14 @@ def generate_file_url(bucket_name: str, file_prefix: str, expiration: int = 3600
     return response
 
 
-def head_file(bucket_name: str, file_prefix: str, s3=None) -> typing.Union[dict, None]:
+def head_file(bucket_name: str, file_prefix: str) -> typing.Union[dict, None]:
     """
     The HEAD operation retrieves metadata from an S3 object.
     :param bucket_name: S3 name of the bucket
     :param file_prefix: S3 prefix location of the file
-    :param s3: Optional S3 client instance
     :return: S3 metadata for the file
     """
-    s3 = s3 if s3 else boto3.client("s3")
+    s3 = boto3.client("s3")
 
     try:
         response = s3.head_object(Bucket=bucket_name, Key=file_prefix)
