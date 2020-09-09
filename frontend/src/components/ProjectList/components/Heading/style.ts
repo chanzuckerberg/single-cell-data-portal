a6eeb8de641dd22d73e23294a933c17fac525492
maniarathi <arathi.mani@chanzuckerberg.com>
2020-09-09 12:35:45 -0700

    Run prettier

--- frontend/src/components/ProjectList/components/Heading/style.ts
+++ frontend/src/components/ProjectList/components/Heading/style.ts
@@ -12,7 +12,7 @@ export const Wrapper = styled.div`
   display: flex;
   padding-bottom: 9px;
   border-bottom: 1px solid black;
-  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1)
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
 `;
 
 export const Name = styled.div`
