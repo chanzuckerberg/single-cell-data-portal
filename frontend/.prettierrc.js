65982355b160a9ad639707de147f6bf9ca570ad5
Timmy Huang <thuang@chanzuckerberg.com>
2020-09-09 09:36:36 -0700

    #562 Add more information to corpora data portal.

--- frontend/.prettierrc.js
+++ frontend/.prettierrc.js
@@ -1,4 +1,7 @@
 module.exports = {
+  // (thuang): Should be always since prettier v2.0
+  // https://prettier.io/docs/en/options.html#arrow-function-parentheses
+  arrowParens: "always",
   endOfLine: "lf",
   tabWidth: 2,
   trailingComma: "es5",
