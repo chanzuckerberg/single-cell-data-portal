dad15c022802ae1fe6d5ad6a73bde36a05ce4074
Timmy Huang <tihuan@users.noreply.github.com>
2020-09-09 17:13:11 -0700

    thuang-499-smoke-test (#589)
    Add DP smoke test suite + PR smoke test hook
    
    Thank you!
    
    QA steps:
    1. `npm install`
    2. Copy `frontend/src/configs/local.js` to `frontend/src/configs/configs.js`
    3. `npm run smoke-test-with-local-backend` -- this will start FE server with Gatsby prod build and test against local BE server
    4. Alternatively, you can also copy `frontend/src/configs/dev.js` to `frontend/src/configs/configs.js`, and then run `npm run smoke-test-prod-build` -- this will start FE server with Gatsby prod build and test against Dev env BE server
    
    fixes #499

---
+++ frontend/e2e/features/homepage.test.ts
@@ -0,0 +1,17 @@
+import { goToPage } from "e2e/utils/helpers";
+import { TEST_URL } from "../common/constants";
+import { getTestTag, getText } from "../utils/selectors";
+
+describe("Homepage", () => {
+  it("renders the expected elements", async () => {
+    await goToPage(TEST_URL);
+
+    await expect(page).toHaveSelector(getTestTag("logo"));
+    await expect(page).toHaveSelector(getText("Dataset name"));
+    await expect(page).toHaveSelector(getText("View in cellxgene"));
+    await expect(page).toHaveSelector(getText("More information"));
+
+    await expect(page).toHaveSelector(getTestTag("dataset-name"));
+    await expect(page).toHaveSelector(getTestTag("view-dataset-link"));
+  });
+});
