527a6085a42a828399338e3eb9728ef16f14cba0
Timmy Huang <thuang@chanzuckerberg.com>
2020-09-11 17:01:51 -0700

    modify modal style

--- frontend/src/components/ProjectList/components/Dataset/components/DownloadDataset/index.tsx
+++ frontend/src/components/ProjectList/components/Dataset/components/DownloadDataset/index.tsx
@@ -3,14 +3,17 @@ import Modal from "src/components/common/Modal";
 import Content from "./components/Content";
 
 const DownloadDataset: FC = () => {
-  const [isOpen, setIsOpen] = React.useState(false);
+  // DEBUG
+  // DEBUG
+  // DEBUG
+  const [isOpen, setIsOpen] = React.useState(true);
 
   const toggleOpen = () => setIsOpen(!isOpen);
 
   return (
     <div>
       <button onClick={toggleOpen}>Download</button>
-      <Modal isOpen={isOpen} onClose={toggleOpen}>
+      <Modal title="Download Dataset" isOpen={isOpen} onClose={toggleOpen}>
         <Content />
       </Modal>
     </div>
