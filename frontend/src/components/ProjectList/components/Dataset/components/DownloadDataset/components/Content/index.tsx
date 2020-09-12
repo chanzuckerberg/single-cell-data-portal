754d53f20fadacfe5b3b11fbba56ad419b231b91
Timmy Huang <thuang@chanzuckerberg.com>
2020-09-11 17:01:51 -0700

    modify modal style

--- frontend/src/components/ProjectList/components/Dataset/components/DownloadDataset/components/Content/index.tsx
+++ frontend/src/components/ProjectList/components/Dataset/components/DownloadDataset/components/Content/index.tsx
@@ -2,7 +2,11 @@ import React, { FC } from "react";
 import { Wrapper } from "./style";
 
 const Content: FC = () => {
-  return <Wrapper>Modal content here</Wrapper>;
+  return (
+    <Wrapper>
+      Modal content here<input type="phone"></input>
+    </Wrapper>
+  );
 };
 
 export default Content;
--- frontend/src/components/common/Modal/style.ts
+++ frontend/src/components/common/Modal/style.ts
@@ -1,8 +1,24 @@
 import { Dialog } from "@blueprintjs/core";
 import styled from "styled-components";
+import { fontStyle } from "../theme";
 
 export const StyledDialog = styled(Dialog)`
   && {
+    .bp3-heading {
+      ${fontStyle}
+      font-size: 16px;
+      line-height: 19px;
+    }
+
+    ${fontStyle}
+
+    background-color: white;
     padding-bottom: 0px;
+    box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.302065);
+    border-radius: 4px;
+
+    /* (thuang): Default Blueprint width */
+    min-width: 500px;
+    width: unset;
   }
 `;
