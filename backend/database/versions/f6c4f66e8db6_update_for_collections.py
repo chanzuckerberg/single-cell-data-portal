df0de1f847707b9f84f9b8bc9c68c1a2d406f7f7
Trent Smith <trent.smith@chanzuckerberg.com>
2020-10-30 14:07:55 -0700

    Downgrade Migration

--- backend/database/versions/f6c4f66e8db6_update_for_collections.py
+++ backend/database/versions/f6c4f66e8db6_update_for_collections.py
@@ -16,9 +16,9 @@ branch_labels = None
 depends_on = None
 
 # Enum 'type' for PostgreSQL
-enum_projectlink = 'projectlink'
+enum_projectlink = "projectlink"
 # Set temporary enum 'type' for PostgreSQL
-tmp_enum_projectlink = 'tmp_' + enum_projectlink
+tmp_enum_projectlink = "tmp_" + enum_projectlink
 
 # Options for Enum
 old_options = ("RAW_DATA", "PROTOCOL", "SUMMARY", "OTHER")
@@ -27,6 +27,19 @@ new_options = sorted(["DOI", "RAW_DATA", "PROTOCOL", "LAB_WEBSITE", "OTHER"])
 # Create enum fields
 old_type = sa.Enum(*old_options, name=enum_projectlink)
 new_type = sa.Enum(*new_options, name=enum_projectlink)
+
+
+def add_nullable_false(table: str, column: str, type, default):
+    op.add_column(table, sa.Column(column, type, nullable=True))
+    op.execute(f"UPDATE {table} SET {column} = {default}")
+    op.alter_column(table, column, nullable=False)
+
+
+def add_enum(name: str, values: list):
+    values.sort()
+    _enum = postgresql.ENUM(*values, name=name.lower())
+    _enum.create(op.get_bind())
+
 
 def upgrade():
     # For more information about the changes executed here, please visit the design document and the approved
@@ -37,7 +50,8 @@ def upgrade():
         prefix1 = "" if table == "project" else "collection_"
         prefix2 = "" if table == "project" else "project_"
         op.add_column(
-            table, sa.Column(f"{prefix1}visibility", sa.Enum("PUBLIC", "PRIVATE", name="collectionvisibility"), nullable=True)
+            table,
+            sa.Column(f"{prefix1}visibility", sa.Enum("PUBLIC", "PRIVATE", name="collectionvisibility"), nullable=True),
         )
         op.execute(f"UPDATE {table} SET {prefix1}visibility = 'PUBLIC' WHERE {prefix2}status = 'LIVE'")
         op.execute(f"UPDATE {table} SET {prefix1}visibility = 'PRIVATE' WHERE {prefix2}status = 'EDIT'")
@@ -46,79 +60,89 @@ def upgrade():
             op.alter_column(table, f"{prefix2}id", nullable=False, new_column_name=f"{prefix1}id")
         op.drop_column(table, f"{prefix2}status")
 
-    def _add_nullable_false(table, column, type, default):
-        op.add_column(table, sa.Column(column, type, nullable=True))
-        op.execute(f"UPDATE {table} SET {column} = {default}")
-        op.alter_column(table, column, nullable=False)
-
     # Create Visibility Enum
-    visibility_enum = postgresql.ENUM("PUBLIC", "PRIVATE", name="collectionvisibility")
-    visibility_enum.create(op.get_bind())
+    add_enum("collectionvisibility", ["PUBLIC", "PRIVATE"])
 
     # Remove (id, status) Primary and Foreign Key Constraint
     op.execute("ALTER TABLE project DROP CONSTRAINT project_pkey CASCADE")
 
     # Update Project Table
     _add_visibility("project")
+    op.add_column("project", sa.Column("contact_email", sa.String(), nullable=True))
+    op.add_column("project", sa.Column("contact_name", sa.String(), nullable=True))
+    add_nullable_false("project", "data_submission_policy_version", sa.String(), default="0")
+    op.add_column("project", sa.Column("obfuscated_uuid", sa.String(), nullable=True))
     op.create_primary_key("project_pkey", "project", ["id", "visibility"])
-    op.add_column('project', sa.Column('contact_email', sa.String(), nullable=True))
-    op.add_column('project', sa.Column('contact_name', sa.String(), nullable=True))
-    _add_nullable_false("project", 'data_submission_policy_version', sa.String(), default="0")
-    op.add_column('project', sa.Column('obfuscated_uuid', sa.String(), nullable=True))
     op.drop_constraint("project_owner_fkey", "project", type_="foreignkey")
-    op.drop_column('project', 'processing_state')
-    op.drop_column('project', 's3_bucket')
-    op.drop_column('project', 'needs_attestation')
-    op.drop_column('project', 'tc_uri')
-    op.drop_column('project', 'validation_state')
+    op.drop_column("project", "processing_state")
+    op.drop_column("project", "s3_bucket")
+    op.drop_column("project", "needs_attestation")
+    op.drop_column("project", "tc_uri")
+    op.drop_column("project", "validation_state")
 
     # Update Project Link Table
     _add_visibility("project_link")
     op.create_foreign_key(
         None, "project_link", "project", ["collection_id", "collection_visibility"], ["id", "visibility"]
     )
+    # Rename current enum type to tmp_
+    op.execute("DELETE FROM project_link WHERE project_link.link_type = 'SUMMARY'")
+    op.execute("ALTER TYPE " + enum_projectlink + " RENAME TO " + tmp_enum_projectlink)
+    # Create new enum type in db
+    new_type.create(op.get_bind())
+    # Update column to use new enum type
+    op.execute(
+        "ALTER TABLE project_link ALTER COLUMN link_type TYPE "
+        + enum_projectlink
+        + " USING link_type::text::"
+        + enum_projectlink
+    )
+    # Drop old enum type
+    op.execute("DROP TYPE " + tmp_enum_projectlink)
 
     # Update Dataset Table
     _add_visibility("dataset")
+    op.add_column("dataset", sa.Column("cell_count", sa.Integer(), nullable=True))
+    op.add_column("dataset", sa.Column("is_valid", sa.Boolean(), nullable=True))
     op.create_foreign_key(None, "dataset", "project", ["collection_id", "collection_visibility"], ["id", "visibility"])
-    op.add_column('dataset', sa.Column('cell_count', sa.Integer(), nullable=True))
-    op.add_column('dataset', sa.Column('is_valid', sa.Boolean(), nullable=True))
-    op.drop_column('dataset', 'publication_doi')
-    op.drop_column('dataset', 'source_data_location')
-    op.drop_column('dataset', 'preprint_doi')
+    op.drop_column("dataset", "publication_doi")
+    op.drop_column("dataset", "source_data_location")
+    op.drop_column("dataset", "preprint_doi")
 
     # Update Deployment_Directory Table
     op.drop_column("deployment_directory", "environment")
 
     # Remove Tables
-    op.drop_table('dataset_contributor')
-    op.drop_table('contributor')
-    op.drop_table('user')
+    op.drop_table("dataset_contributor")
+    op.drop_table("contributor")
+    op.drop_table("user")
 
-    # Drop Project Status Enum
+    # Drop Unused Enums
     sa.Enum(name="projectstatus").drop(op.get_bind(), checkfirst=False)
-
-    # Update Link Type Enum
-    # Rename current enum type to tmp_
-    op.execute("DELETE FROM project_link WHERE project_link.link_type = 'SUMMARY';")
-    op.execute('ALTER TYPE ' + enum_projectlink + ' RENAME TO ' + tmp_enum_projectlink)
-    # Create new enum type in db
-    new_type.create(op.get_bind())
-    # Update column to use new enum type
-    op.execute('ALTER TABLE project_link ALTER COLUMN link_type TYPE ' + enum_projectlink + ' USING link_type::text::' + enum_projectlink)
-    # Drop old enum type
-    op.execute('DROP TYPE ' + tmp_enum_projectlink)
+    sa.Enum(name="validationstate").drop(op.get_bind(), checkfirst=False)
+    sa.Enum(name="processingstate").drop(op.get_bind(), checkfirst=False)
     # ### end Alembic commands ###
 
 
 def downgrade():
-    # ### commands auto generated by Alembic###
+    def _add_status(table):
+        prefix1 = "" if table == "project" else "project_"
+        prefix2 = "" if table == "project" else "collection_"
+        op.add_column(
+            table, sa.Column(f"{prefix1}status", sa.Enum("PUBLIC", "PRIVATE", name="projectstatus"), nullable=True)
+        )
+        op.execute(f"UPDATE {table} SET {prefix1}status = 'LIVE' WHERE {prefix2}visibility = 'PUBLIC'")
+        op.execute(f"UPDATE {table} SET {prefix1}status = 'EDIT' WHERE {prefix2}visibility = 'PRIVATE'")
+        op.alter_column(table, f"{prefix1}status", nullable=False)
+        if table != "project":
+            op.alter_column(table, f"{prefix2}id", nullable=False, new_column_name=f"{prefix1}id")
+        op.drop_column(table, f"{prefix2}visibility")
 
     # Create Status Enum
-    status_enum = postgresql.ENUM("LIVE", "EDIT", name="projectstatus")
-    status_enum.create(op.get_bind())
+    add_enum("processingState", ["NA", "IN_VALIDATION", "IN_ARTIFACT_CREATION", "IN_DEPLOYMENT"])
+    add_enum("projectstatus", ["LIVE", "EDIT"])
+    add_enum("validationstate", ["NOT_VALIDATED", "VALID", "INVALID"])
 
-    # Create User Table
     op.create_table(
         "user",
         sa.Column("id", sa.VARCHAR(), autoincrement=False, nullable=False),
@@ -133,12 +157,46 @@ def downgrade():
         sa.PrimaryKeyConstraint("id", name="user_pkey"),
     )
     op.execute("""INSERT INTO public.user (id) SELECT DISTINCT owner FROM project""")
-    op.create_foreign_key("project_owner_fkey", "project", "user", ["owner"], ["id"])
+
+    # Create Contributors Table
+    op.create_table(
+        "contributor",
+        sa.Column("id", sa.VARCHAR(), autoincrement=False, nullable=False),
+        sa.Column("name", sa.VARCHAR(), autoincrement=False, nullable=True),
+        sa.Column("institution", sa.VARCHAR(), autoincrement=False, nullable=True),
+        sa.Column("email", sa.VARCHAR(), autoincrement=False, nullable=True),
+        sa.Column(
+            "created_at", postgresql.TIMESTAMP(), server_default=sa.text("now()"), autoincrement=False, nullable=False
+        ),
+        sa.Column(
+            "updated_at", postgresql.TIMESTAMP(), server_default=sa.text("now()"), autoincrement=False, nullable=False
+        ),
+        sa.PrimaryKeyConstraint("id", name="contributor_pkey"),
+        postgresql_ignore_search_path=False,
+    )
+
+    # Create Contributor Dataset Table
+    op.create_table(
+        "dataset_contributor",
+        sa.Column("id", sa.VARCHAR(), autoincrement=False, nullable=False),
+        sa.Column("contributor_id", sa.VARCHAR(), autoincrement=False, nullable=False),
+        sa.Column("dataset_id", sa.VARCHAR(), autoincrement=False, nullable=False),
+        sa.Column(
+            "created_at", postgresql.TIMESTAMP(), server_default=sa.text("now()"), autoincrement=False, nullable=False
+        ),
+        sa.Column(
+            "updated_at", postgresql.TIMESTAMP(), server_default=sa.text("now()"), autoincrement=False, nullable=False
+        ),
+        sa.ForeignKeyConstraint(["contributor_id"], ["contributor.id"], name="dataset_contributor_contributor_id_fkey"),
+        sa.ForeignKeyConstraint(["dataset_id"], ["dataset.id"], name="dataset_contributor_dataset_id_fkey"),
+        sa.PrimaryKeyConstraint("id", name="dataset_contributor_pkey"),
+    )
 
     # Remove (id, status) Primary and Foreign Key Constraint
     op.execute("ALTER TABLE project DROP CONSTRAINT project_pkey CASCADE")
 
     # Update Project Table
+    _add_status("project")
     op.add_column(
         "project",
         sa.Column(
@@ -148,11 +206,7 @@ def downgrade():
             nullable=True,
         ),
     )
-    op.add_column(
-        "project",
-        sa.Column("status", postgresql.ENUM("LIVE", "EDIT", name="projectstatus"), autoincrement=False, default="EDIT"),
-    )
-    op.add_column("project", sa.Column("s3_bucket", sa.VARCHAR(), autoincrement=False, nullable=True))
+
     op.add_column(
         "project",
         sa.Column(
@@ -162,46 +216,45 @@ def downgrade():
             nullable=True,
         ),
     )
-    op.execute("UPDATE project SET status = 'LIVE' WHERE visibility = 'PUBLIC'")
-    op.execute("UPDATE project SET status = 'EDIT' WHERE visibility = 'PRIVATE'")
+    op.add_column("project", sa.Column("tc_uri", sa.VARCHAR(), autoincrement=False, nullable=True))
+    op.add_column("project", sa.Column("needs_attestation", sa.BOOLEAN(), autoincrement=False, nullable=True))
+    op.add_column("project", sa.Column("s3_bucket", sa.VARCHAR(), autoincrement=False, nullable=True))
     op.create_primary_key("project_pkey", "project", ["id", "status"])
-    op.drop_column("project", "visibility")
+    op.create_foreign_key("project_owner_fkey", "project", "user", ["owner"], ["id"])
     op.drop_column("project", "obfuscated_uuid")
+    op.drop_column("project", "data_submission_policy_version")
+    op.drop_column("project", "contact_name")
+    op.drop_column("project", "contact_email")
 
     # Update Project Link Table
-    op.add_column(
-        "project_link",
-        sa.Column(
-            "project_status", postgresql.ENUM("LIVE", "EDIT", name="projectstatus"), autoincrement=False, default="EDIT"
-        ),
+    _add_status("project_link")
+    op.create_foreign_key(None, "project_link", "project", ["project_id", "project_status"], ["id", "status"])
+    op.execute("DELETE FROM project_link WHERE project_link.link_type = 'DOI'")
+    op.execute("DELETE FROM project_link WHERE project_link.link_type = 'LAB_WEBSITE'")
+    op.execute("ALTER TYPE " + enum_projectlink + " RENAME TO " + tmp_enum_projectlink)
+    # Create new enum type in db
+    old_type.create(op.get_bind())
+    # Update column to use new enum type
+    op.execute(
+        "ALTER TABLE project_link ALTER COLUMN link_type TYPE "
+        + enum_projectlink
+        + " USING link_type::text::"
+        + enum_projectlink
     )
-    op.execute("UPDATE project_link SET project_status = 'LIVE' WHERE collection_visibility = 'PUBLIC'")
-    op.execute("UPDATE project_link SET project_status = 'EDIT' WHERE collection_visibility = 'PRIVATE'")
-    op.alter_column("project_link", "collection_id", nullable=False, new_column_name="project_id")
-    op.create_foreign_key(
-        "project_link_project_id_fkey", "project_link", "project", ["project_id", "project_status"], ["id", "status"]
-    )
-    op.drop_column("project_link", "collection_visibility")
-
-    # Update Columns in Project Links table
+    # Drop old enum type
+    op.execute("DROP TYPE " + tmp_enum_projectlink)
 
     # Update Deployment Directory Table
     op.add_column("deployment_directory", sa.Column("environment", sa.VARCHAR(), autoincrement=False, nullable=True))
 
     # Update Dataset Table
+    _add_status("dataset")
+    op.add_column("dataset", sa.Column("preprint_doi", sa.VARCHAR(), autoincrement=False, nullable=True))
     op.add_column("dataset", sa.Column("source_data_location", sa.VARCHAR(), autoincrement=False, nullable=True))
-    op.add_column(
-        "dataset",
-        sa.Column(
-            "project_status", postgresql.ENUM("LIVE", "EDIT", name="projectstatus"), autoincrement=False, default="EDIT"
-        ),
-    )
-    op.alter_column("dataset", "collection_id", nullable=False, new_column_name="project_id")
-    op.execute("UPDATE dataset SET project_status = 'LIVE' WHERE collection_visibility = 'PUBLIC'")
-    op.execute("UPDATE dataset SET project_status = 'EDIT' WHERE collection_visibility = 'PRIVATE'")
+    op.add_column("dataset", sa.Column("publication_doi", sa.VARCHAR(), autoincrement=False, nullable=True))
     op.create_foreign_key("fk_project", "dataset", "project", ["project_id", "project_status"], ["id", "status"])
-    op.drop_column("dataset", "collection_visibility")
     op.drop_column("dataset", "is_valid")
+    op.drop_column("dataset", "cell_count")
 
     # Drop collection visibility Enum
     sa.Enum(name="collectionvisibility").drop(op.get_bind(), checkfirst=False)
