181f4a5df08e4ffbb2ffa18ed398873f09ae03cf
Seve Badajoz <sbadajoz@chanzuckerberg.com>
2020-11-03 15:21:19 -0800

    create styling for collection page

---
+++ frontend/src/views/Collection/style.ts
@@ -0,0 +1,13 @@
+import { Button } from "@blueprintjs/core";
+import styled from "styled-components";
+
+export const StyledButton = styled(Button)`
+  margin-left: 16px;
+`;
+
+export const TopBar = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  flex-direction: row;
+`;
