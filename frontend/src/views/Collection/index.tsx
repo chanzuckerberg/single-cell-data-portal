40e3f62c18b36f81d10b1548e0bf0f7d1730f7a1
Seve Badajoz <sbadajoz@chanzuckerberg.com>
2020-11-03 17:57:14 -0800

    create and style collection view

--- frontend/src/views/Collection/index.tsx
+++ frontend/src/views/Collection/index.tsx
@@ -1,14 +1,112 @@
+import { Button, Classes, H3, H4, Intent, UL } from "@blueprintjs/core";
+import { IconNames } from "@blueprintjs/icons";
 import { RouteComponentProps } from "@reach/router";
+import psl from "psl";
 import React, { FC } from "react";
+import { LINK_TYPE } from "src/common/entities";
+import { useCollection } from "src/common/queries/collections";
+import {
+  CenterAlignedDiv,
+  CollectionButtons,
+  CollectionInfo,
+  DatasetContainer,
+  Description,
+  LinkContainer,
+  StyledButton,
+  ViewGrid,
+} from "./style";
 
 interface RouteProps {
-  id: string;
+  id?: string;
 }
 
 export type Props = RouteComponentProps<RouteProps>;
 
+const getDomain = (url: string): string | null => {
+  let hostname;
+  if (url.indexOf("//") > -1) {
+    hostname = url.split("/")[2];
+  } else {
+    hostname = url.split("/")[0];
+  }
+  const parsedURL = psl.parse(hostname);
+  if (parsedURL.error) {
+    console.error(parsedURL.input, parsedURL.error);
+    return null;
+  }
+  return parsedURL.domain;
+};
+
+const RenderEmptyDatasets = () => {
+  return (
+    <CenterAlignedDiv>
+      <H4>No datasets uploaded</H4>
+      <div>
+        Before you begin uploading dataset files:
+        <UL>
+          <li>
+            You must validate your dataset locally. We provide a local CLI
+            script to do this. Learn More
+          </li>
+          <li>
+            We only support adding datasets in the h5ad format at this time.
+          </li>
+        </UL>
+      </div>
+      <Button
+        intent={Intent.PRIMARY}
+        outlined
+        text={"Add Dataset from Dropbox"}
+      />
+    </CenterAlignedDiv>
+  );
+};
+
 const Collection: FC<Props> = ({ id }) => {
-  return <>Collection {id}</>;
+  const { data: collection } = useCollection(id ?? "");
+  if (!collection) return null;
+
+  return (
+    <ViewGrid>
+      <CollectionInfo>
+        <H3>{collection.name}</H3>
+        <Description>{collection.description}</Description>
+        <LinkContainer>
+          {collection.links?.map(({ url, type }) => {
+            if (!Object.values(LINK_TYPE).includes(type)) return null;
+            const domain = getDomain(url);
+            if (domain)
+              return (
+                <React.Fragment key={`${type}+${url}`}>
+                  <span className={Classes.TEXT_MUTED}>{type}</span>
+                  <a href={url}>{domain}</a>
+                </React.Fragment>
+              );
+          })}
+        </LinkContainer>
+      </CollectionInfo>
+      <CollectionButtons>
+        <StyledButton
+          icon={IconNames.TRASH}
+          text={"Delete"}
+          minimal
+          intent={Intent.DANGER}
+        />
+        <StyledButton text={"Share"} outlined intent={Intent.PRIMARY} />
+        <StyledButton text={"Publish"} intent={Intent.PRIMARY} />
+      </CollectionButtons>
+      <DatasetContainer>
+        {
+          // eslint-disable-next-line no-constant-condition
+          collection.datasets.length > 0 &&
+          // eslint-disable-next-line sonarjs/no-redundant-boolean
+          false ? /* DATASETS VIEW GOES HERE */ null : (
+            <RenderEmptyDatasets />
+          )
+        }
+      </DatasetContainer>
+    </ViewGrid>
+  );
 };
 
 export default Collection;
--- frontend/src/views/Collection/style.ts
+++ frontend/src/views/Collection/style.ts
@@ -11,3 +11,56 @@ export const TopBar = styled.div`
   justify-content: space-between;
   flex-direction: row;
 `;
+
+export const ViewGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(8, 1fr);
+  width: 100%;
+`;
+
+export const CollectionInfo = styled.div`
+  grid-column: 1 / span 5;
+  margin-bottom: 24px;
+`;
+
+export const LinkContainer = styled.div`
+  display: grid;
+  grid-template-columns: max-content auto;
+  column-gap: 16px;
+  row-gap: 8px;
+  margin-top: 8px;
+`;
+
+export const CollectionButtons = styled.div`
+  grid-column: 6 / span 3;
+  justify-self: end;
+`;
+
+export const Description = styled.div`
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 18px;
+  letter-spacing: -0.1px;
+  text-align: left;
+`;
+
+export const DatasetContainer = styled.div`
+  grid-column: 1 / span 8;
+  border: 1px solid #e1e8ed;
+  border-radius: 3px;
+`;
+
+export const CenterAlignedDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 340px;
+  justify-content: center;
+  align-items: center;
+  margin: auto auto;
+  height: 229px;
+  font-weight: 400;
+  line-height: 18px;
+  letter-spacing: -0.10000000149011612px;
+  text-align: left;
+`;
