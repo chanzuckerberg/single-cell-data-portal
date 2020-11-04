bcb1037c7a518c840caa7069e4ad5d01fadb7e4f
Seve Badajoz <sbadajoz@chanzuckerberg.com>
2020-11-03 17:59:42 -0800

    add top margin

--- frontend/src/views/Collection/style.ts
+++ frontend/src/views/Collection/style.ts
@@ -16,6 +16,7 @@ export const ViewGrid = styled.div`
   display: grid;
   grid-template-columns: repeat(8, 1fr);
   width: 100%;
+  margin-top: 128px;
 `;
 
 export const CollectionInfo = styled.div`
