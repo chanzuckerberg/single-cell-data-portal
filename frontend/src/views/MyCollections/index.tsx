0971ffdc24f4139afed7093fe3c4dc46e9f2660b
Seve Badajoz <sbadajoz@chanzuckerberg.com>
2020-11-10 14:53:06 -0800

    create collection table

--- frontend/src/views/MyCollections/index.tsx
+++ frontend/src/views/MyCollections/index.tsx
@@ -1,11 +1,14 @@
-import { H1, Text } from "@blueprintjs/core";
+import { H1, HTMLTable, Text } from "@blueprintjs/core";
 import React, { FC } from "react";
 import { useCollections } from "src/common/queries/collections";
+import CollectionRow from "src/components/collections/components/CollectionRow";
 import { ViewGrid } from "../globalStyle";
 import { StyledCreateCollection, TitleAndDescription } from "./style";
 
 const MyCollections: FC<Props> = () => {
-  const { data: collection, isLoading } = useCollections();
+  const { data: collections, isFetching } = useCollections();
+  if (isFetching && !collections) return null;
+  if (!collections) return null;
   return (
     <ViewGrid>
       <TitleAndDescription>
@@ -16,6 +19,23 @@ const MyCollections: FC<Props> = () => {
         </Text>
       </TitleAndDescription>
       <StyledCreateCollection />
+      <HTMLTable bordered style={{ gridColumn: "1/9" }}>
+        <thead>
+          <tr>
+            <th>Collection</th>
+            <th>Organ</th>
+            <th>Assay</th>
+            <th>Species</th>
+            <th>Cell Count</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {collections?.map((collection) => (
+            <CollectionRow id={collection.id} key={collection.id} />
+          ))}
+        </tbody>
+      </HTMLTable>
     </ViewGrid>
   );
 };
