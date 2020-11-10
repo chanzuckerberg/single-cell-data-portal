3cca0f0edb0d72d93cfa58b6950cc32fa7371757
Jessica Gadling <jgadling@chanzuckerberg.com>
2020-11-10 13:12:42 -0800

    Fix container-init

--- frontend/container_init.sh
+++ frontend/container_init.sh
@@ -4,6 +4,9 @@ echo " ====="
 echo "| starting frontend container"
 echo " ====="
 echo
+if [ ! -e ./node_modules ]; then
+  mkdir -p ./node_modules
+fi
 ln -sf /opt/node_app/node_modules/* ./node_modules/.
 cp src/configs/local.js src/configs/configs.js
 exec gatsby develop --host 0.0.0.0
