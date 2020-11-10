da702d5faf0ecb2e9903e92cb1c51cd6dc124043
Seve Badajoz <sbadajoz@chanzuckerberg.com>
2020-11-10 11:58:21 -0800

    title and create button

--- frontend/src/views/MyCollections/index.tsx
+++ frontend/src/views/MyCollections/index.tsx
@@ -1,7 +1,23 @@
+import { H1, Text } from "@blueprintjs/core";
 import React, { FC } from "react";
+import { useCollections } from "src/common/queries/collections";
+import { ViewGrid } from "../globalStyle";
+import { StyledCreateCollection, TitleAndDescription } from "./style";
 
-const MyCollections: FC = () => {
-  return <>My Collections</>;
+const MyCollections: FC<Props> = () => {
+  const { data: collection, isLoading } = useCollections();
+  return (
+    <ViewGrid>
+      <TitleAndDescription>
+        <H1>My Collections</H1>
+        <Text>
+          A list of collections you have either created, or have been shared
+          with you
+        </Text>
+      </TitleAndDescription>
+      <StyledCreateCollection />
+    </ViewGrid>
+  );
 };
 
 export default MyCollections;
---
+++ frontend/src/views/MyCollections/style.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+import CreateCollection from "../../components/CreateCollectionModal";
+
+export const TitleAndDescription = styled.div`
+  grid-column: 1/6;
+`;
+
+export const StyledCreateCollection = styled(CreateCollection)`
+  grid-column: 8;
+`;
