54c75cb85a8b31aad86ad832aadf24cc0d9403b7
maniarathi <arathi.mani@chanzuckerberg.com>
2020-11-22 21:29:48 -0500

    Fix uuid

--- tests/scale/scale_test_portal_apis.py
+++ tests/scale/scale_test_portal_apis.py
@@ -9,7 +9,7 @@ DEV_COLLECTION_IDS = [
     "673637cf-dcb7-45e1-bb88-72a27c50c8ca",
     "a55cac37-629f-4d28-9b3f-4ece7a7ac174",
     "e7c47289-22fe-440e-ba24-a4434ac3f379",
-    "18e6337b-8c36-4977-9ac8-ca8aa5494482",
+    "18e6337b-8c36-4977-9ac8-ca8aa5494481",
 ]
 
 
