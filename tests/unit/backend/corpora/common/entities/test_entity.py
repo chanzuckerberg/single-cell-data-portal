c98ecca2c53eb128e5d5b8f4ee594feac8811bc5
Trent Smith <trent.smith@chanzuckerberg.com>
2020-11-23 10:35:29 -0800

    fmt

--- tests/unit/backend/corpora/common/entities/test_entity.py
+++ tests/unit/backend/corpora/common/entities/test_entity.py
@@ -4,13 +4,9 @@ from backend.corpora.common.corpora_orm import DbDatasetProcessingStatus, Upload
 from backend.corpora.common.entities.dataset import Entity
 
 
-
 class TestEntity(unittest.TestCase):
     def test__create_sub_object(self):
-        test_params = {
-            "row": {"upload_status": UploadStatus.WAITING},
-            "db_table": DbDatasetProcessingStatus
-        }
+        test_params = {"row": {"upload_status": UploadStatus.WAITING}, "db_table": DbDatasetProcessingStatus}
 
         with self.subTest(test_params):
             result = Entity._create_sub_object(**test_params)
@@ -24,10 +20,7 @@ class TestEntity(unittest.TestCase):
             self.assertEqual("test_dataset_id", result.dataset_id)
             self.assertEqual(UploadStatus.WAITING, result.upload_status)
 
-        test_params={
-            "row": {"fake_row": UploadStatus.WAITING},
-            "db_table": DbDatasetProcessingStatus
-        }
+        test_params = {"row": {"fake_row": UploadStatus.WAITING}, "db_table": DbDatasetProcessingStatus}
         with self.subTest(test_params):
             with self.assertRaises(TypeError):
                 Entity._create_sub_object(**test_params)
