1ac18df4b9d083030ce6365267e382c861f4464b
Timmy Huang <thuang@chanzuckerberg.com>
2020-11-24 13:05:08 -0800

    use Reach Router Link

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,14 +1,24 @@
+import { Intent } from "@blueprintjs/core";
+import { Link } from "@reach/router";
 import React, { FC } from "react";
+import { ROUTES } from "src/common/constants/routes";
 import { HomepageLink } from "../common/HomepageLink";
 import AuthButtons from "./components/AuthButtons";
-import { MainWrapper, Wrapper } from "./style";
+import { MainWrapper, MyCollectionsButton, Right, Wrapper } from "./style";
 
 const Header: FC = () => {
   return (
     <Wrapper>
       <MainWrapper>
         <HomepageLink />
-        <AuthButtons />
+        <Right>
+          <Link to={ROUTES.MY_COLLECTIONS}>
+            <MyCollectionsButton intent={Intent.PRIMARY} minimal>
+              My Collections
+            </MyCollectionsButton>
+          </Link>
+          <AuthButtons />
+        </Right>
       </MainWrapper>
     </Wrapper>
   );
