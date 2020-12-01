fb71dfb00891d72a0e0cecb005f54dc5e94ebebf
Michael Barrientos <mbarrientos@chanzuckerberg.com>
2020-12-01 13:39:54 -0800

    Move TestDatabse import inside of function

--- scripts/init_dev_db.py
+++ scripts/init_dev_db.py
@@ -9,7 +9,6 @@ from sqlalchemy_utils import database_exists, create_database
 pkg_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))  # noqa
 sys.path.insert(0, pkg_root)  # noqa
 
-from tests.unit.backend.corpora.fixtures.database import TestDatabase
 from backend.corpora.common.corpora_config import CorporaDbConfig
 
 if __name__ == "__main__":
@@ -18,4 +17,7 @@ if __name__ == "__main__":
     if not database_exists(engine.url):
         print("Database does not exist, creating database")
         create_database(engine.url)
+        # TODO(mbarrien): HACK HACK HACK! TestDatabase drops database upon import.
+        # Import inside function to avoid that undesired behavior.
+        from tests.unit.backend.corpora.fixtures.database import TestDatabase
         TestDatabase(real_data=True)
