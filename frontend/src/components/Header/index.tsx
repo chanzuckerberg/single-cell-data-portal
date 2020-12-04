579b8474ff832c24d1af9436f52ea8ebf96b7c47
Timmy Huang <thuang@chanzuckerberg.com>
2020-12-04 10:10:59 -0800

    thuang-fix-my-collections-only-if-auth

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -5,17 +5,22 @@ import { ROUTES } from "src/common/constants/routes";
 import { get } from "src/common/featureFlags";
 import { FEATURES } from "src/common/featureFlags/features";
 import { BOOLEAN } from "src/common/localStorage/set";
+import { useUserInfo } from "src/common/queries/auth";
 import { HomepageLink } from "../common/HomepageLink";
 import AuthButtons from "./components/AuthButtons";
 import { MainWrapper, MyCollectionsButton, Right, Wrapper } from "./style";
 
 const Header: FC = () => {
+  const isAuth = get(FEATURES.AUTH) === BOOLEAN.TRUE;
+
+  const { data: userInfo } = useUserInfo(isAuth);
+
   return (
     <Wrapper>
       <MainWrapper>
         <HomepageLink />
         <Right>
-          {get(FEATURES.CREATE_COLLECTION) === BOOLEAN.TRUE && (
+          {userInfo?.name && (
             <Link to={ROUTES.MY_COLLECTIONS}>
               <MyCollectionsButton intent={Intent.PRIMARY} minimal>
                 My Collections
