3dac3408c41363f0330deeb1ae333b1cfce994de
Seve Badajoz <sbadajoz@chanzuckerberg.com>
2020-12-07 11:06:14 -0800

    enable only on true

--- frontend/src/components/CreateCollectionModal/index.tsx
+++ frontend/src/components/CreateCollectionModal/index.tsx
@@ -25,7 +25,7 @@ const CreateCollection: FC<{ className?: string }> = ({ className }) => {
   const urlParams = new URLSearchParams(window.location.search);
   const param = urlParams.get(QUERY_PARAMETERS.LOGIN_MODULE_REDIRECT);
 
-  const shouldModuleOpen = param !== null && param?.toLowerCase() !== "false";
+  const shouldModuleOpen = param?.toLowerCase() === "true";
 
   const [isOpen, setIsOpen] = useState(shouldModuleOpen);
   const { data: userInfo, isLoading } = useUserInfo(isAuth);
