688060594acd3141105bec7e01507a914bf66625
Jessica Gadling <jgadling@chanzuckerberg.com>
2020-12-09 16:22:18 -0800

    Don't run gatsby in dev mode

--- frontend/container_init.sh
+++ frontend/container_init.sh
@@ -13,4 +13,10 @@ if [ ! -z "$API_URL" ]; then
 else
   cp src/configs/local.js src/configs/configs.js
 fi
-exec gatsby develop --host 0.0.0.0
+# Build and run without dev mode in remote dev env.
+if [ "${DEPLOYMENT_STAGE}" == "rdev" ]; then
+    npm run build
+    exec gatsby serve --host 0.0.0.0
+else
+    exec gatsby develop --host 0.0.0.0
+fi
