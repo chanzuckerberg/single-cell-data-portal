5565a1045fb31a7e7658631f0a6b468ee4fa94b5
Sandeep Mandala <smandala@chanzuckerberg.com>
2020-12-15 21:07:14 -0800

    delete db changes

--- scripts/populate_db.py
+++ scripts/populate_db.py
@@ -5,7 +5,7 @@ import sys
 
 import click
 from sqlalchemy import create_engine
-from sqlalchemy_utils import database_exists, create_database
+from sqlalchemy_utils import database_exists, create_database, drop_database
 
 pkg_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))  # noqa
 sys.path.insert(0, pkg_root)  # noqa
@@ -21,7 +21,8 @@ os.environ["DEPLOYMENT_STAGE"] = env
 @click.option('--create-schema/--skip-create-schema', default=False, help="Create schema if it doesn't exist")
 @click.option('--recreate-db/--skip-recreate-db', default=True, help='Drop and recreate DB tables.')
 @click.option('--populate-data/--skip-populate', default=True, help='Add test data to db.')
-def run_db_stuff(create_schema, recreate_db, populate_data):
+@click.option('--drop-db/--skip-drop-db', default=False, help='Drop Database.')
+def run_db_stuff(create_schema, recreate_db, populate_data, drop_db):
     # Create schema.
     if create_schema:
         engine = create_engine(CorporaDbConfig().database_uri)
@@ -40,6 +41,16 @@ def run_db_stuff(create_schema, recreate_db, populate_data):
     # Populate test data.
     if populate_data:
         testdb.populate_test_data()
+
+    # Drop database
+    if drop_db:
+        engine = create_engine(CorporaDbConfig().database_uri)
+        if database_exists(engine.url):
+            print("Database exists, dropping database")
+            drop_database(engine.url)
+        else:
+            print("Database does not exists")
+            exit(1)
 
 if __name__ == "__main__":
     run_db_stuff()
