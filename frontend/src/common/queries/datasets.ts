1f5cdf9ba2ff026c56a5e668b0f66de21b8c2e58
Seve Badajoz <sbadajoz@chanzuckerberg.com>
2020-12-21 13:50:48 -0800

    create dataset status query

---
+++ frontend/src/common/queries/datasets.ts
@@ -0,0 +1,27 @@
+import { useQuery } from "react-query";
+import { API_URL } from "src/configs/configs";
+import { API } from "../API";
+import { DatasetUploadStatus } from "../entities";
+import { apiTemplateToUrl } from "../utils/apiTemplateToUrl";
+import { DEFAULT_FETCH_OPTIONS } from "./common";
+import { ENTITIES } from "./entities";
+
+export const USE_DATASET_STATUS = {
+  entities: [ENTITIES.DATASET_STATUS],
+  id: "dataset",
+};
+
+async function fetchDatasetStatus(
+  _: unknown,
+  dataset_uuid: string
+): Promise<DatasetUploadStatus> {
+  const url = apiTemplateToUrl(API_URL + API.DATASET_STATUS, { dataset_uuid });
+  return await (await fetch(url, DEFAULT_FETCH_OPTIONS)).json();
+}
+
+export function useDatasetStatus(dataset_uuid: string) {
+  return useQuery<DatasetUploadStatus>(
+    [USE_DATASET_STATUS, dataset_uuid],
+    fetchDatasetStatus
+  );
+}
