bb1b7f7b096920cd42fc6ad36728132720a11ecf
Trent Smith <trent.smith@chanzuckerberg.com>
2020-12-22 12:36:54 -0800

    lint

--- tests/unit/backend/chalice/api_server/generate_data_mixin.py
+++ tests/unit/backend/chalice/api_server/generate_data_mixin.py
@@ -1,8 +1,5 @@
-import unittest
 
-from backend.corpora.common.corpora_orm import DbDatasetProcessingStatus
 from backend.corpora.common.entities import Collection, Dataset
-from backend.corpora.common.utils.db_utils import db_session_manager
 from tests.unit.backend.utils import BogusCollectionParams, BogusDatasetParams
 
 
@@ -10,6 +7,7 @@ class GenerateDataMixin:
     """
     Use to populate the database with test data that should be cleanup after the test
     """
+
     def generate_collection(self, **params) -> Collection:
         _collection = Collection.create(**BogusCollectionParams.get(**params))
         # Cleanup collection after test
