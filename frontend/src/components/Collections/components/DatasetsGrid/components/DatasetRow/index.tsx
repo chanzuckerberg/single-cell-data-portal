9d1cb777a598a657ac5bd0ad1d2908501fed20ec
Seve Badajoz <sbadajoz@chanzuckerberg.com>
2021-01-04 13:28:05 -0800

    add skeleton for uploading

--- frontend/src/components/Collections/components/DatasetsGrid/components/DatasetRow/index.tsx
+++ frontend/src/components/Collections/components/DatasetsGrid/components/DatasetRow/index.tsx
@@ -1,7 +1,11 @@
-import { Button, Checkbox, Intent, Spinner } from "@blueprintjs/core";
+import { Button, Checkbox, Classes, Intent, Spinner } from "@blueprintjs/core";
 import loadable from "@loadable/component";
 import React, { FC } from "react";
-import { Dataset, DatasetUploadStatus } from "src/common/entities";
+import {
+  Dataset,
+  DatasetUploadStatus,
+  UPLOAD_STATUS,
+} from "src/common/entities";
 import { useDatasetStatus } from "src/common/queries/datasets";
 import {
   DetailsCell,
@@ -28,7 +32,10 @@ const AsyncPopover = loadable(
     )
 );
 
-const conditionalPopover = (values: string[]) => {
+const skeletonDiv = <div className={Classes.SKELETON}>PLACEHOLDER_TEXT</div>;
+
+const conditionalPopover = (values: string[], datasetStatus: UPLOAD_STATUS) => {
+  if (datasetStatus !== UPLOAD_STATUS.UPLOADED) return <td>{skeletonDiv}</td>;
   if (!values || values.length === 0) {
     return <LeftAlignedDetailsCell>-</LeftAlignedDetailsCell>;
   }
@@ -47,7 +54,6 @@ const DatasetRow: FC<Props> = ({ dataset, checkHandler, file }) => {
 
   let { name } = dataset;
   if (!name) name = file?.name ?? dataset.id;
-
   const { data: datasetStatus, isError } = useDatasetStatus(dataset.id);
   if (isError) console.error(datasetStatus);
   if (!datasetStatus) return null;
@@ -61,11 +67,15 @@ const DatasetRow: FC<Props> = ({ dataset, checkHandler, file }) => {
           <div>{name}</div>
         </TitleContainer>
       </DetailsCell>
-      {conditionalPopover(tissue)}
-      {conditionalPopover(assay)}
-      {conditionalPopover(disease)}
-      {conditionalPopover(organism)}
-      <RightAlignedDetailsCell>{cell_count || "-"}</RightAlignedDetailsCell>
+      {conditionalPopover(tissue, datasetStatus.upload_status)}
+      {conditionalPopover(assay, datasetStatus.upload_status)}
+      {conditionalPopover(disease, datasetStatus.upload_status)}
+      {conditionalPopover(organism, datasetStatus.upload_status)}
+      <RightAlignedDetailsCell>
+        {datasetStatus.upload_status !== UPLOAD_STATUS.UPLOADED
+          ? skeletonDiv
+          : cell_count}
+      </RightAlignedDetailsCell>
       <RightAlignedDetailsCell>
         {isUploading ? (
           renderUploadStatus(datasetStatus)
