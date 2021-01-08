78792264458d48a9aed05802da2dd4ef3086299a
Seve Badajoz <sbadajoz@chanzuckerberg.com>
2021-01-08 15:39:41 -0800

    lint

--- frontend/src/components/Collections/components/Grid/components/Row/CollectionRow/index.tsx
+++ frontend/src/components/Collections/components/Grid/components/Row/CollectionRow/index.tsx
@@ -7,13 +7,13 @@ import {
   VISIBILITY_TYPE,
 } from "src/common/entities";
 import { useCollection } from "src/common/queries/collections";
-import { StyledCell } from "../../../common/style";
-import { aggregateDatasetsMetadata } from "../../../common/utils";
 import {
   LeftAlignedDetailsCell,
   RightAlignedDetailsCell,
+  StyledCell,
   StyledRow,
-} from "../common/style";
+} from "../../../common/style";
+import { aggregateDatasetsMetadata } from "../../../common/utils";
 import { CollectionTitleText } from "./style";
 
 interface Props {
