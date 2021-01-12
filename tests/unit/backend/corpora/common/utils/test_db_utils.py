6d9b5bb91cd157409c5053725c9af48795bce126
Trent Smith <1429913+Bento007@users.noreply.github.com>
2021-01-12 11:26:11 -0800

    Publish a public collection (#781)
    - Add POST /dp/v1/collections/{collection_uuid}/publish to openapi
    - Remove POST /dp/v1/submission/{collection_uuid}/publish
    - Modify `Collection.if_owner` to return a Collection, not a DBCollection. This is for consistency.
    - Add clone to db_utils to make a clone of the db_object. This is used to replace the private collection with a public one.
    - Add `Collection.publish` to publish a private collection
    - Modify `DbUtils.delete` to pass if the row to delete does not exist. This is because while testing publish, we ran into errors cleaning up rows that were already deleted by the publishing code.
    - Add `post` to handle publishing
    - Move BaseAPITest to its own file. Dependent imports have been updated.
    - Add BaseAuthAPITest to simplify setting up tests that use the `MockOauthServer`. Dependent tests have been modified.

--- tests/unit/backend/corpora/common/utils/test_db_utils.py
+++ tests/unit/backend/corpora/common/utils/test_db_utils.py
@@ -1,5 +1,5 @@
 from backend.corpora.common.corpora_orm import DbDataset
-from backend.corpora.common.utils.db_utils import DbUtils, db_session_manager
+from backend.corpora.common.utils.db_utils import DbUtils, db_session_manager, clone
 from backend.corpora.common.utils.exceptions import CorporaException
 from tests.unit.backend.fixtures.data_portal_test_case import DataPortalTestCase
 
@@ -20,3 +20,24 @@ class TestDBSessionManager(DataPortalTestCase):
                 manager.session.query(DbDataset).filter([DbDataset.id == "test_dataset_id"]).update(
                     {DbDataset.id: None}
                 )
+
+
+class TestClone(DataPortalTestCase):
+    def test_positive(self):
+        revision = 1
+        dataset_id = "1234"
+        dataset = DbDataset(id=dataset_id, revision=revision)
+        new_pk = "new_primary_key"
+        clone_dataset = clone(dataset, primary_key={"id": new_pk})
+        self.assertNotEqual(dataset.id, clone_dataset.id)
+        self.assertIsInstance(clone_dataset, dataset.__class__)
+        self.assertEqual(clone_dataset.id, new_pk)
+        self.assertEqual(clone_dataset.revision, revision)
+
+    def test_bad_field(self):
+        revision = 1
+        dataset_id = "1234"
+        dataset = DbDataset(id=dataset_id, revision=revision)
+        new_pk = "new_primary_key"
+        with self.assertRaises(TypeError):
+            clone(dataset, primary_key={"id": new_pk}, fake_field=False)
