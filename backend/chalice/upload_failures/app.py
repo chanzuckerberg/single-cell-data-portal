73311ea0141b65628b743307367d6b54dbc28ba6
srm78 <60442184+srm78@users.noreply.github.com>
2021-01-12 16:00:44 -0800

    lambda upload failures support for happy path (#805)
    * lambda container changes

--- backend/chalice/upload_failures/app.py
+++ backend/chalice/upload_failures/app.py
@@ -17,5 +17,8 @@ def handle_failure(event, context):
     dataset_uuid = event["dataset_uuid"]
     object_key = os.path.join(os.environ.get("REMOTE_DEV_PREFIX", ""), dataset_uuid).strip("/")
     delete_many_from_s3(os.environ["ARTIFACT_BUCKET"], object_key)
-    delete_many_from_s3(f"hosted-cellxgene-{os.environ['DEPLOYMENT_STAGE']}", object_key)
+    cellxgene_bucket = f"hosted-cellxgene-{os.environ['DEPLOYMENT_STAGE']}"
+    if os.getenv("CELLXGENE_BUCKET"):
+        cellxgene_bucket = os.getenv("CELLXGENE_BUCKET")
+    delete_many_from_s3(cellxgene_bucket, object_key)
     update_dataset_processing_status_to_failed(dataset_uuid, event["error"]["Cause"])
