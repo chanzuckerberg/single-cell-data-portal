1d59d6f016cea0af17d62a2d926f034d806b9f0b
Seve Badajoz <sbadajoz@chanzuckerberg.com>
2021-01-14 16:08:00 -0800

    create delete dataset query

--- frontend/src/common/queries/datasets.ts
+++ frontend/src/common/queries/datasets.ts
@@ -3,7 +3,7 @@ import { API_URL } from "src/configs/configs";
 import { API } from "../API";
 import { DatasetUploadStatus } from "../entities";
 import { apiTemplateToUrl } from "../utils/apiTemplateToUrl";
-import { DEFAULT_FETCH_OPTIONS } from "./common";
+import { DEFAULT_FETCH_OPTIONS, DELETE_FETCH_OPTIONS } from "./common";
 import { ENTITIES } from "./entities";
 
 export const USE_DATASET_STATUS = {
@@ -27,4 +27,21 @@ export function useDatasetStatus(dataset_uuid: string, shouldFetch: boolean) {
     fetchDatasetStatus,
     { enabled: shouldFetch, refetchInterval: REFETCH_INTERVAL_MS }
   );
+}
+
+export const USE_DELETE_DATASET = {
+  entities: [ENTITIES.DATASET],
+  id: "dataset",
+};
+
+async function deleteDataset(
+  _: unknown,
+  dataset_uuid: string
+): Promise<boolean> {
+  const url = apiTemplateToUrl(API_URL + API.DATASET, { dataset_uuid });
+  return await (await fetch(url, DELETE_FETCH_OPTIONS)).json();
+}
+
+export function useDeleteDataset(dataset_uuid: string) {
+  return useQuery<boolean>([USE_DELETE_DATASET, dataset_uuid], deleteDataset);
 }
