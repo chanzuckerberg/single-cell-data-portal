c2c061f48d551e8268377a7b94b073752030d593
Seve Badajoz <sbadajoz@chanzuckerberg.com>
2021-01-26 15:55:00 -0800

    add delete frontend to tests and incorporate feedback

--- frontend/tests/features/collection.test.ts
+++ frontend/tests/features/collection.test.ts
@@ -1,10 +1,22 @@
+import { ROUTES } from "src/common/constants/routes";
+import { apiTemplateToUrl } from "src/common/utils/apiTemplateToUrl";
+import { API_URL } from "src/configs/configs";
 import { TEST_ENV, TEST_URL } from "tests/common/constants";
 import { goToPage } from "tests/utils/helpers";
 import { getText } from "tests/utils/selectors";
 
 const describeIfCalledByDevEnv = TEST_ENV === "dev" ? describe : describe.skip;
 
-describeIfCalledByDevEnv("CRUD Collection", async () => {
+/*  eslint-disable sort-keys */
+const TEST_COLLECTION = {
+  name: "TEST COLLECTION",
+  description: "TEST DESCRIPTION",
+  contactName: "TEST NAME",
+  contactEmail: "TEST@example.com",
+};
+/* eslint-enable sort-keys */
+
+describeIfCalledByDevEnv("Collection", async () => {
   it("creates and deletes a collection", async () => {
     await goToPage(TEST_URL);
     // LOG IN
@@ -12,11 +24,13 @@ describeIfCalledByDevEnv("CRUD Collection", async () => {
     await page.waitForNavigation({ url: TEST_URL, waitUntil: "networkidle" });
     // Create collection
     await page.click(getText("Create Collection"));
-    await page.fill("#name", "TEST COLLECTION");
-    await page.fill("#description", "TEST DESCRIPTION");
-    await page.fill("#contact-name", "TEST");
-    await page.fill("#contact-email", "TEST@example.coms");
-    await page.click("input[type=checkbox]");
+    await page.fill("#name", TEST_COLLECTION.name);
+    await page.fill("#description", TEST_COLLECTION.description);
+    await page.fill("#contact-name", TEST_COLLECTION.contactName);
+    await page.fill("#contact-email", TEST_COLLECTION.contactEmail);
+    await page.click(
+      getText("I agree to cellxgene's data submission policies.")
+    );
     const [request, response] = await Promise.all([
       page.waitForEvent("request"),
       page.waitForEvent("response"),
@@ -25,18 +39,21 @@ describeIfCalledByDevEnv("CRUD Collection", async () => {
 
     const cookie = request.headers().cookie;
 
-    const collectionID = (await response.json()) as object;
+    const { collectionID } = (await response.json()) as {
+      collectionID: string;
+    };
 
-    await expect(page).toHaveSelector("h3");
-    // CLICK DELETE COLLECTION BUTTON HERE
-    // TEMP: FETCH ENDPOINT TO DELETE COLLECTION
-    const deleteResponse = await fetch(
-      `https://api.cellxgene.dev.single-cell.czi.technology/dp/v1/collections/${collectionID}`,
-      { headers: { cookie }, method: "DELETE" }
+    await expect(page).toHaveText(TEST_COLLECTION.name);
+
+    // Try delete
+    await page.click("Delete");
+    await page.click("Delete Collection");
+
+    await goToPage(
+      apiTemplateToUrl(API_URL + ROUTES.PRIVATE_COLLECTION, {
+        id: collectionID,
+      })
     );
-
-    expect(deleteResponse.status).toBe(202);
-    await page.reload();
-    await expect(page).not.toHaveSelector("h3");
+    await expect(page).not.toHaveText(TEST_COLLECTION.name);
   });
 });
