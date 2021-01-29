c6c8f201c79b2ad8b0a8df10a72cd8b6773dd1b6
Trent Smith <trent.smith@chanzuckerberg.com>
2021-01-29 12:06:53 -0800

    lint

--- backend/corpora/common/corpora_config.py
+++ backend/corpora/common/corpora_config.py
@@ -70,8 +70,8 @@ class CorporaAuthConfig(SecretConfig):
         }
         template["issuer"].append(self.api_base_url + "/" if not self.api_base_url.endswith("/") else self.api_base_url)
         if self.config.get("api_signin_url") and os.environ["DEPLOYMENT_STAGE"] == "dev":
-            # Adding the API sign in URL to the list of allow token issues. This allow the API to accept Auth token generated
-            # for testing.
+            # Adding the API sign in URL to the list of allow token issues. This allow the API to accept Auth token
+            # generated for testing.
             template["issuer"].append(self.api_signin_url)
 
         return template
