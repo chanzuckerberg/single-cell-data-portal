b0f925e3713cb6dc42ef931c369266caf9dab384
Sandeep Mandala <smandala@chanzuckerberg.com>
2021-02-02 15:58:07 -0800

    add sample api using token

--- oidc_token.py
+++ oidc_token.py
@@ -1,7 +1,8 @@
+import base64
 import requests
 import json
 
-OIDC_TOKEN_URL="http://oidc/connect/token"
+OIDC_TOKEN_URL="http://localhost:4011/connect/token"
 USERS_CONFIGURATION_PATH="oauth/users.json"
 CLIENTS_CONFIGURATION_PATH="oauth/clients-config.json"
 
@@ -18,18 +19,31 @@ client_data = data[0]
 client_id = client_data['ClientId']
 client_secret = client_data['ClientSecrets'][0]
 
-print(client_id, client_secret, user_name, password)
-
 token_payload = {
     'client_id': client_id, 
     'client_secret': client_secret, 
     'grant_type': 'password', 
     'Username': user_name, 
-    'password': password
+    'password': password,
 }
 
-# Retrieve access token using password grant_type
 response = requests.post(OIDC_TOKEN_URL, data=token_payload)
 json_data = json.loads(response.text)
-print("access_token:", json_data["access_token"])
-print("refresh_token:", json_data["refresh_token"])
+
+json_data["id_token"] = json_data["access_token"]
+access_token = json_data["access_token"]
+id_token = json_data["id_token"]
+refresh_token = json_data["refresh_token"]
+
+
+print(f"\naccess_token: {access_token}\n")
+print(f"id_token: {id_token}\n")
+print(f"refresh_token: {refresh_token}\n")
+
+cookies = {
+    'cxguser' : base64.b64encode(json.dumps(json_data).encode("utf-8")).decode()
+}
+
+response = requests.get("http://localhost:5000/dp/v1/collections", cookies=cookies)
+json_data = json.loads(response.text)
+print("Collections:", json_data['collections'])
