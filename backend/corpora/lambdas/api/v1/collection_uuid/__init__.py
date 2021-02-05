371cb8bcd6ef53825a857f7a6307b0ddc6b2de3a
Trent Smith <trent.smith@chanzuckerberg.com>
2021-02-04 18:52:20 -0800

    fmt

--- backend/corpora/lambdas/api/v1/collection_uuid/__init__.py
+++ backend/corpora/lambdas/api/v1/collection_uuid/__init__.py
@@ -1,8 +1,6 @@
 from flask import make_response
 
-from .....common.utils.db_utils import db_session_manager
 
 
 def delete(collection_uuid: str):
-    with db_session_manager() as session:
-        return make_response({}, 202)
+    return make_response({}, 202)
