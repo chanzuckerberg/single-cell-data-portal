12b05585eadbb5f95d50cd124a932dc1d81faf11
Seve Badajoz <sbadajoz@chanzuckerberg.com>
2021-02-10 14:57:03 -0800

    test cleanup/fixes

--- frontend/tests/features/collection.test.ts
+++ frontend/tests/features/collection.test.ts
@@ -19,30 +19,27 @@ const TEST_COLLECTION = {
 describe("Collection", async () => {
   describeIfDeployed("Logged In Tests", () => {
     it("creates and deletes a collection", async () => {
-      // DEBUG
-      console.log(await context.storageState());
-      console.log((await context.storageState()).origins[0].localStorage);
-
       await goToPage();
       await login();
 
       const collectionId = await createCollection();
 
       // Try delete
-      await page.click("Delete");
-      await page.click("Delete Collection");
+      await page.click(getText("Delete"));
+      await page.click(getText("Delete Collection"));
 
       await goToPage(
         apiTemplateToUrl(API_URL + ROUTES.PRIVATE_COLLECTION, {
           id: collectionId,
         })
       );
-      await expect(page).not.toHaveText(TEST_COLLECTION.name);
+      await expect(page).not.toHaveSelector(getText(TEST_COLLECTION.name));
     });
 
     describe("Publish a collection", () => {
       describe("when no dataset", () => {
         it("shows disabled publish button", async () => {
+          await goToPage();
           await login();
 
           await createCollection();
@@ -60,23 +57,23 @@ describe("Collection", async () => {
 
 async function createCollection(): Promise<string> {
   await page.click(getText("Create Collection"));
-  await page.fill("#name", TEST_COLLECTION.name);
-  await page.fill("#description", TEST_COLLECTION.description);
-  await page.fill("#contact-name", TEST_COLLECTION.contactName);
-  await page.fill("#contact-email", TEST_COLLECTION.contactEmail);
+  await page.type("#name", TEST_COLLECTION.name, { delay: 50 });
+  await page.type("#description", TEST_COLLECTION.description, { delay: 50 });
+  await page.type("#contact-name", TEST_COLLECTION.contactName, { delay: 50 });
+  await page.type("#contact-email", TEST_COLLECTION.contactEmail, {
+    delay: 50,
+  });
   await page.click(getText("I agree to cellxgene's data submission policies."));
-
-  const [, response] = await Promise.all([
-    page.waitForEvent("request"),
+  const [response] = await Promise.all([
     page.waitForEvent("response"),
-    await page.click(getText("Create")),
+    page.click(getText("Create")),
   ]);
 
   const { collectionId } = (await response.json()) as {
     collectionId: string;
   };
 
-  await expect(page).toHaveText(TEST_COLLECTION.name);
+  await expect(page).toHaveSelector(getText(TEST_COLLECTION.name));
 
   return collectionId;
 }
