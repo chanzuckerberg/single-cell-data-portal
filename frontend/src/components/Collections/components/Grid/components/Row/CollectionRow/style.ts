e8bd3ecb949fc86b0eceeb2b140c8a6a114e3c55
Seve Badajoz <sbadajoz@chanzuckerberg.com>
2021-02-16 13:53:12 -0800

    tweak collection title styling

--- frontend/src/components/Collections/components/Grid/components/Row/CollectionRow/style.ts
+++ frontend/src/components/Collections/components/Grid/components/Row/CollectionRow/style.ts
@@ -1,8 +1,11 @@
+import { PT_TEXT_COLOR } from "src/components/common/theme";
 import styled from "styled-components";
 
 // Collection Title Column
-export const CollectionTitleText = styled.a`
+export const CollectionTitleText = styled.div`
+  color: ${PT_TEXT_COLOR};
   font-size: 14px;
+  font-weight: semibold;
   line-height: 18px;
   letter-spacing: -0.1px;
 `;
