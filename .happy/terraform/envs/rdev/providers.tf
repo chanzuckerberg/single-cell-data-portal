d695aa61a7ca91931ba00555b36b2c4a0c204941
Jessica Gadling <jgadling@chanzuckerberg.com>
2021-02-18 09:55:14 -0800

    Move providers up a level.

--- .happy/terraform/envs/rdev/providers.tf
+++ .happy/terraform/envs/rdev/providers.tf
@@ -1,12 +1,8 @@
 provider aws {
-
-  version = "~> 3.19.0"
+  version = "~> 3.28.0"
   region  = "us-west-2"
-
-
   assume_role {
     role_arn = "arn:aws:iam::${var.aws_account_id}:role/${var.aws_role}"
   }
-
   allowed_account_ids = [var.aws_account_id]
 }
---
+++ .happy/terraform/envs/stage/providers.tf
@@ -0,0 +1,8 @@
+provider aws {
+  version = "~> 3.28.0"
+  region  = "us-west-2"
+  assume_role {
+    role_arn = "arn:aws:iam::${var.aws_account_id}:role/${var.aws_role}"
+  }
+  allowed_account_ids = [var.aws_account_id]
+}
