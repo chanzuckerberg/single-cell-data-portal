5bdb0a3a5424701d1b2d8e974b7cb6dd6663fdd9
Seve Badajoz <sbadajoz@chanzuckerberg.com>
2021-02-22 11:09:01 -0800

    tweak imported fonts

--- frontend/src/components/seo.tsx
+++ frontend/src/components/seo.tsx
@@ -77,7 +77,7 @@ const SEO: FC<Props> = ({ description, lang, meta = [], title }) => {
     >
       <link rel="preconnect" href="https://fonts.gstatic.com" />
       <link
-        href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+        href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&display=swap"
         rel="stylesheet"
       />
     </Helmet>
