9e32d84cd9b866b372675cc7ca35dc6dc3987eca
Seve Badajoz <sbadajoz@chanzuckerberg.com>
2021-03-15 11:55:40 -0700

    fetch secret

--- frontend/jest/playwright-globalSetup.js
+++ frontend/jest/playwright-globalSetup.js
@@ -1,4 +1,25 @@
+const {
+  SecretsManagerClient,
+  GetSecretValueCommand,
+} = require("@aws-sdk/client-secrets-manager");
 const featureFlags = require("./featureFlags");
+
+const client = new SecretsManagerClient({
+  endpoint: process.env["BOTO_ENDPOINT_URL"],
+});
+
+const secretValueRequest = {
+  SecretId: "corpora/backend/dev/auth0-secret['test_account_password']",
+};
+
+const command = new GetSecretValueCommand(secretValueRequest);
+
 module.exports = async () => {
+  try {
+    const secret = await client.send(command);
+    process.env.TEST_ACCOUNT_PASS = secret;
+  } catch (error) {
+    console.error(error);
+  }
   process.env.STORAGE = JSON.stringify(featureFlags);
 };
