48145b1a2a17e3c8916bd02875f189c44f79b130
Seve Badajoz <sbadajoz@chanzuckerberg.com>
2021-04-02 14:47:26 -0700

    single path for login

--- frontend/tests/utils/helpers.ts
+++ frontend/tests/utils/helpers.ts
@@ -1,10 +1,4 @@
-import {
-  TEST_EMAIL,
-  TEST_ENV,
-  TEST_PASSWORD,
-  TEST_URL,
-  TEST_USERNAME,
-} from "../common/constants";
+import { TEST_PASSWORD, TEST_URL, TEST_USERNAME } from "../common/constants";
 import { getText } from "./selectors";
 
 export const TIMEOUT_MS = 3 * 1000;
@@ -20,25 +14,18 @@ export async function login() {
     await expect(page).toHaveSelector(getText("My Collections"), {
       timeout: TIMEOUT_MS,
     });
+    console.log("LOG IN NOT FOUND");
   } catch (error) {
     const url = page.url();
-    if (TEST_ENV === "happy") {
-      await page.click(getText("Log In"));
-      await page.fill('[name="Username"]', TEST_USERNAME);
-      await page.fill('[name="Password"]', TEST_PASSWORD);
-      await Promise.all([
-        page.waitForNavigation({ waitUntil: "networkidle" }),
-        page.click('[value="login"]'),
-      ]);
-    } else {
-      await page.click(getText("Log In"));
-      await page.fill('[name="email"]', TEST_EMAIL);
-      await page.fill('[name="password"]', TEST_PASSWORD);
-      await Promise.all([
-        page.waitForNavigation({ waitUntil: "networkidle" }),
-        page.click('[name="submit"]'),
-      ]);
-    }
+    const username = TEST_USERNAME;
+    console.log(username);
+    await page.click(getText("Log In"));
+    await page.fill('[name="Username"], [name="email"]', username);
+    await page.fill('[name="Password"], [name="password"]', TEST_PASSWORD);
+    await Promise.all([
+      page.waitForNavigation({ waitUntil: "networkidle" }),
+      page.click('[value="login"], [name="submit"]'),
+    ]);
 
     expect(page.url()).toContain(url);
   }
