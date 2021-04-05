bfe61920fe028331dd587a98eb31b3510586d3ea
Trent Smith <1429913+Bento007@users.noreply.github.com>
2021-04-05 14:06:37 -0700

    Adding endpoint to delete a geneset (#1024)
    * Adding endpoint to delete a geneset
    * add tests that dataset exists
    
    Co-authored-by: Madison Dunitz <madison.dunitz@chanzuckerberg.com>
    Co-authored-by: Timmy Huang <tihuan@users.noreply.github.com>

---
+++ backend/corpora/lambdas/api/v1/gene_sets.py
@@ -0,0 +1,24 @@
+from flask import make_response, jsonify, g
+
+from ....common.corpora_orm import CollectionVisibility
+from ....common.entities.geneset import Geneset
+from ....common.utils.exceptions import (
+    ForbiddenHTTPException,
+)
+
+
+def delete(geneset_uuid: str, user: str):
+    """
+    Deletes an existing geneset
+    """
+    db_session = g.db_session
+    geneset = Geneset.get(db_session, geneset_uuid)
+    accepted_response = "", 202
+    if not geneset:
+        return accepted_response
+    if geneset.collection.owner != user:
+        raise ForbiddenHTTPException()
+    if geneset.collection_visibility == CollectionVisibility.PUBLIC:
+        return make_response(jsonify("Cannot delete a public geneset"), 405)
+    geneset.delete()
+    return accepted_response
