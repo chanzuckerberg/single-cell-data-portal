fbb2673f41b8bebd0ec85b81e4c7164e0789c18e
Seve Badajoz <sbadajoz@chanzuckerberg.com>
2021-04-21 17:06:49 -0700

    grab feature flags everywhere

--- frontend/src/common/featureFlags/index.ts
+++ frontend/src/common/featureFlags/index.ts
@@ -1,6 +1,7 @@
 import { get as getLocalStorage } from "src/common/localStorage/get";
 import { BOOLEAN, set as setLocalStorage } from "src/common/localStorage/set";
 import { isSSR } from "../utils/isSSR";
+import { FEATURES } from "./features";
 
 const FEATURE_FLAG_PREFIX = "cxg-ff-";
 
@@ -9,15 +10,13 @@ export function get(key: string): string | null {
   if (isSSR()) return null;
 
   const params = new URLSearchParams(window.location.search);
-
-  const URLValue = params.get(key);
-
-  if (URLValue !== null) {
-    const URLValueAsBooleanString: BOOLEAN =
-      URLValue === "true" ? BOOLEAN.TRUE : BOOLEAN.FALSE;
-
-    setLocalStorage(FEATURE_FLAG_PREFIX + key, URLValueAsBooleanString);
-  }
+  params.forEach((value, key) => {
+    if (Object.values(FEATURES).includes(key as FEATURES)) {
+      const URLValueAsBooleanString =
+        value === "true" ? BOOLEAN.TRUE : BOOLEAN.FALSE;
+      setLocalStorage(FEATURE_FLAG_PREFIX + key, URLValueAsBooleanString);
+    }
+  });
 
   return getLocalStorage(FEATURE_FLAG_PREFIX + key);
 }
