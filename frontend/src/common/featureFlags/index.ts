ce77f6aede57283fe5daa3df6c03a8d12f1db1d7
Seve Badajoz <sbadajoz@chanzuckerberg.com>
2021-04-21 17:47:22 -0700

    make a bit more efficient

--- frontend/src/common/featureFlags/index.ts
+++ frontend/src/common/featureFlags/index.ts
@@ -1,6 +1,7 @@
 import { get as getLocalStorage } from "src/common/localStorage/get";
 import { BOOLEAN, set as setLocalStorage } from "src/common/localStorage/set";
 import { isSSR } from "../utils/isSSR";
+import { removeParams } from "../utils/removeParams";
 import { FEATURES } from "./features";
 
 const FEATURE_FLAG_PREFIX = "cxg-ff-";
@@ -9,16 +10,20 @@ const FEATURE_FLAG_PREFIX = "cxg-ff-";
 export function get(key: string): string | null {
   if (isSSR()) return null;
 
+  const newlyAddedParams: Array<string> = [];
   const params = new URLSearchParams(window.location.search);
-  if (params) {
+  if (window.location.search.indexOf("?") !== -1) {
     Object.values(FEATURES).forEach((feature) => {
-      if (feature in params) {
+      if (params.has(feature)) {
         const URLValueAsBooleanString =
           params.get(feature) === "true" ? BOOLEAN.TRUE : BOOLEAN.FALSE;
         setLocalStorage(FEATURE_FLAG_PREFIX + feature, URLValueAsBooleanString);
+        newlyAddedParams.push(feature);
       }
     });
   }
+
+  removeParams(newlyAddedParams);
 
   return getLocalStorage(FEATURE_FLAG_PREFIX + key);
 }
