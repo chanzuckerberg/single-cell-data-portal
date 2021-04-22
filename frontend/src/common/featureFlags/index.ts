167cf4d17957b1a47c3c9a5c520aafde47b59624
Seve Badajoz <sbadajoz@chanzuckerberg.com>
2021-04-21 17:10:33 -0700

    iterate over features instead of params

--- frontend/src/common/featureFlags/index.ts
+++ frontend/src/common/featureFlags/index.ts
@@ -10,13 +10,15 @@ export function get(key: string): string | null {
   if (isSSR()) return null;
 
   const params = new URLSearchParams(window.location.search);
-  params.forEach((value, key) => {
-    if (Object.values(FEATURES).includes(key as FEATURES)) {
-      const URLValueAsBooleanString =
-        value === "true" ? BOOLEAN.TRUE : BOOLEAN.FALSE;
-      setLocalStorage(FEATURE_FLAG_PREFIX + key, URLValueAsBooleanString);
-    }
-  });
+  if (params) {
+    Object.values(FEATURES).forEach((feature) => {
+      if (feature in params) {
+        const URLValueAsBooleanString =
+          params.get(feature) === "true" ? BOOLEAN.TRUE : BOOLEAN.FALSE;
+        setLocalStorage(FEATURE_FLAG_PREFIX + feature, URLValueAsBooleanString);
+      }
+    });
+  }
 
   return getLocalStorage(FEATURE_FLAG_PREFIX + key);
 }
