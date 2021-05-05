2be950498af6fbab44470e71453e81a79257b43c
Trent Smith <trent.smith@chanzuckerberg.com>
2021-05-05 15:44:58 -0700

    changing geneset S3 name to {dataset_id}-genesets.csv

--- tests/unit/backend/corpora/common/entities/test_dataset.py
+++ tests/unit/backend/corpora/common/entities/test_dataset.py
@@ -345,10 +345,9 @@ class TestDataset(CorporaTestCaseUsingMockAWS):
 
     def test__generate_tidy_csv_for_all_linked_genesets__stores_file_in_correct_location(self):
         collection = self.generate_collection(self.session)
-        deployment_directory_params = dict(url="http://test_domain.example/e/test_dataset_id.cxg/")
-        dataset = self.generate_dataset(
-            self.session, collection=collection, deployment_directories=[deployment_directory_params]
-        )
+        dataset = self.generate_dataset(self.session, collection=collection)
+        deployment_directory_params = dict(url=f"http://test_domain.example/e/{dataset.id}.cxg/")
+        dataset.update(deployment_directories=[deployment_directory_params])
         genes0 = [
             {"gene_symbol": "HBB", "gene_description": "gene 1", "additional_params": {}},
             {"gene_symbol": "HBA1", "gene_description": "gene 2", "additional_params": {}},
@@ -370,6 +369,8 @@ class TestDataset(CorporaTestCaseUsingMockAWS):
         with tempfile.TemporaryDirectory() as temp_dir_name:
             csv_file = dataset.generate_tidy_csv_for_all_linked_genesets(temp_dir_name)
             s3_file = dataset.copy_csv_to_s3(csv_file)
+        expected_suffix = f"{dataset.id}-genesets.csv"
+        self.assertTrue(s3_file.endswith(expected_suffix), msg=f"{s3_file} does not end with {expected_suffix}")
         stored_files = [x.key for x in self.cellxgene_bucket.objects.all()]
         self.assertIn(s3_file, stored_files)
 
