22df268f83e51572c30aa07dee141680723d0fca
Madison Dunitz <dunitzm@gmail.com>
2021-05-07 13:45:56 -0700

    sp but something isnt working

--- tests/unit/backend/corpora/common/entities/datasets/test_revision.py
+++ tests/unit/backend/corpora/common/entities/datasets/test_revision.py
@@ -12,38 +12,34 @@ class TestDatasetRevision(TestDataset):
             original_artifacts = dataset.pop("artifacts")
             for i in range(0, len(rev_artifacts)):
                 for key in rev_artifacts[i].keys():
-                    if key in ("id", "dataset_id"):
-                        self.assertNotEqual(rev_artifacts[i][key], original_artifacts[i][key])
-                    else:
-                        self.assertEqual(rev_artifacts[i][key], original_artifacts[i][key])
+                    self.compare_original_and_revision(original_artifacts[i], rev_artifacts[i], key,
+                                                       ("dataset_id", "id"))
 
         with self.subTest("deployment is correctly created and points to correct s3 uri "):
             rev_deployment = rev_dataset.pop("deployment_directories")[0]
             original_deployment = dataset.pop("deployment_directories")[0]
             for key in rev_deployment.keys():
-                if key in ("dataset_id", "id"):
-                    self.assertNotEqual(rev_deployment[key], original_deployment[key])
-                else:
-                    self.assertEqual(rev_deployment[key], original_deployment[key])
+                self.compare_original_and_revision(original_deployment, rev_deployment, key,
+                                                   ("dataset_id", "id"))
 
         with self.subTest("Test processing status copied over"):
             rev_processing_status = rev_dataset.pop("processing_status")
             original_processing_status = dataset.pop("processing_status")
             for key in rev_processing_status.keys():
-                if key in ("dataset_id", "id"):
-                    self.assertNotEqual(rev_processing_status[key], original_processing_status[key])
-                else:
-                    self.assertEqual(rev_processing_status[key], original_processing_status[key])
+                self.compare_original_and_revision(original_processing_status, rev_processing_status, key,
+                                                   ("dataset_id", "id"))
 
-        with self.subTest("revision count is correctly incremented"):
-            self.assertEqual(rev_dataset.pop("revision"), dataset.pop("revision") + 1)
         with self.subTest("revision points at a different collection"):
             revision_collection = rev_dataset.pop("collection")
             dataset_1_collection = dataset.pop("collection")
             self.assertNotEqual(revision_collection, dataset_1_collection)
         with self.subTest("metadata of revised matches original"):
             for key in rev_dataset.keys():
-                if key in ("revision", "original_id", "id", "collection_visibility"):
-                    self.assertNotEqual(rev_dataset[key], dataset[key])
-                else:
-                    self.assertEqual(rev_dataset[key], dataset[key])
+                self.compare_original_and_revision(dataset, rev_dataset, key,
+                                                   ("original_id", "id", "collection_visibility"))
+
+    def compare_original_and_revision(self, original, revision, key, unique_fields):
+        if key in unique_fields:
+            self.assertNotEqual(original[key], revision[key])
+        else:
+            self.assertEqual(original[key], revision[key])
