2c54e832ed976584d339225ce3b8d532a484b900
Seve Badajoz <sbadajoz@chanzuckerberg.com>
2021-05-12 13:06:36 -0700

    switch from imported transform to mock

--- frontend/jest/e2e.config.js
+++ frontend/jest/e2e.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   globalSetup: "./jest/playwright-globalSetup.js",
   moduleDirectories: ["node_modules", "<rootDir>"],
+  moduleNameMapper: {
+    "\\.svg$": "<rootDir>/jest/__mocks__/svgMock.js",
+  },
   preset: "jest-playwright-preset",
   rootDir: "../",
   setupFilesAfterEnv: ["expect-playwright", "./jest/playwright.setup.js"],
@@ -8,6 +11,5 @@ module.exports = {
   testRunner: "jest-circus/runner",
   transform: {
     "^.+\\.(tsx?|jsx?)$": `<rootDir>/jest/jest-preprocess.js`,
-    "^.+\\.svg$": "jest-svg-transformer",
   },
 };
