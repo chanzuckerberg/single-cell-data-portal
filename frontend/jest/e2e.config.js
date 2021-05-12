b50ef553c066db6deb73121a37602e9c7641d949
Seve Badajoz <sbadajoz@chanzuckerberg.com>
2021-05-11 17:02:05 -0700

    fix tests

--- frontend/jest/e2e.config.js
+++ frontend/jest/e2e.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   globalSetup: "./jest/playwright-globalSetup.js",
-  moduleDirectories: ["node_modules", "."],
+  moduleDirectories: ["node_modules", "<rootDir>"],
   preset: "jest-playwright-preset",
   rootDir: "../",
   setupFilesAfterEnv: ["expect-playwright", "./jest/playwright.setup.js"],
@@ -8,5 +8,6 @@ module.exports = {
   testRunner: "jest-circus/runner",
   transform: {
     "^.+\\.(tsx?|jsx?)$": `<rootDir>/jest/jest-preprocess.js`,
+    "^.+\\.svg$": "jest-svg-transformer",
   },
 };
