7e5077bd58fe7a068c06043886a7921e42311ea2
Severiano Badajoz <sbadajoz@chanzuckerberg.com>
2021-05-14 11:33:48 -0700

    fix: validate DOI path, sort datasets by cell count (#1169)
    * check that something is in the path of the url
    
    * sort by cell count

--- frontend/src/components/Collections/components/Grid/components/DatasetsGrid/index.tsx
+++ frontend/src/components/Collections/components/Grid/components/DatasetsGrid/index.tsx
@@ -48,7 +48,7 @@ const DatasetsGrid: FC<Props> = ({
         </tr>
       </thead>
       <tbody>
-        {sortByCreatedAtAscending(datasets).map((dataset) => (
+        {sortByCellCountDescending(datasets).map((dataset) => (
           <DatasetRow
             visibility={visibility}
             accessType={accessType}
@@ -65,8 +65,10 @@ const DatasetsGrid: FC<Props> = ({
   );
 };
 
-function sortByCreatedAtAscending(datasets: Dataset[]): Dataset[] {
-  return datasets?.sort((a, b) => a.created_at - b.created_at) || [];
+export function sortByCellCountDescending(datasets: Dataset[]): Dataset[] {
+  return (
+    datasets?.sort((a, b) => (b.cell_count ?? 0) - (a.cell_count ?? 0)) || []
+  );
 }
 
 export default DatasetsGrid;
