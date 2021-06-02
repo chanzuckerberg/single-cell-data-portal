1d679b7ccd6f4f7a5d5a14a0c40cb03809136dc2
Madison Dunitz <dunitzm@gmail.com>
2021-06-02 13:30:05 -0400

    dont import shared base for db tests

--- tests/unit/backend/corpora/common/test_corpora_orm.py
+++ tests/unit/backend/corpora/common/test_corpora_orm.py
@@ -9,6 +9,7 @@ from tests.unit.backend.fixtures.data_portal_test_case import DataPortalTestCase
 Test_Base = declarative_base(cls=TransformingBase)
 
 
+
 class DbParent(Test_Base):
     __tablename__ = "parent"
 
