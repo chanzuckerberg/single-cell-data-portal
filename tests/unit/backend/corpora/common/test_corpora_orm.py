38ce4f2936d6fa1c11bab2e994139a5da565dab7
Madison Dunitz <dunitzm@gmail.com>
2021-06-02 13:30:05 -0400

    dont import shared base for db tests

--- tests/unit/backend/corpora/common/test_corpora_orm.py
+++ tests/unit/backend/corpora/common/test_corpora_orm.py
@@ -1,10 +1,12 @@
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
-from backend.corpora.common.corpora_orm import Base
+from backend.corpora.common.corpora_orm import TransformingBase
 from backend.corpora.common.utils.db_session import DBSessionMaker, db_session_manager
 from tests.unit.backend.fixtures.data_portal_test_case import DataPortalTestCase
 
+Base = declarative_base(cls=TransformingBase)
 
 class DbParent(Base):
     __tablename__ = "parent"
