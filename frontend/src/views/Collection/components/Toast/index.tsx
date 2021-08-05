2c925909eab9c829abfb09c3586cdf1d80a0a789
Timmy Huang <thuang@chanzuckerberg.com>
2021-08-04 19:25:55 -0700

    fix: 1310 Add dataset refresh unexpected error

--- frontend/src/views/Collection/components/Toast/index.tsx
+++ frontend/src/views/Collection/components/Toast/index.tsx
@@ -1,16 +1,21 @@
 import { IToaster, IToastProps, Position, Toaster } from "@blueprintjs/core";
 
+/**
+ * (thuang): We can't lazy-load toaster via `.create()` on the first `.show()`
+ * call, because sometimes `.show()` is called inside React lifecycle hooks.
+ */
 let toaster: IToaster | null = null;
 
-const Toast = {
-  show(props: IToastProps) {
-    if (!toaster) {
-      toaster = Toaster.create({
-        position: Position.TOP,
-      });
-    }
+// (thuang): Instantiates on the client side only
+if (typeof document !== "undefined") {
+  toaster = Toaster.create({
+    position: Position.TOP,
+  });
+}
 
-    toaster.show(props);
+const Toast = {
+  show(props: IToastProps): void {
+    toaster?.show(props);
   },
 };
 
