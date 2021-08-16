ffcffa90a85b33edf07c3d55026d15aed403ae86
Leslie Park <ypark@chanzuckerberg.com>
2021-08-16 15:21:10 -0700

    Linter fixes.

--- tests/unit/backend/corpora/api_server/test_v1_collection.py
+++ tests/unit/backend/corpora/api_server/test_v1_collection.py
@@ -169,7 +169,6 @@ class TestCollection(BaseAuthAPITest):
                     "collection_id": "test_collection_id",
                     "collection_visibility": "PUBLIC",
                     "cell_type": [{"label": "test_cell_type", "ontology_term_id": "test_opo"}],
-                    "is_primary_data": True,
                     "X_normalization": "test_X_normalization",
                     "X_approximate_distribution": "NORMAL",
                     "is_valid": False,
