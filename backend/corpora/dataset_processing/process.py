eafc10d2180f86d0d76e19becae39420d9a381b7
Emanuele Bezzi <ebezzi@chanzuckerberg.com>
2021-09-13 11:06:49 -0400

    feat: 1297 use cellxgene-schema with labels (#1400)

--- backend/corpora/dataset_processing/process.py
+++ backend/corpora/dataset_processing/process.py
@@ -435,9 +435,11 @@ def process_cxg(local_filename, dataset_id, cellxgene_bucket):
     update_db(dataset_id, metadata, processing_status=dict(conversion_cxg_status=status))
 
 
-def validate_h5ad_file(dataset_id, local_filename):
+def validate_h5ad_file_and_add_labels(dataset_id, local_filename):
     update_db(dataset_id, processing_status=dict(validation_status=ValidationStatus.VALIDATING))
-    val_proc = subprocess.run(["cellxgene-schema", "validate", local_filename], capture_output=True)
+    output_filename = f"withlabels.{local_filename}"
+    commands = ["cellxgene-schema", "validate", "--add-labels", output_filename, local_filename]
+    val_proc = subprocess.run(commands, capture_output=True)
     if val_proc.returncode != 0:
         logger.error("Validation failed!")
         logger.error(f"stdout: {val_proc.stdout}")
@@ -459,6 +461,14 @@ def validate_h5ad_file(dataset_id, local_filename):
             validation_status=ValidationStatus.VALID,
         )
         update_db(dataset_id, processing_status=status)
+        return output_filename
+
+
+def clean_up_local_file(local_filename):
+    try:
+        os.remove(local_filename)
+    except Exception:
+        pass
 
 
 def log_batch_environment():
@@ -486,15 +496,18 @@ def process(dataset_id, dropbox_url, cellxgene_bucket, artifact_bucket):
         "local.h5ad",
     )
 
-    validate_h5ad_file(dataset_id, local_filename)
+    # No file cleanup needed due to docker run-time environment.
+    # To implement proper cleanup, tests/unit/backend/corpora/dataset_processing/test_process.py
+    # will have to be modified since it relies on a shared local file
 
+    file_with_labels = validate_h5ad_file_and_add_labels(dataset_id, local_filename)
     # Process metadata
-    metadata = extract_metadata(local_filename)
+    metadata = extract_metadata(file_with_labels)
     update_db(dataset_id, metadata)
 
     # create artifacts
-    process_cxg(local_filename, dataset_id, cellxgene_bucket)
-    create_artifacts(local_filename, dataset_id, artifact_bucket)
+    process_cxg(file_with_labels, dataset_id, cellxgene_bucket)
+    create_artifacts(file_with_labels, dataset_id, artifact_bucket)
     update_db(dataset_id, processing_status=dict(processing_status=ProcessingStatus.SUCCESS))
 
 
--- tests/unit/processing_container/test_process.py
+++ tests/unit/processing_container/test_process.py
@@ -57,8 +57,9 @@ class TestDatasetProcessing(CorporaTestCaseUsingMockAWS):
         make_loom(str(self.real_h5ad_filename))
 
     # TODO: Remove mocking of validate_h5ad_file after validation process is updated to be 2.0 compliant.
-    @patch("backend.corpora.dataset_processing.process.validate_h5ad_file")
+    @patch("backend.corpora.dataset_processing.process.validate_h5ad_file_and_add_labels")
     def test_main(self, mock_validate_h5ad):
+        mock_validate_h5ad.return_value = "local.h5ad"
         url = self.presigned_url
         dataset = self.generate_dataset(
             self.session, collection_id="test_collection_id", collection_visibility=CollectionVisibility.PUBLIC.name
