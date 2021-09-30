7bb4e9066fe604581dd9f1d12b64f49f351b5d82
Dimple Jethani <djethani@chanzuckerberg.com>
2021-09-30 14:57:12 -0700

    feat: migrate data_submission_policy_version to be nullable

---
+++ backend/database/versions/23bade351f23_update_data_submission_policy_version.py
@@ -0,0 +1,30 @@
+"""update_data_submission_policy_version
+
+Revision ID: 23bade351f23
+Revises: 424e875043d3
+Create Date: 2021-09-24 16:41:33.297342
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '23bade351f23'
+down_revision = '424e875043d3'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.alter_column('project', 'data_submission_policy_version',
+               existing_type=sa.VARCHAR(),
+               nullable=True)
+
+
+def downgrade():
+    op.execute("UPDATE project SET data_submission_policy_version='' WHERE data_submission_policy_version=null")
+
+    op.alter_column('project', 'data_submission_policy_version',
+               existing_type=sa.VARCHAR(),
+               nullable=False)
