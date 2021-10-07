6bfdec245a1e0ec146149e0098e5dc854f9a58da
Emanuele Bezzi <ebezzi@chanzuckerberg.com>
2021-10-07 13:25:44 -0400

    fix: plausible config

--- frontend/src/configs/staging.js
+++
@@ -1,9 +0,0 @@
-const { PLAUSIBLE_DATA_DOMAIN_STAGING } = require("./common");
-
-const configs = {
-  API_URL: "https://api.cellxgene.staging.single-cell.czi.technology",
-  PLAUSIBLE_DATA_DOMAIN: PLAUSIBLE_DATA_DOMAIN_STAGING,
-  SENTRY_DEPLOYMENT_ENVIRONMENT: "staging",
-};
-
-if (typeof module !== "undefined") module.exports = configs;
