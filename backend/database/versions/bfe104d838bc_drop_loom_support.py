a8f918f4b36c366ba2fa8b30d326397e7bc2af14
blrnw3 <bmasscheleinrodgers@chanzuckerberg.com>
2021-10-08 12:34:08 -0700

    add mig, empty for now

---
+++ backend/database/versions/bfe104d838bc_drop_loom_support.py
@@ -0,0 +1,24 @@
+"""drop_loom_support
+
+Revision ID: bfe104d838bc
+Revises: 23bade351f23
+Create Date: 2021-10-08 12:32:38.874124
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'bfe104d838bc'
+down_revision = '23bade351f23'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
