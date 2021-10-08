4fe12e85eff89226d278b4588b3b43e393f86ffc
blrnw3 <bmasscheleinrodgers@chanzuckerberg.com>
2021-10-08 13:03:43 -0700

    weird fmt but ok

--- backend/database/versions/bfe104d838bc_drop_loom_support.py
+++ backend/database/versions/bfe104d838bc_drop_loom_support.py
@@ -8,8 +8,8 @@ Create Date: 2021-10-08 12:32:38.874124
 from alembic import op
 
 # revision identifiers, used by Alembic.
-revision = 'bfe104d838bc'
-down_revision = '23bade351f23'
+revision = "bfe104d838bc"
+down_revision = "23bade351f23"
 branch_labels = None
 depends_on = None
 
@@ -19,7 +19,9 @@ def upgrade():
 
 
 def downgrade():
-    op.execute("""
+    op.execute(
+        """
         ALTER TABLE dataset_processing_status
         ADD COLUMN conversion_loom_status conversionstatus DEFAULT 'N/A'
-    """)
+    """
+    )
