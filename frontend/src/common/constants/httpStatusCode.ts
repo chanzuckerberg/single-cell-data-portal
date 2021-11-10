2a80dd55feb27faff1ed8671a146dd976f4266cd
Seve Badajoz <sbadajoz@chanzuckerberg.com>
2021-11-10 11:34:10 -0800

    ALL CAPS

--- frontend/src/common/constants/httpStatusCode.ts
+++ frontend/src/common/constants/httpStatusCode.ts
@@ -3,7 +3,7 @@
  * Hypertext Transfer Protocol (HTTP) response status codes.
  * @see {@link https://en.wikipedia.org/wiki/List_of_HTTP_status_codes}
  */
-enum HttpStatusCode {
+enum HTTP_STATUS_CODE {
   /**
    * The server has received the request headers and the client should proceed to send the request body
    * (in the case of a request for which a body needs to be sent; for example, a POST request).
