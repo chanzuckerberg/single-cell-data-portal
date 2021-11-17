cc1548ddb650b39a95eae689b50cdc2e4974705e
Emanuele Bezzi <ebezzi@chanzuckerberg.com>
2021-11-17 13:27:16 -0500

    Add infra

--- .happy/terraform/modules/sfn/main.tf
+++ .happy/terraform/modules/sfn/main.tf
@@ -4,14 +4,15 @@ resource "aws_sfn_state_machine" "state_machine" {
 
   definition = <<EOF
 {
-    "StartAt": "Manage Batch task",
+    "StartAt": "DownloadValidate",
     "States": {
-      "Manage Batch task": {
+      "DownloadValidate": {
         "Type": "Task",
         "Resource": "arn:aws:states:::batch:submitJob.sync",
+        "Next": "CxgSeuratParallel",
         "Parameters": {
           "JobDefinition": "${var.job_definition_arn}",
-          "JobName": "processing",
+          "JobName": "download-validate",
           "JobQueue": "${var.job_queue_arn}",
           "ContainerOverrides": {
             "Environment": [
@@ -22,11 +23,15 @@ resource "aws_sfn_state_machine" "state_machine" {
               {
                 "Name": "DATASET_ID",
                 "Value.$": "$.dataset_uuid"
+              },
+              {
+                "Name": "STEP_NAME",
+                "Value": "download-validate"
               }
             ]
           }
         },
-        "End": true,
+        "ResultPath": null,
         "TimeoutSeconds": 36000,
         "Retry": [
           {
@@ -48,6 +53,99 @@ resource "aws_sfn_state_machine" "state_machine" {
           }
         ]
       },
+      "CxgSeuratParallel": {
+        "Type": "Parallel",
+        "End": true,
+        "ResultPath": null,
+        "Branches": [
+          {
+            "StartAt": "Cxg",
+            "States": {
+              "Cxg": {
+                "Type": "Task",
+                "End": true,
+                "Resource": "arn:aws:states:::batch:submitJob.sync",
+                "Parameters": {
+                  "JobDefinition": "${var.job_definition_arn}",
+                  "JobName": "cxg",
+                  "JobQueue": "${var.job_queue_arn}",
+                  "ContainerOverrides": {
+                    "Environment": [
+                      {
+                        "Name": "DATASET_ID",
+                        "Value.$": "$.dataset_uuid"
+                      },
+                      {
+                        "Name": "STEP_NAME",
+                        "Value": "cxg"
+                      }
+                    ]
+                  }
+                },
+                "ResultPath": null,
+                "TimeoutSeconds": 36000,
+                "Retry": [
+                  {
+                    "ErrorEquals": [
+                      "States.TaskFailed"
+                    ],
+                    "IntervalSeconds": 1,
+                    "BackoffRate": 2,
+                    "MaxAttempts": 2
+                  }
+                ]
+              }
+            }
+          },
+          {
+            "StartAt": "Seurat",
+            "States": {
+              "Seurat": {
+                "Type": "Task",
+                "End": true,
+                "Resource": "arn:aws:states:::batch:submitJob.sync",
+                "Parameters": {
+                  "JobDefinition": "${var.job_definition_arn}",
+                  "JobName": "cxg",
+                  "JobQueue": "${var.job_queue_arn}",
+                  "ContainerOverrides": {
+                    "Environment": [
+                      {
+                        "Name": "DATASET_ID",
+                        "Value.$": "$.dataset_uuid"
+                      },
+                      {
+                        "Name": "STEP_NAME",
+                        "Value": "seurat"
+                      }
+                    ]
+                  }
+                },
+                "TimeoutSeconds": 36000,
+                "Retry": [
+                  {
+                    "ErrorEquals": [
+                      "States.TaskFailed"
+                    ],
+                    "IntervalSeconds": 1,
+                    "BackoffRate": 2,
+                    "MaxAttempts": 2
+                  }
+                ]
+              }
+            }
+          }
+        ],
+        "Catch": [
+            {
+            "ErrorEquals": [
+                "States.ALL"
+            ],
+            "Next": "HandleErrors",
+            "ResultPath": "$.error"
+            }
+        ]
+      },
       "HandleErrors": {
         "Type": "Task",
         "InputPath": "$",
@@ -55,6 +153,67 @@ resource "aws_sfn_state_machine" "state_machine" {
         "End": true
       }
     }
+}
+EOF
+}
+
+resource "aws_sfn_state_machine" "state_machine_seurat" {
+  name     = "dp-${var.deployment_stage}-${var.custom_stack_name}-seurat-sfn"
+  role_arn = var.role_arn
+
+  definition = <<EOF
+{
+  "StartAt": "Seurat",
+  "States": {
+    "Seurat": {
+      "Type": "Task",
+      "End": true,
+      "Resource": "arn:aws:states:::batch:submitJob.sync",
+      "Parameters": {
+        "JobDefinition": "${var.job_definition_arn}",
+        "JobName": "cxg",
+        "JobQueue": "${var.job_queue_arn}",
+        "ContainerOverrides": {
+          "Environment": [
+            {
+              "Name": "DATASET_ID",
+              "Value.$": "$.dataset_uuid"
+            },
+            {
+              "Name": "STEP_NAME",
+              "Value": "seurat"
+            }
+          ]
+        }
+      },
+      "TimeoutSeconds": 36000,
+      "Retry": [
+        {
+          "ErrorEquals": [
+            "States.TaskFailed"
+          ],
+          "IntervalSeconds": 1,
+          "BackoffRate": 2,
+          "MaxAttempts": 2
+        }
+      ],
+      "Catch": [
+        {
+          "ErrorEquals": [
+            "States.ALL"
+          ],
+          "Next": "HandleErrors",
+          "ResultPath": "$.error"
+        }
+      ]
+    },
+    "HandleErrors": {
+      "Type": "Task",
+      "InputPath": "$",
+      "Resource": "${var.lambda_error_handler}",
+      "End": true
+    }
+  }
 }
 EOF
 }
