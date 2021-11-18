732105259c87e93b31bf0d1cde2207685c1ae1c2
Seve Badajoz <sbadajoz@chanzuckerberg.com>
2021-11-18 11:09:19 -0800

    tsc

--- frontend/src/common/utils/typeGuards.ts
+++ frontend/src/common/utils/typeGuards.ts
@@ -2,7 +2,7 @@ import { Collection } from "../entities";
 import { TombstonedCollection } from "../queries/collections";
 
 export function isTombstonedCollection(
-  collection: TombstonedCollection | Collection | undefined
+  collection: TombstonedCollection | Collection | undefined | null
 ): collection is TombstonedCollection {
   return (
     !!collection && "tombstone" in collection && collection.tombstone === true
