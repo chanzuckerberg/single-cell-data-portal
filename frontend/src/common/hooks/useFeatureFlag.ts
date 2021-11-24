364cee36d010865b46ed037617e8de49ee30a8bd
Fran McDade <franmcdade@Frans-MBP-2.hub>
2021-11-25 09:17:01 +1000

    feat: left align site (#1656)

---
+++ frontend/src/common/hooks/useFeatureFlag.ts
@@ -0,0 +1,35 @@
+// Core dependencies
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+// App dependencies
+import { ROUTES } from "src/common/constants/routes";
+import { get } from "src/common/featureFlags";
+import { FEATURES } from "src/common/featureFlags/features";
+import { BOOLEAN } from "src/common/localStorage/set";
+
+/**
+ * Determine if feature is available to user. Redirect to route, if specified.
+ * @param featureFlag - Name of feature to check if available to user.
+ * @param redirectRoute - Route to redirect user to - if any - if feature is not available to them.
+ * @returns True if feature is available to user.
+ */
+export function useFeatureFlag(
+  featureFlag: FEATURES,
+  redirectRoute?: ROUTES
+): boolean {
+  const router = useRouter();
+
+  /* Flag indicating if feature is available to user. */
+  const [isFilterEnabled, setIsFilterEnabled] = useState<boolean>(false);
+
+  /* Update state of enabled flag and redirect user if feature is not available to them. */
+  useEffect(() => {
+    const enabled = get(featureFlag) === BOOLEAN.TRUE;
+    setIsFilterEnabled(enabled);
+    if (!enabled && redirectRoute) {
+      router.push(redirectRoute);
+    }
+  }, [featureFlag, redirectRoute, router]);
+
+  return isFilterEnabled;
+}
