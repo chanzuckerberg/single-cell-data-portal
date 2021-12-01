33cea669c052c52295d208ef7daf7a5534009d83
Fran McDade <franmcdade@Frans-MBP-2.hub>
2021-12-01 14:53:52 +1000

    Sidebar layout added.

--- frontend/src/pages/collections.tsx
+++ frontend/src/pages/collections.tsx
@@ -1,5 +1,8 @@
+import SidebarLayout from "src/components/Layout/components/sidebarLayout";
 import Collections from "src/views/Collections";
 
 const Page = (): JSX.Element => <Collections />;
+
+Page.Layout = SidebarLayout;
 
 export default Page;
