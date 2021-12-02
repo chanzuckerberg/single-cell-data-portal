24f9e92a45796136d5dd38bd4ff102811d5ad473
MillenniumFalconMechanic <mim@clevercanary.com>
2021-12-01 19:59:01 -0800

    Added stubbed collections index.

--- frontend/src/common/API.ts
+++ frontend/src/common/API.ts
@@ -2,7 +2,9 @@ export enum API {
   DATASET = "/dp/v1/datasets/{dataset_uuid}",
   DATASET_ASSET_DOWNLOAD_LINK = "/dp/v1/datasets/{dataset_uuid}/asset/{asset_uuid}",
   DATASET_STATUS = "/dp/v1/datasets/{dataset_uuid}/status",
+  DATASETS_INDEX = "/dp/v1/datasets/index", // Filter-specific endpoint
   COLLECTIONS = "/dp/v1/collections",
+  COLLECTIONS_INDEX = "/dp/v1/collections/index", // Filter-specific endpoint
   COLLECTION = "/dp/v1/collections/{id}",
   COLLECTION_UPLOAD_LINKS = "/dp/v1/collections/{id}/upload-links",
   COLLECTION_PUBLISH = "/dp/v1/collections/{id}/publish",
