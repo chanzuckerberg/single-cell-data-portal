5ad8969fbc3988f4aa32a7d91648a8c1d6a961ae
Fran McDade <franmcdade@Frans-MBP-2.hub>
2021-12-05 08:44:52 +1000

    feat: list basic metadata categories in left sidebar on collections view (#1614)

---
+++ frontend/src/components/common/Filter/common/style.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Filter = styled.div`
+  display: grid;
+  gap: 8px;
+  margin-bottom: 12px;
+`;
---
+++ frontend/src/components/common/Filter/components/FilterMenu/index.tsx
@@ -0,0 +1,92 @@
+import { InputGroup, Menu, MenuItem } from "@blueprintjs/core";
+import { IconNames } from "@blueprintjs/icons";
+import { Fragment, useEffect, useRef, useState } from "react";
+import {
+  CategoryValueView,
+  OnFilterFn,
+} from "src/common/hooks/useCategoryFilter";
+import {
+  CATEGORY_KEY,
+  FilterCategoryValuesFn,
+  OnUpdateSearchValueFn,
+} from "src/components/common/Filter/common/entities";
+import {
+  InputGroupWrapper,
+  MAX_DISPLAYABLE_MENU_ITEMS,
+  MenuItemsWrapper,
+  MenuItemWrapper,
+  MenuWrapper,
+  NoMatches,
+} from "./style";
+
+interface Props {
+  categoryKey: CATEGORY_KEY;
+  filterCategoryValues: FilterCategoryValuesFn;
+  multiselect: boolean;
+  onFilter: OnFilterFn;
+  onUpdateSearchValue: OnUpdateSearchValueFn;
+  searchable: boolean;
+  values: CategoryValueView[];
+}
+
+export default function FilterMenu({
+  categoryKey,
+  filterCategoryValues,
+  multiselect,
+  onFilter,
+  onUpdateSearchValue,
+  searchable,
+  values,
+}: Props): JSX.Element {
+  const menuRef = useRef<HTMLSpanElement>(null);
+  const [menuWidth, setMenuWidth] = useState(0);
+  const [searchValue, setSearchValue] = useState<string>("");
+  const menuItems = searchable
+    ? filterCategoryValues(values, searchValue)
+    : values;
+  const emptyItems = menuItems.length === 0;
+  const scrollable = menuItems.length > MAX_DISPLAYABLE_MENU_ITEMS;
+  const MenuItemsScroller =
+    searchable && scrollable ? MenuItemsWrapper : Fragment;
+
+  useEffect(() => {
+    if (menuRef.current) {
+      setMenuWidth(menuRef.current.children[0]?.clientWidth);
+    }
+  }, []);
+
+  return (
+    <MenuWrapper ref={menuRef} width={menuWidth}>
+      <Menu>
+        {searchable ? (
+          <InputGroupWrapper>
+            <InputGroup
+              leftIcon={IconNames.SEARCH}
+              onChange={(changeEvent) =>
+                onUpdateSearchValue(changeEvent, setSearchValue)
+              }
+              placeholder="Search"
+            />
+          </InputGroupWrapper>
+        ) : null}
+        {emptyItems ? (
+          <NoMatches shouldDismissPopover={false} text={"No matches found"} />
+        ) : (
+          <MenuItemsScroller>
+            {menuItems.map(({ key, count, selected }) => (
+              <MenuItemWrapper key={key} isSelected={selected}>
+                <MenuItem
+                  icon={selected ? IconNames.TICK : IconNames.BLANK}
+                  labelElement={count}
+                  onClick={() => onFilter(categoryKey, key)}
+                  shouldDismissPopover={!multiselect}
+                  text={key}
+                />
+              </MenuItemWrapper>
+            ))}
+          </MenuItemsScroller>
+        )}
+      </Menu>
+    </MenuWrapper>
+  );
+}
