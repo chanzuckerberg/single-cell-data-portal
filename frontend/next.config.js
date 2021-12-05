2171fd85c50553ae312fe1e323e22db7bb90f904
MillenniumFalconMechanic <mim@clevercanary.com>
2021-12-05 13:08:00 -0800

    Added / redirect to /datasets.

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -58,4 +58,16 @@ module.exports = {
       },
     ];
   },
+
+  // Redirect / to /datasets.
+  // TODO(cc) - disable on go-live if filter feature flag is still enabled (#1718)
+  async redirects() {
+    return [
+      {
+        destination: "/datasets",
+        permanent: true,
+        source: "/",
+      },
+    ];
+  },
 };
