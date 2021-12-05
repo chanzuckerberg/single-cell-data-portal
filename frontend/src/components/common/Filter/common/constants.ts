112d002783902ff75fbf869698cf4296f9acb4ed
MillenniumFalconMechanic <mim@clevercanary.com>
2021-12-04 17:54:18 -0800

    wip

---
+++ frontend/src/components/common/Filter/common/constants.ts
@@ -0,0 +1,9 @@
+// App dependencies
+import { CATEGORY_KEY } from "src/components/common/Filter/common/entities";
+
+/* Configuration for displaying categories and their category values. */
+export const CATEGORY_VIEW_CONFIG = {
+  [CATEGORY_KEY.ASSAY]: {
+    cate,
+  },
+};
