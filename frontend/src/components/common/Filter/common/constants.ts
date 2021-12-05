6051319ae8145e367a0a4dfb68374d20ea8d3587
MillenniumFalconMechanic <mim@clevercanary.com>
2021-12-04 18:31:16 -0800

    Added sort of category values.

--- frontend/src/components/common/Filter/common/constants.ts
+++
@@ -1,9 +0,0 @@
-// App dependencies
-import { CATEGORY_KEY } from "src/components/common/Filter/common/entities";
-
-/* Configuration for displaying categories and their category values. */
-export const CATEGORY_VIEW_CONFIG = {
-  [CATEGORY_KEY.ASSAY]: {
-    cate,
-  },
-};
