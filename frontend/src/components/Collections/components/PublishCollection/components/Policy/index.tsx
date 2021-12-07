36554c1907c0ce0533618de13c866b0b8f562a13
Seve Badajoz <sbadajoz@chanzuckerberg.com>
2021-12-07 13:02:50 -0800

    feat: update data submission policy to 2.0

--- frontend/src/components/Collections/components/PublishCollection/components/Policy/index.tsx
+++ frontend/src/components/Collections/components/PublishCollection/components/Policy/index.tsx
@@ -2,12 +2,9 @@ import { Button, Collapse, Intent } from "@blueprintjs/core";
 import { IconNames } from "@blueprintjs/icons";
 import { FC, useState } from "react";
 import { GRAY } from "src/components/common/theme";
-import DirectIdentifiers from "./components/DirectPersonalIdentifiers";
 import {
   BulletWrapper,
-  ButtonWrapper,
   ContentWrapper,
-  StyledButton,
   StyledIcon,
   Text,
   Wrapper,
@@ -16,28 +13,11 @@ import {
 export const POLICY_BULLETS = {
   items: [
     "I give CZI permission to display, distribute, and create derivative works (e.g. visualizations) of this data for purposes of offering the cellxgene portal, and I have the authority to give this permission.",
-    <div key="1">
-      It is my responsibility to ensure that this data is not identifiable. In
-      particular, I commit that I will remove any{" "}
-      <DirectIdentifiers>
-        <ButtonWrapper>
-          <StyledButton>direct personal identifiers</StyledButton>
-        </ButtonWrapper>
-      </DirectIdentifiers>{" "}
-      in the metadata portions of the data, and that CZI may further contact me
-      if it believes more work is needed to de-identify it.
-    </div>,
-    <div key="2">
-      If I choose to publish this data <b>publicly</b> on the cellxgene portal,
-      I understand that (1) anyone will be able to access it subject to a CC-BY
-      license, meaning they can download, share, and use the data without
-      restriction beyond providing attribution to me and (2) the Collection
-      details I’ve provided above (collection name, description, and my contact
-      information) will be made public on the cellxgene portal too.
-    </div>,
-    "I understand that I have the ability to delete my data from the cellxgene portal if I later change my mind. This will not undo any prior downloads or shares of my data.",
+    "It is my responsibility to ensure that this data is not identifiable. In particular, I commit that I will remove any direct personal identifiers in the metadata portions of the data, and that CZI may further contact me if it believes more work is needed to de-identify it.",
+    "If I choose to publish this data publicly on the cellxgene portal, I understand that (1) anyone will be able to access it subject to a CC-BY license, meaning they can download, share, and use the data without restriction beyond providing attribution to the original data contributor(s) and (2) the Collection details (including collection name, description, my name, and the contact information for the datasets in this Collection) will be made public on the cellxgene portal too.",
+    "I understand that I have the ability to delete the data that I have published from the cellxgene portal if I later choose to. This however will not undo any prior downloads or shares of such data.",
   ],
-  version: "1.0",
+  version: "2.0",
 };
 
 const Policy: FC = () => {
