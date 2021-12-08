f97a08f299558cc2923223264c68ebdc40674913
Mim Hastie <mim@clevercanary.com>
2021-12-08 13:15:38 -0800

    Update frontend/src/components/common/Modal/index.tsx
    Co-authored-by: Timmy Huang <tihuan@users.noreply.github.com>

--- frontend/src/components/common/Modal/index.tsx
+++ frontend/src/components/common/Modal/index.tsx
@@ -15,6 +15,9 @@ const Modal: FC<Props> = ({ className, onClose, title, isOpen, children }) => {
       isOpen={isOpen}
       onClose={onClose}
       className={className}
+      // Remove this prop after upgrading Blueprint Tooltip library
+      // Current bug is after closing the modal, tooltip will reappear, as shown in the link below:
+      // https://codesandbox.io/s/blueprint-tooltip-bug-forked-jw9z2?file=/src/index.tsx
       shouldReturnFocusOnClose={false}
     >
       {children}
