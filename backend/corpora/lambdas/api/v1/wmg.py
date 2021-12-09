25cc29e8a6e6dd95b80d62c0072cd748911d7e8f
Andrew Tolopko <atolopko@chanzuckerberg.com>
2021-12-09 09:10:46 -0500

    lint

--- backend/corpora/lambdas/api/v1/wmg.py
+++ backend/corpora/lambdas/api/v1/wmg.py
@@ -7,11 +7,7 @@ WMG_DATA_S3_OBJ_PREFIX = "lung-tissue-10x-human-20211208/"
 
 def __request_wmg_s3_object(key: str) -> str:
     s3 = boto3.client("s3")
-    return (
-        s3.get_object(Bucket=WMG_DATA_BUCKET, Key=f"${WMG_DATA_S3_OBJ_PREFIX}/{key}")["Body"]
-        .read()
-        .decode("utf-8")
-    )
+    return s3.get_object(Bucket=WMG_DATA_BUCKET, Key=f"${WMG_DATA_S3_OBJ_PREFIX}/{key}")["Body"].read().decode("utf-8")
 
 
 def get_genes():
