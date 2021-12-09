3c70562e648c5238b26a4f9992274155c96ee8bc
Trent Smith <trent.smith@chanzuckerberg.com>
2021-12-08 16:46:23 -0800

    add additional check for links

--- backend/corpora/lambdas/api/v1/collection.py
+++ backend/corpora/lambdas/api/v1/collection.py
@@ -114,7 +114,7 @@ def create_collection(body: object, user: str):
         name=body["name"],
         description=body["description"],
         owner=user,
-        links=body["links"],
+        links=body.get("links", []),
         contact_name=body["contact_name"],
         contact_email=body["contact_email"],
         curator_name=body.get("curator_name", ""),
