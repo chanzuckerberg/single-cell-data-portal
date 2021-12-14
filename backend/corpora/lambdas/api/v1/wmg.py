1b64580959a835d30350e7865848db0f34a5c645
Timmy Huang <thuang@chanzuckerberg.com>
2021-12-14 12:59:46 -0800

    feat: Wheres My Gene Prototype

---
+++ backend/corpora/lambdas/api/v1/wmg.py
@@ -0,0 +1,22 @@
+import boto3
+from flask import make_response
+
+WMG_DATA_BUCKET = "wmg-prototype-data-dev-public"
+WMG_DATA_S3_OBJ_PREFIX = "lung-tissue-10x-human-20220112"
+
+
+def __request_wmg_s3_object(key: str) -> str:
+    s3 = boto3.client("s3")
+    return s3.get_object(Bucket=WMG_DATA_BUCKET, Key=f"{WMG_DATA_S3_OBJ_PREFIX}/{key}")["Body"].read().decode("utf-8")
+
+
+def get_genes():
+    return make_response(__request_wmg_s3_object("lung_tissue_genes.json"), 200)
+
+
+def get_cell_types():
+    return make_response(__request_wmg_s3_object("lung_tissue_cell_types.json"), 200)
+
+
+def get_gene_expression(gene_name):
+    return make_response(__request_wmg_s3_object(f"genes/{gene_name}.json"), 200)
---
+++ frontend/src/views/WheresMyGene/style.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  padding: 20px;
+`;
+
+export const Gap = styled.div`
+  height: 200px;
+`;
