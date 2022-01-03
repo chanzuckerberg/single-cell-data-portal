4e7dbe250d667dce4ec74313338325c532230cc3
Trent Smith <trent.smith@chanzuckerberg.com>
2022-01-03 14:39:58 -0800

    Revert "feat: block publish of duplicates (#1652)"
    This reverts commit ebe0662671935cbb787c4f89b6a36e975340a34a.

--- backend/corpora/lambdas/api/v1/collection_uuid/publish.py
+++ backend/corpora/lambdas/api/v1/collection_uuid/publish.py
@@ -1,36 +1,12 @@
 from flask import make_response, g
 
 from .....common.corpora_orm import CollectionVisibility
-from .....common.entities import Collection, DatasetAsset
+from .....common.entities import Collection
 from .....common.utils.exceptions import ConflictException
 
 from .....api_server.db import dbconnect
-from .....common.utils.exceptions import ForbiddenHTTPException, ServerErrorHTTPException
+from .....common.utils.exceptions import ForbiddenHTTPException
 from backend.corpora.lambdas.api.v1.collection import _owner_or_allowed
-
-
-def check_for_duplicate_datasets(collection: Collection) -> bool:
-    """
-    Check if duplicate datasets are found. Return true on the first duplicate match.
-    :param collection:
-    :return: True if duplicates detected, otherwise return false.
-    """
-    etags = set()
-    for dataset in collection.datasets:
-        if not dataset.tombstone:
-            for artifact in dataset.artifacts:
-                _artifact = DatasetAsset(artifact)
-                metadata = _artifact.get_s3_metadata()
-                if not metadata:
-                    raise ServerErrorHTTPException(
-                        "Failed to check datasets for duplications. Unable to find associated artifacts."
-                    )
-                etag = metadata["ETag"]
-                if etag not in etags:
-                    etags.add(etag)
-                else:
-                    return True
-    return False
 
 
 @dbconnect
@@ -46,8 +22,6 @@ def post(collection_uuid: str, body: object, user: str):
         raise ForbiddenHTTPException()
     if all([dataset.tombstone for dataset in collection.datasets]):
         raise ConflictException(detail="The collection must have a least one dataset.")
-    if check_for_duplicate_datasets(collection):
-        raise ConflictException(detail="The collection cannot have duplicate datasets.")
 
     data_submission_policy_version = body["data_submission_policy_version"]
     collection.publish(data_submission_policy_version=data_submission_policy_version)
