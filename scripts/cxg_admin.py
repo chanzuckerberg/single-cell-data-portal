ddb2a8195f5d70a15d78ff49a537da8db7c42c0a
Trent Smith <trent.smith@chanzuckerberg.com>
2022-01-04 13:32:36 -0800

    support name field

--- scripts/cxg_admin.py
+++ scripts/cxg_admin.py
@@ -581,15 +581,15 @@ def update_curator_names(ctx, access_token):
         logger.info("Gathering collections with no curator.")
         with db_session_manager() as session:
             from sqlalchemy import null
-            filter = (Collection.table.curator_name == null()) | (Collection.table.curator_name == '')
-            owners = [result.owner for result in session.query(Collection.table.owner).filter(filter).distinct().all()]
-        return owners
+            _filter = (Collection.table.curator_name == null()) | (Collection.table.curator_name == '')
+            _owners = [result.owner for result in session.query(Collection.table.owner).filter(_filter).distinct().all()]
+        return _owners
 
     def get_owner_info_from_auth0(owner, access_token):
         logger.info(f"Retrieving curator info for owner:{owner} from Auth0.")
         url_manager = furl(
             f"{auth0_api}/api/v2/users/{owner}",
-            query_params=dict(fields=",".join(["given_name", "family_name"]), include_fields="true"),
+            query_params=dict(fields=",".join(["given_name", "family_name", "name"]), include_fields="true"),
         )
         response = requests.get(url_manager.url, headers={"Authorization": f"Bearer {access_token}"})
         response.raise_for_status()
@@ -597,7 +597,8 @@ def update_curator_names(ctx, access_token):
             wait = time.time() - response.headers["X-RateLimit-Reset"]
             time.sleep(wait)
         body = response.json()
-        return f"{body.get('given_name', '')} {body.get('family_name', '')}".strip()
+        name = f"{body.get('given_name', '')} {body.get('family_name', '')}".strip() or body.get('name', '')
+        return name
 
     def update_database_curator_name(owner_id, owner_name):
         logger.info(f"Updating collections owned by {owner_id} with curator_name:{owner_name}.")
@@ -608,7 +609,6 @@ def update_curator_names(ctx, access_token):
                 if collection.visibility == CollectionVisibility.PUBLIC:
                     collection.data_submission_policy_version = "2.0"
 
-    # access_token = get_auth0_access_token()
     unique_owners = get_collections_without_curator()
     owners = dict()
     bad_owners = []
