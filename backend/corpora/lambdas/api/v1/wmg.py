099f2da49fffe1eec59065647ac0626a126c1bf7
Andrew Tolopko <atolopko@chanzuckerberg.com>
2022-01-12 13:41:27 -0500

    update backend data to latest
    the data reflects latest cell_type ordering algo

--- backend/corpora/lambdas/api/v1/wmg.py
+++ backend/corpora/lambdas/api/v1/wmg.py
@@ -2,7 +2,7 @@ import boto3
 from flask import make_response
 
 WMG_DATA_BUCKET = "wmg-prototype-data-dev-public"
-WMG_DATA_S3_OBJ_PREFIX = "lung-tissue-10x-human-20211208"
+WMG_DATA_S3_OBJ_PREFIX = "lung-tissue-10x-human-lung-tissue-10x-human-20220112"
 
 
 def __request_wmg_s3_object(key: str) -> str:
