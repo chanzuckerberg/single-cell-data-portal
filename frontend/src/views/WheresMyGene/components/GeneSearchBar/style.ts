e15f94e405c0413c8fb6abba48dbd1a540e8e590
Timmy Huang <tihuan@users.noreply.github.com>
2022-01-13 13:05:53 -0800

    feat: Wheres My Gene Prototype (#1834)
    * feat: Wheres My Gene Prototype
    
    * docs: note wmg prototype-level backend code
    
    Co-authored-by: Andrew Tolopko <atolopko@chanzuckerberg.com>

---
+++ frontend/src/views/WheresMyGene/components/GeneSearchBar/style.ts
@@ -0,0 +1,13 @@
+import { Classes } from "@blueprintjs/core";
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 80vw;
+  margin-bottom: 20px;
+`;
+
+export const MultiSelectWrapper = styled.div`
+  .${Classes.INPUT} {
+    height: auto;
+  }
+`;
