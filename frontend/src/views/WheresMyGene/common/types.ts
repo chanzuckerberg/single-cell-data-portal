e7b4d0813374d1aef0ece70268ae098145fdb2c2
Timmy Huang <tihuan@users.noreply.github.com>
2022-01-26 16:00:26 -0800

    refactor: wheres my gene (#1876)
    * refactor: wheres my gene
    
    * update types

--- frontend/src/views/WheresMyGene/common/types.ts
+++ frontend/src/views/WheresMyGene/common/types.ts
@@ -1,10 +1,13 @@
 import { CSSProperties } from "react";
 
-export interface CellTypeAndGenes {
+/** This object holds a cell type and its gene expressions. */
+export interface CellTypeSummary {
+  /** cellType id */
   id: string;
+  /** cellType name */
   name: string;
-  expressions?: {
-    [geneName: string]: GeneExpression;
+  geneExpressions?: {
+    [geneName: string]: CellTypeGeneExpressionSummaryData;
   };
 }
 
@@ -13,21 +16,40 @@ export interface CellType {
   name: string;
 }
 
-export interface RawGeneExpression {
-  gene_name: string;
-  cell_types: GeneExpression[];
+/**
+ * This object holds a gene and all the cell types that express this gene.
+ */
+export interface GeneExpressionSummary {
+  /** gene name */
+  name: string;
+  cellTypeGeneExpressionSummaries: CellTypeGeneExpressionSummaryData[];
 }
 
-export interface GeneExpression {
-  // cellTypeId
+/** This is the original data shape in API response for `CellTypeGeneExpressionSummaryData` */
+export interface RawCellTypeGeneExpressionSummaryData {
+  /** cellTypeId */
   id: string;
+  /** percentage of the current subset of cells that express this gene */
   pc: number;
+  /** mean expression of the current subset of cells that express this gene */
   me: number;
+}
+
+/**
+ * This object describes the cell type id and its gene expression metadata given
+ * the current subset of cells.
+ */
+export interface CellTypeGeneExpressionSummaryData {
+  /** cellTypeId */
+  id: string;
+  /** percentage of the current subset of cells that express this gene */
+  percentage: number;
+  /** mean expression of the current subset of cells that express this gene */
+  meanExpression: number;
 }
 
 export interface Gene {
   id: string;
   name: string;
   style?: CSSProperties;
-  cell_types?: GeneExpression[];
 }
--- frontend/src/views/WheresMyGene/components/HeatMap/style.ts
+++ frontend/src/views/WheresMyGene/components/HeatMap/style.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-
+import { X_AXIS_CHART_HEIGHT, Y_AXIS_CHART_WIDTH } from "./utils";
 export const Container = styled.div`
   height: 75vh;
   width: 80vw;
@@ -21,3 +21,63 @@ export const Loader = styled.div`
   box-shadow: 0px 0px 0px rgba(16, 22, 26, 0.1),
     0px 4px 8px rgba(16, 22, 26, 0.2);
 `;
+
+export const ChartContainer = styled.div`
+  left: 0px;
+  top: 0px;
+  position: absolute;
+
+  ${({ width, height }: { width: number; height: number }) => {
+    return `
+      width: ${width}px;
+      height: ${height}px;
+    `;
+  }})}
+`;
+
+export const XAxisWrapper = styled.div`
+  background-color: white;
+  height: ${X_AXIS_CHART_HEIGHT};
+  position: sticky;
+  top: 0;
+  z-index: 2;
+
+  ${xAxisWidth}
+`;
+
+export const XAxisMask = styled.div`
+  background-color: white;
+  height: ${X_AXIS_CHART_HEIGHT};
+  width: ${Y_AXIS_CHART_WIDTH};
+  position: sticky;
+  left: 0;
+`;
+
+export const XAxisContainer = styled.div`
+  background-color: white;
+  height: ${X_AXIS_CHART_HEIGHT};
+  position: absolute;
+
+  ${xAxisWidth}
+`;
+
+export const YAxisContainer = styled.div`
+  background-color: white;
+  width: ${Y_AXIS_CHART_WIDTH};
+  position: sticky;
+  top: 0;
+  left: 0;
+  z-index: 1;
+
+  ${({ height }: { height: number }) => {
+    return `
+      height: ${height}px;
+    `;
+  }}
+`;
+
+function xAxisWidth({ width }: { width: number }) {
+  return `
+    width: ${width}px;
+  `;
+}
