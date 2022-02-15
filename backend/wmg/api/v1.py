4faf7c0d9f82437afa659d7ffb41043ffa4a29ec
Andrew Tolopko <atolopko@chanzuckerberg.com>
2022-02-15 18:52:37 -0500

    feat: stub wmg query endpoint (WIP)

--- backend/wmg/api/v1.py
+++ backend/wmg/api/v1.py
@@ -1,5 +1,6 @@
 from uuid import UUID, uuid4
 
+import connexion
 from flask import jsonify
 
 _logger = None
@@ -10,10 +11,19 @@ def latest_snapshot():
 
 
 def primary_filter_dimensions(snapshot_id: UUID):
-    organism_terms = [dict(id='oid1', label='olbl1'),
-                      dict(id='oid2', label='olbl2')]
-    tissue_type_terms = [dict(id='ttid1', label='ttlbl1'),
-                         dict(id='ttid2', label='ttlbl2')]
+    organism_terms = [dict(oid1='olbl1'),
+                      dict(oid2='olbl2')]
+    tissue_type_terms = [dict(ttid1='ttlbl1'),
+                         dict(ttid2='ttlbl2')]
     result = dict(organism_terms=organism_terms,
                   tissue_type_terms=tissue_type_terms)
     return jsonify(result)
+
+
+def query(snapshot_id: UUID):
+    request = connexion.request.json
+    print(snapshot_id)
+    print(request['filter'])
+    print(request['response_option'])
+    return jsonify(dict(expression_summary={}, term_id_labels=dict(genes=[], cell_types=[])))
+
--- tests/unit/backend/wmg/api/v1.py
+++ tests/unit/backend/wmg/api/v1.py
@@ -18,7 +18,7 @@ class WmgApiTests(unittest.TestCase):
 
         self.assertEqual(200, response.status_code)
 
-    def test__latest_snapshot__returns_correct_body(self):
+    def test__latest_snapshot__returns_valid_response_body(self):
         response = self.app.get("/wmg/v1/snapshots/latest")
 
         snapshot_id = json.loads(response.data)['snapshot_id']
@@ -34,13 +34,55 @@ class WmgApiTests(unittest.TestCase):
 
         self.assertEqual(200, response.status_code)
 
-    def test__primary_filter_dimensions__returns_correct_body(self):
+    def test__primary_filter_dimensions__returns_valid_response_body(self):
         snapshot_id = uuid.uuid4()
 
         response = self.app.get(f"/wmg/v1/snapshots/{snapshot_id}/primary_filter_dimensions")
 
-        expected = dict(organism_terms=[dict(id='oid1', label='olbl1'),
-                                        dict(id='oid2', label='olbl2')],
-                        tissue_type_terms=[dict(id='ttid1', label='ttlbl1'),
-                                           dict(id='ttid2', label='ttlbl2')])
+        expected = dict(organism_terms=[dict(oid1='olbl1'),
+                                        dict(oid2='olbl2')],
+                        tissue_type_terms=[dict(ttid1='ttlbl1'),
+                                           dict(ttid2='ttlbl2')])
+
         self.assertEqual(expected, json.loads(response.data))
+
+    def test__query__minimal_valid_request_returns_200(self):
+        snapshot_id = uuid.uuid4()
+
+        request = dict(
+                filter=dict(
+                        gene_term_ids=['gene1'],
+                        organism_term_id='organism1',
+                        tissue_type_term_ids=['tissuetype1']),
+                response_option='include_filter_dims_include_dataset_links')
+
+        response = self.app.post(f"/wmg/v1/snapshots/{snapshot_id}/query", json=request)
+
+        self.assertEqual(200, response.status_code)
+
+    def test__query__minimal_valid_request_returns_valid_response_body(self):
+        snapshot_id = uuid.uuid4()
+
+        request = dict(
+                filter=dict(
+                        gene_term_ids=['gene1'],
+                        organism_term_id='organism1',
+                        tissue_type_term_ids=['tissuetype1']),
+                response_option='include_filter_dims_include_dataset_links')
+
+        response = self.app.post(f"/wmg/v1/snapshots/{snapshot_id}/query", json=request)
+
+        self.assertEqual(dict(expression_summary={},
+                              term_id_labels=dict(
+                                      genes=[],
+                                      cell_types=[])),
+                         json.loads(response.data))
+
+    def test__query__invalid_request_returns_400(self):
+        snapshot_id = uuid.uuid4()
+
+        response = self.app.post(f"/wmg/v1/snapshots/{snapshot_id}/query", json={})
+
+        self.assertEqual(400, response.status_code)
+        self.assertEqual("'filter' is a required property",
+                         json.loads(response.data)['detail'])
