0b3c1147e9d8db9d682e1cb5def7e32db57532df
Andrew Tolopko <atolopko@chanzuckerberg.com>
2022-02-15 16:22:32 -0500

    feat: stub wmg primary_filter_dimensions endpoint

--- backend/wmg/api/v1.py
+++ backend/wmg/api/v1.py
@@ -2,6 +2,18 @@ from uuid import UUID, uuid4
 
 from flask import jsonify
 
+_logger = None
 
-def latest_snapshot() -> UUID:
+
+def latest_snapshot():
     return jsonify(snapshot_id=uuid4().hex)
+
+
+def primary_filter_dimensions(snapshot_id: UUID):
+    organism_terms = [dict(id='oid1', label='olbl1'),
+                      dict(id='oid2', label='olbl2')]
+    tissue_type_terms = [dict(id='ttid1', label='ttlbl1'),
+                         dict(id='ttid2', label='ttlbl2')]
+    result = dict(organism_terms=organism_terms,
+                  tissue_type_terms=tissue_type_terms)
+    return jsonify(result)
--- tests/unit/backend/wmg/api/v1.py
+++ tests/unit/backend/wmg/api/v1.py
@@ -15,16 +15,32 @@ class WmgApiTests(unittest.TestCase):
 
     def test__latest_snapshot__returns_200(self):
         response = self.app.get("/wmg/v1/snapshots/latest")
+
         self.assertEqual(200, response.status_code)
 
     def test__latest_snapshot__returns_correct_body(self):
         response = self.app.get("/wmg/v1/snapshots/latest")
+
         snapshot_id = json.loads(response.data)['snapshot_id']
         try:
             uuid.UUID(snapshot_id)
         except ValueError:
             self.fail()
 
+    def test__primary_filter_dimensions__returns_200(self):
+        snapshot_id = uuid.uuid4()
 
-if __name__ == '__main__':
-    unittest.main()
+        response = self.app.get(f"/wmg/v1/snapshots/{snapshot_id}/primary_filter_dimensions")
+
+        self.assertEqual(200, response.status_code)
+
+    def test__primary_filter_dimensions__returns_correct_body(self):
+        snapshot_id = uuid.uuid4()
+
+        response = self.app.get(f"/wmg/v1/snapshots/{snapshot_id}/primary_filter_dimensions")
+
+        expected = dict(organism_terms=[dict(id='oid1', label='olbl1'),
+                                        dict(id='oid2', label='olbl2')],
+                        tissue_type_terms=[dict(id='ttid1', label='ttlbl1'),
+                                           dict(id='ttid2', label='ttlbl2')])
+        self.assertEqual(expected, json.loads(response.data))
