11c224b7ca11049dfd088b064d752fcdaaa2a9cc
Andrew Tolopko <atolopko@chanzuckerberg.com>
2022-02-16 10:10:32 -0500

    feat: stub wmg query endpoint

--- backend/wmg/api/v1.py
+++ backend/wmg/api/v1.py
@@ -1,3 +1,4 @@
+from typing import List
 from uuid import UUID, uuid4
 
 import connexion
@@ -11,19 +12,48 @@ def latest_snapshot():
 
 
 def primary_filter_dimensions(snapshot_id: UUID):
-    organism_terms = [dict(oid1='olbl1'),
-                      dict(oid2='olbl2')]
-    tissue_type_terms = [dict(ttid1='ttlbl1'),
-                         dict(ttid2='ttlbl2')]
-    result = dict(organism_terms=organism_terms,
-                  tissue_type_terms=tissue_type_terms)
+    organism_terms = [dict(oid1="olbl1"), dict(oid2="olbl2")]
+    tissue_type_terms = [dict(ttid1="ttlbl1"), dict(ttid2="ttlbl2")]
+    result = dict(organism_terms=organism_terms, tissue_type_terms=tissue_type_terms)
     return jsonify(result)
 
 
 def query(snapshot_id: UUID):
     request = connexion.request.json
-    print(snapshot_id)
-    print(request['filter'])
-    print(request['response_option'])
-    return jsonify(dict(expression_summary={}, term_id_labels=dict(genes=[], cell_types=[])))
 
+    gene_term_ids = request["filter"]["gene_term_ids"]
+    tissue_type_term_ids = request["filter"]["tissue_type_term_ids"]
+
+    # TODO: Move response building logic outside of API layer.
+    # This currently just builds a minimal valid response with dummy data.
+    cell_type_term_ids = [f"CL{n:05d}" for n in range(3)]
+
+    def build_viz_dot(cell_type_term_id: str, me: float, pct: float, n: int) -> dict:
+        return dict(id=cell_type_term_id, me=me, pc=pct, n=n)
+
+    def build_viz_dots(cell_type_term_ids_: List[str]) -> List[dict]:
+        return [build_viz_dot(cell_type_term_id, 0.0, 0.0, 0) for cell_type_term_id in cell_type_term_ids_]
+
+    def build_tissues_types(tissue_type_term_ids_: List[str]) -> dict[str, List]:
+        return dict(
+            [(tissue_type_term_id, build_viz_dots(cell_type_term_ids)) for tissue_type_term_id in tissue_type_term_ids_]
+        )
+
+    def build_genes(gene_term_ids_: List[str]) -> dict[str, dict]:
+        return dict([(gene_term_id, build_tissues_types(tissue_type_term_ids)) for gene_term_id in gene_term_ids_])
+
+    def build_gene_id_label_mapping(gene_term_ids_) -> List[dict]:
+        return [{gene_term_id: f"{gene_term_id}_label"} for gene_term_id in gene_term_ids_]
+
+    def build_cell_type_id_label_mapping(cell_type_term_ids_) -> List[dict]:
+        return [{gene_term_id: f"{gene_term_id}_label"} for gene_term_id in cell_type_term_ids_]
+
+    return jsonify(
+        dict(
+            expression_summary=build_genes(gene_term_ids),
+            term_id_labels=dict(
+                genes=build_gene_id_label_mapping(gene_term_ids),
+                cell_types=build_cell_type_id_label_mapping(cell_type_term_ids),
+            ),
+        )
+    )
--- tests/unit/backend/wmg/api/v1.py
+++ tests/unit/backend/wmg/api/v1.py
@@ -1,13 +1,13 @@
 import json
 import unittest
 import uuid
+from pprint import pprint
 
 from backend.corpora.api_server.app import app
 from unit.backend.corpora.fixtures.environment_setup import EnvironmentSetup
 
 
 class WmgApiTests(unittest.TestCase):
-
     def setUp(self):
         super().setUp()
         with EnvironmentSetup(dict(APP_NAME="corpora-api")):
@@ -21,7 +21,7 @@ class WmgApiTests(unittest.TestCase):
     def test__latest_snapshot__returns_valid_response_body(self):
         response = self.app.get("/wmg/v1/snapshots/latest")
 
-        snapshot_id = json.loads(response.data)['snapshot_id']
+        snapshot_id = json.loads(response.data)["snapshot_id"]
         try:
             uuid.UUID(snapshot_id)
         except ValueError:
@@ -39,10 +39,10 @@ class WmgApiTests(unittest.TestCase):
 
         response = self.app.get(f"/wmg/v1/snapshots/{snapshot_id}/primary_filter_dimensions")
 
-        expected = dict(organism_terms=[dict(oid1='olbl1'),
-                                        dict(oid2='olbl2')],
-                        tissue_type_terms=[dict(ttid1='ttlbl1'),
-                                           dict(ttid2='ttlbl2')])
+        expected = dict(
+            organism_terms=[dict(oid1="olbl1"), dict(oid2="olbl2")],
+            tissue_type_terms=[dict(ttid1="ttlbl1"), dict(ttid2="ttlbl2")],
+        )
 
         self.assertEqual(expected, json.loads(response.data))
 
@@ -50,33 +50,66 @@ class WmgApiTests(unittest.TestCase):
         snapshot_id = uuid.uuid4()
 
         request = dict(
-                filter=dict(
-                        gene_term_ids=['gene1'],
-                        organism_term_id='organism1',
-                        tissue_type_term_ids=['tissuetype1']),
-                response_option='include_filter_dims_include_dataset_links')
+            filter=dict(gene_term_ids=["gene1"], organism_term_id="organism1", tissue_type_term_ids=["tissuetype1"]),
+            response_option="include_filter_dims_include_dataset_links",
+        )
 
         response = self.app.post(f"/wmg/v1/snapshots/{snapshot_id}/query", json=request)
 
         self.assertEqual(200, response.status_code)
 
-    def test__query__minimal_valid_request_returns_valid_response_body(self):
+    def test__query__valid_request_returns_valid_response_body(self):
         snapshot_id = uuid.uuid4()
 
         request = dict(
-                filter=dict(
-                        gene_term_ids=['gene1'],
-                        organism_term_id='organism1',
-                        tissue_type_term_ids=['tissuetype1']),
-                response_option='include_filter_dims_include_dataset_links')
+            filter=dict(
+                gene_term_ids=["gene1", "gene2"],
+                organism_term_id="organism1",
+                tissue_type_term_ids=["tissuetype1", "tissuetype2"],
+            ),
+            response_option="include_filter_dims_include_dataset_links",
+        )
 
         response = self.app.post(f"/wmg/v1/snapshots/{snapshot_id}/query", json=request)
 
-        self.assertEqual(dict(expression_summary={},
-                              term_id_labels=dict(
-                                      genes=[],
-                                      cell_types=[])),
-                         json.loads(response.data))
+        expected = {
+            "expression_summary": {
+                "gene1": {
+                    "tissuetype1": [
+                        {"id": "CL00000", "me": 0.0, "n": 0, "pc": 0.0},
+                        {"id": "CL00001", "me": 0.0, "n": 0, "pc": 0.0},
+                        {"id": "CL00002", "me": 0.0, "n": 0, "pc": 0.0},
+                    ],
+                    "tissuetype2": [
+                        {"id": "CL00000", "me": 0.0, "n": 0, "pc": 0.0},
+                        {"id": "CL00001", "me": 0.0, "n": 0, "pc": 0.0},
+                        {"id": "CL00002", "me": 0.0, "n": 0, "pc": 0.0},
+                    ],
+                },
+                "gene2": {
+                    "tissuetype1": [
+                        {"id": "CL00000", "me": 0.0, "n": 0, "pc": 0.0},
+                        {"id": "CL00001", "me": 0.0, "n": 0, "pc": 0.0},
+                        {"id": "CL00002", "me": 0.0, "n": 0, "pc": 0.0},
+                    ],
+                    "tissuetype2": [
+                        {"id": "CL00000", "me": 0.0, "n": 0, "pc": 0.0},
+                        {"id": "CL00001", "me": 0.0, "n": 0, "pc": 0.0},
+                        {"id": "CL00002", "me": 0.0, "n": 0, "pc": 0.0},
+                    ],
+                },
+            },
+            "term_id_labels": {
+                "cell_types": [
+                    {"CL00000": "CL00000_label"},
+                    {"CL00001": "CL00001_label"},
+                    {"CL00002": "CL00002_label"},
+                ],
+                "genes": [{"gene1": "gene1_label"}, {"gene2": "gene2_label"}],
+            },
+        }
+        pprint(expected)
+        self.assertEqual(expected, json.loads(response.data))
 
     def test__query__invalid_request_returns_400(self):
         snapshot_id = uuid.uuid4()
@@ -84,5 +117,4 @@ class WmgApiTests(unittest.TestCase):
         response = self.app.post(f"/wmg/v1/snapshots/{snapshot_id}/query", json={})
 
         self.assertEqual(400, response.status_code)
-        self.assertEqual("'filter' is a required property",
-                         json.loads(response.data)['detail'])
+        self.assertEqual("'filter' is a required property", json.loads(response.data)["detail"])
