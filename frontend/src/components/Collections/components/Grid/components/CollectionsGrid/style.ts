f58222e3594dc90ec4182c826cd70be3be998034
Fran McDade <franmcdade@Frans-MacBook-Pro-2.local>
2022-02-21 16:50:37 +1000

    feat: Add publication column to collections index (#1912)

--- frontend/src/components/Collections/components/Grid/components/CollectionsGrid/style.ts
+++ frontend/src/components/Collections/components/Grid/components/CollectionsGrid/style.ts
@@ -5,9 +5,4 @@ export const CollectionsGrid = styled(Grid)`
   grid-template-columns:
     minmax(0, 8fr)
     minmax(0, 6fr) repeat(2, minmax(0, 5fr)) minmax(0, 3fr);
-
-  th:first-of-type,
-  td:first-of-type {
-    grid-column: 1 / 3; /* review grid column allocation for collection name when publications column added */
-  }
 `;
