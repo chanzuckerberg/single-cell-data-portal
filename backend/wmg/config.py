2e55ecd37bc4b03f35d79e3e46702d05c4e7788b
Andrew Tolopko <atolopko@chanzuckerberg.com>
2022-03-09 08:57:03 -0500

    fmt, lint

--- backend/wmg/config.py
+++ backend/wmg/config.py
@@ -4,15 +4,10 @@ from backend.corpora.common.utils.secret_config import SecretConfig
 
 
 class WmgConfig(SecretConfig):
-
     def __init__(self, *args, **kwargs):
         super().__init__("backend", secret_name="wmg_config", **kwargs)
 
     def get_defaults_template(self):
-        deployment_stage = os.getenv('DEPLOYMENT_STAGE', 'test')
-        defaults_template = {
-            "bucket": f"wmg-{deployment_stage}"
-        }
+        deployment_stage = os.getenv("DEPLOYMENT_STAGE", "test")
+        defaults_template = {"bucket": f"wmg-{deployment_stage}"}
         return defaults_template
-
-
