65fb7287aa31317d2b6698517c63c1a7790549f5
Madison Dunitz <madison.dunitz@chanzuckerberg.com>
2022-03-09 17:51:00 -0800

    pipeline skeleton with skeleton tests

---
+++ backend/wmg/data/load.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+def update_s3_resources():
+    """
+    Copy cube and cube related files to s3 under the current timestamp
+    """
+    pass
+
+
+def remove_oldest_data():
+    """
+    Remove all data stored under the oldest timestamp
+    """
+    pass
+
+
+def update_latest_snapshot(time_stamp):
+    """
+    Update static timestamp in s3 to match the latest
+    """
+    pass
+
+
---
+++ backend/wmg/data/transform.py
@@ -0,0 +1,16 @@
+from typing import Dict
+
+
+def get_cells_by_tissue_type(corpus_group: str) -> Dict:
+    """
+    Return a list of all associated cell type ontologies for each tissue contained in the
+    provided corpus
+    """
+    pass
+
+
+def generate_cell_ordering(cell_type_by_tissue: Dict):
+    """
+    Use graphiz to map all the cells assoicated with a tissue to the ontology tree and return their correct order
+    """
+    pass
