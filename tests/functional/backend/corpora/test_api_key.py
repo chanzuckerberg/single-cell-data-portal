f2268dbc22e598a75316dc95346837e86a6c2c7a
Trent Smith <trent.smith@chanzuckerberg.com>
2022-03-10 20:48:51 -0800

    update smoke test

--- tests/functional/backend/corpora/test_api_key.py
+++ tests/functional/backend/corpora/test_api_key.py
@@ -16,21 +16,18 @@ class TestApiKey(BaseFunctionalTestCase):
 
         response = requests.post(f"{self.api}/dp/v1/auth/key", headers=headers)
         self.assertEqual(201, response.status_code)
-        self.assertIn("key", response.json().keys())
-        key_id_1 = response.json()["id"]
+        key_1 = response.json()["key"]
 
         response = requests.get(f"{self.api}/dp/v1/auth/key", headers=headers)
         self.assertEqual(200, response.status_code)
-        self.assertEqual(key_id_1, response.json()['id'])
 
         response = requests.post(f"{self.api}/dp/v1/auth/key", headers=headers)
         self.assertEqual(201, response.status_code)
-        key_id_2 = response.json()["id"]
-        self.assertNotEqual(key_id_1, key_id_2)
+        key_2 = response.json()["key"]
+        self.assertNotEqual(key_1, key_2)
 
         response = requests.get(f"{self.api}/dp/v1/auth/key", headers=headers)
         self.assertEqual(200, response.status_code)
-        self.assertEqual(key_id_2, response.json()['id'])
 
         response = requests.delete(f"{self.api}/dp/v1/auth/key", headers=headers)
         self.assertEqual(202, response.status_code)
