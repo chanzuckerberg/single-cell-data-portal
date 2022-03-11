c3ba89ae7ad4e66b0489bfb0961e4660c370cd3c
Trent Smith <trent.smith@chanzuckerberg.com>
2022-03-10 16:49:14 -0800

    feat: add access token generation curation api endpoint

---
+++ backend/corpora/lambdas/api/v1/curation/auth/token.py
@@ -0,0 +1,20 @@
+from flask import request, make_response
+from jose import JWTError
+
+from backend.corpora.common.auth0_manager import auth0_management_session
+from backend.corpora.common.corpora_config import CorporaAuthConfig
+from backend.corpora.common.utils.api_key import verify
+from backend.corpora.common.utils.exceptions import UnauthorizedError
+
+
+def post():
+    api_key = request.headers['x-api-key']
+    config = CorporaAuthConfig()
+    try:
+        verify(api_key, config.api_key_secret)
+    except JWTError:
+        raise UnauthorizedError("The API key is invalid")
+    else:
+        username = api_key.split('.')[-1]
+        token = auth0_management_session.generate_access_token(username, api_key)
+        return make_response(token, 201)
