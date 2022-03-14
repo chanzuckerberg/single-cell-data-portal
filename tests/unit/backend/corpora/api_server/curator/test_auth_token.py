86cf489b96e3645d360e448eff6f7f8d0eaf2c3d
Trent Smith <trent.smith@chanzuckerberg.com>
2022-03-14 16:58:35 -0700

    fix test names

--- tests/unit/backend/corpora/api_server/curator/test_auth_token.py
+++ tests/unit/backend/corpora/api_server/curator/test_auth_token.py
@@ -8,7 +8,7 @@ from tests.unit.backend.corpora.api_server.base_api_test import BaseAPITest
 class TestAuthToken(BaseAPITest):
     @patch("backend.corpora.lambdas.api.v1.auth.keys.auth0_management_session")
     @patch("backend.corpora.lambdas.api.v1.auth.keys.CorporaAuthConfig")
-    def test_post_token(self, auth0_management_session, CorporaAuthConfig):
+    def test__post_token__201(self, auth0_management_session, CorporaAuthConfig):
         test_secret = "password1234"
         test_email = "user@email.com"
         test_user_id = "test_user_id"
@@ -16,13 +16,15 @@ class TestAuthToken(BaseAPITest):
         auth0_management_session.get_user_api_key_identity = Mock(return_value={"email": test_email})
         auth0_management_session.generate_access_token = Mock(return_value={"access_token": "OK"})
         user_api_key = generate(test_user_id, test_secret)
-        token = self.app.post("/curator/v1/auth/token", headers={"x-api-key": user_api_key}).json()["access_token"]
+        response = self.app.post("/curator/v1/auth/token", headers={"x-api-key": user_api_key})
+        self.assertEqual(201, response.status_code)
+        token = response.json()["access_token"]
         self.assertEqual("OK", token)
         auth0_management_session.get_user_api_key_identity.assert_called_once_with(test_user_id)
         auth0_management_session.generate_access_token.assert_called_once_with(test_email)
 
     @patch("backend.corpora.lambdas.api.v1.auth.keys.CorporaAuthConfig")
-    def test_post_token(self, CorporaAuthConfig):
+    def test__post_token__401(self, CorporaAuthConfig):
         test_secret = "password1234"
         test_user_id = "test_user_id"
         CorporaAuthConfig().api_key_secret = test_secret
