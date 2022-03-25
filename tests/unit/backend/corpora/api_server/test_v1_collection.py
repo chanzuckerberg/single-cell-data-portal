82bce13070ceda4cd5fc0df34d703f88112f5bed
Emanuele Bezzi <ebezzi@chanzuckerberg.com>
2022-03-25 16:08:39 -0700

    Fix tests

--- tests/unit/backend/corpora/api_server/test_v1_collection.py
+++ tests/unit/backend/corpora/api_server/test_v1_collection.py
@@ -769,6 +769,8 @@ class TestCollection(BaseAuthAPITest):
             visibility=CollectionVisibility.PUBLIC.name,
             owner="test_user_id",
             publisher_metadata={"journal": "Nature"},
+            published_at=datetime.now(),
+            revised_at=datetime.now(),
         )
 
         collection_id_private = self.generate_collection(
@@ -776,7 +778,7 @@ class TestCollection(BaseAuthAPITest):
         ).id
 
         collection_id_tombstone = self.generate_collection(
-            self.session, visibility=CollectionVisibility.PUBLIC.name, tombstone=True, owner="test_user_id"
+            self.session, visibility=CollectionVisibility.PUBLIC.name, tombstone=True, owner="test_user_id",
         ).id
 
         test_url = furl(path="/dp/v1/collections/index")
@@ -794,8 +796,8 @@ class TestCollection(BaseAuthAPITest):
         self.assertEqual(actual_collection["id"], collection.id)
         self.assertEqual(actual_collection["name"], collection.name)
         self.assertNotIn("description", actual_collection)
-        self.assertEqual(actual_collection["published_at"], collection.published_at)
-        self.assertEqual(actual_collection["revised_at"], collection.revised_at)
+        self.assertEqual(actual_collection["published_at"], collection.published_at.timestamp())
+        self.assertEqual(actual_collection["revised_at"], collection.revised_at.timestamp())
         self.assertEqual(actual_collection["publisher_metadata"], collection.publisher_metadata)
 
 
