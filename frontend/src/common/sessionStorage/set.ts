d34aca44f4aa9292e15a94a9a7d93789c4b753f3
MillenniumFalconMechanic <mim@clevercanary.com>
2022-03-30 11:56:52 -0700

    feat: added filter state. #1811.

---
+++ frontend/src/common/sessionStorage/set.ts
@@ -0,0 +1,29 @@
+/**
+ * Key names of valid session storage keys.
+ */
+export enum KEYS {
+  "FILTER_DATASETS" = "cxg-filter-datasets",
+  "FILTER_COLLECTIONS" = "cxg-filter-collections",
+  "SIDE_BAR_DATASETS" = "cxg-sidebar-datasets",
+  "SIDE_BAR_COLLECTIONS" = "cxg-sidebar-collections",
+}
+
+/**
+ * Add string value to session storage.
+ * @param key - Key name of value to add to session storage.
+ * @param value - String value to add to session storage.
+ */
+export function set(key: KEYS, value: string): void {
+  if (typeof window === "undefined") return;
+  window?.sessionStorage?.setItem(key, value);
+}
+
+/**
+ * Add object to session storage.
+ * @param key - Key name of value to add to session storage.
+ * @param value - Object value to add to session storage.
+ */
+export function setObject<T>(key: KEYS, value: T): void {
+  if (typeof window === "undefined") return;
+  window?.sessionStorage?.setItem(key, JSON.stringify(value));
+}
