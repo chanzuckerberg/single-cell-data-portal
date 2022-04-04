3b26f73c98a769a973a0e1729dfa9e988b21a8c5
Emanuele Bezzi <ebezzi@chanzuckerberg.com>
2022-04-04 16:10:29 -0400

    feat: wmg pipeline

--- backend/wmg/data/load_cube.py
+++ backend/wmg/data/load_cube.py
@@ -1,19 +1,82 @@
-def update_s3_resources():
+import time
+import boto3
+import os
+import subprocess
+
+from backend.wmg.data.snapshot import CELL_TYPE_ORDERINGS_FILENAME
+
+
+stack_name = os.environ.get("REMOTE_DEV_PREFIX")
+wmg_bucket_name = os.environ.get("WMG_BUCKET")
+artifact_bucket_name = os.environ.get("ARTIFACT_BUCKET")
+
+
+def _get_wmg_bucket_path():
+    if stack_name:
+        return f"s3://{wmg_bucket_name}{stack_name}"
+    else:
+        return f"s3://{wmg_bucket_name}"
+
+
+def update_s3_resources(corpus_name):
     """
     Copy cube and cube related files to s3 under the current timestamp
     """
-    pass
+    timestamp = int(time.time())
+    upload_cube_to_s3(corpus_name, timestamp)
+    upload_cell_ordering_to_s3(timestamp)
+    update_latest_snapshot_identifier(timestamp)
+    remove_oldest_datasets(timestamp)
 
-
-def remove_previous_snapshot():
+def remove_oldest_datasets(timestamp):
     """
     Remove all data stored under the oldest timestamp
     """
-    pass
+    s3 = boto3.resource("s3")
+    wmg_bucket = s3.Bucket(wmg_bucket_name)
+    objects = wmg_bucket.objects.filter(Prefix=stack_name.strip("/")) if stack_name else wmg_bucket.objects.all()
+
+    def enumerate_timestamps_and_objects(objects):
+        for obj in objects:
+            try:
+                tokens = obj.key.split("/")
+                timestamp_token = tokens[1] if stack_name else tokens[0]
+                is_timestamp = timestamp_token[:10].isdigit()
+                if is_timestamp:
+                    yield (timestamp_token, obj)
+            except Exception:
+                pass
+
+    candidate_to_delete = [obj for obj in enumerate_timestamps_and_objects(objects)]
+
+    timestamps = sorted(list(set([x[0] for x in candidate_to_delete])))
+
+    if len(timestamps) > 2:
+        timestamps_to_delete = list(timestamps)[:-2]
+    else:
+        timestamps_to_delete = []
+
+    for timestamp, object in candidate_to_delete:
+        if timestamp in timestamps_to_delete:
+            object.delete()
 
 
-def update_latest_snapshot_identifier(time_stamp):
+def upload_cube_to_s3(group_name, timestamp):
+    sync_command = ["aws", "s3", "sync", f"{group_name}/cube", f"{_get_wmg_bucket_path()}/{timestamp}/cube"]
+    subprocess.run(sync_command)
+
+
+def upload_cell_ordering_to_s3(timestamp):
+    sync_command = ["aws", "s3", "cp", CELL_TYPE_ORDERINGS_FILENAME, f"{_get_wmg_bucket_path()}/{timestamp}/{CELL_TYPE_ORDERINGS_FILENAME}"]
+    subprocess.run(sync_command)
+
+
+def update_latest_snapshot_identifier(timestamp):
     """
     Update static timestamp in s3 to match the latest
     """
-    pass
+    s3 = boto3.resource("s3")
+    file_key = "latest_snapshot_identifier"
+    file_path = f"{stack_name.strip('/')}/{file_key}" if stack_name else f"{file_key}"
+    object = s3.Object(wmg_bucket_name, file_path)
+    object.put(Body=str(timestamp))
--- backend/wmg/data/transform.py
+++ backend/wmg/data/transform.py
@@ -1,4 +1,10 @@
 from typing import Dict
+import tiledb
+import pandas as pd
+
+from backend.wmg.data.snapshot import CELL_TYPE_ORDERINGS_FILENAME
+from pronto import Ontology
+import pygraphviz as pgv
 
 
 def get_cells_by_tissue_type(corpus_group: str) -> Dict:
@@ -6,11 +12,72 @@ def get_cells_by_tissue_type(corpus_group: str) -> Dict:
     Return a list of all associated cell type ontologies for each tissue contained in the
     provided corpus
     """
-    pass
+    with tiledb.open(f"{corpus_group}/obs", "r") as obs:
+        cell_tissue_types = (
+            obs.query(attrs=[], dims=["tissue_ontology_term_id", "cell_type_ontology_term_id"])
+            .df[:]
+            .drop_duplicates()
+            .sort_values(by="tissue_ontology_term_id")
+        )
+    unique_tissue_ontology_term_id = cell_tissue_types.tissue_ontology_term_id.unique()
+    cell_type_by_tissue = {}
+    for x in unique_tissue_ontology_term_id:
+        cell_type_by_tissue[x] = cell_tissue_types.loc[
+            cell_tissue_types["tissue_ontology_term_id"] == x, "cell_type_ontology_term_id"
+        ]
+
+    return cell_type_by_tissue
 
 
 def generate_cell_ordering(cell_type_by_tissue: Dict) -> None:
     """
-    Use graphiz to map all the cells assoicated with a tissue to the ontology tree and return their correct order
+    Use graphviz to map all the cells assoicated with a tissue to the ontology tree and return their correct order
     """
-    pass
+    onto = Ontology.from_obo_library("cl-basic.obo")
+
+    def compute_ordering(cells, root):
+        ancestors = [list(onto[t].superclasses()) for t in cells if t in onto]
+        ancestors = [i for s in ancestors for i in s]
+        ancestors = set(ancestors)
+
+        G = pgv.AGraph()
+        for a in ancestors:
+            for s in a.subclasses(with_self=False, distance=1):
+                if s in ancestors:
+                    G.add_edge(a.id, s.id)
+
+        G.layout(prog="dot")
+
+        positions = {}
+        for n in G.iternodes():
+            pos = n.attr["pos"].split(",")
+            positions[n] = (float(pos[0]), float(pos[1]))
+
+        ancestor_ids = [a.id for a in ancestors]
+
+        def recurse(node):
+            if node in cells:
+                yield (node)
+            children = [
+                (c, positions[c.id]) for c in onto[node].subclasses(with_self=False, distance=1) if c.id in ancestor_ids
+            ]
+            sorted_children = sorted(children, key=lambda x: x[1][0])
+            for child in sorted_children:
+                yield from recurse(child[0].id)
+
+        ordered_list = list(dict.fromkeys(recurse(root)))
+        return ordered_list
+
+    mapping = {}
+    for tissue, cell_df in cell_type_by_tissue.items():
+        cells = list(cell_df)
+        ordered_cells = compute_ordering(cells, "CL:0000003")
+        mapping[tissue] = ordered_cells
+
+    data = []
+    for tissue, cells in mapping.items():
+        for i, cell in enumerate(cells):
+            data.append((tissue, cell, i))
+
+    df = pd.DataFrame(data, columns=["tissue_ontology_term_id", "cell_type_ontology_term_id", "order"])
+    df.to_json(CELL_TYPE_ORDERINGS_FILENAME)
