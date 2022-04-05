0366ff8dc029a717441e4b514a31543940fa0971
Timmy Huang <tihuan@users.noreply.github.com>
2022-04-05 16:42:55 -0700

    fix(wmg): update info panel text (#2278)

--- frontend/src/views/WheresMyGene/components/InfoPanel/components/Methodology/index.tsx
+++ frontend/src/views/WheresMyGene/components/InfoPanel/components/Methodology/index.tsx
@@ -9,10 +9,9 @@ export default function Methodology(): JSX.Element {
       <Content>
         After filtering cells with low coverage (less than 500 genes expressed),
         gene counts are normalized independently for each cell by converting
-        counts to quantiles and obtaining the corresponding values from the
-        standard normal distribution. Then normalized cell vectors are
-        concatenated along the gene axis. The algorithm is summarized in detail
-        in{" "}
+        counts to quantiles and obtaining the corresponding values from a normal
+        distribution. Then normalized cell vectors are concatenated along the
+        gene axis. The algorithm is summarized in detail in{" "}
         <a href={EXTERNAL_LINKS.WMG_DOC} rel="noopener" target="_blank">
           our documentation
         </a>
