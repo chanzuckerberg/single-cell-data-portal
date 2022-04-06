f70e2e15f4b75f8345d9a89194b9ae619e49f0b6
Timmy Huang <thuang@chanzuckerberg.com>
2022-04-06 14:52:35 -0700

    update Tooltip Cell Type

--- frontend/src/views/WheresMyGene/components/HeatMap/components/Chart/index.tsx
+++ frontend/src/views/WheresMyGene/components/HeatMap/components/Chart/index.tsx
@@ -254,7 +254,7 @@ export default memo(function Chart({
       },
       {
         dataRows: [
-          { label: "Cell Name", value: cellType.name },
+          { label: "Cell Type", value: cellType.name },
           {
             label: "Tissue Composition",
             value: tissuePercentage + "%" || "",
