0b6429feff801621ea3f0bd50ef4b2af9749ab94
Emanuele Bezzi <ebezzi@chanzuckerberg.com>
2022-04-11 10:44:30 -0400

    Fix typo

--- tests/unit/backend/wmg/data/test_wmg_cube.py
+++ tests/unit/backend/wmg/data/test_wmg_cube.py
@@ -5,7 +5,7 @@ from backend.wmg.data import tiledb
 from backend.wmg.data.schemas.cube_schema import cell_counts_schema
 from backend.wmg.data.snapshot import CELL_COUNTS_CUBE_NAME
 from backend.wmg.data.wmg_cube import create_cell_count_cube
-from unit.backend.wmg.fixtures.test_snapshot import create_temp_wmg_snapshot
+from backend.wmg.fixtures.test_snapshot import create_temp_wmg_snapshot
 
 
 @unittest.skip(
