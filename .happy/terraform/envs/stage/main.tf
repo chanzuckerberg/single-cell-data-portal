354786990ef95deb5cc0d508ca25eeb40da36031
Emanuele Bezzi <ebezzi@chanzuckerberg.com>
2022-04-13 16:45:26 -0400

    Update .happy/terraform/envs/stage/main.tf

--- .happy/terraform/envs/stage/main.tf
+++ .happy/terraform/envs/stage/main.tf
@@ -13,7 +13,7 @@ module stack {
   frontend_url                 = "https://cellxgene.staging.single-cell.czi.technology"
   backend_url                  = "https://api.cellxgene.staging.single-cell.czi.technology"
   stack_prefix                 = ""
-  batch_container_memory_limit = 60000
+  batch_container_memory_limit = 300000
   backend_instance_count       = 4
 
   wait_for_steady_state        = var.wait_for_steady_state
