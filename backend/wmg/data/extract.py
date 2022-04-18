9c972a9a9f6b999b1a7f70ef2a5e20b96a8b24ca
Emanuele Bezzi <ebezzi@chanzuckerberg.com>
2022-04-18 15:03:34 -0400

    fix: multiple assays support in wmg/extract.py

--- backend/wmg/data/extract.py
+++ backend/wmg/data/extract.py
@@ -40,9 +40,9 @@ def get_dataset_s3_uris():
             )
             .all()
         )
-        for dataset in published_dataset_non_null_assays:
-            if dataset[1]["ontology_term_id"] in included_assay_ontologies:
-                dataset_ids.append(dataset[0])
+        for dataset_id, assays in published_dataset_non_null_assays:
+            if any(assay["ontology_term_id"] for assay in assays):
+                dataset_ids.append(dataset_id)
 
         s3_uris = DatasetAsset.s3_uris_for_datasets(session, dataset_ids, DatasetArtifactFileType.H5AD)
     return s3_uris
