fea9c80823e39e8ebb6dd632b2db42ea412a9803
Kuni <kkatsuya@chanzuckerberg.com>
2022-04-20 18:47:50 -0700

    chore: refactoring (#2143)

--- backend/corpora/common/upload_sfn.py
+++ backend/corpora/common/upload_sfn.py
@@ -2,9 +2,17 @@ import boto3
 import json
 import time
 
+from sqlalchemy.orm import Session
+
 from .corpora_config import CorporaConfig
 import os
 
+from .corpora_orm import CollectionVisibility, ProcessingStatus
+from .entities import Collection, Dataset
+from .utils.exceptions import MaxFileSizeExceededException, InvalidFileFormatException, NonExistentCollectionException, \
+    InvalidProcessingStateException, NonExistentDatasetException
+from .utils.math_utils import GB
+from ..lambdas.api.v1.collection import _owner_or_allowed
 
 _stepfunctions_client = None
 
@@ -25,3 +33,46 @@ def start_upload_sfn(collection_uuid, dataset_uuid, url):
         input=json.dumps(input_parameters),
     )
     return response
+
+
+def upload(db_session: Session, collection_uuid: str, user: str, url: str, file_size: int, file_extension: str,
+           dataset_id: str = None) -> str:
+    max_file_size_gb = CorporaConfig().upload_max_file_size_gb * GB
+    if file_size is not None and file_size > max_file_size_gb:
+        raise MaxFileSizeExceededException(f"{url} exceeds the maximum allowed file size of {max_file_size_gb} Gb")
+
+    allowed_file_formats = CorporaConfig().upload_file_formats
+    if file_extension not in allowed_file_formats:
+        raise InvalidFileFormatException(f"{url} must be in the file format(s): {allowed_file_formats}")
+
+    collection = Collection.get_collection(
+        db_session,
+        collection_uuid,
+        visibility=CollectionVisibility.PRIVATE,  # Do not allow changes to public Collections
+        owner=_owner_or_allowed(user),
+    )
+    if not collection:
+        raise NonExistentCollectionException(f"Collection {collection_uuid} does not exist")
+
+    if dataset_id:
+        # Update dataset
+        dataset = Dataset.get(db_session, dataset_id)
+        if collection_uuid == dataset.collection_id:
+            if dataset.processing_status.processing_status in [ProcessingStatus.SUCCESS, ProcessingStatus.FAILURE]:
+                dataset.reprocess()
+            else:
+                raise InvalidProcessingStateException(
+                    f"Unable to reprocess dataset {dataset_id}: {dataset.processing_status.processing_status=}")
+        else:
+            raise NonExistentDatasetException(f"Dataset {dataset_id} does not exist")
+
+    else:
+        # Add new dataset
+        dataset = Dataset.create(db_session, collection=collection)
+
+    dataset.update(processing_status=dataset.new_processing_status())
+
+    # Start processing link
+    start_upload_sfn(collection_uuid, dataset.id, url)
+
+    return dataset.id
---
+++ backend/corpora/dataset_submissions/app.py
@@ -0,0 +1,113 @@
+import logging
+import os
+import re
+from typing import Tuple, Optional
+from urllib.parse import unquote_plus
+
+from sqlalchemy.orm import Session
+
+from backend.corpora.common.entities import Collection
+from backend.corpora.common.upload_sfn import upload
+from backend.corpora.common.utils.db_session import db_session_manager
+from backend.corpora.common.utils.exceptions import CorporaException
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+UUID_REGEX = "[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}"
+
+
+def dataset_submissions_handler(s3_event: dict, context) -> None:
+    """
+    Lambda function invoked when a dataset is uploaded to s3://cellxgene-dataset-submissions-{dev,staging,prod}
+    :param s3_event: Lambda's event object
+    :param context: Lambda's context object
+    :return:
+    """
+    logger.info(f"@@@@@ {s3_event=}")
+    logger.info(f"@@@@@ {os.environ.get('DATASET_SUBMISSIONS_BUCKET')=}")
+    logger.info(f"@@@@@ {os.environ.get('REMOTE_DEV_PREFIX', '')=}")
+
+    expected_bucket = os.environ.get("DATASET_SUBMISSIONS_BUCKET", "")
+
+    # s3://cellxgene-curation-submissions-{dev,staging,prod}/<collection_id>/<curator_tag>
+
+    for record in s3_event["Records"]:
+        region, bucket, key, size = parse_s3_event_record(record)
+        logger.info(f"{region=}, {bucket=}, {key=}, {size=}")
+
+        if expected_bucket != bucket:
+            raise CorporaException(
+                f"Infrastructure misconfiguration: Expected S3 event from bucket '{expected_bucket}' "
+                "but received event from bucket '{bucket}' instead")
+
+        collection_uuid, incoming_curator_tag = parse_key(key)
+
+        if collection_uuid and incoming_curator_tag:
+            extension = get_extension(incoming_curator_tag)
+            logger.info(f"{collection_uuid=}, {incoming_curator_tag=}, {extension=}")
+
+            with db_session_manager() as session:
+                session: Session
+
+                collection_owner, dataset_uuid = get_dataset_info(session, collection_uuid, incoming_curator_tag)
+                if not collection_owner:
+                    raise CorporaException(f"Collection {collection_uuid} does not exist")
+
+                s3_url = f"https://{bucket}.s3.{region}.amazonaws.com/{key}"
+                upload(session, collection_uuid, user=collection_owner, url=s3_url, file_size=size,
+                       file_extension=extension, dataset_id=dataset_uuid)
+        else:
+            raise CorporaException(f"Missing collection UUID and/or curator tag for {key=}")
+
+
+def parse_s3_event_record(s3_event_record: dict) -> Tuple[str, str, str, int]:
+    """
+    Parses the S3 event record and returns the bucket name, object key and object size
+    :param s3_event_record:
+    :return:
+    """
+    region = s3_event_record["awsRegion"]
+    bucket = s3_event_record["s3"]["bucket"]["name"]
+    key = unquote_plus(s3_event_record["s3"]["object"]["key"], encoding="utf-8")
+    size = s3_event_record["s3"]["object"]["size"]
+    return region, bucket, key, size
+
+
+def parse_key(key: str) -> Tuple[Optional[str], Optional[str]]:
+    """
+    Parses the S3 object key to extract the collection UUID and curator tag, ignoring the REMOTE_DEV_PREFIX
+    :param key:
+    :return:
+    """
+    rdev_prefix = os.environ.get("REMOTE_DEV_PREFIX", "").strip("/")
+    if rdev_prefix:
+        key = key.replace(f"{rdev_prefix}/", "")
+
+    matched = re.match(f"^({UUID_REGEX})/(.*)$", key)
+    if matched:
+        collection_uuid, curator_tag = matched.groups()
+        return collection_uuid.lower(), curator_tag
+    else:
+        return None, None
+
+
+def get_extension(path: str) -> str:
+    extension = os.path.splitext(path)[1]
+    return extension.replace(".", "")
+
+
+def get_dataset_info(session: Session, collection_uuid: str, incoming_curator_tag: str) -> Tuple[
+        Optional[str], Optional[str]]:
+    collection = Collection.get_collection(session=session, collection_uuid=collection_uuid)
+
+    dataset_uuid = None
+
+    if not collection:
+        return None, dataset_uuid
+
+    for dataset in collection.datasets:
+        if dataset.curator_tag == incoming_curator_tag:
+            dataset_uuid = dataset.id
+            break
+
+    return collection.owner, dataset_uuid
