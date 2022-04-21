4ffda642ac3affff281c3889e29fc73f7e63b4da
Daniel Hegeman <daniel.hegeman@chanzuckerberg.com>
2022-04-21 13:48:20 -0700

    fix: introduce useCallback for query invalidation function (#2404)
    * fix: introduce useCallback for query invalidation function
    
    * fix: do not wrap useCallback in separate function

--- frontend/src/views/Collection/components/DatasetTab/index.tsx
+++ frontend/src/views/Collection/components/DatasetTab/index.tsx
@@ -1,6 +1,6 @@
 import { Button, Intent, UL } from "@blueprintjs/core";
 import { IconNames } from "@blueprintjs/icons";
-import { FC, useState } from "react";
+import { FC, useCallback, useState } from "react";
 import { useQueryClient } from "react-query";
 import { Collection, Dataset } from "src/common/entities";
 import { FEATURES } from "src/common/featureFlags/features";
@@ -49,9 +49,9 @@ const DatasetTab: FC<Props> = ({
   const isDatasetPresent =
     datasets?.length > 0 || Object.keys(uploadedFiles).length > 0;
 
-  const invalidateCollectionQuery = () => {
+  const invalidateCollectionQuery = useCallback(() => {
     queryClient.invalidateQueries([USE_COLLECTION, collectionId]);
-  };
+  }, [collectionId]);
 
   const addNewFile = (mutationFunction = uploadLink, originalId?: string) => {
     return (newFile: UploadingFile) => {
