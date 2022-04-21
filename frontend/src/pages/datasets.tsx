d2473340e68f945800e75c0a77b3f463f30f7968
Mim Hastie <mim@clevercanary.com>
2022-04-21 13:20:45 -0700

    fix: removed datasets page. #2401 (#2403)

--- frontend/src/pages/datasets.tsx
+++
@@ -1,8 +0,0 @@
-import SidebarLayout from "src/components/Layout/components/sidebarLayout";
-import Datasets from "src/views/Datasets";
-
-const Page = (): JSX.Element => <Datasets />;
-
-Page.Layout = SidebarLayout;
-
-export default Page;
