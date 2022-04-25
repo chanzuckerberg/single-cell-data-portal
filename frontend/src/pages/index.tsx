b782c04eda296e8d59dc420929a79a6866318628
Seve Badajoz <sbadajoz@chanzuckerberg.com>
2022-04-25 15:14:21 -0700

    tmp index

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,7 +1,25 @@
 import SidebarLayout from "src/components/Layout/components/sidebarLayout";
-import Datasets from "src/views/Datasets";
 
-const Page = (): JSX.Element => <Datasets />;
+// export const getStaticProps = async () => {
+//   const files = fs.readdirSync(path.join("doc-site"));
+//   const docPages = files.reduce((acc, filename) => {
+//     if (fs.lstatSync(path.join("doc-site", filename)).isDirectory()) {
+//       console.log(path.join("doc-site", filename), " IS DIRECTORY");
+//       return acc;
+//     } else {
+//       console.log(path.join("doc-site", filename), " IS FILE");
+//     }
+//     const markdownWithMeta = fs.readFileSync(
+//       path.join("doc-site", filename),
+//       "utf-8"
+//     );
+//     const { data: frontMatter } = matter(markdownWithMeta);
+//     acc.push({
+//       frontMatter,
+//       slug: filename.split(".")[0],
+//     });
+//     return acc;
+//   }, new Array<{ frontMatter: Record<string, any>; slug: string }>());
 
 Page.Layout = SidebarLayout;
 
