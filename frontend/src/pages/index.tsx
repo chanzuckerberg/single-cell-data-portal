4d83016a86b33a8de66916ffad0dd4c21af5a35e
Seve Badajoz <sbadajoz@chanzuckerberg.com>
2022-04-25 15:14:21 -0700

    Revert "tmp index"
    This reverts commit 081556b4439d12e2c36943d38ff71ad607a9f97b.

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,25 +1,7 @@
 import SidebarLayout from "src/components/Layout/components/sidebarLayout";
+import Datasets from "src/views/Datasets";
 
-// export const getStaticProps = async () => {
-//   const files = fs.readdirSync(path.join("doc-site"));
-//   const docPages = files.reduce((acc, filename) => {
-//     if (fs.lstatSync(path.join("doc-site", filename)).isDirectory()) {
-//       console.log(path.join("doc-site", filename), " IS DIRECTORY");
-//       return acc;
-//     } else {
-//       console.log(path.join("doc-site", filename), " IS FILE");
-//     }
-//     const markdownWithMeta = fs.readFileSync(
-//       path.join("doc-site", filename),
-//       "utf-8"
-//     );
-//     const { data: frontMatter } = matter(markdownWithMeta);
-//     acc.push({
-//       frontMatter,
-//       slug: filename.split(".")[0],
-//     });
-//     return acc;
-//   }, new Array<{ frontMatter: Record<string, any>; slug: string }>());
+const Page = (): JSX.Element => <Datasets />;
 
 Page.Layout = SidebarLayout;
 
