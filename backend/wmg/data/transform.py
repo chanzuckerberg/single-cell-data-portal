6761694400d94cd714fa2b369b3225e7ff01e1bb
Emanuele Bezzi <ebezzi@chanzuckerberg.com>
2022-04-26 16:45:15 -0400

    open cube

--- backend/wmg/data/transform.py
+++ backend/wmg/data/transform.py
@@ -6,7 +6,7 @@ from backend.wmg.data.ontology_labels import ontology_term_label, gene_term_labe
 from typing import Dict, List, Iterable
 import json
 
-from backend.wmg.data.snapshot import CELL_TYPE_ORDERINGS_FILENAME, PRIMARY_FILTER_DIMENSIONS_FILENAME
+from backend.wmg.data.snapshot import CELL_TYPE_ORDERINGS_FILENAME, EXPRESSION_SUMMARY_CUBE_NAME, PRIMARY_FILTER_DIMENSIONS_FILENAME
 
 
 def get_cell_types_by_tissue(corpus_group: str) -> Dict:
@@ -92,9 +92,9 @@ def generate_cell_ordering(snapshot_path: str, cell_type_by_tissue: Dict) -> Non
 def generate_primary_filter_dimensions(snapshot_path: str):
 
     # TODO: remove them from WmgQuery (next 4 following functions)
-    def list_primary_filter_dimension_term_ids(self, primary_dim_name: str):
+    def list_primary_filter_dimension_term_ids(cube, primary_dim_name: str):
         return (
-            self._snapshot.expression_summary_cube.query(attrs=[], dims=[primary_dim_name])
+            cube.query(attrs=[], dims=[primary_dim_name])
             .df[:]
             .groupby([primary_dim_name])
             .first()
@@ -102,10 +102,10 @@ def generate_primary_filter_dimensions(snapshot_path: str):
         )
 
     def list_grouped_primary_filter_dimensions_term_ids(
-        self, primary_dim_name: str, group_by_dim: str
+        cube, primary_dim_name: str, group_by_dim: str
     ) -> Dict[str, List[str]]:
         return (
-            self._snapshot.expression_summary_cube.query(attrs=[], dims=[primary_dim_name, group_by_dim])
+            cube.query(attrs=[], dims=[primary_dim_name, group_by_dim])
             .df[:]
             .drop_duplicates()
             .groupby(group_by_dim)
@@ -123,31 +123,33 @@ def generate_primary_filter_dimensions(snapshot_path: str):
     def build_ontology_term_id_label_mapping(ontology_term_ids: Iterable[str]) -> List[dict]:
         return [{ontology_term_id: ontology_term_label(ontology_term_id)} for ontology_term_id in ontology_term_ids]
 
+    with tiledb.open(f"{snapshot_path}/{EXPRESSION_SUMMARY_CUBE_NAME}") as cube:
 
-    # gene terms are grouped by organism, and represented as a nested lists in dict, keyed by organism
-    organism_gene_ids: dict[str, List[str]] = list_grouped_primary_filter_dimensions_term_ids(
-        "gene_ontology_term_id", group_by_dim="organism_ontology_term_id"
-    )
-    organism_gene_terms = {
-        organism_term_id: build_gene_id_label_mapping(gene_term_ids)
-        for organism_term_id, gene_term_ids in organism_gene_ids.items()
-    }
+        # gene terms are grouped by organism, and represented as a nested lists in dict, keyed by organism
+        organism_gene_ids: dict[str, List[str]] = list_grouped_primary_filter_dimensions_term_ids(
+            "gene_ontology_term_id", group_by_dim="organism_ontology_term_id"
+        )
+        organism_gene_terms = {
+            organism_term_id: build_gene_id_label_mapping(gene_term_ids)
+            for organism_term_id, gene_term_ids in organism_gene_ids.items()
+        }
 
-    # tissue terms are grouped by organism, and represented as a nested lists in dict, keyed by organism
-    organism_tissue_ids: dict[str, List[str]] = list_grouped_primary_filter_dimensions_term_ids(
-        "tissue_ontology_term_id", group_by_dim="organism_ontology_term_id"
-    )
-    organism_tissue_terms = {
-        organism_term_id: build_ontology_term_id_label_mapping(tissue_term_ids)
-        for organism_term_id, tissue_term_ids in organism_tissue_ids.items()
-    }
+        # tissue terms are grouped by organism, and represented as a nested lists in dict, keyed by organism
+        organism_tissue_ids: dict[str, List[str]] = list_grouped_primary_filter_dimensions_term_ids(
+            "tissue_ontology_term_id", group_by_dim="organism_ontology_term_id"
+        )
+        organism_tissue_terms = {
+            organism_term_id: build_ontology_term_id_label_mapping(tissue_term_ids)
+            for organism_term_id, tissue_term_ids in organism_tissue_ids.items()
+        }
 
-    result = dict(
-        organism_terms=build_ontology_term_id_label_mapping(
-            list_primary_filter_dimension_term_ids("organism_ontology_term_id")
-        ),
-        tissue_terms=organism_tissue_terms,
-        gene_terms=organism_gene_terms,
-    )
-    with open(f"{snapshot_path}/{PRIMARY_FILTER_DIMENSIONS_FILENAME}", "w") as f:
-        json.dump(result, f)
+        result = dict(
+            organism_terms=build_ontology_term_id_label_mapping(
+                list_primary_filter_dimension_term_ids("organism_ontology_term_id")
+            ),
+            tissue_terms=organism_tissue_terms,
+            gene_terms=organism_gene_terms,
+        )
+        
+        with open(f"{snapshot_path}/{PRIMARY_FILTER_DIMENSIONS_FILENAME}", "w") as f:
+            json.dump(result, f)
