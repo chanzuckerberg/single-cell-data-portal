3ab8ab110731bf162b7f714c0faa8239d8ee9886
Daniel Hegeman <daniel.hegeman@chanzuckerberg.com>
2022-04-27 00:18:18 -0700

    fix: function name masking error

--- backend/corpora/lambdas/api/v1/authorization.py
+++ backend/corpora/lambdas/api/v1/authorization.py
@@ -1,6 +1,6 @@
 from backend.corpora.common.utils.authorization_checks import (
     is_user_owner_or_allowed as is_user_owner_or_allowed_common,
-    owner_or_allowed,
+    owner_or_allowed as owner_or_allowed_common,
 )
 
 
@@ -9,4 +9,4 @@ def is_user_owner_or_allowed(token_info: dict, owner: str):
 
 
 def owner_or_allowed(token_info: dict):
-    return owner_or_allowed(token_info["scope"], token_info.get("sub"))
+    return owner_or_allowed_common(token_info["scope"], token_info.get("sub"))
