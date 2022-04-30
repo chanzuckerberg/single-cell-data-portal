ea8616f7d4cbaa492a2f6f30d5629917f8089653
Daniel Hegeman <daniel.hegeman@chanzuckerberg.com>
2022-04-29 17:14:26 -0700

    fix: addresses import errors (#2450)
    * fix: import module syntax error
    
    * fix: function name masking error
    
    * fix: argument order typo
    
    * use empty string if no scope is provided
    
    Co-authored-by: Trent Smith <trent.smith@chanzuckerberg.com>

--- backend/corpora/lambdas/api/v1/authorization.py
+++ backend/corpora/lambdas/api/v1/authorization.py
@@ -1,12 +1,12 @@
 from backend.corpora.common.utils.authorization_checks import (
     is_user_owner_or_allowed as is_user_owner_or_allowed_common,
-    owner_or_allowed,
+    owner_or_allowed as owner_or_allowed_common,
 )
 
 
 def is_user_owner_or_allowed(token_info: dict, owner: str):
-    return is_user_owner_or_allowed_common(token_info.get("sub"), token_info["scope"], owner)
+    return is_user_owner_or_allowed_common(token_info.get("sub"), token_info.get("scope", ""), owner)
 
 
 def owner_or_allowed(token_info: dict):
-    return owner_or_allowed(token_info["scope"], token_info.get("sub"))
+    return owner_or_allowed_common(token_info.get("sub"), token_info.get("scope", ""))
