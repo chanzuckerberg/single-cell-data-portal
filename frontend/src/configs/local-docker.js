6f5cff449a1ae96bdb51a58e20e6beee42170857
Trent Smith <1429913+Bento007@users.noreply.github.com>
2022-05-03 13:58:53 -0700

    feat(curation api): generate temporary s3 credentials (#2455)
    * Cleaning up the openapi specs
    
    * setup initial function
    
    * Adding credential generation
    
    * Add iam policy and operationId
    
    * Fix access token generation
    
    * adding authentication endpoints for curaton api token.
    
    * undo
    
    * lint
    
    * using role from config
    
    * Log all messages as JSON.
    
    * fix(rdev): increase the frontend memory to 4096MB
    
    * Fixing policy generation
    
    * remove extras
    
    * add logs
    
    * Fix logging and roleSessionName
    
    * limit fmt to directories containing managed code.
    
    * only changing the local docker ports
    
    * Fixing ports
    
    * start tests
    
    * - add secrets to script needed to run local-dev
    - add test cases
    - fix response format
    
    * removing extra auth code
    - using replacing user with token_info in places we need to verify scope.
    - scope can be an array or  a space separated string
    
    * lint
    
    * remove unused exception
    
    * define auth0_domain
    
    * fix tests
    
    * unit test fix
    
    * move authorization functions
    
    * fmt
    
    * moving authorization logic to common so it can be used in the s3 upload lambda
    
    * Update backend/corpora/common/utils/authorization_checks.py
    
    Co-authored-by: Daniel Hegeman <daniel.hegeman@chanzuckerberg.com>
    
    * Update backend/corpora/lambdas/api/v1/authorization.py
    
    Co-authored-by: Daniel Hegeman <daniel.hegeman@chanzuckerberg.com>
    
    * fix: import auth utils from correct file
    
    'is_user_owner_or_allowed' and 'owner_or_allowed' were being imported
    from common/utils/authorization_checks.py when in fact it seems they
    should have been imported from api/v1/authorization.py
    
    * fix: addresses import errors (#2450)
    
    * fix: import module syntax error
    
    * fix: function name masking error
    
    * fix: argument order typo
    
    * use empty string if no scope is provided
    
    Co-authored-by: Trent Smith <trent.smith@chanzuckerberg.com>
    
    * style: lint
    
    * suggested comments
    
    * adding operationId for POST s3-credentials
    
    * chore: added missing scope argument
    
    * chore: made scope optional
    
    * chore: Black format
    
    * chore: forgot the important parameter!
    
    * test fix
    
    * test fix
    
    * test fix
    
    * style: lint fix
    
    * fix tests
    
    * chore: removed unnecessary is_api_call parameter
    
    Co-authored-by: Daniel Hegeman <daniel.hegeman@chanzuckerberg.com>
    Co-authored-by: Kuni <kkatsuya@chanzuckerberg.com>

--- frontend/src/configs/local-docker.js
+++ frontend/src/configs/local-docker.js
@@ -3,7 +3,7 @@ const { PLAUSIBLE_DATA_DOMAIN_STAGING } = require("./common");
 // Temporarily route API calls to the backend app to http://backend
 // instead of http://localhost
 const configs = {
-  API_URL: "http://backend:5000",
+  API_URL: "http://backend:5050",
   PLAUSIBLE_DATA_DOMAIN: PLAUSIBLE_DATA_DOMAIN_STAGING,
 };
 
