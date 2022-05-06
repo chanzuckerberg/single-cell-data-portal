2f4b82cb92bc0fc7ebf40262fe26eac210cbc641
Fran McDade <franmcdade@Frans-MacBook-Pro-2.local>
2022-05-06 17:57:40 +1000

    added ResizeObserver hook for collection description element resizing

--- frontend/src/components/Collection/components/CollectionDescription/index.tsx
+++ frontend/src/components/Collection/components/CollectionDescription/index.tsx
@@ -1,5 +1,6 @@
 import { Intent } from "@blueprintjs/core";
-import React, { useCallback, useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { useResizeObserver } from "src/common/hooks/useResizeObserver";
 import {
   CollectionDescription as Description,
   DescriptionText,
@@ -26,6 +27,8 @@ export default function CollectionDescription({
     EllipsisMode.NONE
   );
   const isEllipsis = ellipsisMode === EllipsisMode.ON;
+  const descriptionRect = useResizeObserver(descriptionRef);
+  const { scrollHeight: descriptionScrollHeight } = descriptionRect || {};
 
   /**
    * Updates the state of ellipsis mode by toggling between "ON" and "OFF" values.
@@ -40,22 +43,13 @@ export default function CollectionDescription({
   };
 
   /**
-   * Updates ellipsis mode state.
+   * Ellipsis mode state updates with changes to the description paragraph scroll height.
    */
-  const onUpdateMode = useCallback((): void => {
-    if (descriptionRef.current) {
-      const el = descriptionRef.current;
-      setEllipsisMode((currentMode) => getEllipsisMode(el, currentMode));
-    }
-  }, []);
-
   useEffect(() => {
-    onUpdateMode(); /* Initialize ellipsis mode. */
-    window.addEventListener("resize", onUpdateMode);
-    return () => {
-      document.removeEventListener("resize", onUpdateMode);
-    };
-  }, [onUpdateMode]);
+    setEllipsisMode((currentMode) =>
+      getEllipsisMode(descriptionScrollHeight, currentMode)
+    );
+  }, [descriptionScrollHeight]);
 
   return (
     <Description data-test-id="collection-description">
@@ -79,29 +73,29 @@ export default function CollectionDescription({
  * - "ON" - paragraph is ellipsis view with the option to toggle view, or
  * - "OFF" - paragraph is in full view with the option to toggle view, or
  * - "NONE" - paragraph is in full view with no option to toggle view as it does not require ellipsis.
- * @param el - description paragraph element.
- * @param currentMode - current ellipsis mode.
- * @returns EllipsisMode
+ * @param elScrollHeight - description paragraph scroll height.
+ * @param currentMode - current ellipsis mode for the description paragraph.
+ * @returns Ellipsis mode for the description paragraph.
  */
 function getEllipsisMode(
-  el: HTMLParagraphElement,
+  elScrollHeight: number | undefined,
   currentMode: EllipsisMode
 ): EllipsisMode {
-  /* Grab the measurement of element height, including hidden content due to overflow. */
-  const elScrollHeight = el.scrollHeight;
-  /* Calculate the number of lines for the content fully rendered. */
-  const elLineCount = elScrollHeight / DESCRIPTION_LINE_HEIGHT_PX;
+  if (elScrollHeight) {
+    /* Calculate the number of lines for the content fully rendered. */
+    const elLineCount = elScrollHeight / DESCRIPTION_LINE_HEIGHT_PX;
 
-  if (elLineCount <= MAX_LINE_COUNT) {
-    /* Element does not have hidden content and line count is within allowable limit. */
-    /* Mode is "NONE" - ellipsis mode not required. */
-    return EllipsisMode.NONE;
-  }
+    if (elLineCount <= MAX_LINE_COUNT) {
+      /* Element does not have hidden content and line count is within allowable limit. */
+      /* Mode is "NONE" - ellipsis mode not required. */
+      return EllipsisMode.NONE;
+    }
 
-  if (currentMode === EllipsisMode.NONE) {
-    /* Line count exceeds allowable limit, and ellipsis mode is "NONE". */
-    /* Change mode to "ON" - ellipsis mode required. */
-    return EllipsisMode.ON;
+    if (currentMode === EllipsisMode.NONE) {
+      /* Line count exceeds allowable limit, and ellipsis mode is "NONE". */
+      /* Change mode to "ON" - ellipsis mode required. */
+      return EllipsisMode.ON;
+    }
   }
 
   return currentMode;
@@ -117,7 +111,6 @@ function getModeText(currentMode: EllipsisMode): string {
   if (currentMode === EllipsisMode.ON) {
     return "Show More";
   }
-
   return "Show Less";
 }
 
