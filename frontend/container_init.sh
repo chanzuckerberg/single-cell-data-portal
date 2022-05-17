cfb69ae5495eab338ed3138169f4a9a691a0ef9f
Trent Smith <trent.smith@chanzuckerberg.com>
2022-05-17 12:29:46 -0700

    remove comments

--- frontend/container_init.sh
+++ frontend/container_init.sh
@@ -4,13 +4,6 @@ echo " ====="
 echo "| starting frontend container"
 echo " ====="
 echo
-#mkdir -p ./node_modules
-#ln -sf /opt/node_app/node_modules/* /opt/node_app/node_modules/.bin ./node_modules/.
-#if [ ! -z "$API_URL" ]; then
-#  cat src/configs/$DEPLOYMENT_STAGE.js | envsubst >src/configs/configs.js
-#else
-#  cp src/configs/local.js src/configs/configs.js
-#fi
 
 # If user passed a command line, run it in place of the server
 if [ $# -ne 0 ]; then
