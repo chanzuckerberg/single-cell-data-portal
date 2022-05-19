405f79f0f87ce35b9aace6991e60d161ddf70585
Trent Smith <trent.smith@chanzuckerberg.com>
2022-05-19 14:17:15 -0700

    Add POST POST /curation/collections/{public_collection_uuid}/revision
    - Add tests
    - remove path_public_collection_uuid from curation-api.yml to simplify integration

---
+++ tests/unit/backend/corpora/api_server/curator/collection/test_curation_revision.py
@@ -0,0 +1,41 @@
+from backend.corpora.common.corpora_orm import CollectionVisibility
+from tests.unit.backend.corpora.api_server.base_api_test import BaseAuthAPITest
+
+
+class TestPostRevision(BaseAuthAPITest):
+    def test__post_revision__no_auth(self):
+        collection_uuid = self.generate_collection(self.session, visibility=CollectionVisibility.PUBLIC.name).id
+        response = self.app.post(f"/curation/v1/collections/{collection_uuid}/revision")
+        self.assertEqual(401, response.status_code)
+
+    def test__post_revision__Not_Public(self):
+        collection_uuid = self.generate_collection(self.session).id
+        headers = {"Authorization": "Bearer " + self.make_super_curator_token(), "Content-Type": "application/json"}
+        response = self.app.post(f"/curation/v1/collections/{collection_uuid}/revision", headers=headers)
+        self.assertEqual(403, response.status_code)
+
+    def test__post_revision__Not_Owner(self):
+        collection_uuid = self.generate_collection(
+            self.session, visibility=CollectionVisibility.PUBLIC.name, owner="someone else"
+        ).id
+        response = self.app.post(
+            f"/curation/v1/collections/{collection_uuid}/revision",
+            headers=self.get_auth_headers(),
+        )
+        self.assertEqual(403, response.status_code)
+
+    def test__post_revision__OK(self):
+        collection_uuid = self.generate_collection(self.session, visibility=CollectionVisibility.PUBLIC.name).id
+        response = self.app.post(
+            f"/curation/v1/collections/{collection_uuid}/revision",
+            headers=self.get_auth_headers(),
+        )
+        self.assertEqual(201, response.status_code)
+        self.assertNotEqual(collection_uuid, response.json["revision_id"])
+
+    def test__post_revision__Super_Curator(self):
+        collection_uuid = self.generate_collection(self.session, visibility=CollectionVisibility.PUBLIC.name).id
+        headers = {"Authorization": "Bearer " + self.make_super_curator_token(), "Content-Type": "application/json"}
+        response = self.app.post(f"/curation/v1/collections/{collection_uuid}/revision", headers=headers)
+        self.assertEqual(201, response.status_code)
+        self.assertNotEqual(collection_uuid, response.json["revision_id"])
