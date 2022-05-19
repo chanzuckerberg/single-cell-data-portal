e8337bf912260963a7d12b5a51c4a86dec0099b6
Seve Badajoz <sbadajoz@chanzuckerberg.com>
2022-05-19 11:04:31 -0700

    rework the path generation for navigation sidebar

--- frontend/src/pages/docs/[...slug].tsx
+++ frontend/src/pages/docs/[...slug].tsx
@@ -1,75 +1,128 @@
-// const DOC_SITE_FOLDER_NAME = "doc-site";
+import fs from "fs";
+import matter from "gray-matter";
+import { GetStaticPaths } from "next";
+import { MDXRemote, MDXRemoteSerializeResult } from "next-mdx-remote";
+import { serialize } from "next-mdx-remote/serialize";
+import Image from "next/image";
+import pathTool from "path";
 
-// function recursiveFileGrab(...root: Array<string>): Array<Array<string>> {
-//   const paths = fs.readdirSync(pathTool.join(DOC_SITE_FOLDER_NAME, ...root));
-//   return paths.reduce((acc, path) => {
-//     const builtFullPath = pathTool.join(DOC_SITE_FOLDER_NAME, ...root, path);
-//     if (fs.lstatSync(builtFullPath).isFile()) {
-//       if (path.endsWith(".mdx")) acc.push([...root, path]);
-//     } else {
-//       acc.push(...recursiveFileGrab(...root, path));
-//     }
-//     return acc;
-//   }, new Array<Array<string>>());
-// }
+const DOC_SITE_FOLDER_NAME = "doc-site";
 
-// export const getStaticPaths: GetStaticPaths = async () => {
-//   const filePath = recursiveFileGrab();
-//   const paths = filePath.map((filePath) => {
-//     const slug = filePath;
-//     slug[slug.length - 1] = slug[slug.length - 1].replace(".mdx", "");
-//     return {
-//       params: {
-//         slug,
-//       },
-//     };
-//   });
-//   return {
-//     fallback: false,
-//     paths,
-//   };
-// };
-
-// export const getStaticProps = async ({
-//   params: { slug },
-// }: {
-//   params: { slug: Array<string> };
-// }): Promise<{
-//   props: Props;
-// }> => {
-//   const markdownWithMeta = fs.readFileSync(
-//     pathTool.join("doc-site", slug.join("/") + ".mdx"),
-//     "utf-8"
-//   );
-//   const { data: frontMatter, content } = matter(markdownWithMeta);
-//   const mdxSource = await serialize(content);
-//   return {
-//     props: {
-//       frontMatter,
-//       mdxSource,
-//       slug,
-//     },
-//   };
-// };
-
-// const components = { Image };
-
-// interface Props {
-//   frontMatter: Record<string, any>;
-//   mdxSource: MDXRemoteSerializeResult;
-//   slug: Array<string>;
-// }
-
-// const BlogPage = ({ mdxSource }: Props) => {
-//   return (
-//     <>
-//       <MDXRemote {...mdxSource} components={components} />
-//     </>
-//   );
-// };
-
-function BlogPage(): JSX.Element {
-  return <div />;
+interface Directory {
+  dirName: string;
+  files: Array<string>;
+  subDirectories: Array<Directory>;
 }
+
+const CACHED_FILE_PATHS = new Map<Array<string>, Directory>();
+function filePaths(...root: Array<string>): Directory {
+  const cacheStore = CACHED_FILE_PATHS && CACHED_FILE_PATHS.get(root);
+  if (cacheStore) return cacheStore;
+
+  // const paths = fs.readdirSync(pathTool.join(DOC_SITE_FOLDER_NAME, ...root));
+  // const builtPaths = paths.reduce((acc, path) => {
+  //   const builtFullPath = pathTool.join(DOC_SITE_FOLDER_NAME, ...root, path);
+  //   if (fs.lstatSync(builtFullPath).isFile()) {
+  //     if (path.endsWith(".mdx")) acc.push([...root, path]);
+  //   } else {
+  //     acc.push(...filePaths(...root, path));
+  //   }
+  //   return acc;
+  // }, {} as Directory);};
+  const newDirectory = {
+    dirName: root[root.length - 1] ?? "",
+    files: [],
+    subDirectories: [],
+  } as Directory;
+
+  const paths = fs.readdirSync(pathTool.join(DOC_SITE_FOLDER_NAME, ...root));
+  paths.forEach((path) => {
+    const builtFullPath = pathTool.join(DOC_SITE_FOLDER_NAME, ...root, path);
+    if (fs.lstatSync(builtFullPath).isFile()) {
+      if (path.endsWith(".mdx"))
+        newDirectory.files.push(path.replace(".mdx", ""));
+    } else {
+      newDirectory.subDirectories.push(filePaths(...root, path));
+    }
+  });
+  CACHED_FILE_PATHS.set(root, newDirectory);
+  return newDirectory;
+}
+
+// need to fix the index path
+// There are incorrect paths that are reminants from previous recursive calls
+function generatePaths(
+  directory: Directory,
+  slugs = new Array<{ params: { slug: Array<string> } }>(),
+  indexPath = new Array<string>()
+): Array<{ params: { slug: Array<string> } }> {
+  const newIndexPath = [...indexPath];
+  if (directory.dirName) newIndexPath.push(directory.dirName);
+  directory.files.forEach((file) => {
+    slugs.push({ params: { slug: [...newIndexPath, file] } });
+  });
+  if (directory.subDirectories.length > 0) {
+    directory.subDirectories.forEach((subDirectory) => {
+      slugs.push(...generatePaths(subDirectory, slugs, newIndexPath));
+    });
+  }
+  return slugs;
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const filePath = filePaths();
+  // console.log(filePath);
+  const paths = generatePaths(filePath);
+  paths.forEach((path) => {
+    console.log(path.params.slug);
+  });
+
+  return {
+    fallback: false,
+    paths,
+  };
+};
+
+export const getStaticProps = async ({
+  params: { slug },
+}: {
+  params: { slug: Array<string> };
+}): Promise<{
+  props: Props;
+}> => {
+  const markdownWithMeta = fs.readFileSync(
+    pathTool.join("doc-site", slug.join("/") + ".mdx"),
+    "utf-8"
+  );
+  const { data: frontMatter, content } = matter(markdownWithMeta);
+  const mdxSource = await serialize(content);
+  return {
+    props: {
+      frontMatter,
+      mdxSource,
+      slug,
+    },
+  };
+};
+
+const components = { Image };
+
+interface Props {
+  frontMatter: Record<string, any>;
+  mdxSource: MDXRemoteSerializeResult;
+  slug: Array<string>;
+}
+
+const PageNavigator = ({ pages }) => {
+  return <div></div>;
+};
+
+const BlogPage = ({ mdxSource }: Props) => {
+  return (
+    <>
+      <MDXRemote {...mdxSource} components={components} />
+    </>
+  );
+};
 
 export default BlogPage;
