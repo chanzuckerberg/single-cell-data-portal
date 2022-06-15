5711e5eaf525803cb8ef82cceb4bcb2800fb344f
Seth Feingold <sfeingold1104@gmail.com>
2022-06-15 17:10:37 -0500

    updating blue color on lander and header cta to primary 400 - #0073ff

--- frontend/src/pages/landing-page/icons/cellxgene-icon-small.tsx
+++ frontend/src/pages/landing-page/icons/cellxgene-icon-small.tsx
@@ -18,9 +18,9 @@ const CellxgeneIconSmall = (props: IconProps): JSX.Element => {
         fillRule="evenodd"
         clipRule="evenodd"
         d="M20 0V20H0V0H20ZM16.6667 7.5H7.5V16.6667H16.6667V7.5ZM5.83333 7.5H3.33333V16.6667H5.83333V7.5ZM16.6667 3.33333H7.5V5.83333H16.6667V3.33333Z"
-        fill="#3867FA"
+        fill="#0073ff"
       />
-      <path d="M14.1667 10H10V14.1667H14.1667V10Z" fill="#3867FA" />
+      <path d="M14.1667 10H10V14.1667H14.1667V10Z" fill="#0073ff" />
     </svg>
   );
 };
