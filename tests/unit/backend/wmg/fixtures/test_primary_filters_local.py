372d6f3ebabb198fac86caa6aa43823425ed3f07
Alexander Tarashansky <atarashansky@CZIMACOS3990.local>
2022-06-17 11:36:38 -0700

    simplify filter for test

--- tests/unit/backend/wmg/fixtures/test_primary_filters_local.py
+++ tests/unit/backend/wmg/fixtures/test_primary_filters_local.py
@@ -1,42 +1,27 @@
 test_organism_terms = [
     {"organism_ontology_term_id_0": "Mus musculus"},
-    {"organism_ontology_term_id_1": "Callithrix jacchus"},
-    {"organism_ontology_term_id_2": "Homo sapiens"}
+    {"organism_ontology_term_id_1": "Homo sapiens"}
 ]
 
 test_tissue_terms = {
     "organism_ontology_term_id_0": [
         {"tissue_ontology_term_id_0": "primary motor cortex"},
         {"tissue_ontology_term_id_1": "urethra"},
-        {"tissue_ontology_term_id_2": "large intestine"},
     ],
     "organism_ontology_term_id_1": [
         {"tissue_ontology_term_id_0": "primary motor cortex"},
         {"tissue_ontology_term_id_1": "urethra"},
-        {"tissue_ontology_term_id_2": "large intestine"},
-    ],
-    "organism_ontology_term_id_2": [
-        {"tissue_ontology_term_id_0": "primary motor cortex"},
-        {"tissue_ontology_term_id_1": "urethra"},
-        {"tissue_ontology_term_id_2": "large intestine"},
     ],
 }
 test_gene_terms = {
     "organism_ontology_term_id_0": [
         {"gene_ontology_term_id_0": "TSPAN6"},
         {"gene_ontology_term_id_1": "TNMD"},
-        {"gene_ontology_term_id_2": "SCYL3"},
     ],
     "organism_ontology_term_id_1": [
         {"gene_ontology_term_id_0": "TSPAN6"},
         {"gene_ontology_term_id_1": "TNMD"},
-        {"gene_ontology_term_id_2": "SCYL3"},
     ],
-    "organism_ontology_term_id_2": [
-        {"gene_ontology_term_id_0": "TSPAN6"},
-        {"gene_ontology_term_id_1": "TNMD"},
-        {"gene_ontology_term_id_2": "SCYL3"},
-    ],        
 }
 
 test_snapshot_id = "test-snapshot-id"
