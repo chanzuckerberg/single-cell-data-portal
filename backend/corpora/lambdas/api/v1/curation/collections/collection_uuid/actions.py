39e6e938c39e5feb316709abbef9e2f1f0823b05
Daniel Hegeman <daniel.hegeman@chanzuckerberg.com>
2022-06-22 11:15:18 -0700

    feat: GET /v1/collections/{collection_uuid} (#2707)
    * feat: try get_collections_list for GET /collections
    
    * package organism as array
    
    * remove logging
    
    * add docstrings
    
    * fix function rename typo
    
    * update tests
    
    * rename Collection class method
    
    * try getting root path
    
    * take 2: log root path
    
    * determine collections url in modular fashion, including rdev
    
    * refactor: use TransformingBase::to_dict for transforming objects
    
    * fix: nullability of attrs in link api schema
    
    Also: refactor tests
    
    * fix: visibility and auth
    
    - visibility should default to PUBLIC
    - raise UnauthorizedError if no credentials accompany a PRIVATE request
    
    * fix: update test for private query with no auth - 401
    
    * fix: update API description
    
    * add processing_status to response
    
    * suggested changes
    
    * rename list method for Collection
    
    * move collections_base_url logic to config default template
    
    * refactor and rename tests
    
    * perform status subtest on private Collections
    
    Public Collections cannot have PENDING dataset processing status
    
    * feat: add GET /collections/{collection_uuid}
    
    * refactor: ignore line length
    
    * Dataset response shape improvements
    
    * consolidate logic, add access_type to response
    
    * flesh out tests
    
    * remove logging statement
    
    * refactor: small nits, readability
    
    * refactor: tests
    
    * refactor: move handlers into appropriate files
    
    * refactor: pull column selectors used by API out to common.py
    
    Co-authored-by: Trent Smith <trent.smith@chanzuckerberg.com>

--- backend/corpora/lambdas/api/v1/curation/collections/collection_uuid/actions.py
+++ backend/corpora/lambdas/api/v1/curation/collections/collection_uuid/actions.py
@@ -1,9 +1,12 @@
-from flask import g
+from flask import g, jsonify
 
+from ..common import EntityColumns
 from backend.corpora.api_server.db import dbconnect
 from backend.corpora.common.corpora_orm import CollectionVisibility
-from backend.corpora.common.utils.http_exceptions import MethodNotAllowedException
+from backend.corpora.common.entities import Collection
+from backend.corpora.common.utils.http_exceptions import MethodNotAllowedException, NotFoundHTTPException
 from backend.corpora.lambdas.api.v1.authorization import owner_or_allowed
+from backend.corpora.lambdas.api.v1.curation.collections.common import reshape_for_curation_api_and_is_allowed
 from backend.corpora.lambdas.api.v1.common import get_collection_else_forbidden
 
 
@@ -16,3 +19,16 @@ def delete(collection_uuid: str, token_info: dict):
     else:
         collection.delete()
     return "", 204
+
+
+@dbconnect
+def get(collection_uuid: str, token_info: dict):
+    db_session = g.db_session
+    collection = Collection.get_collection(db_session, collection_uuid, include_tombstones=True)
+    if not collection:
+        raise NotFoundHTTPException
+    collection_response: dict = collection.to_dict_keep(EntityColumns.columns_for_collection_uuid)
+
+    reshape_for_curation_api_and_is_allowed(collection_response, token_info, uuid_provided=True)
+
+    return jsonify(collection_response)
