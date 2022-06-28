8fdc926d28c66f433f5fdb5b943980b195a8e490
Madison Dunitz <madison.dunitz@chanzuckerberg.com>
2022-06-28 14:45:39 +0200

    pipeline skeleton

---
+++ backend/corpus_asset_pipelines/integrated_corpus/job.py
@@ -0,0 +1,20 @@
+import logging
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+
+def _extract():
+    pass
+
+
+def _transform():
+    pass
+
+
+def _load():
+    pass
+
+
+def run():
+    passss
