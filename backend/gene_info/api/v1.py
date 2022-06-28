9f3750134530090e1baf4c8f22d60e979f6ea359
Siena Cizdziel <scizdziel@CZIMACOS4051.local>
2022-06-28 08:58:34 -0700

    requested changes

--- backend/gene_info/api/v1.py
+++ backend/gene_info/api/v1.py
@@ -1,6 +1,5 @@
 import string
 from flask import jsonify, make_response
-import xml.etree.ElementTree as ET
 from backend.gene_info.api.ncbi_provider import NCBIAPIException, NCBIProvider, NCBIUnexpectedResultException
 
 
@@ -20,7 +19,7 @@ def gene_info(geneID: string):
         gene_info_tree = provider.fetch_gene_info_tree(uid)
     except NCBIAPIException:
         return make_response(jsonify("Failed fetch of NCBI database, API key issue"), 404)
-    gene_info = parse_gene_info_tree(gene_info_tree)
+    gene_info = provider.parse_gene_info_tree(gene_info_tree)
     return make_response(
         jsonify(
             dict(
@@ -31,30 +30,4 @@ def gene_info(geneID: string):
             )
         ),
         200,
-    )
-
-
-def parse_gene_info_tree(tree_response):
-    # parse NCBI XML response into relevant values to return by gene_info API
-    result_tree = ET.ElementTree(ET.fromstring(tree_response))
-    root = result_tree.getroot()
-    synonyms = []
-    summary = ""
-    name = ""
-    if len(root) > 0:
-        for x in root[0]:
-            if x.tag == "Entrezgene_summary":
-                summary = x.text
-            elif x.tag == "Entrezgene_gene":
-                if len(x) > 0:
-                    for y in x[0]:
-                        if y.tag == "Gene-ref_desc":
-                            name = y.text
-                        elif y.tag == "Gene-ref_syn":
-                            for syn in y:
-                                synonyms.append(syn.text)
-    return dict(
-        name=name,
-        summary=summary,
-        synonyms=synonyms,
     )
