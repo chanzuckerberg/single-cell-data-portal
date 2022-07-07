1bcff2892c61b62667873b84a92d4aeeba4ec8f4
Andrew Tolopko <atolopko@chanzuckerberg.com>
2022-07-07 14:58:56 -0400

    add s3 data env mirroring to deployment GHA

--- backend/scripts/mirror_deploy_env_s3_data.sh
+++ backend/scripts/mirror_deploy_env_s3_data.sh
@@ -12,9 +12,16 @@ if [[ ! "$DEST_DEPLOY_ENV" =~ ^(dev|staging)$ ]]; then
 fi
 
 # TODO: Remove `--copy-props metadata-directive` once IAM roles have {Get,Put}ObjectTagging perms added
-S3_SYNC_CMD="aws s3 sync --copy-props metadata-directive --no-progress"
+# TODO: Remove --dryrun after testing
+S3_SYNC_CMD="aws s3 sync --delete --copy-props metadata-directive --no-progress --dryrun"
 
-# TODO: Read full source buckets and use real dest buckets
+
+# TODO: Uncomment after testing with below commands
+# $S3_SYNC_CMD s3://cellxgene-db-dump-prod/ s3://cellxgene-db-dump-${DEST_DEPLOY_ENV}/
+# $S3_SYNC_CMD s3://corpora-data-prod/ s3://corpora-data-${DEST_DEPLOY_ENV}/
+# $S3_SYNC_CMD s3://hosted-cellxgene-prod/ s3://hosted-cellxgene-${DEST_DEPLOY_ENV}/
+# $S3_SYNC_CMD s3://cellxgene-wmg-prod/ s3://cellxgene-wmg-${DEST_DEPLOY_ENV}/
+$S3_SYNC_CMD s3://cellxgene-db-dump-prod/ s3://atolopko-tmp/cellxgene-db-dump-${DEST_DEPLOY_ENV}/
 $S3_SYNC_CMD s3://corpora-data-prod/0b696cf4-513e-4e59-b6ac-78d76409e6f8/ s3://atolopko-tmp/corpora-data-${DEST_DEPLOY_ENV}/
 $S3_SYNC_CMD s3://hosted-cellxgene-prod/00099d5e-154f-4a7a-aa8d-fa30c8c0c43c.cxg/ s3://atolopko-tmp/hosted-cellxgene-${DEST_DEPLOY_ENV}/
 $S3_SYNC_CMD s3://cellxgene-wmg-prod/1651599970/ s3://atolopko-tmp/cellxgene-wmg-${DEST_DEPLOY_ENV}/
