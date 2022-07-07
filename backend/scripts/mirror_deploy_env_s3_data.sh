b2bb7c8a103b6624618791e210624b2038aa44ee
Andrew Tolopko <atolopko@chanzuckerberg.com>
2022-07-07 11:57:19 -0400

    rm --dryrun, TODO, names

--- backend/scripts/mirror_deploy_env_s3_data.sh
+++ backend/scripts/mirror_deploy_env_s3_data.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Sync S3 data from production environment to a specified destination environment
+# Sync S3 data from production environment to a specified destination deployment environment
 
 set -e
 
@@ -11,8 +11,10 @@ if [[ ! "$DEST_DEPLOY_ENV" =~ ^(dev|staging)$ ]]; then
   exit 1
 fi
 
-S3_SYNC_CMD="aws s3 sync --copy-props metadata-directive --no-progress --dryrun"
+# TODO: Remove `--copy-props metadata-directive` once IAM roles have {Get,Put}ObjectTagging perms added
+S3_SYNC_CMD="aws s3 sync --copy-props metadata-directive --no-progress"
 
+# TODO: Read full source buckets and use real dest buckets
 $S3_SYNC_CMD s3://corpora-data-prod/0b696cf4-513e-4e59-b6ac-78d76409e6f8/ s3://atolopko-tmp/corpora-data-${DEST_DEPLOY_ENV}/
 $S3_SYNC_CMD s3://hosted-cellxgene-prod/00099d5e-154f-4a7a-aa8d-fa30c8c0c43c.cxg/ s3://atolopko-tmp/hosted-cellxgene-${DEST_DEPLOY_ENV}/
 $S3_SYNC_CMD s3://cellxgene-wmg-prod/1651599970/ s3://atolopko-tmp/cellxgene-wmg-${DEST_DEPLOY_ENV}/
