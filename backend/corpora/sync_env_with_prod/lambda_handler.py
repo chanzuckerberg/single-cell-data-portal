3ae7d4b701d000d9978b40f9c5188effa44eb1b4
Andrew Tolopko <atolopko@chanzuckerberg.com>
2022-07-12 14:41:31 -0400

    fmt

--- backend/corpora/sync_env_with_prod/lambda_handler.py
+++ backend/corpora/sync_env_with_prod/lambda_handler.py
@@ -5,9 +5,9 @@ from backend.corpora.sync_env_with_prod.load_db import load_db
 
 def run(event, context):
     action = event.get("action")
-    if action == 'dump':
+    if action == "dump":
         copy_relational_db(event, context)
-    elif action == 'load':
+    elif action == "load":
         load_db(CorporaDbConfig().database_uri, event.get("db_dump_s3_bucket"), event.get("db_dump_s3_key"))
     else:
         return {"statusCode": 400}
