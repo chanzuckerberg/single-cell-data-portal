8aecd67e169578b966da3eb19579b95f3482b974
Nayib Gloria <55710092+nayib-jose-gloria@users.noreply.github.com>
2022-07-14 11:22:30 -0400

    feat: Use distinct UploadKeyPrefix for super curators (#2846)
    * Improve readability of dataset processing failure Slack alert by using code block
    
    * fix makefile command typo in local dev doc
    
    * merge with origin main
    
    * generate distinct UploadKeyPrefix for super curators fetching credentials
    
    * add and use is_super_curator util function
    
    * fix tests
    
    * lint fix

--- backend/corpora/common/utils/authorization_checks.py
+++ backend/corpora/common/utils/authorization_checks.py
@@ -8,15 +8,19 @@ def has_scope(required_scope: str, scope: Union[list, str]) -> bool:
     return True if required_scope in scopes else False
 
 
+def is_super_curator(scope: Union[list, str]) -> bool:
+    return has_scope(CorporaConstants.SUPER_CURATOR_SCOPE, scope)
+
+
 def is_user_owner_or_allowed(user: str, scope: Union[list, str], owner: str) -> bool:
     """
     Check if the user has ownership on a collection, or if it has superuser permissions
     """
-    return user == owner or (has_scope(CorporaConstants.SUPER_CURATOR_SCOPE, scope))
+    return user == owner or is_super_curator(scope)
 
 
 def owner_or_allowed(user: str, scope: Union[list, str]) -> Optional[str]:
     """
     Returns None if the user is superuser, `user` otherwise. Used for SQL Query where conditions
     """
-    return None if has_scope(CorporaConstants.SUPER_CURATOR_SCOPE, scope) else user
+    return None if is_super_curator(scope) else user
