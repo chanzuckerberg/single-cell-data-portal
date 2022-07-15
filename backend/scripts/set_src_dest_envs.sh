2ab2b31f66d7566ec2a6924a26fa3aed20b2d646
Andrew Tolopko <atolopko@chanzuckerberg.com>
2022-07-15 14:50:22 -0400

    make allowwed src/dev env combinations explicit

--- backend/scripts/set_src_dest_envs.sh
+++ backend/scripts/set_src_dest_envs.sh
@@ -3,17 +3,24 @@
 export SRC_ENV=${1:-prod}
 export DEST_ENV=${2:-dev}
 
-if [[ ! "$SRC_ENV" =~ ^(dev|staging|prod)$ ]]; then
-  echo ERROR: invalid value for source deployment env: \"$SRC_ENV\"
-  exit 1
+# allow prod->dev and prod->staging
+if [[ "$SRC_ENV" == prod && "$DEST_ENV" =~ ^(dev|staging)$ ]]; then
+    exit 0
 fi
 
-if [[ ! "$DEST_ENV" =~ ^(dev|staging)$ ]]; then
-  echo ERROR: invalid value for destination deployment env: \"$DEST_ENV\"
-  exit 1
+# allow dev->dev for testing
+if [[ "$SRC_ENV" == dev && "$DEST_ENV" == dev ]]; then
+    read -n 1 -p "ATTENTION: You are mirroring the dev env to itself. Useful for testing the mirroring operation, but otherwise useless. Proceed? (Y/n) " ANS
+    [[ $ANS == 'Y' ]] || exit 1
+    exit 0
 fi
 
-if [[ "$DEST_ENV" == staging && "$SRC_ENV" == dev ]]; then
-  echo ERROR: cannot mirror from $SRC_ENV to $DEST_ENV
-  exit 1
+# allow staging->dev for testing
+if [[ "$SRC_ENV" == staging && "$DEST_ENV" == dev ]]; then
+    read -n 1 -p "ATTENTION: You are mirroring the staging env to dev. Useful for testing fixes in dev for data-related bugs found in staging, but otherwise useless. Proceed? (Y/n) " ANS
+    [[ $ANS == 'Y' ]] || exit 1
+    exit 0
 fi
+
+echo "ERROR: invalid value src/dest envs: $SRC_ENV -> $DEST_ENV"
+exit 1
