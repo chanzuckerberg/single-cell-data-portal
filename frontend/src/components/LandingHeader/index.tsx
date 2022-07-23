4c1bbf42ce4bba9fe72d3126bc19be2d74038d68
Timmy Huang <thuang@chanzuckerberg.com>
2022-07-22 17:15:48 -0700

    chore(analytics): Add analytics to Landing Page

--- frontend/src/components/LandingHeader/index.tsx
+++ frontend/src/components/LandingHeader/index.tsx
@@ -76,6 +76,9 @@ const LandingHeader: FC = () => {
                 <LinkWrapper>
                   <Link href={ROUTES.HOMEPAGE} passHref>
                     <AnchorButton
+                      onClick={() => {
+                        track(EVENTS.DATASETS_CLICK_NAV);
+                      }}
                       active={isRouteActive(pathname, ROUTES.HOMEPAGE)}
                       href="passHref"
                       minimal
@@ -86,6 +89,9 @@ const LandingHeader: FC = () => {
                 <LinkWrapper>
                   <Link href={ROUTES.COLLECTIONS} passHref>
                     <AnchorButton
+                      onClick={() => {
+                        track(EVENTS.COLLECTIONS_CLICK_NAV);
+                      }}
                       active={isRouteActive(pathname, ROUTES.COLLECTIONS)}
                       href="passHref"
                       minimal
@@ -111,6 +117,11 @@ const LandingHeader: FC = () => {
               {/* CHANGE TO "/" ON PROD */}
               {pathname === "/landing-page" && (
                 <HiringLink
+                  onClick={() => {
+                    track(EVENTS.BROWSE_CAREERS_CLICKED, {
+                      button: "we're hiring",
+                    });
+                  }}
                   href="https://chanzuckerberg.com/careers/career-opportunities/?team=data,design,engineering,product,technical-program-management&initiative=science&gh_src=20d9f28d1us"
                   target="_blank"
                   rel="noopener"
@@ -132,6 +143,9 @@ const LandingHeader: FC = () => {
               )}
               <LinkWrapper>
                 <AnchorButton
+                  onClick={() => {
+                    track(EVENTS.BROWSE_DOCUMENTATION_CLICKED);
+                  }}
                   active={isRouteActive(pathname, ROUTES.DOCS)}
                   href={ROUTES.DOCS}
                   rel="noopener"
