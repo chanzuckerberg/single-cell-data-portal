7407861f37e6accfb2d1458db46ce80d4ec0332a
ashin-czi <ashin@contractor.chanzuckerberg.com>
2022-08-02 12:02:05 -0400

    docs: scExpression page title rebrand change

--- frontend/src/views/WheresMyGene/components/Main/index.tsx
+++ frontend/src/views/WheresMyGene/components/Main/index.tsx
@@ -237,7 +237,7 @@ export default function WheresMyGene(): JSX.Element {
   return (
     <>
       <Head>
-        <title>cellxgene | scExpression</title>
+        <title>CellxGene | scExpression</title>
       </Head>
 
       <SideBar
