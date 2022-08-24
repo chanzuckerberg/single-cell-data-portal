27e544d7335f334f9d010cec785e94dd210f7265
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2022-08-24 15:42:23 +1000

    feat: Add omnisearch to tissue filter (#2921)

--- frontend/src/components/common/Filter/components/FilterContent/components/FilterViews/style.ts
+++ frontend/src/components/common/Filter/components/FilterContent/components/FilterViews/style.ts
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export const ViewsMenu = styled.span`
   display: grid;
+  flex: 1;
   grid-auto-columns: 1fr auto;
   grid-auto-flow: column;
   padding: 6px;
