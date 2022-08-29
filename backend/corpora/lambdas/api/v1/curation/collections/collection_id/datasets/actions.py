352ceda9fc895cd1b92aee557faac3e4f7735ae1
Trent Smith <trent.smith@chanzuckerberg.com>
2022-08-29 11:05:30 -0700

    Add consistent error message when collection uuid is invalid

--- backend/corpora/lambdas/api/v1/curation/collections/collection_id/datasets/actions.py
+++ backend/corpora/lambdas/api/v1/curation/collections/collection_id/datasets/actions.py
@@ -13,8 +13,8 @@ from backend.corpora.common.utils.regex import DATASET_ID_REGEX, CURATOR_TAG_PRE
 from backend.corpora.lambdas.api.v1.authorization import owner_or_allowed
 from backend.corpora.lambdas.api.v1.common import (
     get_dataset_else_error,
-    get_collection_else_forbidden,
     delete_dataset_common,
+    get_collection_else_forbidden,
 )
 from backend.corpora.lambdas.api.v1.curation.collections.common import EntityColumns, reshape_dataset_for_curation_api
 
