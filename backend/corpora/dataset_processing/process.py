fbf377663d7b5160ddb8419e8b8b6f7ff48939b6
nayib-jose-gloria <ngloria@chanzuckerberg.com>
2022-09-07 15:19:21 -0400

    feat: don't retry non-transient dataset processing failures

--- backend/corpora/dataset_processing/process.py
+++ backend/corpora/dataset_processing/process.py
@@ -670,7 +670,6 @@ def main():
     log_batch_environment()
     dataset_id = os.environ["DATASET_ID"]
     step_name = os.environ["STEP_NAME"]
-    is_last_attempt = os.environ["AWS_BATCH_JOB_ATTEMPT"] == os.getenv("MAX_ATTEMPTS", "1")
     return_value = 0
     logger.info(f"Processing dataset {dataset_id}")
     try:
@@ -703,14 +702,21 @@ def main():
         cancel_dataset(dataset_id)
     except (ValidationFailed, ProcessingFailed, ConversionFailed) as e:
         (status,) = e.args
-        if is_last_attempt:
-            update_db(dataset_id, processing_status=status)
+        update_db(dataset_id, processing_status=status)
         logger.exception("An Error occurred while processing.")
         return_value = 1
     except Exception as e:
         (status,) = e.args
-        if is_last_attempt and isinstance(status, dict):
+        if isinstance(status, dict):
             update_db(dataset_id, processing_status=status)
+        else:
+            if step_name == "download-validate":
+                update_db(dataset_id, processing_status={"validation_status": ValidationStatus.INVALID,
+                                                         "validation_message": e})
+            elif step_name == "seurat":
+                update_db(dataset_id, processing_status={"rds_status": ConversionStatus.FAILED})
+            elif step_name == "cxg":
+                update_db(dataset_id, processing_status={"cxg_status": ConversionStatus.FAILED})
         logger.exception(f"An unexpected error occurred while processing the data set: {e}")
         return_value = 1
 
