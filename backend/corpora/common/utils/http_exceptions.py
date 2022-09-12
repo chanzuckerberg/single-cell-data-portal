911a498b20e5814ab167921a5f78ae21722e891b
Trent Smith <trent.smith@chanzuckerberg.com>
2022-09-12 10:47:17 -0700

    update the name of the default detail in error message

--- backend/corpora/common/utils/http_exceptions.py
+++ backend/corpora/common/utils/http_exceptions.py
@@ -22,10 +22,10 @@ class TooLargeHTTPException(ProblemException):
 
 
 class InvalidParametersHTTPException(ProblemException):
-    _detail = "One or more parameters is invalid."
+    _default_detail = "One or more parameters is invalid."
 
     def __init__(self, detail: str = None, *args, **kwargs) -> None:
-        detail = detail if detail else self._detail
+        detail = detail if detail else self._default_detail
         super().__init__(status=400, title="Bad Request", detail=detail, *args, **kwargs)
 
 
