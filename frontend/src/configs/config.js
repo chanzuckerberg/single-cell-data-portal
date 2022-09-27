64939fb1d5cdfea3e519a0f1a03157579592203d
Jake Heath <jheath@chanzuckerberg.com>
2022-09-27 11:48:09 -0700

    fix: add service platforms to docker-compose

---
+++ frontend/src/configs/config.js
@@ -0,0 +1,9 @@
+const { PLAUSIBLE_DATA_DOMAIN_STAGING } = require("./common");
+
+const configs = {
+  API_URL: "https://wmg-test-backend.rdev.single-cell.czi.technology",
+  PLAUSIBLE_DATA_DOMAIN: PLAUSIBLE_DATA_DOMAIN_STAGING,
+  SENTRY_DEPLOYMENT_ENVIRONMENT: "staging",
+};
+
+if (typeof module !== "undefined") module.exports = configs;
