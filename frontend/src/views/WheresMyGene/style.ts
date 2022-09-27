7491c988328081bae292081441e54f9453218c62
ashin-czi <109984998+ashin-czi@users.noreply.github.com>
2022-09-27 09:44:38 -0400

    Feat/wmg download 2 (#3320)
    * feat: skeleton placeholders
    
    * feat: moving organism dropdown to left sidebar
    
    * fix: Adding back right sidebar with sourcedata

--- frontend/src/views/WheresMyGene/style.ts
+++ frontend/src/views/WheresMyGene/style.ts
@@ -7,16 +7,11 @@ import { FOOTER_HEIGHT_PX } from "src/components/Footer/style";
 import { HEADER_HEIGHT_PX } from "src/components/Header/style";
 import { SidebarMainWrapper } from "src/components/Layout/style";
 
-// (thuang): This is needed to prevent the footer covering the heatmap when the
-// viewport height is shorter than the full height.
-const WRAPPER_BOTTOM_OFFSET_PX = 20;
-
 export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   flex: 1;
   height: calc(100vh - ${HEADER_HEIGHT_PX}px - ${FOOTER_HEIGHT_PX}px);
-  padding-bottom: ${WRAPPER_BOTTOM_OFFSET_PX}px;
 `;
 
 export const Gap = styled.div`
@@ -30,11 +25,11 @@ export const Top = styled.div`
 `;
 
 export const SideBarWrapper = styled(SideBar)`
-  max-height: calc(100vh - ${HEADER_HEIGHT_PX}px - ${FOOTER_HEIGHT_PX}px);
+  max-height: calc(100vh - ${HEADER_HEIGHT_PX}px);
 `;
 
 export const SideBarPositioner = styled(RawSideBarPositioner)`
-  max-height: calc(100vh - ${HEADER_HEIGHT_PX}px - ${FOOTER_HEIGHT_PX}px);
+  max-height: calc(100vh - ${HEADER_HEIGHT_PX}px);
 `;
 
 export const SideBarLayoutMainWrapper = styled(SidebarMainWrapper)`
