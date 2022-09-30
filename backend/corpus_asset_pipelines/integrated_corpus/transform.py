9d716c428d160e0be2017368e3203c8cde296e8b
Trent Smith <trent.smith@chanzuckerberg.com>
2022-09-30 15:49:09 -0700

    message

--- backend/corpus_asset_pipelines/integrated_corpus/transform.py
+++ backend/corpus_asset_pipelines/integrated_corpus/transform.py
@@ -49,7 +49,7 @@ def get_high_level_tissue(obs: DataFrame) -> DataFrame:
 
     obs = obs.copy()
 
-    tissue_mapper = TissueMapper()  # TODO: Slow
+    tissue_mapper = TissueMapper()  # TODO: Slow. Try to speed up.
 
     tissue_ids_and_labels = obs[["tissue_ontology_term_id", "tissue"]].drop_duplicates().astype(str)
     new_tissue_ids = {}
