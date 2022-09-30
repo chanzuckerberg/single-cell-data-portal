9edb1b7109fd505f1b363a61e8394f02204d7435
Trent Smith <trent.smith@chanzuckerberg.com>
2022-09-30 16:11:04 -0700

    remove incomplete test

--- tests/unit/wmg_processing/test_cube_pipeline.py
+++ tests/unit/wmg_processing/test_cube_pipeline.py
@@ -1,13 +1,11 @@
 import logging
 import os
-import pathlib
 import unittest
 import tempfile
 from unittest import mock
 from unittest.mock import Mock
 import contextlib
 from backend.wmg.data.cube_pipeline import main, logger
-from unit.backend.wmg.fixtures.test_anndata_object import create_anndata_test_object
 
 
 @contextlib.contextmanager
@@ -34,20 +32,10 @@ class TestCubePipe(unittest.TestCase):
             self.assertEqual(1, len(logs.records))
             mock_notify_slack.assert_called_once()
 
-    @mock.patch("backend.corpus_asset_pipelines.integrated_corpus.transform.GENE_EXPRESSION_COUNT_MIN_THRESHOLD", 1)
-    def test_pipeline_creates_files(self):
+    @unittest.skip
+    def test_pipeline_creates_files():
         """
         Test the pipeline successfully copies from mock s3, loads corpus and correctly builds cube/cell_ordering files
         and correctly uploads files to mock s3
         """
-        with tempfile.TemporaryDirectory() as temp_dir:
-            os.mkdir(f"{temp_dir}/datasets")
-            for i in range(2):
-                test_anndata_object = create_anndata_test_object(num_genes=5, num_cells=5)
-                anndata_file_path = f"{temp_dir}/datasets/basic_test_dataset_{i}"
-                os.mkdir(anndata_file_path)
-                anndata_file_name = pathlib.Path(anndata_file_path, "local.h5ad")
-                anndata_file_name.touch()
-                test_anndata_object.write(anndata_file_name, compression="gzip")
-            with change_directory(temp_dir):
-                main()
+        raise NotImplementedError
