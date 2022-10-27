842a485c0a60576390304fe73a5d0020a926607d
Nayib Gloria <55710092+nayib-jose-gloria@users.noreply.github.com>
2022-10-27 17:13:33 -0400

    fix: apply functional test retry config towards POST requests (#3498)
    * fix: apply functional test retry config towards POST requests
    
    Signed-off-by: nayib-jose-gloria <ngloria@chanzuckerberg.com>
    
    * fix: add comment justifying POST request change + simplify allowed_methods set
    
    Signed-off-by: nayib-jose-gloria <ngloria@chanzuckerberg.com>
    
    Signed-off-by: nayib-jose-gloria <ngloria@chanzuckerberg.com>

--- tests/functional/backend/common.py
+++ tests/functional/backend/common.py
@@ -31,7 +31,14 @@ class BaseFunctionalTestCase(unittest.TestCase):
         cls.deployment_stage = os.environ["DEPLOYMENT_STAGE"]
         cls.config = CorporaAuthConfig()
         cls.session = requests.Session()
-        retry_config = Retry(total=7, backoff_factor=2, status_forcelist=[500, 502, 503, 504])
+        # apply retry config to idempotent http methods we use + POST requests, which are currently all either
+        # idempotent (wmg queries) or low risk to rerun in dev/staging. Update if this changes in functional tests.
+        retry_config = Retry(
+            total=7,
+            backoff_factor=2,
+            status_forcelist=[500, 502, 503, 504],
+            allowed_methods={"DELETE", "GET", "HEAD", "PUT" "POST"},
+        )
         cls.session.mount("https://", HTTPAdapter(max_retries=retry_config))
         token = cls.get_auth_token(cls.config.test_account_username, cls.config.test_account_password)
         cls.curator_cookie = cls.make_cookie(token)
