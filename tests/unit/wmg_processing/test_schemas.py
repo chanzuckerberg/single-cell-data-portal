c9505c57d9d62ca55707ceefb394bad704dd3d4c
Ubuntu <ubuntu@ip-10-196-2-134.us-west-2.compute.internal>
2022-11-01 18:01:31 +0000

    update tests

--- tests/unit/wmg_processing/test_schemas.py
+++ tests/unit/wmg_processing/test_schemas.py
@@ -79,7 +79,7 @@ class TestSummaryCubeSchema(unittest.TestCase):
         with tempfile.TemporaryDirectory() as summary_cube_dir:
             create_empty_cube(uri=f"{summary_cube_dir}/cube", schema=expression_summary_schema)
             summary_cube_schema = tiledb.ArraySchema.load(f"{summary_cube_dir}/cube")
-            self.assertEqual(summary_cube_schema.nattr, 10)
+            self.assertEqual(summary_cube_schema.nattr, 9)
             self.assertTrue(summary_cube_schema.has_attr("cell_type_ontology_term_id"))
             self.assertTrue(summary_cube_schema.has_attr("dataset_id"))
             self.assertTrue(summary_cube_schema.has_attr("assay_ontology_term_id"))
@@ -87,7 +87,6 @@ class TestSummaryCubeSchema(unittest.TestCase):
             self.assertTrue(summary_cube_schema.has_attr("disease_ontology_term_id"))
             self.assertTrue(summary_cube_schema.has_attr("ethnicity_ontology_term_id"))
             self.assertTrue(summary_cube_schema.has_attr("sex_ontology_term_id"))
-            self.assertTrue(summary_cube_schema.has_attr("n_cells"))
             self.assertTrue(summary_cube_schema.has_attr("nnz"))
             self.assertTrue(summary_cube_schema.has_attr("sum"))
 
@@ -106,7 +105,7 @@ class TestFmgSummaryCubeSchema(unittest.TestCase):
         with tempfile.TemporaryDirectory() as summary_cube_dir:
             create_empty_cube(uri=f"{summary_cube_dir}/cube", schema=expression_summary_schema)
             summary_cube_schema = tiledb.ArraySchema.load(f"{summary_cube_dir}/cube")
-            self.assertEqual(summary_cube_schema.nattr, 13)
+            self.assertEqual(summary_cube_schema.nattr, 12)
             self.assertTrue(summary_cube_schema.has_attr("gene_ontology_term_id"))
             self.assertTrue(summary_cube_schema.has_attr("tissue_original_ontology_term_id"))
             self.assertTrue(summary_cube_schema.has_attr("dataset_id"))
@@ -115,7 +114,6 @@ class TestFmgSummaryCubeSchema(unittest.TestCase):
             self.assertTrue(summary_cube_schema.has_attr("disease_ontology_term_id"))
             self.assertTrue(summary_cube_schema.has_attr("ethnicity_ontology_term_id"))
             self.assertTrue(summary_cube_schema.has_attr("sex_ontology_term_id"))
-            self.assertTrue(summary_cube_schema.has_attr("n_cells"))
             self.assertTrue(summary_cube_schema.has_attr("nnz"))
             self.assertTrue(summary_cube_schema.has_attr("sum"))
             self.assertTrue(summary_cube_schema.has_attr("sqsum"))
