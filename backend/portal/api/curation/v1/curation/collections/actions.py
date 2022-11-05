656025a38788b111f5a6a0eba0f8bc75a13ff764
Daniel Hegeman <daniel.hegeman@chanzuckerberg.com>
2022-11-04 17:13:02 -0700

    move handlers for following endpoints to correct locations
    - v1/collections -- v1/collections/actions.py
    - v1/collections/{collection_id} -- v1/collections/collection_id.py

--- backend/portal/api/curation/v1/curation/collections/actions.py
+++ backend/portal/api/curation/v1/curation/collections/actions.py
@@ -1,8 +1,9 @@
 from flask import jsonify, g, make_response
+
+from backend.portal.api.collections_common import create_collection_common
 from backend.portal.api.curation.v1.curation.collections.common import reshape_for_curation_api, \
     curation_get_normalized_doi_url
 from backend.portal.api.app.v1.authorization import is_super_curator, owner_or_allowed
-from backend.portal.api.app.v1.collections import create_collection_common
 from backend.common.corpora_orm import CollectionVisibility, DbCollection, ProjectLinkType
 from backend.common.utils.http_exceptions import ForbiddenHTTPException, InvalidParametersHTTPException
 from backend.api_server.db import dbconnect
