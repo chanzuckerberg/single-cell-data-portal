de23ce2171998103ad1e8d9105487e39a5d1e25e
Ubuntu <ubuntu@ip-10-196-2-134.us-west-2.compute.internal>
2022-11-08 14:53:58 +0000

    add warning

--- backend/wmg/data/rankit.py
+++ backend/wmg/data/rankit.py
@@ -1,6 +1,8 @@
 import numba as nb
 import numpy as np
 import scipy as sc
+import logging
+logger = logging.getLogger("wmg")
 
 
 @nb.jit
@@ -26,6 +28,7 @@ def rankit(Xraw: sc.sparse.spmatrix, offset: float = 3.0) -> sc.sparse.csr_matri
     """
     X = Xraw.tocsr(copy=True)  # get Compressed Sparse Row format of raw expression values matrix
     indptr = X.indptr  # get row count
+    warning_raised = False
     for row in range(0, indptr.shape[0] - 1):
         data = X.data[indptr[row] : indptr[row + 1]]
         if len(data) > 0:
@@ -37,4 +40,8 @@ def rankit(Xraw: sc.sparse.spmatrix, offset: float = 3.0) -> sc.sparse.csr_matri
 
             normal_quantiles = sc.stats.norm.ppf(prob_level, loc=offset)
             X.data[indptr[row] : indptr[row + 1]] = normal_quantiles
+        elif not warning_raised:
+            logging.warn("This dataset has at least one row of all zero expressions")
+            warning_raised=True
+
     return X
