40e8d4f53761c6b53c7ae804a67791881f2d21bd
Ubuntu <ubuntu@ip-10-196-2-134.us-west-2.compute.internal>
2022-11-11 16:25:22 +0000

    test

--- tests/unit/backend/wmg/api/test_v1.py
+++ tests/unit/backend/wmg/api/test_v1.py
@@ -874,6 +874,48 @@ class WmgApiV1Tests(unittest.TestCase):
                     self.app.post("/wmg/v1/query", json=two_secondary_filters_request)
                     self.assertEqual(mock_dims.call_count, 2)
 
+    @patch("backend.wmg.api.v1.gene_term_label")
+    @patch("backend.wmg.api.v1.ontology_term_label")
+    @patch("backend.wmg.api.v1.load_snapshot")
+    def test__findmarkers_returns_200_and_correct_response(
+        self, load_snapshot, ontology_term_label, gene_term_label
+    ):
+        dim_size = 1
+        with create_temp_wmg_snapshot(
+            dim_size=dim_size,
+            expression_summary_vals_fn=all_ones_expression_summary_values,
+            cell_counts_generator_fn=all_tens_cell_counts_values,
+        ) as snapshot:
+            # setup up API endpoints to use a mocked cube containing all stat values of 1, for a deterministic
+            # expected query response
+            load_snapshot.return_value = snapshot
+
+            # mock the functions in the ontology_labels module, so we can assert deterministic values in the
+            # "term_id_labels" portion of the response body; note that the correct behavior of the ontology_labels
+            # module is separately unit tested, and here we just want to verify the response building logic is correct.
+            ontology_term_label.side_effect = lambda ontology_term_id: f"{ontology_term_id}_label"
+            gene_term_label.side_effect = lambda gene_term_id: f"{gene_term_id}_label"
+
+            request = dict(
+                target_filters=dict(
+                    cell_type_ontology_term_ids=[
+                        "cell_type_ontology_term_id_0",
+                    ],
+                    organism_ontology_term_id="organism_ontology_term_id_0",
+                    tissue_ontology_term_ids=["tissue_ontology_term_id_0"],
+                ),
+                context_filters=dict(
+                    organism_ontology_term_id="organism_ontology_term_id_0",
+                    tissue_ontology_term_ids=["tissue_ontology_term_id_0"],
+                ),                
+            )
+
+            response = self.app.post("/wmg/v1/findmarkers", json=request)
+
+            self.assertEqual(200, response.status_code)
+            print("HERE")
+            print(json.loads(response.data))
+
 
 # mock the dataset and collection entity data that would otherwise be fetched from the db; in this test
 # we only care that we're building the response correctly from the cube; WMG API integration tests verify
