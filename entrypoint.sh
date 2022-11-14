75ca5c24fee7b51623d6c8890759e1d8238fa4da
Alex Lokshin <alokshin@chanzuckerberg.com>
2022-11-14 15:30:22 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 
-echo "Starting ddtrace"
-ddtrace-run python single-cell-data-portal
-
-echo "Starting gunicorn"
-gunicorn --worker-class gevent --workers 1 --bind 0.0.0.0:5000 backend.api_server.app:app --max-requests 10000 --timeout 180 --keep-alive 5 --log-level info
+echo "Starting ddtrace and gunicorn"
+ddtrace-run gunicorn --worker-class gevent --workers 1 --bind 0.0.0.0:5000 backend.api_server.app:app --max-requests 10000 --timeout 180 --keep-alive 5 --log-level info
