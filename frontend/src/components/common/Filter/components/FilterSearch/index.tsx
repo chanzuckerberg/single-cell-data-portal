ee0e2cfd380efed221b9f3dc43d998082b2f11d3
Fran McDade <frano-m@users.noreply.github.com>
2022-11-23 08:23:42 +1000

    feat: add button to easily clear filter search bars (#3612) (#3617)
    * feat: add button to easily clear filter search bars (#3612)
    
    * Added focus to search input after value is cleared.
    
    * Update to filter search styles.
    
    * Linting.
    
    Co-authored-by: Fran McDade <franmcdade@Frans-MacBook-Pro.local>

--- frontend/src/components/common/Filter/components/FilterSearch/index.tsx
+++ frontend/src/components/common/Filter/components/FilterSearch/index.tsx
@@ -1,6 +1,8 @@
 import { IconNames } from "@blueprintjs/icons";
 import { SetSearchValueFn } from "src/components/common/Filter/components/FilterSearch/common/useFilterSearch";
-import { ViewSearch } from "./style";
+import { ClearIconButton, ViewSearch } from "./style";
+import { Icon } from "czifui";
+import { useRef } from "react";
 
 interface Props {
   className?: string;
@@ -13,13 +15,38 @@ export default function FilterSearch({
   searchValue,
   setSearchValue,
 }: Props): JSX.Element {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  /**
+   * Clears search value and sets focus on the search input element.
+   */
+  const onClearSearch = (): void => {
+    setSearchValue("");
+    inputRef?.current?.focus();
+  };
+
   return (
     <ViewSearch
       autoFocus
       className={className}
+      inputRef={inputRef}
       leftIcon={IconNames.SEARCH}
       onChange={(changeEvent) => setSearchValue(changeEvent.target.value)}
       placeholder="Search"
+      rightElement={
+        searchValue ? (
+          <ClearIconButton
+            onClick={onClearSearch}
+            {...{
+              // TODO(cc) move this back to explicit prop={value} after upgrading SDS to enable type checking again
+              sdsSize: "small",
+            }}
+            sdsType="secondary"
+          >
+            <Icon sdsIcon="xMarkCircle" sdsSize="s" sdsType="iconButton" />
+          </ClearIconButton>
+        ) : undefined
+      }
       value={searchValue}
     />
   );
