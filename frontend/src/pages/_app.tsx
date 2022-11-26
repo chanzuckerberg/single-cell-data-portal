ed6600e56f6a8fb33881e98f2db725d3b5398283
Daniel Hegeman <daniel.hegeman@chanzuckerberg.com>
2022-11-25 23:22:04 -0800

    local dev working for login flow
    Note: user info is not yet included

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,9 +1,10 @@
+import { Auth0Provider } from '@auth0/auth0-react';
 import { ThemeProvider as EmotionThemeProvider } from "@emotion/react";
 import { StyledEngineProvider, ThemeProvider } from "@mui/material/styles";
 import { NextPage } from "next";
 import { AppProps } from "next/app";
 import Script from "next/script";
-import { FC, useEffect } from "react";
+import { FC, useEffect, useState } from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { EVENTS } from "src/common/analytics/events";
@@ -43,32 +44,52 @@ function App({ Component, pageProps }: AppPropsWithLayout): JSX.Element {
   // "...make sure your tracking setup includes the second line as shown below"
   // https://plausible.io/docs/custom-event-goals#1-trigger-custom-events-with-javascript-on-your-site
   useEffect(() => {
+    console.log("PLAUSIBLE");
     window.plausible = window.plausible || tempPlausible;
 
     function tempPlausible(...args: unknown[]): void {
       (window.plausible.q = window.plausible.q || []).push(args);
     }
   }, []);
+  console.log("main body");
+  let windowLocationOrigin;
 
+  const [isMounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    console.log("RERENDERED");
+    console.log(`query client ${queryClient}`); 
+    windowLocationOrigin = window.location.origin;
+    setMounted(true);
+  }, []);
+  console.log(`query client ${queryClient}`);
   return (
-    <>
-      <QueryClientProvider client={queryClient}>
-        <StyledEngineProvider>
-          <EmotionThemeProvider theme={theme}>
-            <ThemeProvider theme={theme}>
-              <Layout>
-                <Component {...pageProps} />
-              </Layout>
-              <ReactQueryDevtools />
-            </ThemeProvider>
-          </EmotionThemeProvider>
-        </StyledEngineProvider>
-      </QueryClientProvider>
-      <Script
-        data-domain={configs.PLAUSIBLE_DATA_DOMAIN}
-        src="https://plausible.io/js/script.js"
-      />
-    </>
+    isMounted ? (
+      <>
+        <Auth0Provider
+          domain="corporanet.local"
+          clientId="local-client-id"
+          redirectUri={windowLocationOrigin}
+        >
+          <QueryClientProvider client={queryClient}>
+            <StylesProvider injectFirst>
+              <EmotionThemeProvider theme={theme}>
+                <ThemeProvider theme={theme}>
+                  <Layout>
+                    <Component {...pageProps} />
+                  </Layout>
+                  <ReactQueryDevtools />
+                </ThemeProvider>
+              </EmotionThemeProvider>
+            </StylesProvider>
+          </QueryClientProvider>
+        </Auth0Provider>
+        <Script
+          data-domain={configs.PLAUSIBLE_DATA_DOMAIN}
+          src="https://plausible.io/js/script.js"
+        />
+      </>
+    ) : (<></>)
   );
 }
 
