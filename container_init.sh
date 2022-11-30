06c29c34ff241f1d6f2aee9130abfa6c0adf7d54
Trent Smith <trent.smith@chanzuckerberg.com>
2022-11-30 13:38:38 -0800

    use file variable
    Signed-off-by: Trent Smith <trent.smith@chanzuckerberg.com>

--- container_init.sh
+++ container_init.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
-echo
-echo " ====="
-echo "| starting backend container"
-echo " ====="
-echo
-
 if [ "${DEPLOYMENT_STAGE}" == "test" ]; then
   # Use locally-generated cert for HTTPS in containerized local environment; deployed envs use ELB
-  echo
-  echo " ====="
-  echo "| Using test cert"
-  echo " ====="
-  echo
-  export HTTPS_CERT_AND_KEY="--certfile /tmp/pkcs12/server.crt --keyfile /tmp/pkcs12/server.key"
+  echo "--certfile /tmp/pkcs12/server.crt --keyfile /tmp/pkcs12/server.key"
 fi
