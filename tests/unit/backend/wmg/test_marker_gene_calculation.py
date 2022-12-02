df0ef4715e54af30be2d782e5991aa35dc2befb3
Ubuntu <ubuntu@ip-10-196-2-134.us-west-2.compute.internal>
2022-12-02 17:45:17 +0000

    update tests

--- tests/unit/backend/wmg/test_marker_gene_calculation.py
+++ tests/unit/backend/wmg/test_marker_gene_calculation.py
@@ -1,6 +1,9 @@
 import unittest
+import json
+import pytest
 from tests.unit.backend.wmg.fixtures.test_snapshot import load_test_fmg_snapshot
 from backend.wmg.pipeline.summary_cubes.calculate_markers import _query_tiledb, get_markers
+from backend.wmg.api.query import retrieve_top_n_markers
 
 TEST_SNAPSHOT = "test-fmg-snapshot"
 
@@ -33,64 +36,31 @@ class MarkerGeneCalculationTest(unittest.TestCase):
     def test__get_markers_ttest(self):
         with load_test_fmg_snapshot(TEST_SNAPSHOT) as snapshot:
             result = get_markers(TARGET_FILTERS, CONTEXT_FILTERS, corpus=snapshot, test="ttest")
+            result = json.loads(
+                json.dumps(result).replace("p_value_ttest", "p_value").replace("effect_size_ttest", "effect_size")
+            )
+            tissue = TARGET_FILTERS["tissue_ontology_term_ids"]
+            celltype = TARGET_FILTERS["cell_type_ontology_term_ids"]
+            organism = TARGET_FILTERS["organism_ontology_term_id"]
 
-            expected = {
-                "ENSG00000170476": {"p_value_ttest": 2.853570751404757e-277, "effect_size_ttest": 2.8502310053712137},
-                "ENSG00000180879": {"p_value_ttest": 3.2048802928401952e-208, "effect_size_ttest": 2.5837366521752467},
-                "ENSG00000132465": {"p_value_ttest": 7.223446844565219e-271, "effect_size_ttest": 2.3984059302087104},
-                "ENSG00000134285": {"p_value_ttest": 2.4746219866006915e-227, "effect_size_ttest": 1.8600029299922847},
-                "ENSG00000166562": {"p_value_ttest": 1.9153063397143112e-215, "effect_size_ttest": 1.8400333201504637},
-                "ENSG00000051108": {"p_value_ttest": 2.842480381188694e-211, "effect_size_ttest": 1.6856227945515443},
-                "ENSG00000099958": {"p_value_ttest": 3.357622968333614e-205, "effect_size_ttest": 1.6013719201864456},
-                "ENSG00000211592": {"p_value_ttest": 7.105551700160005e-93, "effect_size_ttest": 1.4867776488396038},
-                "ENSG00000118363": {"p_value_ttest": 1.7808284501343359e-183, "effect_size_ttest": 1.476018358457155},
-                "ENSG00000100219": {"p_value_ttest": 1.2807063177018397e-178, "effect_size_ttest": 1.4613032640890151},
-            }
-            self.assertDictEqual(result, expected)
+            expected = snapshot.marker_genes_cube.df[(tissue, organism, celltype)]
+            expected = retrieve_top_n_markers(expected, "ttest", 10)
+            for k in result:
+                assert pytest.approx(result[k]) == expected[k]
 
     def test__get_markers_binomtest(self):
         with load_test_fmg_snapshot(TEST_SNAPSHOT) as snapshot:
             result = get_markers(TARGET_FILTERS, CONTEXT_FILTERS, corpus=snapshot, test="binomtest")
-            expected = {
-                "ENSG00000099958": {
-                    "p_value_binomtest": 1.9613120062451643e-147,
-                    "effect_size_binomtest": 7.27287154339715,
-                },
-                "ENSG00000170476": {
-                    "p_value_binomtest": 7.046819867598114e-151,
-                    "effect_size_binomtest": 5.982245009212843,
-                },
-                "ENSG00000134285": {
-                    "p_value_binomtest": 8.680479676114522e-137,
-                    "effect_size_binomtest": 3.6003438891602255,
-                },
-                "ENSG00000132465": {
-                    "p_value_binomtest": 2.7014577109759077e-141,
-                    "effect_size_binomtest": 3.5873641915459094,
-                },
-                "ENSG00000166562": {
-                    "p_value_binomtest": 4.700596957883503e-129,
-                    "effect_size_binomtest": 2.3480413711693804,
-                },
-                "ENSG00000173110": {
-                    "p_value_binomtest": 5.911640500830793e-28,
-                    "effect_size_binomtest": 1.802670539695546,
-                },
-                "ENSG00000211592": {
-                    "p_value_binomtest": 8.619975795729593e-90,
-                    "effect_size_binomtest": 1.563336930583122,
-                },
-                "ENSG00000171311": {
-                    "p_value_binomtest": 4.5140965972544436e-07,
-                    "effect_size_binomtest": 1.3592692844275622,
-                },
-                "ENSG00000118363": {
-                    "p_value_binomtest": 1.3670165462597359e-101,
-                    "effect_size_binomtest": 1.2869060309287068,
-                },
-                "ENSG00000051108": {
-                    "p_value_binomtest": 1.7892111295524904e-104,
-                    "effect_size_binomtest": 1.1517488666121325,
-                },
-            }
-            self.assertDictEqual(result, expected)
+            result = json.loads(
+                json.dumps(result)
+                .replace("p_value_binomtest", "p_value")
+                .replace("effect_size_binomtest", "effect_size")
+            )
+            tissue = TARGET_FILTERS["tissue_ontology_term_ids"]
+            celltype = TARGET_FILTERS["cell_type_ontology_term_ids"]
+            organism = TARGET_FILTERS["organism_ontology_term_id"]
+
+            expected = snapshot.marker_genes_cube.df[(tissue, organism, celltype)]
+            expected = retrieve_top_n_markers(expected, "binomtest", 10)
+            for k in result:
+                assert pytest.approx(result[k]) == expected[k]
