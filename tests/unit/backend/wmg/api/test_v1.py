cd9ac3c6f801502a54f81bf7f30b230d6cb324ca
Ubuntu <ubuntu@ip-10-196-2-134.us-west-2.compute.internal>
2022-12-05 17:57:52 +0000

    update

--- tests/unit/backend/wmg/api/test_v1.py
+++ tests/unit/backend/wmg/api/test_v1.py
@@ -902,20 +902,19 @@ class WmgApiV1Tests(unittest.TestCase):
 
             response = self.app.post("/wmg/v1/markers", json=request)
             received = json.loads(response.data)
-            print("Look here")
-            print(received)
+
             expected = {
                 "marker_genes": {
                     "ENSG00000051108": {"effect_size": 1.6856228113174438, "p_value": 0.0},
                     "ENSG00000099958": {"effect_size": 1.6013718843460083, "p_value": 0.0},
                     "ENSG00000100219": {"effect_size": 1.4613032341003418, "p_value": 0.0},
                     "ENSG00000118363": {"effect_size": 1.4760183095932007, "p_value": 0.0},
-                    "ENSG00000132465": {"effect_size": 2.3984060287475586, "p_value": 0.0},
-                    "ENSG00000134285": {"effect_size": 1.860002875328064, "p_value": 0.0},
+                    "ENSG00000132465": {"effect_size": 2.4143028259277344, "p_value": 0.0},
+                    "ENSG00000134285": {"effect_size": 1.8657838106155396, "p_value": 0.0},
                     "ENSG00000166562": {"effect_size": 1.8400332927703857, "p_value": 0.0},
                     "ENSG00000170476": {"effect_size": 2.8502309322357178, "p_value": 0.0},
-                    "ENSG00000180879": {"effect_size": 2.5837366580963135, "p_value": 0.0},
-                    "ENSG00000211592": {"effect_size": 1.486777663230896, "p_value": 0.0},
+                    "ENSG00000180879": {"effect_size": 2.5727155208587646, "p_value": 0.0},
+                    "ENSG00000211592": {"effect_size": 1.5220520496368408, "p_value": 0.0},
                 },
                 "snapshot_id": "test-fmg-snapshot",
             }
