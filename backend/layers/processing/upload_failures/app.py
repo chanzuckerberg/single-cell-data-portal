e048750862cb50d7a1cb9cee5295269613568790
Emanuele Bezzi <ebezzi@chanzuckerberg.com>
2022-12-05 14:02:30 -0500

    failure layer

--- backend/layers/processing/upload_failures/app.py
+++ backend/layers/processing/upload_failures/app.py
@@ -1,8 +1,10 @@
 import json
+import logging
 import os
 
 from backend.common.utils.aws import delete_many_from_s3
-from backend.common.utils.result_notification import dataset_processing_slack_notification
+from backend.common.utils.json import CustomJSONEncoder
+from backend.common.utils.result_notification import format_failed_batch_issue_slack_alert, notify_slack
 from backend.layers.business.business import BusinessLogic
 from backend.layers.persistence.persistence import DatabaseProvider
 from backend.layers.common.entities import DatasetProcessingStatus, DatasetStatusKey, DatasetVersionId
@@ -13,7 +15,7 @@ business_logic = BusinessLogic(database_provider, None, None, None, None)
 
 def handle_failure(event: dict, context) -> None:
     dataset_id = event["dataset_id"]
-    dataset_processing_slack_notification(dataset_id)
+    trigger_slack_notification(dataset_id)
     update_dataset_processing_status_to_failed(dataset_id, event["error"]["Cause"])
 
     error_step_name = get_error_step_name(event)
@@ -37,7 +39,7 @@ def update_dataset_processing_status_to_failed(dataset_id, error=None) -> None:
             DatasetProcessingStatus.FAILURE
         )
     # If dataset not in db dont worry about updating its processing status
-    except AttributeError:
+    except Exception:
         pass
 
 
@@ -50,3 +52,49 @@ def get_error_step_name(event: dict) -> str:
     if "JobName" in error_cause_dict:
         error_job_name = error_cause_dict["JobName"]
     return error_job_name
+
+
+def trigger_slack_notification(dataset_id):
+    dataset = business_logic.get_dataset_version(DatasetVersionId(dataset_id))
+    if dataset is None:
+        return 
+    collection_id = dataset.collection_id
+    collection = business_logic.get_collection_version_from_canonical(collection_id)
+    if collection is None:
+        return
+    collection_id, collection_owner = collection.version_id, collection.owner
+    processing_status = dataset.status.to_json()
+
+    data = {
+        "blocks": [
+            {
+                "type": "header",
+                "text": {
+                    "type": "plain_text",
+                    "text": "Dataset failed to process:fire:",
+                    "emoji": True,
+                },
+            },
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": f"Dataset processing job failed! @sc-oncall-eng\n"
+                    f"*Owner*: {collection_owner}\n"
+                    f"*Collection*: https://cellxgene.cziscience.com/collections/{collection_id}\n"
+                    f"*Processing Status*:\n",
+                },
+            },
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": f"```{json.dumps(processing_status, cls=CustomJSONEncoder, indent=2, sort_keys=True)}```",
+                },
+            },
+        ]
+    }
+    batch_alert_data = format_failed_batch_issue_slack_alert(data)
+    logger = logging.getLogger(__name__)
+    logger.info(batch_alert_data)
+    notify_slack(data)
