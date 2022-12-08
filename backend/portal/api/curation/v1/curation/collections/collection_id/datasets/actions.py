8c1614d6a11a4558485e800426c74a64180126c0
Emanuele Bezzi <ebezzi@chanzuckerberg.com>
2022-12-08 14:25:37 -0500

    stuff

--- backend/portal/api/curation/v1/curation/collections/collection_id/datasets/actions.py
+++ backend/portal/api/curation/v1/curation/collections/collection_id/datasets/actions.py
@@ -1,14 +1,11 @@
 from flask import g, make_response, jsonify
 
 from backend.api_server.db import dbconnect
-from backend.common.corpora_orm import CollectionVisibility, ProcessingStatus
-from backend.common.entities import Dataset
 from backend.common.utils.http_exceptions import MethodNotAllowedException
 from backend.layers.api.router import get_business_logic
 from backend.layers.auth.user_info import UserInfo
-from backend.portal.api.app.v1.authorization import owner_or_allowed
-from backend.portal.api.collections_common import get_collection_else_forbidden
 from backend.portal.api.curation.v1.curation.collections.common import get_infered_collection_version_else_forbidden, is_owner_or_allowed_else_forbidden
+from backend.layers.common.entities import CollectionVersionId
 
 
 @dbconnect
@@ -21,9 +18,6 @@ def post(token_info: dict, collection_id: str):
     if collection_version.published_at is not None:
         raise MethodNotAllowedException("Collection must be PRIVATE Collection, or a revision of a PUBLIC Collection.")
 
-    business_logic.create_da
+    dataset_id, dataset_version_id = business_logic.create_empty_dataset(CollectionVersionId(collection_id))
 
-    dataset = Dataset.create(
-        db_session, collection=collection, processing_status={"processing_status": ProcessingStatus.INITIALIZED}
-    )
-    return make_response(jsonify({"id": dataset.id}), 201)
+    return make_response(jsonify({"id": dataset_id.id}), 201)
