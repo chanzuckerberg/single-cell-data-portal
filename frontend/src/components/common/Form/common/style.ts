2816f996a3572f9b10afe982edb18906079bc8e1
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2022-12-12 15:52:03 +1000

    Style linting.

--- frontend/src/components/common/Form/common/style.ts
+++ frontend/src/components/common/Form/common/style.ts
@@ -13,8 +13,8 @@ export const formField = (props: CommonThemeProps) => {
   const colors = getColors(props);
   const grey400 = colors?.gray["400"];
   return css`
-    border-radius: 3px;
     border-color: ${grey400} !important; /* required; overrides global.scss input border color specification with important style declaration */
+    border-radius: 3px;
     color: ${PT_TEXT_COLOR};
     height: auto; /* required; upholds line height and height specification (where padding and line height determine overall height) */
     letter-spacing: -0.1px;
@@ -27,30 +27,34 @@ export const formField = (props: CommonThemeProps) => {
 /* Form label styles with targeted shared styles for form field elements i.e. form group, input, textarea and adornments (danger icon). */
 export const StyledFormLabel = styled(Label)`
   /* Form label */
+
   &.${Classes.LABEL} {
     margin-bottom: 0;
   }
 
   /* Form group */
+
   .${Classes.FORM_GROUP} {
     margin-bottom: 0;
   }
 
   /* Input (shared styles for input and textarea) */
+
   &.${Classes.LABEL} .${Classes.INPUT} {
     margin-top: 8px; /* required; overrides BP label input margin top */
   }
 
   /* Input (shared styles for input and textarea) */
+
   .${Classes.INPUT} {
     ${formField}
-
     &:focus {
       border: 1px solid ${PRIMARY_BLUE} !important; /* required; overrides global.scss input border specification with important style declaration */
     }
   }
 
   /* Text area */
+
   textarea.${Classes.INPUT} {
     height: 32px; /* required; used by TextArea component to set the height of the component on first mount */
     min-height: 32px;
@@ -58,6 +62,7 @@ export const StyledFormLabel = styled(Label)`
   }
 
   /* Danger input */
+
   textarea.${Classes.INPUT}.${Classes.INTENT_DANGER},
     .${Classes.INTENT_DANGER}
     .${Classes.INPUT} {
@@ -69,22 +74,26 @@ export const StyledFormLabel = styled(Label)`
   }
 
   /* Danger icon */
+
   .${Classes.ICON}.${Classes.INTENT_DANGER} {
     color: ${RED.C};
   }
 
   /* Danger icon positioner for input field */
+
   .${Classes.INPUT_GROUP} .${Classes.INPUT_ACTION}:last-child {
     right: 8px;
     top: 8px;
   }
 
   /* Adornment text */
+
   .${Classes.INPUT_GROUP} .${Classes.INPUT_LEFT_CONTAINER} {
     top: 0;
   }
 
   /* Helper text */
+
   .${Classes.FORM_HELPER_TEXT} {
     line-height: 15px;
     margin-top: 8px;
@@ -99,7 +108,7 @@ export const StyledFormLabel = styled(Label)`
 /* Form label styles for select "button" (button to resemble basic form field specification). */
 export const SelectFormLabel = styled.div`
   .${Classes.BUTTON} {
-    ${formField}
+    ${formField};
     border: 1px solid ${LIGHT_GRAY.A}; /* mimics basic form field specification */
     display: flex;
     justify-content: space-between;
