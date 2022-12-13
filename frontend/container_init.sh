db046092b6cacbe34106923ee58e4087117d4aab
Daniel Hegeman <daniel.hegeman@chanzuckerberg.com>
2022-12-12 18:28:50 -0800

    update vars

--- frontend/container_init.sh
+++ frontend/container_init.sh
@@ -21,16 +21,6 @@ fi
 
 # Build and run without dev mode in remote dev env.
 if [ "${DEPLOYMENT_STAGE}" == "test" ]; then
-#  count=$(grep -R oauth/token node_modules/@auth0/auth0-spa-js/src/api.ts | wc -l | awk '{ print $1 }')
-#  if [ $count -ne 1 ]; then
-#    echo "The number of instances of the '/oauth/token' endpoint string in src/api.ts in the @auth0/auth0-spa-js "\
-#    "package has changed. The Auth0 React code should be reassessed for compatibility with the mock oidc test "\
-#    "container, whose token endpoint is just (/connect)/token, not /oauth/token. The Auth0 React code must use /token "\
-#    "rather than /oauth/token."
-#    exit 1
-#  fi
-#  if []
-
   mkdir -p ./node_modules/.next-dev-mobile
   cp -r /tmp/pkcs12/* ./node_modules/.next-dev-mobile
   # next-dev-https looks for cert.pem and key.pem in node_modules/.next-dev-mobile/ (otherwise generates anew 👎)
