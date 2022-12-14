5ae1a7e2a37b066d3cc2632a14cc9bd55cedf3c4
Daniel Hegeman <daniel.hegeman@chanzuckerberg.com>
2022-12-14 14:44:41 -0800

    add auth0 config for rdev

--- frontend/src/configs/rdev.js
+++ frontend/src/configs/rdev.js
@@ -2,6 +2,8 @@ const { PLAUSIBLE_DATA_DOMAIN_STAGING } = require("./common");
 
 const configs = {
   API_URL: "$API_URL",
+  AUTH0_CLIENT_ID: "R6GwxkCT5JwI8udLdlOtL3reVvy41QW5",
+  AUTH0_DOMAIN: "login.cellxgene.dev.single-cell.czi.technology",
   PLAUSIBLE_DATA_DOMAIN: PLAUSIBLE_DATA_DOMAIN_STAGING,
 };
 
