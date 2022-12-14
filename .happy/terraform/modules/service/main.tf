92922930d01e7157aa7f8501b461993473bb69e4
Alex Lokshin <alokshin@chanzuckerberg.com>
2022-12-14 11:13:33 -0500

    Added a gunicorn com.datadoghq.ad.instances label

--- .happy/terraform/modules/service/main.tf
+++ .happy/terraform/modules/service/main.tf
@@ -131,7 +131,7 @@ resource "aws_ecs_task_definition" "task_definition" {
     "dockerLabels": {
       "com.datadoghq.ad.check_names": "[\"gunicorn\"]",
       "com.datadoghq.ad.init_configs": "[{}]",
-      "com.datadoghq.ad.instances":"[{ \"proc_name\": \"backend.api_server.app:app\" }]"
+      "com.datadoghq.ad.instances":"[{ \"proc_name\": \"backend.api_server.app:app\", \"gunicorn\": \"/usr/local/bin/gunicorn\" }]"
     },
     "essential": true,
     "image": "${var.image}",
