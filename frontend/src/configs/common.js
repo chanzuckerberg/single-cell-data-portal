15112d59a9fff83d869e7f6f711010eeb12785c2
Timmy Huang <thuang@chanzuckerberg.com>
2022-12-15 10:30:22 -0800

    refactor(3598): feedback

--- frontend/src/configs/common.js
+++ frontend/src/configs/common.js
@@ -1,6 +1,8 @@
 const PLAUSIBLE_DATA_DOMAIN_STAGING =
   "cellxgene.staging.single-cell.czi.technology";
 
+const DEV_AUTH0_CLIENT_ID = "R6GwxkCT5JwI8udLdlOtL3reVvy41QW5";
+
 if (typeof module !== "undefined") {
-  module.exports = { PLAUSIBLE_DATA_DOMAIN_STAGING };
+  module.exports = { DEV_AUTH0_CLIENT_ID, PLAUSIBLE_DATA_DOMAIN_STAGING };
 }
--- frontend/src/configs/rdev.js
+++ frontend/src/configs/rdev.js
@@ -1,8 +1,11 @@
-const { PLAUSIBLE_DATA_DOMAIN_STAGING } = require("./common");
+const {
+  DEV_AUTH0_CLIENT_ID,
+  PLAUSIBLE_DATA_DOMAIN_STAGING,
+} = require("./common");
 
 const configs = {
   API_URL: "$API_URL",
-  AUTH0_CLIENT_ID: "R6GwxkCT5JwI8udLdlOtL3reVvy41QW5",
+  AUTH0_CLIENT_ID: DEV_AUTH0_CLIENT_ID,
   AUTH0_DOMAIN: "login.cellxgene.dev.single-cell.czi.technology",
   PLAUSIBLE_DATA_DOMAIN: PLAUSIBLE_DATA_DOMAIN_STAGING,
 };
