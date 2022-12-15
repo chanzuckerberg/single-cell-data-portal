284fd950786788b930f5716bf1567287f56bf94b
Timmy Huang <thuang@chanzuckerberg.com>
2022-12-15 10:30:22 -0800

    refactor(3598): feedback

--- frontend/src/configs/staging.js
+++ frontend/src/configs/staging.js
@@ -1,8 +1,11 @@
-const { PLAUSIBLE_DATA_DOMAIN_STAGING } = require("./common");
+const {
+  DEV_AUTH0_CLIENT_ID,
+  PLAUSIBLE_DATA_DOMAIN_STAGING,
+} = require("./common");
 
 const configs = {
   API_URL: "https://api.cellxgene.staging.single-cell.czi.technology",
-  AUTH0_CLIENT_ID: "R6GwxkCT5JwI8udLdlOtL3reVvy41QW5",
+  AUTH0_CLIENT_ID: DEV_AUTH0_CLIENT_ID,
   AUTH0_DOMAIN: "login.cellxgene.staging.single-cell.czi.technology",
   PLAUSIBLE_DATA_DOMAIN: PLAUSIBLE_DATA_DOMAIN_STAGING,
   SENTRY_DEPLOYMENT_ENVIRONMENT: "staging",
