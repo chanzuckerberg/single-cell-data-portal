93451fbee8bda2a09059ded76209b88877f8e932
Daniel Hegeman <daniel.hegeman@chanzuckerberg.com>
2022-12-20 01:10:29 -0800

    add audience as variable, remove refresh token

--- frontend/src/configs/prod.js
+++ frontend/src/configs/prod.js
@@ -1,5 +1,6 @@
 const configs = {
   API_URL: "https://api.cellxgene.cziscience.com",
+  AUTH0_AUDIENCE: "https://api.cellxgene.cziscience.com/",
   AUTH0_CLIENT_ID: "bfpvd3NSj5vnsDuURtqXtZHc2EL0BZvo",
   AUTH0_DOMAIN: "login.cellxgene.cziscience.com",
   PLAUSIBLE_DATA_DOMAIN: "cellxgene.cziscience.com",
