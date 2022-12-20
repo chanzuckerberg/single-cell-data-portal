157d0872109e028a7283045a4c2c4a4b64e736b2
Daniel Hegeman <daniel.hegeman@chanzuckerberg.com>
2022-12-20 01:10:29 -0800

    add audience as variable, remove refresh token

--- frontend/src/configs/staging.js
+++ frontend/src/configs/staging.js
@@ -5,6 +5,7 @@ const {
 
 const configs = {
   API_URL: "https://api.cellxgene.staging.single-cell.czi.technology",
+  AUTH0_AUDIENCE: "api.cellxgene.staging.single-cell.czi.technology",
   AUTH0_CLIENT_ID: DEV_AUTH0_CLIENT_ID,
   AUTH0_DOMAIN: "login.cellxgene.staging.single-cell.czi.technology",
   PLAUSIBLE_DATA_DOMAIN: PLAUSIBLE_DATA_DOMAIN_STAGING,
