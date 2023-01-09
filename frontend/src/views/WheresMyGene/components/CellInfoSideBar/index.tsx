bb10575882ddab459f1c2e9645a85a21afb1af2f
Seve Badajoz <severiano.badajoz@chanzuckerberg.com>
2023-01-09 15:37:37 -0800

    feat(fmg): add analytics

--- frontend/src/views/WheresMyGene/components/CellInfoSideBar/index.tsx
+++ frontend/src/views/WheresMyGene/components/CellInfoSideBar/index.tsx
@@ -8,7 +8,9 @@ import {
   TableRow,
   Tooltip,
 } from "czifui";
-import React, { useCallback, useContext } from "react";
+import React, { useCallback, useContext, useEffect, useState } from "react";
+import { track } from "src/common/analytics";
+import { EVENTS } from "src/common/analytics/events";
 import { ROUTES } from "src/common/constants/routes";
 import { useMarkerGenes } from "src/common/queries/wheresMyGene";
 import { BetaChip } from "src/components/Header/style";
@@ -30,6 +32,7 @@ export interface CellInfoBarProps {
   cellInfoCellType: Exclude<State["cellInfoCellType"], null>;
   tissueName: string;
 }
+
 function CellInfoSideBar({
   cellInfoCellType,
   tissueName,
@@ -53,13 +56,25 @@ function CellInfoSideBar({
     if (!data) return;
     const genes = Object.keys(data.marker_genes);
     navigator.clipboard.writeText(genes.join(", "));
+    track(EVENTS.WMG_FMG_COPY_GENES_CLICKED);
   }, [data]);
 
   const handleDisplayGenes = useCallback(() => {
     if (!data || !dispatch) return;
     const genes = Object.keys(data.marker_genes);
     dispatch(addSelectedGenes(genes));
+    track(EVENTS.WMG_FMG_ADD_GENES_CLICKED);
   }, [data, dispatch]);
+
+  const [hoverTime, setHoverTime] = useState({ start: -1, end: -1 });
+
+  useEffect(() => {
+    const { start, end } = hoverTime;
+    if (start !== -1 && end !== -1 && end - start > 2 * 1000) {
+      track(EVENTS.WMG_FMG_QUESTION_BUTTON_HOVER);
+      setHoverTime({ start: -1, end: -1 });
+    }
+  }, [hoverTime]);
 
   if (isLoading || !data) return null;
 
@@ -85,6 +100,7 @@ function CellInfoSideBar({
                     href={ROUTES.FMG_DOCS} 
                     rel="noopener" 
                     target="_blank" 
+                    onClick={() => track(EVENTS.WMG_FMG_DOCUMENTATION_CLICKED)}
                   >
                     Click to read more about the identification method.
                   </a>
@@ -97,6 +113,10 @@ function CellInfoSideBar({
               sdsType="secondary"
               isAllCaps={false}
               style={{ fontWeight: "500" }}
+              onHoverStart={() => setHoverTime({ start: Date.now(), end: -1 })}
+              onHoverEnd={() =>
+                setHoverTime({ start: hoverTime.start, end: Date.now() })
+              }
             >
               <StyledIconImage src={questionMarkIcon} />
             </TooltipButton>
