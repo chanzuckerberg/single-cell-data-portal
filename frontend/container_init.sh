23e74360e065eb5bbae0a4cbe4d716a5127cc49d
Jessica Gadling <jgadling@chanzuckerberg.com>
2023-01-09 17:39:51 -0800

    Handle local dev better.

--- frontend/container_init.sh
+++ frontend/container_init.sh
@@ -4,15 +4,6 @@ echo " ====="
 echo "| starting frontend container"
 echo " ====="
 echo
-if [ ! -e ./node_modules ]; then
-  mkdir -p ./node_modules
-fi
-ln -sf /opt/node_app/node_modules/* /opt/node_app/node_modules/.bin ./node_modules/.
-
-if [ -z "$API_URL" ]; then
-  cp src/configs/local.js src/configs/configs.js
-fi
-
 
 # If user passed a command line, run it in place of the server
 if [ $# -ne 0 ]; then
@@ -25,20 +16,25 @@ set -x # config
 # We build the next.js app during our CI process, but then we need to be able
 # to run the resulting docker image in dev/rdev/staging/prod. This isn't really
 # possible to control well with next.js native utilities, so we have to rewrite
-# any environment-specific configuration in our *pre-built* nextjs app on 
+# any environment-specific configuration in our *pre-built* nextjs app on
 # container startup.
 #
 # This function is *intentionally* idempotent - it can be executed multiple
 # times with different api_url / deployment_env vars and it should get the
 # built app to a sane state on each run.
 function apply_path {
-  envDataFile="./runtime_configs/${DEPLOYMENT_STAGE}"
+  envDataOriginalFile="./runtime_configs/${DEPLOYMENT_STAGE}.env"
+  if [ ! -e $envDataOriginalFile ]; then
+    echo "ERROR: Invalid DEPLOYMENT_STAGE var, please fix me"
+    exit 1
+  fi
+  envDataFile="./runtime_configs/current_config"
   nextFolder='.next'
   nextFolderBackup='.next.original'
 
   # Expand any env vars in the original runtime var file and write it
   # to the file we're going to use for replacing vars in our next build.
-  envsubst < $envDataFile.env > $envDataFile
+  envsubst < $envDataOriginalFile > $envDataFile
 
   # Make a backup of the original next build files on the first run
   # and in subsequent runs, start from that backup set of files
@@ -57,11 +53,11 @@ function apply_path {
     if [ "${line:0:1}" == "#" ] || [ "${line}" == "" ]; then
       continue
     fi
-    
+
     # read variable names and values
     configName="$(cut -d'=' -f1 <<<"$line")"
     configValue="$(cut -d'=' -f2 <<<"$line")"    # get system env
-    
+
     # replace the variable names in our built app with the appropriate values. Use rg (ripgrep) for speed.
     echo "Replace: ${configName} with: ${configValue}"
     rg $configName --files-with-matches -0 $nextFolder | xargs -0 sed -i "s#$configName#$configValue#g"
@@ -70,6 +66,7 @@ function apply_path {
 
 # Build and run without dev mode in remote dev env.
 if [ "${DEPLOYMENT_STAGE}" == "test" ]; then
+  cp src/configs/local.js src/configs/configs.js
   mkdir -p ./node_modules/.next-dev-mobile
   cp -r /tmp/pkcs12/* ./node_modules/.next-dev-mobile
   # next-dev-https looks for cert.pem and key.pem in node_modules/.next-dev-mobile/ (otherwise generates anew 👎)
