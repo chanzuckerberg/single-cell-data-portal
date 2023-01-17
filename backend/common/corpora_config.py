f05b49c8f530f3f08195aea92f26399da32cfada
Daniel Hegeman <daniel.hegeman@chanzuckerberg.com>
2023-01-17 01:06:53 -0800

    fix: local dev database config / secret name
    Recent commit 4e6c4e2876 simplified the '../database_local' secret name
    to '../database'. However, local dev case exceptions still caused the
    database config to look for a '../database_local' secret.

--- backend/common/corpora_config.py
+++ backend/common/corpora_config.py
@@ -41,7 +41,7 @@ class CorporaDbConfig(SecretConfig):
     def __init__(self, *args, **kwargs):
         super().__init__(
             component_name="backend",
-            secret_name=f"database{'_local' if 'CORPORA_LOCAL_DEV' in os.environ else ''}",
+            secret_name=f"database",
             **kwargs,
         )
 
