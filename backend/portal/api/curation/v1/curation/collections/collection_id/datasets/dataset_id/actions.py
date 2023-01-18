0c61e078e03929181e5e841102d124ef036b7bd1
Trent Smith <1429913+Bento007@users.noreply.github.com>
2023-01-18 09:16:14 -0800

    fix(curation api): GET /collections/{collection_id}/datasets/{dataset_id} (#3995)
    * fix(curation api): GET /collections/{collection_id}/datasets/{dataset_id}
    - added tests for revision_of in GET /collections/{collection_id}/datasets/{dataset_id}
    - revision_of only refers to the published dataset when revising a dataset. Otherwise it is NULL.
    - fix typing in test code
    
    * return the canonical dataset id when POSTing a dataset.

--- backend/portal/api/curation/v1/curation/collections/collection_id/datasets/dataset_id/actions.py
+++ backend/portal/api/curation/v1/curation/collections/collection_id/datasets/dataset_id/actions.py
@@ -50,14 +50,9 @@ def get(collection_id: str, dataset_id: str = None):
     # A canonical url should be only used in two cases:
     # 1. the collection version is unpublished but it's not a revision
     # 2. the collection version is published
-    use_canonical_url = (
-        collection_version.canonical_collection.originally_published_at is None
-        or collection_version.published_at is not None
-    )
+    use_canonical_url = collection_version.is_initial_unpublished_version() or collection_version.is_published()
 
-    response_body = reshape_dataset_for_curation_api(
-        version, collection_version.published_at is not None, use_canonical_url
-    )
+    response_body = reshape_dataset_for_curation_api(version, collection_version.is_published(), use_canonical_url)
     return make_response(jsonify(response_body), 200)
 
 
