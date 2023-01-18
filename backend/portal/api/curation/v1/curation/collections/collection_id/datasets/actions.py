05137e318af30759b515cf6ca6d973414a39a2a1
Emanuele Bezzi <ebezzi@chanzuckerberg.com>
2023-01-18 11:39:34 -0500

    chore: POST /datasets returns the canonical dataset_id

--- backend/portal/api/curation/v1/curation/collections/collection_id/datasets/actions.py
+++ backend/portal/api/curation/v1/curation/collections/collection_id/datasets/actions.py
@@ -19,6 +19,6 @@ def post(token_info: dict, collection_id: str):
     if collection_version.published_at is not None:
         raise MethodNotAllowedException("Collection must be PRIVATE Collection, or a revision of a PUBLIC Collection.")
 
-    dataset_id, _ = business_logic.create_empty_dataset(collection_version.version_id)
+    _, dataset_id = business_logic.create_empty_dataset(collection_version.version_id)
 
     return make_response(jsonify({"id": dataset_id.id}), 201)
