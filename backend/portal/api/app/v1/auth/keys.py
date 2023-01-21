25aefcf7ff35ef72f68c672747f0b905aee641b2
Daniel Hegeman <daniel.hegeman@chanzuckerberg.com>
2023-01-20 18:51:17 -0800

    extract email from token instead of cookie

--- backend/portal/api/app/v1/auth/keys.py
+++ backend/portal/api/app/v1/auth/keys.py
@@ -1,13 +1,15 @@
+import connexion
 from flask import make_response
 
 from backend.common.auth0_manager import auth0_management_session
 from backend.common.corpora_config import CorporaAuthConfig
 from backend.common.utils.api_key import generate
 from backend.common.utils.http_exceptions import NotFoundHTTPException
-from backend.portal.api.app.v1.authentication import get_userinfo
 
 
 def get(user: str):
+    print(f"\nget for key has user: {user}\n")
+    print(f"\n connextion toekn info {connexion.context['token_info']}")
     identity = auth0_management_session.get_user_api_key_identity(user)
     if not identity:
         raise NotFoundHTTPException()
@@ -15,7 +17,7 @@ def get(user: str):
 
 
 def post(user: str):
-    userinfo = get_userinfo()
+    print(f"\n email is {connexion.context['token_info']['email']}")
     config = CorporaAuthConfig()
     days_to_live = config.days_to_live
     if not isinstance(days_to_live, (int, float)):
@@ -29,7 +31,7 @@ def post(user: str):
 
     # Generate a new key
     password = generate(user, config.api_key_secret, days_to_live)
-    api_key_id = auth0_management_session.store_api_key(password, userinfo["email"])
+    api_key_id = auth0_management_session.store_api_key(password, connexion.context["token_info"]["email"])
     auth0_management_session.link_api_key(user, api_key_id)
     return make_response({"key": password}, 201)
 
