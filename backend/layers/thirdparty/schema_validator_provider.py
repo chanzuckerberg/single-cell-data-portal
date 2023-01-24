24f77b36c4739688a37a99eb8146cf1d58a817c1
Emanuele Bezzi <ebezzi@chanzuckerberg.com>
2023-01-24 12:32:17 -0500

    fix: add validation_message back to the pipeline (#4050)
    * fix: add validation_message back to the pipeline
    
    * Linter

--- backend/layers/thirdparty/schema_validator_provider.py
+++ backend/layers/thirdparty/schema_validator_provider.py
@@ -10,7 +10,11 @@ class SchemaValidatorProvider(SchemaValidatorProviderInterface):
     def validate_and_save_labels(self, input_file: str, output_file: str) -> Tuple[bool, list, bool]:
         """
         Runs `cellxgene-schema validate` on the provided `input_file`. This also saves a labeled copy
-        of the artifact to `output_file`
+        of the artifact to `output_file`.
+        Returns a tuple that contains, in order:
+        1. A boolean that indicates whether the artifact is valid
+        2. A List[str] with the validation errors. This is only defined if the first boolean is false
+        3. A boolean that indicates whether the artifact is Seurat convertible
         """
         from cellxgene_schema import validate
 
