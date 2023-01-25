2c97bf4bdacc9fae2cc3181096dfa1e0c8900f99
atarashansky <atarashansky@CZIMACOS3990.hsd1.ma.comcast.net>
2023-01-25 18:33:11 -0500

    black

--- tests/unit/backend/wmg/test_expression_rollup.py
+++ tests/unit/backend/wmg/test_expression_rollup.py
@@ -11,34 +11,34 @@ class RollupExpressionsAcrossCellTypesTest(unittest.TestCase):
         # fourth cell type is descendant of third cell type
         cell_types = ["CL:0000786", "CL:0000986", "CL:0000980", "CL:0001202"]
         df = pd.DataFrame()
-        df['cell_type_ontology_term_id'] = cell_types
+        df["cell_type_ontology_term_id"] = cell_types
         np.random.seed(0)
         exprs = np.random.rand(len(cell_types), 10)
         for i in range(exprs.shape[1]):
-            df[i]=exprs[:,i]
+            df[i] = exprs[:, i]
 
         df_rollup = rollup_across_cell_type_descendants(df)
         expected = []
 
         df_expected = df.copy()
         expected_exprs = exprs.copy()
-        expected_exprs[0] = exprs[0]+exprs[1]
+        expected_exprs[0] = exprs[0] + exprs[1]
         expected_exprs[2] = exprs[2] + exprs[3]
-        
-        for i in range(expected_exprs.shape[1]):
-            df_expected[i] = expected_exprs[:,i]
 
-        assert np.all(df_expected==df_rollup)    
+        for i in range(expected_exprs.shape[1]):
+            df_expected[i] = expected_exprs[:, i]
+
+        assert np.all(df_expected == df_rollup)
 
     def test__expression_rollup_no_descendants_overlap(self):
         # these cell types are not descendants of each other
         cell_types = ["CL:0000786", "CL:0000980"]
         df = pd.DataFrame()
-        df['cell_type_ontology_term_id'] = cell_types
+        df["cell_type_ontology_term_id"] = cell_types
         np.random.seed(0)
         exprs = np.random.rand(len(cell_types), 10)
         for i in range(exprs.shape[1]):
-            df[i]=exprs[:,i]
+            df[i] = exprs[:, i]
 
         df_rollup = rollup_across_cell_type_descendants(df)
-        assert np.all(df==df_rollup)  
+        assert np.all(df == df_rollup)
