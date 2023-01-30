45e7a9bad43e362a44d26495095d87ba82f6ae7b
Seth Feingold <sfeingold1104@gmail.com>
2023-01-30 11:20:24 -0600

    feat: updating date formatting for collections sitemap

--- frontend/src/pages/collections-sitemap.xml/index.tsx
+++ frontend/src/pages/collections-sitemap.xml/index.tsx
@@ -1,16 +1,41 @@
 import { GetServerSideProps } from "next";
 import { getServerSideSitemap, ISitemapField } from "next-sitemap";
 
-// function to convert epoch/unix timestamp to YYYY-MM-DD format
+// function to convert epoch/unix timestamp to YYYY-MM-DD HH:MM +00:00 format
 function unixToYYYYMMDD(date: string) {
   let myDate = new Date(parseFloat(date) * 1000);
+
+  let month = String(myDate.getMonth() + 1);
+  if (parseFloat(month) < 10) {
+    month = "0" + String(month);
+  }
+
+  let day = String(myDate.getDate());
+  if (parseFloat(day) < 10) {
+    day = "0" + String(day);
+  }
+
+  let hours = String(myDate.getHours());
+  if (parseFloat(hours) < 10) {
+    hours = "0" + String(hours);
+  }
+
+  let minutes = String(myDate.getMinutes());
+  if (parseFloat(minutes) < 10) {
+    minutes = "0" + String(minutes);
+  }
 
   let dateStr =
     myDate.getFullYear() +
     "-" +
-    (myDate.getMonth() + 1) +
+    month +
     "-" +
-    myDate.getDate();
+    day +
+    " " +
+    hours +
+    ":" +
+    minutes +
+    " +00:00";
 
   return dateStr;
 }
