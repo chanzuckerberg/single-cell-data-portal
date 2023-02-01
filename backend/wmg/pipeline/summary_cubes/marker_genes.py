fix(fmg): marker gene calculation ignores cell types that have no healthy cells

backend/wmg/pipeline/summary_cubes/marker_genes.py
@@ -8,6 +8,8 @@ from backend.wmg.data.schemas.marker_gene_cube_schema import marker_genes_schema
 from backend.wmg.data.snapshot import CELL_COUNTS_CUBE_NAME, MARKER_GENES_CUBE_NAME
 from backend.wmg.data.utils import create_empty_cube, log_func_runtime
 from backend.wmg.pipeline.summary_cubes.calculate_markers import get_markers
+from backend.wmg.data.constants import NORMAL_CELL_DISEASE_ONTOLOGY_TERM_ID
+from backend.common.utils.exceptions import MarkerGeneCalculationException
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
@@ -19,10 +21,18 @@ def extract_tissue_celltype_organism(corpus_path: str) -> pd.DataFrame:
     with tiledb.open(f"{corpus_path}/{CELL_COUNTS_CUBE_NAME}") as array:
         yield (
             array.query(
-                attrs=["cell_type_ontology_term_id"], dims=["organism_ontology_term_id", "tissue_ontology_term_id"]
+                attrs=["cell_type_ontology_term_id", "disease_ontology_term_id"],
+                dims=["organism_ontology_term_id", "tissue_ontology_term_id"],
             )
             .df[:]
-            .groupby(["tissue_ontology_term_id", "cell_type_ontology_term_id", "organism_ontology_term_id"])
+            .groupby(
+                [
+                    "tissue_ontology_term_id",
+                    "cell_type_ontology_term_id",
+                    "organism_ontology_term_id",
+                    "disease_ontology_term_id",
+                ]
+            )
             .first()
         )
 
@@ -33,6 +43,16 @@ def create_marker_genes_cube(corpus_path: str):
     Create marker genes cube and write to disk
     """
     with extract_tissue_celltype_organism(corpus_path) as tco:
+        healthy_filter = (
+            tco.index.get_level_values("disease_ontology_term_id").astype("str") == NORMAL_CELL_DISEASE_ONTOLOGY_TERM_ID
+        )
+        if np.any(healthy_filter):
+            tco = tco[healthy_filter]
+        tco = (
+            tco.reset_index()
+            .groupby(["tissue_ontology_term_id", "cell_type_ontology_term_id", "organism_ontology_term_id"])
+            .first()
+        )
         uniq_tissues = tco.index.levels[0]
         tissues = np.array(list(tco.index.get_level_values(0)))
         cell_types = np.array(list(tco.index.get_level_values(1)))
@@ -57,10 +77,16 @@ def create_marker_genes_cube(corpus_path: str):
                 "tissue_ontology_term_ids": [tiss],
                 "organism_ontology_term_id": organism,
             }
-            t_markers = get_markers(target, context, corpus=corpus_path, test="ttest", percentile=0.05, n_markers=None)
-            b_markers = get_markers(
-                target, context, corpus=corpus_path, test="binomtest", percentile=0.3, n_markers=None
-            )
+            try:
+                t_markers = get_markers(
+                    target, context, corpus=corpus_path, test="ttest", percentile=0.05, n_markers=None
+                )
+                b_markers = get_markers(
+                    target, context, corpus=corpus_path, test="binomtest", percentile=0.3, n_markers=None
+                )
+            except MarkerGeneCalculationException:
+                logger.info("Error finding markers for tissue: %s, cell type: %s, organism: %s", tiss, ct, organism)
+                continue
             gc.collect()
 
             all_marker_genes = set(list(t_markers.keys())).union(list(b_markers.keys()))