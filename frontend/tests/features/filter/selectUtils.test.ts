8bda0fd95b784c0294efd94f990c375c3cad36f8
MillenniumFalconMechanic <mim@clevercanary.com>
2023-02-10 15:42:05 -0800

    fix: corrected filter payload field. #4176.

---
+++ frontend/tests/features/filter/selectUtils.test.ts
@@ -0,0 +1,26 @@
+/**
+ * Test suite for select filter-related utils.
+ */
+
+import { expect, test } from "@playwright/test";
+import { buildAnalyticsPayload } from "src/common/hooks/useCategoryFilter/common/selectUtils";
+
+const { describe } = test;
+
+describe("filter", () => {
+  const KEY_PAYLOAD = "payload";
+  const KEY_TISSUE = "tissue";
+  const VALUE_TISSUE = "brain";
+  describe("Analytics", () => {
+    test("builds analytics payload with no payload key", () => {
+      const payload = buildAnalyticsPayload(VALUE_TISSUE);
+      expect(payload[KEY_PAYLOAD]).toBeTruthy();
+      expect(payload[KEY_PAYLOAD]).toEqual(VALUE_TISSUE);
+    });
+    test("builds analytics payload with payload key", () => {
+      const payload = buildAnalyticsPayload(VALUE_TISSUE, KEY_TISSUE);
+      expect(payload[KEY_TISSUE]).toBeTruthy();
+      expect(payload[KEY_TISSUE]).toEqual(VALUE_TISSUE);
+    });
+  });
+});
