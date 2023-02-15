45c9d2c7ad4202e7dcba0f9b20b518d541f4ba12
Emanuele Bezzi <ebezzi@chanzuckerberg.com>
2023-02-15 16:48:54 -0500

    chore: build FE container at build time (#4203)
    * checkpoint.
    
    * install sponge.
    
    * Bump.
    
    * Checkpoint.
    
    * Make it possible to run container init multiple times.
    
    * Add fancier comments.
    
    * Handle local dev better.
    
    * We're running everything as root for now :'(
    
    * patch.
    
    * replace next build with npm run build
    
    * lower memory to 4G
    
    * lower health check interval to 15
    
    * add API URLs to local.js
    
    * fix test
    
    * copy configs
    
    * copy configs
    
    ---------
    
    Co-authored-by: Jessica Gadling <jgadling@chanzuckerberg.com>
    Co-authored-by: Timmy Huang <thuang@chanzuckerberg.com>

--- frontend/src/configs/prod.js
+++
@@ -1,7 +0,0 @@
-const configs = {
-  API_URL: "https://api.cellxgene.cziscience.com",
-  PLAUSIBLE_DATA_DOMAIN: "cellxgene.cziscience.com",
-  SENTRY_DEPLOYMENT_ENVIRONMENT: "production",
-};
-
-if (typeof module !== "undefined") module.exports = configs;
