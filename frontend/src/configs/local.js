f431684c30659b589a04cac98d151ec832c52e12
Timmy Huang <thuang@chanzuckerberg.com>
2023-02-15 12:00:23 -0800

    add API URLs to local.js

--- frontend/src/configs/local.js
+++ frontend/src/configs/local.js
@@ -2,8 +2,15 @@
 // to a new file named `configs.js` in this directory.
 
 const configs = {
+  // Dev
+  // API_URL: "https://api.cellxgene.dev.single-cell.czi.technology",
+  // Staging
+  // API_URL: "https://api.cellxgene.staging.single-cell.czi.technology",
+  // Prod
+  // API_URL: "https://api.cellxgene.cziscience.com",
+  // Local container
   API_URL: "https://backend.corporanet.local:5000",
-  PLAUSIBLE_DATA_DOMAIN: "cellxgene.staging.single-cell.czi.technology"
+  PLAUSIBLE_DATA_DOMAIN: "cellxgene.staging.single-cell.czi.technology",
 };
 
 if (typeof module !== "undefined") module.exports = configs;
