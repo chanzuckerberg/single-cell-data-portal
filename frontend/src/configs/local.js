cb21c86b92f579b1d9a08709d2bb33a5cf09a720
Timmy Huang <thuang@chanzuckerberg.com>
2023-02-15 12:06:41 -0800

    fix test

--- frontend/src/configs/local.js
+++ frontend/src/configs/local.js
@@ -3,13 +3,13 @@
 
 const configs = {
   // Dev
-  // API_URL: "https://api.cellxgene.dev.single-cell.czi.technology",
+  API_URL: "https://api.cellxgene.dev.single-cell.czi.technology",
   // Staging
   // API_URL: "https://api.cellxgene.staging.single-cell.czi.technology",
   // Prod
   // API_URL: "https://api.cellxgene.cziscience.com",
   // Local container
-  API_URL: "https://backend.corporanet.local:5000",
+  // API_URL: "https://backend.corporanet.local:5000",
   PLAUSIBLE_DATA_DOMAIN: "cellxgene.staging.single-cell.czi.technology",
 };
 
