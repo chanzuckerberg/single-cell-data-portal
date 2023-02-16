b024a9544e52f3ced6255971b7ce9bc789ff7b32
Emanuele Bezzi <ebezzi@chanzuckerberg.com>
2023-02-16 12:11:20 -0500

    chore: restore js env files (#4220)

---
+++ frontend/src/configs/prod.js
@@ -0,0 +1,7 @@
+const configs = {
+  API_URL: "https://api.cellxgene.cziscience.com",
+  PLAUSIBLE_DATA_DOMAIN: "cellxgene.cziscience.com",
+  SENTRY_DEPLOYMENT_ENVIRONMENT: "production",
+};
+
+if (typeof module !== "undefined") module.exports = configs;
