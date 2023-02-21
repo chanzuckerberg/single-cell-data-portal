bad7f95b09807c4539c32ad722655a33fe1904c6
Seve Badajoz <severiano.badajoz@chanzuckerberg.com>
2023-02-21 10:14:37 -0800

    use blueprint toast instead of notification

--- frontend/src/views/WheresMyGene/components/GeneSearchBar/components/ShareButton/index.tsx
+++ frontend/src/views/WheresMyGene/components/GeneSearchBar/components/ShareButton/index.tsx
@@ -1,6 +1,10 @@
+import { Intent } from "@blueprintjs/core";
+import { IconNames } from "@blueprintjs/icons";
+import { Tooltip } from "czifui";
 import { useCallback, useContext, useEffect, useState } from "react";
 import { usePrimaryFilterDimensions } from "src/common/queries/wheresMyGene";
 import { isSSR } from "src/common/utils/isSSR";
+import Toast from "src/views/Collection/components/Toast";
 import {
   DispatchContext,
   StateContext,
@@ -15,12 +19,18 @@ export default function ShareButton(): JSX.Element {
   const { selectedFilters, selectedTissues, selectedGenes } = state;
   const { isLoading: isLoadingFilterDims } = usePrimaryFilterDimensions();
   const dispatch = useContext(DispatchContext);
-  const [showURLCopyNotification, setShowURLCopyNotification] = useState(0);
+  // const [showURLCopyNotification, setShowURLCopyNotification] = useState(0);
 
   const copyShareUrl = useCallback(() => {
     if (!dispatch) return;
     generateAndCopyShareUrl(selectedFilters, selectedTissues, selectedGenes);
-    setShowURLCopyNotification((prev) => prev + 1);
+    // setShowURLCopyNotification((prev) => prev + 1);
+    Toast.show({
+      icon: IconNames.LINK,
+      intent: Intent.PRIMARY,
+      message: "Share link copied",
+      timeout: 8000,
+    });
   }, [selectedFilters, selectedTissues, selectedGenes, dispatch]);
 
   useEffect(() => {
@@ -35,7 +45,7 @@ export default function ShareButton(): JSX.Element {
 
   return (
     <>
-      {showURLCopyNotification > 0 && (
+      {/* {showURLCopyNotification > 0 && (
         <StyledNotification
           key={showURLCopyNotification}
           autoDismiss={false}
@@ -44,7 +54,8 @@ export default function ShareButton(): JSX.Element {
         >
           Share link copied
         </StyledNotification>
-      )}
+      )} */}
+
       <StyledButtonDiv className={EXCLUDE_IN_SCREENSHOT_CLASS_NAME}>
         <StyledLabel>Share</StyledLabel>
 
