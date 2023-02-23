2fcfb36f221c286ba8f2092596239711d40e8421
Nayib Gloria <55710092+nayib-jose-gloria@users.noreply.github.com>
2023-02-23 15:19:38 -0500

    chore: Remove old database code (#4251)
    * chore: delete deprecated backend db and entity class code
    
    Signed-off-by: nayib-jose-gloria <ngloria@chanzuckerberg.com>
    
    * delete unused scripts + remove corpora_orm and deprecated entities references where applicable
    
    Signed-off-by: nayib-jose-gloria <ngloria@chanzuckerberg.com>
    
    * lint fixes
    
    Signed-off-by: nayib-jose-gloria <ngloria@chanzuckerberg.com>
    
    * lint fixes
    
    Signed-off-by: nayib-jose-gloria <ngloria@chanzuckerberg.com>
    
    * remove unused enums
    
    Signed-off-by: nayib-jose-gloria <ngloria@chanzuckerberg.com>
    
    * stub out cxg admin scripts that relied on corpora_orm
    
    Signed-off-by: nayib-jose-gloria <ngloria@chanzuckerberg.com>
    
    * linting
    
    Signed-off-by: nayib-jose-gloria <ngloria@chanzuckerberg.com>
    
    ---------
    
    Signed-off-by: nayib-jose-gloria <ngloria@chanzuckerberg.com>

--- scripts/cxg_admin_scripts/dataset_details.py
+++ scripts/cxg_admin_scripts/dataset_details.py
@@ -4,51 +4,7 @@ import sys
 pkg_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "..."))  # noqa
 sys.path.insert(0, pkg_root)  # noqa
 
-from backend.common.entities import Collection, Dataset, DatasetAsset
-from backend.common.utils.db_session import db_session_manager
-
 
 def get_public_dataset_details():
     # id, name, organism, tissue, assay, sex, cell_count, explorer_url, and S3 uris
-    with db_session_manager() as session:
-
-        published_datasets = (
-            session.query(
-                Dataset.table.id,
-                Dataset.table.name,
-                Dataset.table.organism,
-                Dataset.table.tissue,
-                Dataset.table.assay,
-                Dataset.table.sex,
-                Dataset.table.cell_count,
-                Dataset.table.explorer_url,
-            )
-            .join(Dataset.table.collection)
-            .filter(
-                Dataset.table.published == "TRUE",
-                Collection.table.visibility == "PUBLIC",
-                Dataset.table.tombstone == "FALSE",
-            )
-            .all()
-        )
-        datasets = {}
-        for dataset in published_datasets:
-            dataset_data = {}
-            dataset_data["name"] = dataset[1]
-            dataset_data["organisms"] = [x["label"] for x in dataset[2]]
-            dataset_data["tissue"] = [x["label"] for x in dataset[3]]
-            dataset_data["assay"] = [x["label"] for x in dataset[4]]
-            dataset_data["sex"] = [x["label"] for x in dataset[5]]
-            dataset_data["cell_count"] = dataset[6]
-            dataset_data["explorer_url"] = dataset[7]
-            dataset_data["s3_uris"] = []
-            datasets[dataset[0]] = dataset_data
-
-        s3_uris = session.query(DatasetAsset.table.dataset_id, DatasetAsset.table.s3_uri).filter(
-            DatasetAsset.table.dataset_id.in_(datasets.keys())
-        )
-
-        for s3_uri in s3_uris:
-            datasets[s3_uri[0]]["s3_uris"].append(s3_uri[1])
-
-    return datasets
+    pass
--- scripts/cxg_admin_scripts/tombstones.py
+++ scripts/cxg_admin_scripts/tombstones.py
@@ -1,17 +1,10 @@
-import json
 import os
 import sys
 
-import click
 from click import Context
 
 pkg_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "..."))  # noqa
 sys.path.insert(0, pkg_root)  # noqa
-
-from backend.common.entities.collection import Collection
-from backend.common.entities.dataset import Dataset
-from backend.common.utils.db_session import db_session_manager
-from backend.common.utils.json import CustomJSONEncoder
 
 
 def tombstone_collection(ctx: Context, uuid: str):
@@ -29,36 +22,7 @@ def tombstone_collection(ctx: Context, uuid: str):
     :param ctx: command context
     :param uuid: ID that identifies the collection to tombstone
     """
-
-    with db_session_manager() as session:
-        collection = Collection.get_collection(session, uuid, include_tombstones=True)
-
-        if not collection:
-            click.echo(f"Collection:{uuid} not found!")
-            exit(0)
-
-        click.echo(
-            json.dumps(
-                collection.to_dict(remove_attr=["datasets", "links", "genesets"]),
-                sort_keys=True,
-                indent=2,
-                cls=CustomJSONEncoder,
-            )
-        )
-        click.confirm(
-            f"Are you sure you want to tombstone the collection:{uuid} from cellxgene:{ctx.obj['deployment']}?",
-            abort=True,
-        )
-
-        collection.tombstone_collection()
-
-        tombstoned = Collection.get_collection(session, uuid, include_tombstones=True)
-        if tombstoned.tombstone:
-            click.echo(f"Tombstoned collection:{uuid}")
-            exit(0)
-        else:
-            click.echo(f"Failed to tombstone collection:{uuid}")
-            exit(1)
+    pass
 
 
 def tombstone_dataset(ctx, uuid):
@@ -69,27 +33,4 @@ def tombstone_dataset(ctx, uuid):
       ./scripts/cxg_admin.py --deployment staging tombstone-dataset "57cf1b53-af10-49e5-9a86-4bc70d0c92b6"
 
     """
-
-    with db_session_manager() as session:
-        dataset = Dataset.get(session, uuid, include_tombstones=True)
-        if dataset is not None:
-            click.echo(
-                json.dumps(dataset.to_dict(remove_attr=["collection"]), sort_keys=True, indent=2, cls=CustomJSONEncoder)
-            )
-            click.confirm(
-                f"Are you sure you want to delete the dataset:{uuid} from cellxgene:{ctx.obj['deployment']}?",
-                abort=True,
-            )
-            dataset.asset_deletion()
-            dataset.tombstone_dataset_and_delete_child_objects()
-            dataset = Dataset.get(session, uuid)
-            tombstoned = Dataset.get(session, uuid, include_tombstones=True)
-            if tombstoned and dataset is None:
-                click.echo(f"Tombstoned dataset:{uuid}")
-                exit(0)
-            else:
-                click.echo(f"Failed to tombstone dataset:{uuid}")
-                exit(1)
-        else:
-            click.echo(f"Dataset:{uuid} not found!")
-            exit(0)
+    pass
