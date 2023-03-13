f01ab5c5a9101fda6d42110294aed865404d0a54
Andrew Tolopko <atolopko-czi@users.noreply.github.com>
2023-03-13 13:36:54 -0400

    chore: recreate wmg reqs primary only (#4337)
    * Bumps werkzeug from 2.0.3 to 2.2.3.
    
    For security vulnerabilities:
    https://github.com/chanzuckerberg/single-cell-data-portal/security/dependabot/39
    https://github.com/chanzuckerberg/single-cell-data-portal/security/dependabot/38
    
    * bump pillow from 9.0.1 to 9.3.0
    
    For security vulnerability:
    https://github.com/chanzuckerberg/single-cell-data-portal/security/dependabot/31
    
    * move method to avoid fix intra-package dependency
    
    wmg_processing tests relied on wmg.api package, which required otherwise
    unnecessary pip packages (web-related)
    
    * specify only primary wmg pipeline requirements
    
    * add missing wmg pipeline requirements
    
    * lint
    
    * assert almost equal to 7 decimal places
    
    * update to use pandas testing since numpy testing doesnt work with dicts
    
    ---------
    
    Co-authored-by: atarashansky <atarashansky@czimacos3990.lan>

--- backend/wmg/api/v1.py
+++ backend/wmg/api/v1.py
@@ -18,6 +18,7 @@ from backend.wmg.data.query import (
 from backend.wmg.data.rollup import rollup_across_cell_type_descendants
 from backend.wmg.data.schemas.cube_schema import expression_summary_non_indexed_dims
 from backend.wmg.data.snapshot import WmgSnapshot, load_snapshot
+from backend.wmg.data.utils import depluralize, find_all_dim_option_values, find_dim_option_values
 
 # TODO: add cache directives: no-cache (i.e. revalidate); impl etag
 #  https://app.zenhub.com/workspaces/single-cell-5e2a191dad828d52cc78b028/issues/chanzuckerberg/single-cell-data
@@ -113,87 +114,6 @@ def fetch_datasets_metadata(snapshot: WmgSnapshot, dataset_ids: Iterable[str]) -
         snapshot.dataset_dict.get(dataset_id, dict(id=dataset_id, label="", collection_id="", collection_label=""))
         for dataset_id in dataset_ids
     ]
-
-
-def find_all_dim_option_values(snapshot: WmgSnapshot, dimension: str) -> list:
-    all_filter_options = set()
-    for key in snapshot.filter_relationships:
-        if key.startswith(dimension):
-            all_filter_options.add(key)
-        if dimension in snapshot.filter_relationships[key]:
-            all_filter_options = set(all_filter_options).union(snapshot.filter_relationships[key][dimension])
-    return [option.split("__")[1] for option in all_filter_options]
-
-
-def find_dim_option_values(criteria: Dict, snapshot: WmgSnapshot, dimension: str) -> list:
-    """Find values for the specified dimension that satisfy the given filtering criteria,
-    ignoring any criteria specified for the given dimension."""
-
-    filter_options_criteria = dict(criteria)
-    # Remove gene_ontology_term_ids from the criteria as it is not an eligible cross-filter dimension.
-    filter_options_criteria.pop("gene_ontology_term_ids", None)
-
-    # depluralize `dimension` if necessary
-    dimension = dimension[:-1] if dimension[-1] == "s" else dimension
-
-    # each element  in `linked_filter_sets` corresponds to the set of filters linked to the attributes specified for a corresponding criteria key
-    linked_filter_sets = []
-
-    # `all_criteria_attributes` is the set of all attributes specified across all criteria
-    all_criteria_attributes = set()
-
-    for key in filter_options_criteria:
-        attrs = filter_options_criteria[key]
-
-        # depluralize `key` if necessary
-        key = key[:-1] if key[-1] == "s" else key
-
-        # ignore the criteria for the specified dimension
-        if key != dimension:
-            if isinstance(attrs, list):
-                if len(attrs) > 0:
-                    # prepend the key to each attribute value
-                    prefixed_attributes = [key + "__" + val for val in attrs]
-                    all_criteria_attributes = all_criteria_attributes.union(prefixed_attributes)
-
-                    # for each attribute (attr) in `prefixed_attributes`,
-                    # get the set of filters for the specified dimension that are linked to `attr`
-                    linked_filter_set = set()
-                    for attr in prefixed_attributes:
-                        if dimension in snapshot.filter_relationships[attr]:
-                            linked_filter_set = linked_filter_set.union(
-                                set(snapshot.filter_relationships[attr][dimension])
-                            )
-
-                    linked_filter_sets.append(linked_filter_set)
-            else:
-                if attrs != "":
-                    prefixed_attribute = key + "__" + attrs
-                    all_criteria_attributes.add(prefixed_attribute)
-                    if dimension in snapshot.filter_relationships[prefixed_attribute]:
-                        linked_filter_sets.append(set(snapshot.filter_relationships[prefixed_attribute][dimension]))
-
-    # the candidate options are the intersection of the sets of linked filters for each criteria key
-    if len(linked_filter_sets) > 1:
-        candidate_options = linked_filter_sets[0].intersection(*linked_filter_sets[1:])
-    else:
-        candidate_options = linked_filter_sets[0]
-
-    # each valid option MUST be linked to at least one attribute specified in the criteria
-    # otherwise, there will be no data to display if that particular option is selected because
-    # the intersection will be null.
-    valid_options = []
-    for v in candidate_options:
-        loop_back_options = snapshot.filter_relationships[v]
-        all_loop_back_options = []
-        for dim in loop_back_options:
-            all_loop_back_options.extend(loop_back_options[dim])
-
-        if len(set(all_loop_back_options).intersection(all_criteria_attributes)) > 0:
-            valid_options.append(v)
-
-    # remove the prefix from each valid option and return the result
-    return [option.split("__")[1] for option in valid_options]
 
 
 def is_criteria_empty(criteria: WmgFiltersQueryCriteria) -> bool:
@@ -376,7 +296,3 @@ def build_ordered_cell_types_by_tissue_update_depths(x: DataFrame):
                     break
 
     return x
-
-
-def depluralize(x):
-    return x[:-1] if x[-1] == "s" else x
