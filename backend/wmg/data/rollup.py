91f0753a7c470a08ef49f1312af9802686923558
atarashansky <atarashansky@czimacos3990.lan>
2023-03-15 10:50:20 -0700

    fix unit test

--- backend/wmg/data/rollup.py
+++ backend/wmg/data/rollup.py
@@ -150,7 +150,7 @@ def rollup_across_cell_type_descendants(
     summed = summed[tuple(dim_indices)]
     dtypes = numeric_df.dtypes
     for col, array in zip(numeric_df.columns, summed.T):
-        if ignore_cols and col not in ignore_cols:
+        if ignore_cols and col not in ignore_cols or not ignore_cols:
             df[col] = array.astype(dtypes[col])
 
     return df
