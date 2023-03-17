165e4aeaf93c0563277d80820602e06ba8b3d43a
ashin-czi <ashin@contractor.chanzuckerberg.com>
2023-03-17 14:10:53 -0400

    fix(wmg): fixing compare and crossfiltering test

--- frontend/src/common/queries/wheresMyGene.ts
+++ frontend/src/common/queries/wheresMyGene.ts
@@ -807,7 +807,11 @@ function addCellTypeRowToResult({
      * (thuang): We manually indent 4 spaces instead of using CSS, so we don't
      * have to update SVG render function to mimic CSS padding
      */
-    const name = isAggregated ? rawName : `    ${rawName || "unknown"}`;
+    const name = isAggregated 
+      ? rawName 
+      : compareOptionId === COMPARE_OPTION_ID_FOR_UNKNOWN
+        ? `    ${rawName || COMPARE_OPTION_ID_FOR_UNKNOWN}`
+        : `    ${rawName || compareOptionId}`;
 
     if (isAggregated) {
       cellTypeName = rawName;
