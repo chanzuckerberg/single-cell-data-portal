e903dbb0a4ca189edd256bb5ec55a58f7155713b
nayib-jose-gloria <ngloria@chanzuckerberg.com>
2023-03-21 10:14:55 -0400

    implement get dataset version by id
    Signed-off-by: nayib-jose-gloria <ngloria@chanzuckerberg.com>

--- backend/curation/api/v1/curation/dataset_versions/dataset_version_id/actions.py
+++ backend/curation/api/v1/curation/dataset_versions/dataset_version_id/actions.py
@@ -1,4 +1,8 @@
+from flask import jsonify, make_response
+
+from backend.common.utils.http_exceptions import NotFoundHTTPException
 from backend.curation.api.v1.curation.collections.common import (
+    reshape_dataset_for_curation_api,
     validate_uuid_else_forbidden,
 )
 from backend.portal.api.providers import get_business_logic
@@ -11,6 +15,9 @@ def get(dataset_version_id: str):
     business_logic = get_business_logic()
     validate_uuid_else_forbidden(dataset_version_id)
 
-    business_logic.get_dataset_version(dataset_version_id)
-    # get collection version ID
-    pass
+    dataset_version = business_logic.get_dataset_version(dataset_version_id)
+    if dataset_version is None:
+        raise NotFoundHTTPException("Dataset not found")
+
+    response_body = reshape_dataset_for_curation_api(dataset_version, use_canonical_url=False)
+    return make_response(jsonify(response_body), 200)
