5329011d5aaf8350b2d046665d481b0b1fc4d40c
Charles Moga <cmoga@contractor.chanzuckerberg.com>
2023-04-08 14:47:19 +0100

    Added searching tissue

--- frontend/tests/features/wheresMyGene/addTissue.test.ts
+++ frontend/tests/features/wheresMyGene/addTissue.test.ts
@@ -1,14 +1,13 @@
 import { expect, Page, test } from "@playwright/test";
 import { TEST_URL } from "../../common/constants";
 import { ROUTES } from "src/common/constants/routes";
-import {
-  ADD_GENE_BTN,
-  ADD_GENE_LBL,
-  ADD_TISSUE_BTN,
-  ADD_TISSUE_LBL,
-} from "tests/utils/constants";
+import { ADD_TISSUE_BTN, ADD_TISSUE_LBL } from "tests/utils/constants";
 const REGEX = /^\d+\.?\d{0,2}$/;
 const { describe } = test;
+const FMG_EXCLUDE_TISSUES = ["blood"];
+const CELL_COUNT_ID = "cell-count";
+const CELL_TYPE_NAME_ID = "cell-type-name";
+const MARKER_GENE_BUTTON_ID = "marker-gene-button";
 
 function goToWMG(page: Page) {
   return Promise.all([
@@ -19,8 +18,45 @@ function goToWMG(page: Page) {
     page.goto(`${TEST_URL}${ROUTES.WHERE_IS_MY_GENE}`),
   ]);
 }
+async function verifyAddedTissue(page: Page, tissue: string) {
+  // STEP 1 & Add Tissues texts should disappear
+  await expect(page.getByText("STEP 1")).not.toBeVisible();
+  await expect(page.getByTestId("Add Tissues")).not.toBeVisible();
+
+  // selected tissue should be visible
+  await expect(page.getByTestId(`cell-type-labels-${tissue}`)).toBeVisible();
+
+  // verify cell counts: name, icon and count
+  const CELL_COUNTS = page.getByTestId("cell-type-label-count");
+  for (let i = 0; i < (await CELL_COUNTS.count()); i++) {
+    const COUNT = await CELL_COUNTS.nth(i)
+      .getByTestId(CELL_COUNT_ID)
+      .textContent();
+    // cell name
+    expect(
+      CELL_COUNTS.nth(i).getByTestId(CELL_TYPE_NAME_ID).textContent()
+    ).not.toBeUndefined();
+
+    // info icon: if not blood and count is > 25
+    if (
+      !FMG_EXCLUDE_TISSUES.includes(tissue) &&
+      Number(COUNT?.replace(/\D/g, "")) > 25
+    ) {
+      expect(
+        CELL_COUNTS.nth(i).getByTestId(MARKER_GENE_BUTTON_ID)
+      ).toBeVisible();
+    }
+
+    // cell count
+    expect(COUNT?.replace(/\D/g, "")).toMatch(REGEX);
+  }
+}
+
 describe("Add tissue tests", () => {
-  test.only("Should select tissue by keyboard down key", async ({ page }) => {
+  test.only("Should select tissue using keyboard arrow key to select", async ({
+    page,
+  }) => {
+    const TISSUE = "lung";
     await goToWMG(page);
     // click +Tissue button
     await page.getByTestId(ADD_TISSUE_BTN).click();
@@ -32,31 +68,22 @@ describe("Add tissue tests", () => {
 
     // close dropdown
     await page.keyboard.press("Escape");
-    // STEP 1 & Add Tissues texts should disappear
-    await expect(page.getByTestId(ADD_TISSUE_LBL)).not.toBeVisible();
-    await expect(page.getByTestId(ADD_TISSUE_LBL)).not.toBeVisible();
 
-    // select tissue should be visible
-    await expect(page.getByTestId("cell-type-labels-lung")).toBeVisible();
+    // verify selected tissue details
+    await verifyAddedTissue(page, TISSUE);
+  });
 
-    // verify cell counts: name, icon and count
-    const CELL_COUNTS = page.getByTestId("cell-type-label-count");
-    expect(await CELL_COUNTS.count()).toBe(151); // flaky, will break with changes in test data
-    for (let i = 0; i < (await CELL_COUNTS.count()); i++) {
-      // cell name
-      expect(
-        CELL_COUNTS.nth(i).getByTestId("cell-type-name").textContent()
-      ).not.toBeUndefined();
+  test.only("Should select tissue by searching", async ({ page }) => {
+    const TISSUE = "blood";
+    await goToWMG(page);
+    // click +Tissue button
+    await page.getByTestId(ADD_TISSUE_BTN).click();
+    await page.getByPlaceholder("Search").type(TISSUE);
+    await page.getByText(TISSUE).click();
 
-      // info icon
-      // expect(
-      //   CELL_COUNTS.nth(i).getByTestId("marker-gene-button")
-      // ).toBeVisible();
-      // cell count
-      const COUNT = await CELL_COUNTS.nth(i)
-        .getByTestId("cell-count")
-        .textContent();
-      expect(COUNT?.replace("k", "")).toMatch(REGEX);
-    }
+    // close dropdown
+    await page.keyboard.press("Escape");
+    // verify selected tissue details
+    await verifyAddedTissue(page, TISSUE);
   });
 });
