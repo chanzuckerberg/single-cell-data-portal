ba6583e0f40003eb84f226caeb6c1e830354cb06
victorikeokoro6 <vikeokoro@contractor.chanzuckerberg.com>
2023-04-10 17:40:49 +0100

    fixes PR comments

--- frontend/tests/common/utils.ts
+++ frontend/tests/common/utils.ts
@@ -1,6 +1,7 @@
 import { ROUTES } from "src/common/constants/routes";
 import { TEST_URL } from "./constants";
-import { Page } from "@playwright/test";
+import { expect, Page } from "@playwright/test";
+import { getTestID } from "tests/utils/selectors";
 
 export function goToWMG(page: Page) {
   return Promise.all([
@@ -11,3 +12,29 @@ export function goToWMG(page: Page) {
     page.goto(`${TEST_URL}${ROUTES.WHERE_IS_MY_GENE}`),
   ]);
 }
+
+export const selectAndDeselectOption = async (
+  page: Page,
+  filterName: string
+) => {
+  const FIRST_OPTION = '[data-option-index="0"]';
+  // click the filter
+  await page.getByTestId(filterName).click({ position: { x: 0, y: 0 } });
+
+  // select the first option
+  await page.locator(FIRST_OPTION).click();
+
+  // close the pop-up
+  await page.keyboard.press("Escape");
+
+  const filter_label = `${getTestID(filterName)} [role="button"]`;
+  // expect the selected filter to be visible
+  await expect(page.locator(filter_label)).toBeVisible();
+
+  // click the cancel button
+  await page.getByTestId("ClearIcon").click();
+
+  // verify the selected filter is not visible
+  const visibility = await page.locator(filter_label).isVisible();
+  expect(visibility).toBeFalsy();
+};
--- frontend/tests/features/wheresMyGene/landingPageFilter.test.ts
+++ frontend/tests/features/wheresMyGene/landingPageFilter.test.ts
@@ -2,15 +2,10 @@
  * Test suite for select filter-related utils.
  */
 import { expect, test } from "@playwright/test";
-import { goToWMG } from "tests/common/utils";
+import { goToWMG, selectAndDeselectOption } from "tests/common/utils";
 import { isDevStagingProd } from "tests/utils/helpers";
-import { getTestID } from "tests/utils/selectors";
 const CHEVRON_LEFT = '[data-icon="chevron-left"]';
-const FIRST_OPTION = '[data-option-index="0"]';
-const DATASET_FILTER = "dataset-filter";
-const DISEASE_FILTER = "disease-filter";
-const SELF_REPORTED_ETHNICITY_FILTER = "self-reported-ethnicity-filter";
-const SEX_FILTER = "sex-filter";
+
 const { describe, skip } = test;
 
 describe("Left side bar", () => {
@@ -21,103 +16,34 @@ describe("Left side bar", () => {
   });
 
   test("Left side bar collapse and expand", async ({ page }) => {
-    // click cheevron left to collaspe the left tab
+    // click chevron left to collapse the left tab
     await page.locator(CHEVRON_LEFT).click();
 
-    //verify the left tab is collapsed
-
-    expect(await page.getByTestId(DATASET_FILTER).isVisible()).toBeFalsy();
+    // verify the left tab is collapsed
+    expect(await page.getByTestId("dataset-filter").isVisible()).toBeFalsy();
   });
+
   test("Should be able select and de-select options for datasetfilter", async ({
     page,
   }) => {
-    // click the dataset filter
-    await page
-      .getByTestId(DATASET_FILTER)
-
-      .click({ position: { x: 0, y: 0 } });
-
-    // select the first dataset
-    await page.locator(FIRST_OPTION).click();
-
-    //close popup
-    await page.keyboard.press("Escape");
-
-    const filter_label = `${getTestID(DATASET_FILTER)} [role="button"]`;
-    //expect the  selected filter to be visible
-    await expect(page.locator(filter_label)).toBeVisible();
-
-    //click the cancel button
-    await page.getByTestId("ClearIcon").click();
-    const visibility = await page.locator(filter_label).isVisible();
-    expect(visibility).toBeFalsy();
+    await selectAndDeselectOption(page, "dataset-filter");
   });
+
   test("Should be able select and de-select options for disease filter", async ({
     page,
   }) => {
-    // click the dataset filter
-    await page
-      .getByTestId("disease-filter")
-      .click({ position: { x: 0, y: 0 } });
-
-    // select the first dataset
-    await page.locator(FIRST_OPTION).click();
-
-    //click filter again to close pop up
-    await page.keyboard.press("Escape");
-
-    const filter_label = `${getTestID(DISEASE_FILTER)} [role="button"]`;
-    //expect the  selected filter to be visible\
-
-    await expect(page.locator(filter_label)).toBeVisible();
-
-    //click the cancel button
-    await page.getByTestId("ClearIcon").click();
-    const visibility = await page.locator(filter_label).isVisible();
-    expect(visibility).toBeFalsy();
+    await selectAndDeselectOption(page, "disease-filter");
   });
-  test.only("Should be able select and de-select options for Self-Reported Ethnicity filter", async ({
+
+  test("Should be able select and de-select options for Self-Reported Ethnicity filter", async ({
     page,
   }) => {
-    // click the dataset filter
-    await page
-      .getByTestId("self-reported-ethnicity-filter")
-      .click({ position: { x: 0, y: 0 } });
-
-    // select the first dataset
-    await page.locator(FIRST_OPTION).click();
-
-    //close pop up
-    await page.keyboard.press("Escape");
-    const filter_label = `${getTestID(
-      SELF_REPORTED_ETHNICITY_FILTER
-    )} [role="button"]`;
-    //expect the  selected filter to be visible
-    await expect(page.locator(filter_label)).toBeVisible();
-
-    //click the cancel button
-    await page.getByTestId("ClearIcon").click();
-    const visibility = await page.locator(filter_label).isVisible();
-    expect(visibility).toBeFalsy();
+    await selectAndDeselectOption(page, "self-reported-ethnicity-filter");
   });
-  test.only("Should be able select and de-select options for Self-Reported sex-filter", async ({
+
+  test("Should be able select and de-select options for Self-Reported sex-filter", async ({
     page,
   }) => {
-    // click the dataset filter
-    await page.getByTestId("sex-filter").click({ position: { x: 0, y: 0 } });
-
-    // select the first dataset
-    await page.locator(FIRST_OPTION).click();
-
-    // close pop up
-    await page.keyboard.press("Escape");
-    const filter_label = `${getTestID(SEX_FILTER)} [role="button"]`;
-    //expect the  selected filter to be visible
-    await expect(page.locator(filter_label)).toBeVisible();
-
-    //click the cancel button
-    await page.getByTestId("ClearIcon").click();
-    const visibility = await page.locator(filter_label).isVisible();
-    expect(visibility).toBeFalsy();
+    await selectAndDeselectOption(page, "sex-filter");
   });
 });
