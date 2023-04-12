db2ccd879b6ae0f2ccd3f2cf4fba8e1bc5c1cfa6
victorikeokoro6 <vikeokoro@contractor.chanzuckerberg.com>
2023-04-12 02:28:28 +0100

    filter with page data plot

--- frontend/tests/features/wheresMyGene/snapshot.test.ts
+++ frontend/tests/features/wheresMyGene/snapshot.test.ts
@@ -11,6 +11,7 @@ import {
   selectTissueandGeneOption,
 } from "../../utils/xgeneUtils";
 import { isDevStagingProd } from "tests/utils/helpers";
+import { getText } from "tests/utils/selectors";
 const CHEVRON_LEFT = '[data-icon="chevron-left"]';
 
 const { describe, skip } = test;
@@ -34,18 +35,21 @@ describe("Left side bar", () => {
   test.only("should show a visual change when selecting and de-selecting filter option", async ({
     page,
   }) => {
+    await page.pause();
     const elementHandle = await page.locator('[id="heatmap-container-id"]');
 
-    const beforeScreenshotPath = path.resolve("../before-screenshot.png");
-    const afterScreenshotPath = path.resolve("../after-screenshot.png");
-
+    const beforeScreenshotPath = path.resolve("../before-filter.png");
+    const afterScreenshotPath = path.resolve("../after-filter.png");
     await elementHandle.screenshot({ path: beforeScreenshotPath });
     await selectFIlterOption(page, "dataset-filter");
+
+    //wait for filter to reflect on the UI
+    await page.locator(getText("Loading")).waitFor({ state: "hidden" });
     await elementHandle.screenshot({ path: afterScreenshotPath });
 
     expect(
       await elementHandle.screenshot({ path: afterScreenshotPath })
-    ).toMatchSnapshot(beforeScreenshotPath);
+    ).toMatchSnapshot(beforeScreenshotPath, { maxDiffPixels: 5 });
 
     await deSelectFIlterOption(page, "dataset-filter");
   });
