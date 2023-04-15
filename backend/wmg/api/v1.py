2acd8782d71e02748a1c7b393113051c7503fd51
atarashansky <atarashansky@CZIMACOS3990.hsd1.ma.comcast.net>
2023-04-15 14:00:10 -0400

    fix

--- backend/wmg/api/v1.py
+++ backend/wmg/api/v1.py
@@ -183,7 +183,7 @@ def build_filter_dims_values(criteria: WmgFiltersQueryCriteria, snapshot: WmgSna
             dims["self_reported_ethnicity_ontology_term_id"]
         ),
         tissue_terms=build_ontology_term_id_label_mapping(dims["tissue_ontology_term_id"]),
-        cell_type_terms=build_ontology_term_id_label_mapping(dims["cell_type_ontoloy_term_id"]),
+        cell_type_terms=build_ontology_term_id_label_mapping(dims["cell_type_ontology_term_id"]),
     )
 
     return response_filter_dims_values
