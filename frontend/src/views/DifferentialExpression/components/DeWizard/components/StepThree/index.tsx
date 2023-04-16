f5937f601ab14780bc45056db11b4588e870e088
atarashansky <atarashansky@CZIMACOS3990.hsd1.ma.comcast.net>
2023-04-16 19:30:58 -0400

    update

--- frontend/src/views/DifferentialExpression/components/DeWizard/components/StepThree/index.tsx
+++ frontend/src/views/DifferentialExpression/components/DeWizard/components/StepThree/index.tsx
@@ -8,10 +8,15 @@ import {
 import { StateContext } from "src/views/DifferentialExpression/common/store";
 import {
   CopyGenesButton,
-  MarkerStrengthContainer,
-  MarkerStrengthLabel,
+  QueryGroupSubTitle,
+  QueryGroupTitle,
   StyledHTMLTable,
+  TableWrapper,
+  ButtonsWrapper,
+  BackButton,
+  RestartButton,
 } from "./style";
+import { QueryGroupWithNames } from "src/views/DifferentialExpression/common/store/reducer";
 interface Props {
   setStep: (step: number) => void;
 }
@@ -29,19 +34,19 @@ export default function StepThree({ setStep }: Props): JSX.Element {
   const { data, isLoading: isLoadingPrimaryFilters } =
     usePrimaryFilterDimensions();
   const { genes: rawGenes } = data || {};
-  const { organismId, queryGroups } = useContext(StateContext);
+  const { organismId, queryGroupsWithNames } = useContext(StateContext);
 
   useEffect(() => {
     if (
       !rawGenes ||
-      !isLoadingPrimaryFilters ||
-      !isLoading ||
+      isLoadingPrimaryFilters ||
+      isLoading ||
       !rawDifferentialExpressionResults.length
     )
       return;
     const genes = rawGenes[organismId || ""];
     const genesById = genes.reduce((acc, gene) => {
-      return acc.set(gene.name.toLowerCase(), gene);
+      return acc.set(gene.id, gene);
     }, new Map<OntologyTerm["id"], OntologyTerm>());
 
     // map ids to name
@@ -69,6 +74,8 @@ export default function StepThree({ setStep }: Props): JSX.Element {
       return () => {
         const results = differentialExpressionResults[index];
         const genes = results.map((result) => result.name);
+        console.log(genes);
+        console.log(results);
         navigator.clipboard.writeText(genes.join(", "));
       };
     },
@@ -82,47 +89,87 @@ export default function StepThree({ setStep }: Props): JSX.Element {
     setStep(1);
   };
 
+  const namesToShow: string[][] = [];
+  for (const [index, queryGroupWithNames] of (
+    queryGroupsWithNames ?? []
+  ).entries()) {
+    namesToShow.push([]);
+    for (const key in queryGroupWithNames) {
+      for (const value of queryGroupWithNames[
+        key as keyof QueryGroupWithNames
+      ]) {
+        namesToShow[index].push(value);
+      }
+    }
+  }
+
   return (
     <div>
+      <ButtonsWrapper>
+        <BackButton
+          color="primary"
+          size="large"
+          variant="contained"
+          onClick={handleGoBack}
+        >
+          Back
+        </BackButton>
+        <RestartButton
+          color="primary"
+          size="large"
+          variant="contained"
+          onClick={handleStartOver}
+        >
+          Start over
+        </RestartButton>
+      </ButtonsWrapper>
       {isLoading && <div>Loading...</div>}
       {differentialExpressionResults.map((results, index) => {
         return (
-          <StyledHTMLTable condensed bordered={false}>
-            <thead>
-              <tr>
-                <td>Gene </td>
-                <td>P-value</td>
-                <td>Effect size</td>
-              </tr>
-              <tr>
-                <td>
-                  <CopyGenesButton
-                    onClick={handleCopyGenes(index)}
-                    sdsType="primary"
-                    sdsStyle="minimal"
-                    isAllCaps={false}
-                    startIcon={
-                      <Icon sdsIcon="copy" sdsSize="s" sdsType="button" />
-                    }
-                  >
-                    Copy
-                  </CopyGenesButton>
-                </td>
-              </tr>
-            </thead>
-            <tbody>
-              {results.map((result) => {
-                const { name: symbol, pValue, effectSize } = result;
-                return (
-                  <tr key={symbol}>
-                    <td>{symbol}</td>
-                    <td>{pValue}</td>
-                    <td>{effectSize.toPrecision(4)}</td>
+          <div>
+            <QueryGroupTitle>Query Group {index + 1}</QueryGroupTitle>
+            <QueryGroupSubTitle>
+              {namesToShow[index].join(", ")}
+            </QueryGroupSubTitle>
+            <TableWrapper>
+              <StyledHTMLTable condensed bordered={false}>
+                <thead>
+                  <tr>
+                    <td>
+                      <CopyGenesButton
+                        onClick={handleCopyGenes(index)}
+                        sdsType="primary"
+                        sdsStyle="minimal"
+                        isAllCaps={false}
+                        startIcon={
+                          <Icon sdsIcon="copy" sdsSize="s" sdsType="button" />
+                        }
+                      >
+                        Copy Genes
+                      </CopyGenesButton>
+                    </td>
                   </tr>
-                );
-              })}
-            </tbody>
-          </StyledHTMLTable>
+                  <tr>
+                    <td>Gene </td>
+                    <td>P-value</td>
+                    <td>Effect size</td>
+                  </tr>
+                </thead>
+                <tbody>
+                  {results.map((result) => {
+                    const { name: symbol, pValue, effectSize } = result;
+                    return (
+                      <tr key={symbol}>
+                        <td>{symbol}</td>
+                        <td>{pValue.toPrecision(4)}</td>
+                        <td>{effectSize.toPrecision(4)}</td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </StyledHTMLTable>
+            </TableWrapper>
+          </div>
         );
       })}
     </div>
