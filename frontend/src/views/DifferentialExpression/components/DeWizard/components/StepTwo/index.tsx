87257272b1d38b49b32a76bf68086b440b782147
atarashansky <atarashansky@CZIMACOS3990.hsd1.ma.comcast.net>
2023-04-16 10:16:04 -0400

    in progress

--- frontend/src/views/DifferentialExpression/components/DeWizard/components/StepTwo/index.tsx
+++ frontend/src/views/DifferentialExpression/components/DeWizard/components/StepTwo/index.tsx
@@ -25,9 +25,10 @@ export default function StepTwo({ setStep }: Props): JSX.Element {
   const dispatch = useContext(DispatchContext);
 
   const { queryGroups, queryGroupsWithNames } = state;
-
+  console.log(queryGroups);
   const handleGoNext = () => {
     setStep(3);
+    // TODO: dispatch action to fetch DE results
   };
   const handleGoBack = () => {
     setStep(1);
