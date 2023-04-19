1c6aa509aefcde269355ad207d46d502d08503dc
Timmy Huang <tihuan@users.noreply.github.com>
2023-04-18 21:19:41 -0700

    test(e2e): Fix e2e tests increase timeout (#4636)

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -103,8 +103,11 @@ const config: PlaywrightTestConfig = {
 
   testDir: "tests",
 
-  /* Maximum time one test can run for. */
-  timeout: 3 * 60 * 1000,
+  /**
+   * Maximum time one test can run for.
+   * (thuang): 5 mins because FF and Edge need extra time to tear down context
+   */
+  timeout: 5 * 60 * 1000,
 
   use: {
     ...COMMON_PLAYWRIGHT_CONTEXT,
