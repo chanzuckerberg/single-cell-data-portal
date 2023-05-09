cc6b318107b3a0b1d89954fa29752ba08e657608
Charles Moga <cmoga@contractor.chanzuckerberg.com>
2023-05-09 06:43:10 +0100

    disabled merging reports

--- frontend/tests/merge-reports/merge-reports.test.ts
+++ frontend/tests/merge-reports/merge-reports.test.ts
@@ -1,24 +1,24 @@
-import { test } from "@playwright/test";
-import { mergeHTMLReports } from "playwright-merge-html-reports";
+// import { test } from "@playwright/test";
+// import { mergeHTMLReports } from "playwright-merge-html-reports";
 
-mergeHTMLReports([
-  process.cwd() + "/html-reports-1",
-  process.cwd() + "/html-reports-2",
-  process.cwd() + "/html-reports-3",
-]);
+// mergeHTMLReports([
+//   process.cwd() + "/html-reports-1",
+//   process.cwd() + "/html-reports-2",
+//   process.cwd() + "/html-reports-3",
+// ]);
 
-const inputReportPaths = [
-  process.cwd() + "/html-reports-1",
-  process.cwd() + "/html-reports-2",
-  process.cwd() + "/html-reports-3",
-];
+// const inputReportPaths = [
+//   process.cwd() + "/html-reports-1",
+//   process.cwd() + "/html-reports-2",
+//   process.cwd() + "/html-reports-3",
+// ];
 
-const config = {
-  outputFolderName: "html-report", // default value
-  outputBasePath: process.cwd(), // default value
-};
-test.describe("Merge reports", () => {
-  test("Should merge Playwright shard reports", async () => {
-    mergeHTMLReports(inputReportPaths, config);
-  });
-});
+// const config = {
+//   outputFolderName: "html-report", // default value
+//   outputBasePath: process.cwd(), // default value
+// };
+// test.describe("Merge reports", () => {
+//   test("Should merge Playwright shard reports", async () => {
+//     mergeHTMLReports(inputReportPaths, config);
+//   });
+// });
