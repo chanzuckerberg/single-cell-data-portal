8e5e4fc7914bb52b8679c5f174159379a6c0e8f3
ashin-czi <ashin@contractor.chanzuckerberg.com>
2023-05-16 15:10:58 -0400

    feat(cell-cards): Cell Cards Landing Page

---
+++ frontend/src/views/CellCards/common/store/reducer.ts
@@ -0,0 +1,29 @@
+export interface PayloadAction<Payload> {
+  type: keyof typeof REDUCERS;
+  payload: Payload;
+}
+export interface State {}
+
+export const INITIAL_STATE: State = {};
+
+export const REDUCERS = {
+  // while we have no reducers, we need a dummy function to satisfy the type system
+  dummyFunction,
+};
+
+export function reducer(state: State, action: PayloadAction<unknown>): State {
+  const { type } = action;
+
+  const handler = REDUCERS[type];
+
+  if (!handler) {
+    throw new Error(`Unknown action type: ${type}`);
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  return handler(state, action as any);
+}
+
+function dummyFunction(state: State, _action: PayloadAction<null>): State {
+  return state;
+}
