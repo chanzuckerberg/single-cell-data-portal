a8ede953844870b2ac12195ebfd414a01070d82f
ashin-czi <ashin@contractor.chanzuckerberg.com>
2023-05-17 11:29:52 -0400

    updating boilerplate

--- frontend/src/views/CellCards/components/CellCard/components/Description/style.ts
+++
@@ -1,8 +0,0 @@
-import styled from "@emotion/styled";
-import { fontBodyS } from "czifui";
-
-export const CellCardDescription = styled.div`
-  ${fontBodyS}
-  font-wieght: 400;
-  white-space: pre-wrap;
-`;
--- frontend/src/views/CellCards/components/CellCard/components/SourceDataTable/style.ts
+++
@@ -1,9 +0,0 @@
-import styled from "@emotion/styled";
-import { Tag, fontBodyXs } from "czifui";
-
-export const StyledTag = styled(Tag)`
-  ${fontBodyXs}
-  & .MuiChip-label {
-    color: #000000;
-  }
-`;
