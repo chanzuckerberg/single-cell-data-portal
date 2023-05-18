ede014c7bd6c16abfc59868fbdb0b390f4f08002
ashin-czi <ashin@contractor.chanzuckerberg.com>
2023-05-18 15:31:53 -0400

    PR comments

--- frontend/src/views/CellCards/components/CellCard/index.tsx
+++ frontend/src/views/CellCards/components/CellCard/index.tsx
@@ -1,25 +1,13 @@
-import React, {  } from "react";
+import React from "react";
 import { useRouter } from "next/router";
-import {
-  CellCardsView,
-} from "./style";
-
-// enum of available descriptions
-type DescriptionOptions = "GPT3.5" | "Wikipedia" | "OLS v4";
-const availableDescriptions: DescriptionOptions[] = [
-  "GPT3.5",
-  "Wikipedia",
-  "OLS v4",
-];
+import { CellCardsView } from "./style";
 
 export default function CellCard(): JSX.Element {
   const router = useRouter();
 
   return (
     <>
-      <CellCardsView>
-
-      </CellCardsView>
+      <CellCardsView></CellCardsView>
     </>
   );
 }
--- frontend/src/views/CellCards/index.tsx
+++ frontend/src/views/CellCards/index.tsx
@@ -1,20 +1,5 @@
-import { useReducer } from "react";
-import {
-  DispatchContext,
-  INITIAL_STATE,
-  reducer,
-  StateContext,
-} from "./common/store";
 import LandingPage from "./components/LandingPage";
 
 export default function CellCards(): JSX.Element {
-  const [state, dispatch] = useReducer(reducer, INITIAL_STATE);
-
-  return (
-    <DispatchContext.Provider value={dispatch}>
-      <StateContext.Provider value={state}>
-        <LandingPage />
-      </StateContext.Provider>
-    </DispatchContext.Provider>
-  );
+  return <LandingPage />;
 }
