fcfc3ad8d896c52b3be1ff967a61b9adf1e3f4b3
atarashansky <atarashansky@CZIMACOS3990.hsd1.ma.comcast.net>
2023-05-22 11:48:39 -0400

    feat(cell-guide): cell type search bar added

--- frontend/src/views/CellCards/components/LandingPage/index.tsx
+++ frontend/src/views/CellCards/components/LandingPage/index.tsx
@@ -1,11 +1,13 @@
+import CellCardSearchBar from "../CellCardSearchBar";
 import { StyledHeader, Wrapper } from "./style";
 
 export default function LandingPage(): JSX.Element {
   return (
     <Wrapper>
-      <StyledHeader>
+      <StyledHeader data-testid="landing-page-header">
         CellGuide is a comprehensive resource for knowledge about cell types
       </StyledHeader>
+      <CellCardSearchBar />
     </Wrapper>
   );
 }
