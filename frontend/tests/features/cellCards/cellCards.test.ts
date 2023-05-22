fac6ee6499dd7042ec9dd03beb141fea5b96a6cc
atarashansky <atarashansky@CZIMACOS3990.hsd1.ma.comcast.net>
2023-05-22 11:49:08 -0400

    add e2e test

--- frontend/tests/features/cellCards/cellCards.test.ts
+++ frontend/tests/features/cellCards/cellCards.test.ts
@@ -1,21 +1,59 @@
-import { test } from "@playwright/test";
+import { test, Page, expect, Locator } from "@playwright/test";
 import { ROUTES } from "src/common/constants/routes";
-import { goToPage, isDevStagingProd } from "tests/utils/helpers";
+import { goToPage, tryUntil } from "tests/utils/helpers";
 import { TEST_URL } from "../../common/constants";
 
-const { describe, skip } = test;
+const { describe } = test;
+
+const LANDING_PAGE_HEADER = "landing-page-header";
+const CELL_CARD_SEARCH_BAR = "cell-card-search-bar";
+const CELL_CARD_SEARCH_BAR_TEXT_INPUT = "cell-card-search-bar-text-input";
 
 describe("Cell Cards", () => {
-  skip(!isDevStagingProd, "WMG BE API does not work locally or in rdev");
-
   describe("Landing Page", () => {
     test("All LandingPage components are present", async ({ page }) => {
       await goToPage(`${TEST_URL}${ROUTES.CELL_CARDS}`, page);
+      await isElementVisible(page, LANDING_PAGE_HEADER);
+      await isElementVisible(page, CELL_CARD_SEARCH_BAR);
     });
     test("Cell type search bar filters properly and links to a CellCard", async ({
       page,
     }) => {
       await goToPage(`${TEST_URL}${ROUTES.CELL_CARDS}`, page);
+      const element = page.getByTestId(CELL_CARD_SEARCH_BAR_TEXT_INPUT);
+      await element.waitFor();
+      await element.click();
+      // get number of elements with role option in dropdown
+      const numOptionsBefore = await countLocator(page.getByRole("option"));
+      // type in search bar
+      await element.type("neuron");
+      // get number of elements with role option in dropdown
+      const numOptionsAfter = await countLocator(page.getByRole("option"));
+      // check that number of elements with role option in dropdown has decreased
+      expect(numOptionsAfter).toBeLessThan(numOptionsBefore);
+      // check that the first element in the dropdown is the one we searched for
+      const firstOption = (await page.getByRole("option").elementHandles())[0];
+      const firstOptionText = await firstOption?.textContent();
+      expect(firstOptionText).toBe("neuron");
+      // click on first element in dropdown
+      await firstOption?.click();
+      // check that the url has changed to the correct cell card
+      await page.waitForURL(`${TEST_URL}${ROUTES.CELL_CARDS}/CL_0000540`);
+    });
+    test("Cell type search bar keyboard input works properly", async ({
+      page,
+    }) => {
+      await goToPage(`${TEST_URL}${ROUTES.CELL_CARDS}`, page);
+      const element = page.getByTestId(CELL_CARD_SEARCH_BAR_TEXT_INPUT);
+      await element.waitFor();
+      await element.click();
+      await element.type("neuron");
+      // input down arrow key
+      await element.press("ArrowDown");
+      // input enter
+      await element.press("Enter");
+      // check that the url has changed to the correct cell card after browser finishes navigating
+      await page.waitForURL(`${TEST_URL}${ROUTES.CELL_CARDS}/CL_0000540`);
     });
   });
 
@@ -31,14 +69,16 @@ describe("Cell Cards", () => {
   });
 });
 
-// async function waitForElement(page: Page, testId: string) {
-//   await tryUntil(
-//     async () => {
-//       await expect(page.getByTestId(testId)).not.toHaveCount(0);
-//     },
-//     { page }
-//   );
-// }
+async function isElementVisible(page: Page, testId: string) {
+  await tryUntil(
+    async () => {
+      const element = page.getByTestId(testId);
+      const isVisible = await element.isVisible();
+      expect(isVisible).toBe(true);
+    },
+    { page }
+  );
+}
 
 // async function getElementAndClick(page: Page, testID: string) {
 //   await tryUntil(
@@ -48,3 +88,9 @@ describe("Cell Cards", () => {
 //     { page }
 //   );
 // }
+
+// (alec) use this instead of locator.count() to make sure that the element is actually present
+// when counting
+async function countLocator(locator: Locator) {
+  return (await locator.elementHandles()).length;
+}
