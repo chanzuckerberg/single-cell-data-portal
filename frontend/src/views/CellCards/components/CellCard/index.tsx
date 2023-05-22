0e5f740d0aba4a1dbd1553025f1007dfbf656521
atarashansky <atarashansky@CZIMACOS3990.local>
2023-05-22 10:08:37 -0400

    update

--- frontend/src/views/CellCards/components/CellCard/index.tsx
+++ frontend/src/views/CellCards/components/CellCard/index.tsx
@@ -1,10 +1,7 @@
 import React from "react";
-import { useRouter } from "next/router";
 import { CellCardsView } from "./style";
 
 export default function CellCard(): JSX.Element {
-  const router = useRouter();
-
   return (
     <>
       <CellCardsView></CellCardsView>
