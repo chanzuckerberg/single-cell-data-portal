929c50676f5794e6519851440d5dc42f5d03888e
Andrew Shin <109984998+ashin-czi@users.noreply.github.com>
2023-06-12 17:31:45 -0400

    feat(wmg): Left Side Bar Hover Over Tooltips (#4985)
    * feat(wmg): Left Side Bar Hover Over Tooltips
    
    * using correct name for id and className for Sort Genes tooltip
    
    * adding e2e tests for left sidebar tooltips
    
    * making select tissue/gene text as constant and italics

--- frontend/src/common/constants/routes.ts
+++ frontend/src/common/constants/routes.ts
@@ -11,6 +11,7 @@ export enum ROUTES {
   PUBLISHED_DATA_DOCS = "/docs/02__Find%20Published%20Data",
   WMG_DOCS = "/docs/04__Analyze%20Public%20Data/4_2__Gene%20Expression%20Documentation/4_2_1__Get%20Started",
   FMG_DOCS = "/docs/04__Analyze%20Public%20Data/4_2__Gene%20Expression%20Documentation/4_2_5__Find%20Marker%20Genes",
+  WMG_DOCS_ORDERING = "/docs/04__Analyze%20Public%20Data/4_2__Gene%20Expression%20Documentation/4_2_2__Cell%20Type%20and%20Gene%20Ordering",
   SITEMAP = "/sitemap",
   CELL_CARDS = "/cell-guide",
 }
