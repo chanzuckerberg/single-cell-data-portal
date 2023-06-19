02469982b0583fc46d768ff3bf20e0062de54940
nayib-jose-gloria <ngloria@chanzuckerberg.com>
2023-06-19 12:45:03 -0400

    feat: add collection migrate + dataset migrate functions for schema migration
    Signed-off-by: nayib-jose-gloria <ngloria@chanzuckerberg.com>

--- backend/schema_migration/migrate.py
+++ backend/schema_migration/migrate.py
@@ -1,17 +1,33 @@
+import json
 import logging
 import os
 import sys
 from typing import Dict, List
 
+import cellxgene_schema
+
 from backend.layers.business.business import BusinessLogic
 from backend.layers.business.entities import CollectionQueryFilter
+from backend.layers.common.entities import DatasetArtifactType
 from backend.layers.persistence.persistence import DatabaseProvider
 from backend.layers.processing import logger
 from backend.layers.thirdparty.s3_provider import S3Provider
 from backend.layers.thirdparty.uri_provider import UriProvider
-from backend.portal.api.providers import get_business_logic
 
 logger.configure_logging(level=logging.INFO)
+
+
+database_provider = DatabaseProvider()
+s3_provider = S3Provider()
+uri_provider = UriProvider()
+
+business_logic = BusinessLogic(
+    database_provider,
+    None,  # Not required
+    None,  # Not required
+    s3_provider,
+    uri_provider,
+)
 
 
 def gather_collections() -> Dict[str, Dict[str, List[str]]]:
@@ -24,23 +40,22 @@ def gather_collections() -> Dict[str, Dict[str, List[str]]]:
         "private": {"collection_id": ["dataset_id", "dataset_id", ...], ...},
     }
     """
-    businnes_logic: BusinessLogic = get_business_logic()
 
     collections_filter = CollectionQueryFilter()
-    collections = businnes_logic.get_collections(collections_filter)
+    collections = business_logic.get_collections(collections_filter)
 
     response = {"published": {}, "private": {}, "revision": {}}
 
     def get_datasets(_version):
         datasets = []
         for dataset in _version.datasets:
-            datasets.append(dataset.dataset_id.id)
+            datasets.append({"dataset_id": dataset.dataset_id.id, "dataset_version_id": dataset.version_id.id})
         return datasets
 
     has_revision = []  # list of collections to skip if published with an active revision
     for collection in collections:
         if collection.is_published() and collection.collection_id not in has_revision:
-            version = businnes_logic.get_collection_version(collection.version_id)
+            version = business_logic.get_collection_version(collection.version_id)
             response["published"][version.collection_id.id] = get_datasets(version)
         elif collection.is_unpublished_version():
             has_revision.append(
@@ -48,41 +63,74 @@ def gather_collections() -> Dict[str, Dict[str, List[str]]]:
             )  # skip the published version if it hasn't been encountered yet
             response["published"].pop(collection.collection_id.id, None)
             # remove from published if it was already encountered
-            version = businnes_logic.get_collection_version(collection.version_id)
+            version = business_logic.get_collection_version(collection.version_id)
             response["revision"][version.version_id.id] = get_datasets(version)
             # using version id instead of collection id
         elif collection.is_initial_unpublished_version():
-            version = businnes_logic.get_collection_version(collection.version_id)
+            version = business_logic.get_collection_version(collection.version_id)
             response["private"][version.collection_id.id] = get_datasets(version)
     return response
 
 
-def migrate(step_name: str, business_logic: BusinessLogic) -> bool:
+def migrate(step_name: str) -> bool:
     """
     Gets called by the step function at every different step, as defined by `step_name`
     """
     if step_name == "corpus_migrate":
-        corpus_migrate(business_logic)
+        corpus_migrate()
+    if step_name == "collection_migrate":
+        collection_id = os.environ["collection_id"]
+        datasets = json.loads(os.environ["datasets"])
+        can_open_revision = bool(os.environ["can_open_revision"])  # TODO: Check syntax for str -> bool
+        collection_migrate(collection_id, datasets, can_open_revision)
+    if step_name == "dataset_migrate":
+        collection_id = os.environ["collection_id"]
+        dataset_id = os.environ["dataset_id"]
+        dataset_version_id = os.envion["dataset_version_id"]
+        dataset_migrate(collection_id, dataset_id, dataset_version_id)
     return True
 
 
-def corpus_migrate(business_logic: BusinessLogic) -> bool:
+def dataset_migrate(collection_id: str, dataset_id: str, dataset_version_id: str) -> Dict[str, str]:
+    raw_h5ad_uri = [
+        artifact.uri
+        for artifact in business_logic.get_dataset_artifacts(dataset_version_id)
+        if artifact.type == DatasetArtifactType.RAW_H5AD
+    ]
+    bucket_name, object_key = s3_provider.parse_s3_uri(raw_h5ad_uri)
+    s3_provider.download_file(bucket_name, object_key, "raw.h5ad")
+    cellxgene_schema.migrate("raw.h5ad", "migrated.h5ad", collection_id, dataset_id)
+    # TODO: define where this upload bucket actually should be
+    upload_bucket_name = os.environ["UPLOAD_BUCKET"]
+    dst_uri = f"{dataset_version_id}/migrated.h5ad"
+    s3_provider.upload_file("migrated.h5ad", upload_bucket_name, dst_uri)
+    url = f"s3://{upload_bucket_name}/{dst_uri}"
+    new_dataset_version_id, _ = business_logic.ingest_dataset(collection_id, url, None, dataset_version_id)
+    return {"new_dataset_version_id": new_dataset_version_id}
+
+
+def collection_migrate(
+    collection_id: str, datasets: List[Dict[str, str]], can_open_revision: bool
+) -> List[Dict[str, str]]:
+    private_collection_id = collection_id
+    if can_open_revision:
+        private_collection_id = business_logic.create_collection_version(collection_id).version_id.id
+    return [
+        {
+            "collection_id": private_collection_id,
+            "dataset_id": dataset["dataset_id"],
+            "dataset_version_id": dataset["dataset_version_id"],
+        }
+        for dataset in datasets
+    ]
+
+
+def corpus_migrate() -> bool:
     gather_collections()
     return True
 
 
 if __name__ == "__main__":
-    database_provider = DatabaseProvider()
-    s3_provider = S3Provider()
-    uri_provider = UriProvider()
-
-    business_logic = BusinessLogic(
-        database_provider,
-        None,  # Not required
-        None,  # Not required
-        s3_provider,
-        uri_provider,
-    )
     step_name = os.environ["STEP_NAME"]
-    rv = migrate(step_name, business_logic)
+    rv = migrate(step_name)
     sys.exit(rv)
