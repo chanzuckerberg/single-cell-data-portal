89188235a76676b49c4570095dce73c8fde6ca57
Charles Moga <cmoga@contractor.chanzuckerberg.com>
2023-06-21 19:08:17 +0100

    deleted dummy test

--- frontend/tests/features/dummy.test.ts
+++
@@ -1,9 +0,0 @@
-import { expect, test } from "@playwright/test";
-
-const { describe } = test;
-
-describe("Dummy test", () => {
-  test("Should test status reporting", async () => {
-    expect(2).toEqual(3);
-  });
-});
