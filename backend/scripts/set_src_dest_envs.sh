b87f141406e844e5c42bdbb5db844966ec291a85
Trent Smith <trent.smith@chanzuckerberg.com>
2023-07-27 15:30:33 -0700

    support staging
    Signed-off-by: Trent Smith <trent.smith@chanzuckerberg.com>

--- backend/scripts/set_src_dest_envs.sh
+++ backend/scripts/set_src_dest_envs.sh
@@ -16,13 +16,14 @@ elif [[ "$SRC_ENV" == staging && "$DEST_ENV" == dev ]]; then
         read -n 1 -p "ATTENTION: You are mirroring the staging env to dev. Useful for testing fixes in dev for data-related bugs found in staging, but otherwise useless. Proceed? (Y/n) " ANS
         [[ $ANS == 'Y' ]] || exit 1
     fi
-# allow dev->rdev for creating test environments
-elif [[ "$SRC_ENV" == dev && "$DEST_ENV" == rdev ]]; then
+# allow dev||staging->rdev for creating test environments
+elif [[ ("$SRC_ENV" =~ ^(dev|staging)$ && "$DEST_ENV" == rdev) ]]; then
   if [[ -z "$RDEV_ENV" ]]; then
     echo "ERROR: You must specify an rdev environment name when mirroring dev->rdev"
     exit 1
   elif [[ -z $NO_PROMPT ]]; then
-      read -n 1 -p "ATTENTION: You are mirroring the dev env to rdev/${RDEV_ENV}. Useful for testing fixes in dev for data-related bugs found in staging, but otherwise useless. Proceed? (Y/n) " ANS
+      read -n 1 -p "ATTENTION: You are mirroring the ${SRC_ENV} env to rdev/${RDEV_ENV}. Useful for testing fixes in dev for
+      data-related bugs found in staging, but otherwise useless. Proceed? (Y/n) " ANS
       [[ $ANS == 'Y' ]] || exit 1
   fi
 # allow prod->dev and prod->staging
