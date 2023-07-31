f25da3fd0ed0f8a1f410c0cfb94d1fe673efd6e3
Timmy Huang <thuang@chanzuckerberg.com>
2023-07-31 14:52:19 -0700

    fix: e2e WMG tests part 1

--- frontend/tests/features/wheresMyGene/landingPageFilter.test.ts
+++ frontend/tests/features/wheresMyGene/landingPageFilter.test.ts
@@ -1,31 +1,35 @@
 import { expect, test } from "@playwright/test";
 import {
+  WMG_WITH_SEEDED_GENES,
   checkPlotSize,
   checkSourceData,
   conditionallyRunTests,
   deSelectSecondaryFilterOption,
   goToWMG,
-  goToWMGWithSeededState,
   selectSecondaryFilterOption,
-  selectTissueAndGeneOption,
 } from "tests/utils/wmgUtils";
-import { tryUntil } from "tests/utils/helpers";
+import { goToPage, tryUntil } from "tests/utils/helpers";
 import {
   COLOR_SCALE_TOOLTIP_TEXT,
   GROUP_BY_TOOLTIP_TEXT,
   SORT_CELL_TYPES_TOOLTIP_TEXT,
   SORT_GENES_TOOLTIP_TEXT,
 } from "src/views/WheresMyGene/common/constants";
+import { CELL_TYPE_LABELS_ID } from "tests/common/constants";
 
 const SIDE_BAR_TOGGLE_BUTTON_ID = "side-bar-toggle-button";
 
 const { describe } = test;
 
 describe("Left side bar", () => {
-  conditionallyRunTests();
+  /**
+   * TODO(thuang): Remove forceRun when all WMG e2e tests are enabled.
+   * `forceRun` is just to incrementally add tests back in the meantime
+   */
+  conditionallyRunTests({ forceRun: true });
 
   test("Left side bar collapse and expand", async ({ page }) => {
-    await goToWMGWithSeededState(page);
+    await goToWMG(page);
 
     // click chevron left to collapse the left tab
     await page.getByTestId(SIDE_BAR_TOGGLE_BUTTON_ID).click();
@@ -35,59 +39,70 @@ describe("Left side bar", () => {
   });
 
   [
-    ["dataset-filter"],
-    ["disease-filter"],
-    ["self-reported-ethnicity-filter"],
-    ["sex-filter"],
-  ].forEach(([filterOption]) => {
+    "dataset-filter",
+    "disease-filter",
+    "self-reported-ethnicity-filter",
+    "sex-filter",
+  ].forEach((filterOption) => {
     test(`Should be able select and de-select options for ${filterOption} filter`, async ({
       page,
     }) => {
-      await goToWMGWithSeededState(page);
+      await goToPage(WMG_WITH_SEEDED_GENES.URL, page);
+
+      // (thuang): This expands the first tissue
+      await page.getByTestId(CELL_TYPE_LABELS_ID).first().click();
 
       await tryUntil(
         async () => {
           // check the count of source data displayed before adding a filter
           const countBeforeFilter = await checkSourceData(page);
 
-          //check plot height before adding a filter
-          const plotSizeBeforeFilter = await checkPlotSize(page);
-
-          //select a filter
-          await selectSecondaryFilterOption(page, filterOption);
-
-          // check the count of source data displayed after adding a filter
-          const countAfterFilter = await checkSourceData(page);
-
-          //check plot height after adding a filter
-          const plotSizeAfterFilter = await checkPlotSize(page);
-
           // verify source data loading some data
           expect(countBeforeFilter).toBeGreaterThan(0);
 
-          // verify source data changed after filter is applied
-          expect(countBeforeFilter === countAfterFilter).toBeFalsy();
+          // check plot height before adding a filter
+          const plotSizeBeforeFilter = await checkPlotSize(page);
 
-          //verify data plot data loading some data
+          // verify data plot data loading some data
           expect(plotSizeBeforeFilter).toBeGreaterThan(0);
 
-          //verify data plot data changed after filter was applied
-          expect(plotSizeBeforeFilter === plotSizeAfterFilter).toBeFalsy();
+          // select a filter
+          await selectSecondaryFilterOption(page, filterOption);
 
-          //uncheck filter
+          await tryUntil(
+            async () => {
+              // check the count of source data displayed after adding a filter
+              const countAfterFilter = await checkSourceData(page);
+
+              //check plot height after adding a filter
+              const plotSizeAfterFilter = await checkPlotSize(page);
+
+              // verify source data changed after filter is applied
+              expect(countBeforeFilter === countAfterFilter).toBeFalsy();
+
+              // verify data plot data changed after filter was applied
+              expect(plotSizeBeforeFilter === plotSizeAfterFilter).toBeFalsy();
+            },
+            { page }
+          );
+
+          // uncheck filter
           await deSelectSecondaryFilterOption(page, filterOption);
         },
-        { page }
+        {
+          page,
+          /**
+           * (thuang): Give up after N times, because the app state might not
+           * be recoverable at this point
+           */
+          maxRetry: 3,
+        }
       );
     });
   });
 
   test("Left side bar tooltips", async ({ page }) => {
-    // navigate to gene expression page
-    await goToWMG(page);
-
-    //select tissue and gene
-    await selectTissueAndGeneOption(page);
+    await goToPage(WMG_WITH_SEEDED_GENES.URL, page);
 
     // Group By tooltip
     await page.getByTestId("group-by-tooltip-icon").hover();
