dd7c39daf2f5de82a9b2360357ab4a8e33460475
Trent Smith <1429913+Bento007@users.noreply.github.com>
2023-08-02 09:41:54 -0700

    feat(schema-migration): Add ability to restore a dataset to the previous version (#5355)
    - add cxg_admin script to roll back all datasets from a report generated by the schema_migration pipeline
    - modify the shape of the schema-migration report to make it easier to parse in the cxg_admin command.
    - add `restore_previous_dataset_version` to modify a dataset_version associated with a collection_version to the previous dataset_version if it exists.
    - add additional exceptions related to `set_previous_dataset_version`
    - modify `replace_dataset_in_collection_version` to optionally include a `new_dataset_version_id` to replace `old_dataset_version_id`.
    - add `get_previous_dataset_version_id` to the persistence layer to query the database for the previously created `dataset_version` associated with a `dataset_id`.
    - Set the name of the upload sfn name to "migrate_{dataset_version_id}_{timestamp}"

--- backend/layers/business/business_interface.py
+++ backend/layers/business/business_interface.py
@@ -142,3 +142,13 @@ class BusinessLogicInterface:
         self, schema_version: str
     ) -> List[CollectionVersionWithPublishedDatasets]:
         pass
+
+    def restore_previous_dataset_version(
+        self, collection_version_id: CollectionVersionId, dataset_id: DatasetId
+    ) -> None:
+        """
+        Restore the previous dataset version for a dataset.
+        :param collection_version_id: The collection version to restore the dataset version. It must be in a mutable
+        state
+        :param dataset_id: The dataset id to restore the previous version of.
+        """
--- tests/unit/processing/schema_migration/test_publish_and_cleanup.py
+++ tests/unit/processing/schema_migration/test_publish_and_cleanup.py
@@ -1,11 +1,13 @@
 from unittest.mock import Mock, patch
 
-from backend.layers.common.entities import DatasetProcessingStatus, DatasetVersionId
+from backend.layers.common.entities import CollectionId, DatasetProcessingStatus, DatasetVersionId
+from tests.unit.processing.schema_migration.conftest import make_mock_dataset_version
 
 
 @patch("backend.layers.processing.schema_migration.cxs_get_current_schema_version")
+@patch("backend.layers.processing.schema_migration.json.dump")
 class TestPublishAndCleanup:
-    def test_publish_and_cleanup(self, mock_cxs_get_current_schema_version, schema_migrate_and_collections):
+    def test_publish_and_cleanup(self, mock_json, mock_cxs_get_current_schema_version, schema_migrate_and_collections):
         schema_migrate, _ = schema_migrate_and_collections
         mock_cxs_get_current_schema_version.return_value = "1.0.0"
         dataset_status = Mock(processing_status=DatasetProcessingStatus.SUCCESS)
@@ -22,48 +24,56 @@ class TestPublishAndCleanup:
         )
 
         errors = schema_migrate.publish_and_cleanup("collection_version_id", True)
-        assert errors == {}
+        assert errors == []
         schema_migrate.business_logic.publish_collection_version.assert_called_once()
         schema_migrate.s3_provider.delete_files.assert_called_once_with(
             "artifact-bucket", ["successful_dataset_version_id/migrated.h5ad"]
         )
 
     def test_publish_and_cleanup__with_errors(
-        self, mock_cxs_get_current_schema_version, schema_migrate_and_collections
+        self, mock_json, mock_cxs_get_current_schema_version, schema_migrate_and_collections
     ):
         schema_migrate, _ = schema_migrate_and_collections
         mock_cxs_get_current_schema_version.return_value = "1.0.0"
-        dataset_status_success = Mock(processing_status=DatasetProcessingStatus.SUCCESS)
-        dataset_status_failure = Mock(
-            processing_status=DatasetProcessingStatus.FAILURE, validation_message="rds conversion failed"
+        collection_id = CollectionId()
+        failed_dataset = make_mock_dataset_version(
+            version_id="failed_dataset_version_id",
+            status=dict(processing_status=DatasetProcessingStatus.FAILURE, validation_message="rds conversion failed"),
+            metadata=dict(schema_version="1.0.0"),
         )
-        metadata_migrated = Mock(schema_version="1.0.0")
-        metadata_not_migrated = Mock(schema_version="0.9.0")
+        non_migrated_dataset = make_mock_dataset_version(
+            version_id="non_migrated_dataset_version_id",
+            metadata=dict(schema_version="0.9.0"),
+        )
+        datasets = [
+            make_mock_dataset_version(version_id="successful_dataset_version_id"),
+            failed_dataset,
+            non_migrated_dataset,
+        ]
         schema_migrate.business_logic.get_collection_version = Mock()
         schema_migrate.business_logic.get_collection_version.return_value = Mock(
-            datasets=[
-                Mock(
-                    version_id=DatasetVersionId("successful_dataset_version_id"),
-                    status=dataset_status_success,
-                    metadata=metadata_migrated,
-                ),
-                Mock(
-                    version_id=DatasetVersionId("failed_dataset_version_id"),
-                    status=dataset_status_failure,
-                    metadata=metadata_migrated,
-                ),
-                Mock(
-                    version_id=DatasetVersionId("non_migrated_dataset_version_id"),
-                    status=dataset_status_success,
-                    metadata=metadata_not_migrated,
-                ),
-            ]
+            datasets=datasets, collection_id=collection_id
         )
 
         errors = schema_migrate.publish_and_cleanup("collection_version_id", True)
         assert len(errors) == 2
-        assert errors["failed_dataset_version_id"] == "rds conversion failed"
-        assert errors["non_migrated_dataset_version_id"] == "Did Not Migrate."
+        assert {
+            "message": "rds conversion failed",
+            "dataset_processing_status": DatasetProcessingStatus.FAILURE.name,
+            "collection_id": collection_id.id,
+            "collection_version_id": "collection_version_id",
+            "dataset_version_id": failed_dataset.version_id.id,
+            "dataset_id": failed_dataset.dataset_id.id,
+            "rollback": True,
+        } in errors
+        assert {
+            "message": "Did Not Migrate.",
+            "collection_id": collection_id.id,
+            "collection_version_id": "collection_version_id",
+            "dataset_version_id": non_migrated_dataset.version_id.id,
+            "dataset_id": non_migrated_dataset.dataset_id.id,
+            "rollback": False,
+        } in errors
         schema_migrate.business_logic.publish_collection_version.assert_not_called()
         schema_migrate.s3_provider.delete_files.assert_called_once_with(
             "artifact-bucket",
@@ -75,25 +85,23 @@ class TestPublishAndCleanup:
         )
 
     def test_publish_and_cleanup__can_not_publish(
-        self, mock_cxs_get_current_schema_version, schema_migrate_and_collections
+        self, mock_json, mock_cxs_get_current_schema_version, schema_migrate_and_collections
     ):
         schema_migrate, _ = schema_migrate_and_collections
         mock_cxs_get_current_schema_version.return_value = "1.0.0"
-        dataset_status = Mock(processing_status=DatasetProcessingStatus.SUCCESS)
-        metadata = Mock(schema_version="1.0.0")
+        dataset_status = dict(processing_status=DatasetProcessingStatus.SUCCESS)
+        metadata = dict(schema_version="1.0.0")
         schema_migrate.business_logic.get_collection_version = Mock()
         schema_migrate.business_logic.get_collection_version.return_value = Mock(
             datasets=[
-                Mock(
-                    version_id=DatasetVersionId("successful_dataset_version_id"),
-                    status=dataset_status,
-                    metadata=metadata,
+                make_mock_dataset_version(
+                    version_id="successful_dataset_version_id", status=dataset_status, metadata=metadata
                 )
             ]
         )
 
         errors = schema_migrate.publish_and_cleanup("collection_version_id", False)
-        assert errors == {}
+        assert errors == []
         schema_migrate.business_logic.publish_collection_version.assert_not_called()
         schema_migrate.s3_provider.delete_files.assert_called_once_with(
             "artifact-bucket", ["successful_dataset_version_id/migrated.h5ad"]
