chanzuckerberg/single-cell-data-portal

commit dd7c39daf2f5de82a9b2360357ab4a8e33460475

feat(schema-migration): Add ability to restore a dataset to the previous version (#5355)
- add cxg_admin script to roll back all datasets from a report generated by the schema_migration pipeline
- modify the shape of the schema-migration report to make it easier to parse in the cxg_admin command.
- add `restore_previous_dataset_version` to modify a dataset_version associated with a collection_version to the previous dataset_version if it exists.
- add additional exceptions related to `set_previous_dataset_version`
- modify `replace_dataset_in_collection_version` to optionally include a `new_dataset_version_id` to replace `old_dataset_version_id`.
- add `get_previous_dataset_version_id` to the persistence layer to query the database for the previously created `dataset_version` associated with a `dataset_id`.
- Set the name of the upload sfn name to "migrate_{dataset_version_id}_{timestamp}"

backend/layers/business/business.py
@@ -28,6 +28,7 @@ from backend.layers.business.exceptions import (
     DatasetVersionNotFoundException,
     InvalidURIException,
     MaxFileSizeExceededException,
+    NoPreviousDatasetVersionException,
 )
 from backend.layers.common import validation
 from backend.layers.common.cleanup import sanitize
@@ -838,3 +839,34 @@ class BusinessLogic(BusinessLogicInterface):
             collections_with_published_datasets.append(CollectionVersionWithPublishedDatasets(**vars(collection)))
 
         return collections_with_published_datasets
+
+    def restore_previous_dataset_version(
+        self, collection_version_id: CollectionVersionId, dataset_id: DatasetId
+    ) -> None:
+        """
+        Restore the previous dataset version for a dataset.
+        :param collection_version_id: The collection version to restore the dataset version. It must be in a mutable
+        state
+        :param dataset_id: The dataset id to restore the previous version of.
+        """
+
+        collection = self.database_provider.get_collection_version_with_datasets(collection_version_id)
+        if collection.is_published():
+            raise CollectionIsPublishedException(
+                f"Collection {collection_version_id} is published, unable to restore previous dataset version"
+            )
+        current_version: DatasetVersion = [dv for dv in collection.datasets if dv.dataset_id == dataset_id][0]
+        previous_version_id: DatasetVersionId = self.database_provider.get_previous_dataset_version_id(dataset_id)
+        if previous_version_id is None:
+            raise NoPreviousDatasetVersionException(f"No previous dataset version for dataset {dataset_id.id}")
+        logger.info(
+            {
+                "message": "Restoring previous dataset version",
+                "dataset_id": dataset_id.id,
+                "replace_version_id": current_version.version_id.id,
+                "restored_version_id": previous_version_id.id,
+            }
+        )
+        self.database_provider.replace_dataset_in_collection_version(
+            collection_version_id, current_version.version_id, previous_version_id
+        )

backend/layers/persistence/persistence.py
@@ -554,6 +554,8 @@ class DatabaseProvider(DatabaseProviderInterface):
                 for previous_d_id in previous_d_ids:
                     if previous_d_id not in dataset_ids_for_new_collection_version:
                         dataset_ids_to_tombstone.append(previous_d_id)
+
+            # get all dataset versions for the datasets that are being tombstoned
             dataset_version_ids_to_delete_from_s3 = []
             if dataset_ids_to_tombstone:
                 datasets = session.query(DatasetTable).filter(DatasetTable.id.in_(dataset_ids_to_tombstone)).all()
@@ -562,6 +564,7 @@ class DatabaseProvider(DatabaseProviderInterface):
                     dataset_all_versions = session.query(DatasetVersionTable).filter_by(dataset_id=dataset.id).all()
                     dataset_version_ids_to_delete_from_s3.extend([dv.id for dv in dataset_all_versions])
 
+            # update dataset versions for datasets that are not being tombstoned
             dataset_version_ids = session.query(CollectionVersionTable.datasets).filter_by(id=version_id.id).one()[0]
             for dataset_version, dataset in (
                 session.query(DatasetVersionTable, DatasetTable)
@@ -569,7 +572,7 @@ class DatabaseProvider(DatabaseProviderInterface):
                 .filter(DatasetVersionTable.id.in_(dataset_version_ids))
                 .all()
             ):
-                dataset.version_id = dataset_version.id
+                dataset.version_id = dataset_version.id  # point the dataset to the new version
                 if dataset.published_at is None:
                     dataset.published_at = published_at
 
@@ -787,10 +790,18 @@ class DatabaseProvider(DatabaseProviderInterface):
             collection_version.datasets = updated_datasets
 
     def replace_dataset_in_collection_version(
-        self, collection_version_id: CollectionVersionId, old_dataset_version_id: DatasetVersionId
+        self,
+        collection_version_id: CollectionVersionId,
+        old_dataset_version_id: DatasetVersionId,
+        new_dataset_version_id: DatasetVersionId = None,
     ) -> DatasetVersion:
         """
         Replaces an existing mapping between a collection version and a dataset version
+
+        :param collection_version_id: the collection version id
+        :param old_dataset_version_id: the dataset version id to be replaced
+        :param new_dataset_version_id: the dataset version id to replace with. If None is provide a new
+        dataset version will be created.
         """
         # TODO: this method should probably be split into multiple - it contains too much logic
         with self._manage_session() as session:
@@ -798,17 +809,25 @@ class DatabaseProvider(DatabaseProviderInterface):
                 session.query(CollectionVersionTable.collection_id).filter_by(id=collection_version_id.id).one()[0]
             )  # noqa
             dataset_id = session.query(DatasetVersionTable.dataset_id).filter_by(id=old_dataset_version_id.id).one()[0]
-            new_dataset_version_id = DatasetVersionId()
-            new_dataset_version = DatasetVersionTable(
-                id=new_dataset_version_id.id,
-                dataset_id=dataset_id,
-                collection_id=collection_id,
-                dataset_metadata=None,
-                artifacts=list(),
-                status=DatasetStatus.empty().to_json(),
-                created_at=datetime.utcnow(),
-            )
-            session.add(new_dataset_version)
+            if new_dataset_version_id is None:
+                new_dataset_version_id = DatasetVersionId()
+                new_dataset_version = DatasetVersionTable(
+                    id=new_dataset_version_id.id,
+                    dataset_id=dataset_id,
+                    collection_id=collection_id,
+                    dataset_metadata=None,
+                    artifacts=list(),
+                    status=DatasetStatus.empty().to_json(),
+                    created_at=datetime.utcnow(),
+                )
+                session.add(new_dataset_version)
+                new_dataset_version = self._hydrate_dataset_version(new_dataset_version)
+            else:
+                new_dataset_version = self.get_dataset_version(new_dataset_version_id)
+                if CollectionId(str(collection_id)) != new_dataset_version.collection_id:
+                    raise ValueError(
+                        f"Dataset version {new_dataset_version_id} does not belong to collection {collection_id}"
+                    )
 
             collection_version = (
                 session.query(CollectionVersionTable).filter_by(id=collection_version_id.id).one()
@@ -819,7 +838,7 @@ class DatabaseProvider(DatabaseProviderInterface):
             datasets[idx] = uuid.UUID(new_dataset_version_id.id)
             collection_version.datasets = datasets
 
-            return self._hydrate_dataset_version(new_dataset_version)
+            return new_dataset_version
 
     def get_dataset_mapped_version(
         self, dataset_id: DatasetId, get_tombstoned: bool = False
@@ -855,3 +874,19 @@ class DatabaseProvider(DatabaseProviderInterface):
             else:
                 collection_versions = session.query(CollectionVersionTable).filter_by(schema_version=schema_version)
             return [self._row_to_collection_version(row, None) for row in collection_versions.all()]
+
+    def get_previous_dataset_version_id(self, dataset_id: DatasetId) -> Optional[DatasetVersionId]:
+        """
+        Returns the previously created dataset version for a dataset.
+        """
+        with self._manage_session() as session:
+            version_id = (
+                session.query(DatasetVersionTable.id)
+                .filter_by(dataset_id=dataset_id.id)
+                .order_by(DatasetVersionTable.created_at.desc())
+                .offset(1)
+                .first()
+            )
+            if version_id is None:
+                return None
+            return DatasetVersionId(str(version_id.id))

backend/layers/persistence/persistence_interface.py
@@ -221,7 +221,10 @@ class DatabaseProviderInterface:
         """
 
     def replace_dataset_in_collection_version(
-        self, collection_version_id: CollectionVersionId, old_dataset_version_id: DatasetVersionId
+        self,
+        collection_version_id: CollectionVersionId,
+        old_dataset_version_id: DatasetVersionId,
+        new_dataset_version_id: DatasetVersionId = None,
     ) -> DatasetVersion:
         """
         Replaces an existing mapping between a collection version and a dataset version
@@ -236,4 +239,9 @@ class DatabaseProviderInterface:
         """
         Returns a list with all collection versions that match the given schema_version. schema_version may contain
          wildcards.
+        """
+
+    def get_previous_dataset_version_id(self, dataset_id: DatasetId) -> Optional[DatasetVersionId]:
+        """
+        Returns the previously created dataset version for a dataset.
         """

backend/layers/persistence/persistence_mock.py
@@ -425,26 +425,36 @@ class DatabaseProviderMock(DatabaseProviderInterface):
         version.datasets = [d for d in version.datasets if d != dataset_version_id]
 
     def replace_dataset_in_collection_version(
-        self, collection_version_id: CollectionVersionId, old_dataset_version_id: DatasetVersionId
+        self,
+        collection_version_id: CollectionVersionId,
+        old_dataset_version_id: DatasetVersionId,
+        new_dataset_version_id: DatasetVersionId = None,
     ) -> DatasetVersion:
-        new_version_id = DatasetVersionId()
         old_version = self.get_dataset_version(old_dataset_version_id)
         collection_version = self.collections_versions[collection_version_id.id]
-        new_version = DatasetVersion(
-            dataset_id=old_version.dataset_id,
-            version_id=new_version_id,
-            collection_id=collection_version.collection_id,
-            status=DatasetStatus.empty(),
-            metadata=None,
-            artifacts=[],
-            created_at=datetime.utcnow(),
-            canonical_dataset=old_version.canonical_dataset,
-        )
-        self.datasets_versions[new_version_id.id] = new_version
+        if new_dataset_version_id is None:
+            new_dataset_version_id = DatasetVersionId()
+            new_dataset_version = DatasetVersion(
+                dataset_id=old_version.dataset_id,
+                version_id=new_dataset_version_id,
+                collection_id=collection_version.collection_id,
+                status=DatasetStatus.empty(),
+                metadata=None,
+                artifacts=[],
+                created_at=datetime.utcnow(),
+                canonical_dataset=old_version.canonical_dataset,
+            )
+            self.datasets_versions[new_dataset_version_id.id] = new_dataset_version
+        else:
+            new_dataset_version = self.get_dataset_version(new_dataset_version_id)
+            if collection_version.collection_id != new_dataset_version.collection_id:
+                raise ValueError(
+                    f"Dataset version {new_dataset_version_id} does not belong to collection {collection_version.collection_id}"
+                )
 
         idx = next(i for i, e in enumerate(collection_version.datasets) if e == old_dataset_version_id)
-        collection_version.datasets[idx] = new_version_id
-        return copy.deepcopy(new_version)
+        collection_version.datasets[idx] = new_dataset_version_id
+        return copy.deepcopy(new_dataset_version)
 
     def get_dataset_version_status(self, version_id: DatasetVersionId) -> DatasetStatus:
         return copy.deepcopy(self.datasets_versions[version_id.id].status)
@@ -472,3 +482,18 @@ class DatabaseProviderMock(DatabaseProviderInterface):
                 cv for cv in self.collections_versions.values() if cv.schema_version == schema_version
             ]
         return copy.deepcopy(collection_versions)
+
+    def get_previous_dataset_version_id(self, dataset_id: DatasetId) -> Optional[DatasetVersionId]:
+        """
+        Returns the previously created dataset version for a dataset.
+        """
+
+        self.datasets.get(dataset_id.id)
+        datasets = [d for d in self.datasets_versions.values() if d.dataset_id == dataset_id]
+        datasets.sort(key=lambda d: d.created_at, reverse=True)
+        try:
+            previous_dataset = datasets[1]
+        except IndexError:
+            return None
+        else:
+            return copy.deepcopy(previous_dataset.version_id)

.happy/terraform/modules/schema_migration/main.tf
@@ -363,6 +363,7 @@ resource aws_sfn_state_machine sfn_schema_migration {
                 "StepFunctionsStartExecution": {
                   "Type": "Task",
                   "Resource": "arn:aws:states:::states:startExecution.sync:2",
+                  "Name.$": "$.results.sfn_name",
                   "Parameters": {
                     "StateMachineArn": "arn:aws:states:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:stateMachine:dp-${var.deployment_stage}-${var.custom_stack_name}-sfn",
                     "Input": {

backend/layers/business/business_interface.py
@@ -142,3 +142,13 @@ class BusinessLogicInterface:
         self, schema_version: str
     ) -> List[CollectionVersionWithPublishedDatasets]:
         pass
+
+    def restore_previous_dataset_version(
+        self, collection_version_id: CollectionVersionId, dataset_id: DatasetId
+    ) -> None:
+        """
+        Restore the previous dataset version for a dataset.
+        :param collection_version_id: The collection version to restore the dataset version. It must be in a mutable
+        state
+        :param dataset_id: The dataset id to restore the previous version of.
+        """

backend/layers/business/exceptions.py
@@ -31,6 +31,18 @@ class CollectionIsPublishedException(CollectionUpdateException):
     """
 
 
+class CollectionIsNotPublishedException(CollectionUpdateException):
+    """
+    Raised when an operation is performed on a collection that must be published
+    """
+
+
+class NoPreviousDatasetVersionException(BusinessException):
+    """
+    Raised when a previous dataset version is expected, but does not exist
+    """
+
+
 class InvalidLinkException(BusinessException):
     def __init__(self, errors: Optional[List[str]] = None) -> None:
         self.errors: Optional[List[str]] = errors

backend/layers/processing/schema_migration.py
@@ -21,7 +21,7 @@ from backend.layers.common.entities import (
 )
 from backend.layers.processing import logger
 from backend.layers.processing.process_logic import ProcessingLogic
-from backend.layers.thirdparty.step_function_provider import StepFunctionProvider
+from backend.layers.thirdparty.step_function_provider import StepFunctionProvider, sfn_name_generator
 
 logger.configure_logging(level=logging.INFO)
 
@@ -97,10 +97,12 @@ class SchemaMigrate(ProcessingLogic):
             existing_dataset_version_id=DatasetVersionId(dataset_version_id),
             start_step_function=False,  # The schema_migration sfn will start the ingest sfn
         )
+        sfn_name = sfn_name_generator(dataset_version_id, prefix="migrate")
         return {
             "collection_version_id": collection_version_id,
             "dataset_version_id": new_dataset_version_id.id,
             "uri": uri,
+            "sfn_name": sfn_name,
         }
 
     def _check_dataset_is_latest_schema_version(self, dataset: DatasetVersion) -> bool:
@@ -167,9 +169,8 @@ class SchemaMigrate(ProcessingLogic):
         return response
 
     def publish_and_cleanup(self, collection_version_id: str, can_publish: bool) -> Dict[str, str]:
-        errors = dict()
-        collection_version_id = CollectionVersionId(collection_version_id)
-        collection_version = self.business_logic.get_collection_version(collection_version_id)
+        errors = []
+        collection_version = self.business_logic.get_collection_version(CollectionVersionId(collection_version_id))
         cxs_get_current_schema_version()
         object_keys_to_delete = []
         for dataset in collection_version.datasets:
@@ -177,10 +178,28 @@ class SchemaMigrate(ProcessingLogic):
             key_prefix = self.get_key_prefix(dataset_version_id)
             object_keys_to_delete.append(f"{key_prefix}/migrated.h5ad")
             if not self._check_dataset_is_latest_schema_version(dataset):
-                errors[dataset_version_id] = "Did Not Migrate."
+                errors.append(
+                    {
+                        "message": "Did Not Migrate.",
+                        "collection_id": collection_version.collection_id.id,
+                        "collection_version_id": collection_version_id,
+                        "dataset_version_id": dataset_version_id,
+                        "dataset_id": dataset.dataset_id.id,
+                        "rollback": False,
+                    }
+                )
             elif dataset.status.processing_status != DatasetProcessingStatus.SUCCESS:
-                errors[dataset_version_id] = dataset.status.validation_message
-
+                errors.append(
+                    {
+                        "message": dataset.status.validation_message,
+                        "dataset_processing_status": dataset.status.processing_status.name,
+                        "collection_id": collection_version.collection_id.id,
+                        "collection_version_id": collection_version_id,
+                        "dataset_version_id": dataset_version_id,
+                        "dataset_id": dataset.dataset_id.id,
+                        "rollback": True,
+                    }
+                )
         if errors:
             self._store_sfn_response("report", collection_version_id, errors)
         elif can_publish:

commit 8ece111bccf6be8d260fe13f13bf52e9cf7cbbd2

fix: do not assume latest DatasetVersion is active
Change relevant db provider methods to strictly rely on datasets
associated with latest CollectionVersion as the 'active' DatasetVersion
for a given DatasetId. This fixes a bug introduced by #5355 which
allows rollback to previous DatasetVersions without deleting the
most-recently-created DatasetVersion row.

backend/layers/business/business.py
@@ -694,26 +694,16 @@ class BusinessLogic(BusinessLogicInterface):
     ) -> Optional[DatasetVersion]:
         """
         Given a canonical dataset id, returns its mapped dataset version, i.e. the dataset version
-        that belongs to the most recently published collection. Otherwise will return the most recent
+        that belongs to the most recently published collection. If never published, will return the most recent
         unpublished version.
         """
-        dataset_version = self.database_provider.get_dataset_mapped_version(dataset_id, get_tombstoned=get_tombstoned)
+        dataset_version = self.database_provider.get_dataset_mapped_version(dataset_id, get_tombstoned)
         if dataset_version is not None:
             if dataset_version.canonical_dataset.tombstoned:
                 raise DatasetIsTombstonedException()
             return dataset_version
-        else:
-            canonical_dataset = self.database_provider.get_canonical_dataset(dataset_id)
-            if not canonical_dataset:
-                return None
-            # dataset has never been published, so fetch its most recently created version
-            latest = datetime.fromtimestamp(0)
-            unpublished_dataset = None
-            for dataset_version in self.database_provider.get_all_versions_for_dataset(dataset_id):
-                if dataset_version.created_at > latest:
-                    latest = dataset_version.created_at
-                    unpublished_dataset = dataset_version
-            return unpublished_dataset
+        # Dataset has never been published
+        return self.database_provider.get_most_recent_active_dataset_version(dataset_id)
 
     def get_prior_published_versions_for_dataset(self, dataset_id: DatasetId) -> List[PublishedDatasetVersion]:
         """

backend/layers/persistence/persistence.py
@@ -596,6 +596,28 @@ class DatabaseProvider(DatabaseProviderInterface):
                     return None
             return self._hydrate_dataset_version(dataset_version)
 
+    def get_most_recent_active_dataset_version(self, dataset_id: DatasetId) -> Optional[DatasetVersion]:
+        """
+        Returns the must recently active Dataset version for a canonical dataset_id
+        """
+        with self._manage_session() as session:
+            dataset_versions = session.query(DatasetVersionTable).filter_by(dataset_id=dataset_id.id).all()
+            if not dataset_versions:
+                return None
+            dataset_versions_map = {dv.id: dv for dv in dataset_versions}
+            # Dataset version ids in the latest Collection version
+            collection_id = dataset_versions[0].collection_id
+            dataset_version_ids_rows = (
+                session.query(CollectionVersionTable.datasets)
+                .filter_by(collection_id=collection_id)
+                .order_by(CollectionVersionTable.created_at.desc())
+                .all()
+            )
+            for dataset_row in dataset_version_ids_rows:
+                for dv_id in dataset_versions_map:
+                    if dv_id in dataset_row.datasets:
+                        return self._hydrate_dataset_version(dataset_versions_map.get(dv_id))
+
     def get_all_versions_for_dataset(self, dataset_id: DatasetId) -> List[DatasetVersion]:
         """
         Returns all dataset versions for a canonical dataset_id

backend/layers/persistence/persistence_interface.py
@@ -138,6 +138,11 @@ class DatabaseProviderInterface:
         Returns a dataset version by id.
         """
 
+    def get_most_recent_active_dataset_version(self, dataset_id: DatasetId) -> Optional[DatasetVersion]:
+        """
+        Returns the must recent, active Dataset version for a canonical dataset_id
+        """
+
     def get_all_versions_for_dataset(self, dataset_id: DatasetId) -> List[DatasetVersion]:
         """
         Returns all dataset versions for a canonical dataset_id

backend/layers/persistence/persistence_mock.py
@@ -322,6 +322,28 @@ class DatabaseProviderMock(DatabaseProviderInterface):
             dataset_versions.append(dataset_version)
         return dataset_versions
 
+    def get_most_recent_active_dataset_version(self, dataset_id: DatasetId) -> Optional[DatasetVersion]:
+        """
+        Returns the must recent, active Dataset version for a canonical dataset_id
+        """
+        dataset_versions = list(filter(lambda dv: dv.dataset_id == dataset_id, self.datasets_versions.values()))
+        if not dataset_versions:
+            return None
+        dataset_versions_map = {str(dv.version_id): dv for dv in dataset_versions}
+        collection_id = dataset_versions[0].collection_id
+        cv_dataset_versions_ids = [
+            cv.datasets
+            for cv in sorted(
+                filter(lambda cv: cv.collection_id == collection_id, self.collections_versions.values()),
+                key=lambda cv: cv.created_at,
+                reverse=True,
+            )
+        ]
+        for cv_dataset_versions in cv_dataset_versions_ids:
+            for dv_id in dataset_versions_map:
+                if DatasetVersionId(dv_id) in cv_dataset_versions:
+                    return self._update_dataset_version_with_canonical(dataset_versions_map.get(dv_id))
+
     def get_all_versions_for_dataset(self, dataset_id: DatasetId) -> List[DatasetVersion]:
         versions = []
         for dataset_version in self.datasets_versions.values():