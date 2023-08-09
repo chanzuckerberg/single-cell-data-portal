e621a2e1c0d6ca1afe00e80bf7bfce71f5429e0f
Prathap Sridharan <psridharan@chanzuckerberg.com>
2023-08-08 17:20:10 -0700

    Filter rejected observations before constructing expression summary cubes

--- backend/wmg/pipeline/summary_cubes/extract.py
+++ backend/wmg/pipeline/summary_cubes/extract.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 import tiledb
 
@@ -19,7 +20,12 @@ def extract_obs_data(tdb_group: str, cube_dims: list) -> pd.DataFrame:
     Extract obs (cell) data from the concatenated corpus
     """
     with tiledb.open(f"{tdb_group}/{OBS_ARRAY_NAME}") as obs:
-        cell_labels = obs.query(use_arrow=False).df[:]
+        obs_df = obs.query(use_arrow=False).df[:]
+
+        # filter out observations in the 'filter_cells' attribute
+        cells_to_keep_bool_index = np.logical_not(obs_df["filter_cells"])
+        cell_labels = obs_df[cells_to_keep_bool_index]
+
     cell_labels.sort_values(by=["obs_idx"], inplace=True, ignore_index=True)
 
     cell_labels = pd.DataFrame(
