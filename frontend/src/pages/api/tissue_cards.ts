45182f865ad82372f00e85cd89a573cd9241e64e
atarashansky <atarashansky@chanzuckerberg.com>
2023-08-11 11:48:13 -0400

    fix

--- frontend/src/pages/api/tissue_cards.ts
+++ frontend/src/pages/api/tissue_cards.ts
@@ -1,8 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { readJson } from "src/common/utils/api/readJson";
 
-const allTissues = readJson(
+export const allTissues = readJson(
   "src/views/CellGuide/common/fixtures/allTissues.json"
+);
+
+export const allTissueDescriptions = readJson(
+  "src/views/CellGuide/common/fixtures/tissueDescriptions.json"
 );
 
 async function handler(_: NextApiRequest, res: NextApiResponse) {
