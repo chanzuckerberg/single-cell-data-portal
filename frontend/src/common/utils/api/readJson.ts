59555cfc2520a02bd36c6171685f5ccc1118abc5
atarashansky <atarashansky@chanzuckerberg.com>
2023-08-11 09:37:23 -0400

    fix

--- frontend/src/common/utils/api/readJson.ts
+++ frontend/src/common/utils/api/readJson.ts
@@ -1,6 +1,6 @@
 import fs from "fs";
 
-export function readJson(path: string): unknown {
+export function readJson(path: string): any {
   const data = fs.readFileSync(path);
   return JSON.parse(data.toString("utf8"));
 }
