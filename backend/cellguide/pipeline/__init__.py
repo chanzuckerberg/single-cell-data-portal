c97b7d53df3041e0a699d1b20d0ea8cd3d1b34f3
atarashansky <atarashansky@chanzuckerberg.com>
2023-08-14 11:14:29 -0400

    revert __init__

--- backend/cellguide/pipeline/__init__.py
+++ backend/cellguide/pipeline/__init__.py
@@ -1,16 +1,5 @@
 import sys
-import time
 
-from backend.cellguide.pipeline.ontology_tree import run as run_ontology_tree_pipeline
-
-
-def run_cellguide_pipeline():
-    output_directory = f"cellguide_pipeline_output__{int(time.time())}"
-
-    # Run ontology tree pipeline
-    run_ontology_tree_pipeline(output_directory)
-
-
+# currently a no-op
 if __name__ == "__main__":
-    run_cellguide_pipeline()
     sys.exit()
