d5d213165d3bcf00cb4dff5a677fec6705bef8d5
atarashansky <atarashansky@chanzuckerberg.com>
2023-08-14 15:49:01 -0400

    feat(cellguide): build ontologyTreeStatePerCellType.json (#5486)
    * feat(cellguide): build ontologyTreeStatePerCellType.json
    
    * update init
    
    * doc
    
    * update doc
    
    * update return types

--- tests/unit/cellguide_pipeline/test_ontology_tree.py
+++ tests/unit/cellguide_pipeline/test_ontology_tree.py
@@ -48,9 +48,14 @@ class OntologyTreeBuilderTests(unittest.TestCase):
     def test__ontology_tree_builder(self):
         with open("tests/unit/cellguide_pipeline/fixtures/ontology_graph.json", "r") as f:
             expected__ontology_graph = json.load(f)
+        with open("tests/unit/cellguide_pipeline/fixtures/all_states_per_cell_type.json", "r") as f:
+            expected__all_states_per_cell_type = json.load(f)
         with load_realistic_test_snapshot(TEST_SNAPSHOT) as snapshot:
             cell_counts_df = snapshot.cell_counts_cube.df[:]
             tree_builder = OntologyTreeBuilder(cell_counts_df)
-            ontology_graph = tree_builder.ontology_graph
 
+            ontology_graph = tree_builder.ontology_graph
             self.assertTrue(compare_dicts(ontology_graph, expected__ontology_graph))
+
+            all_states_per_cell_type = tree_builder.get_ontology_tree_state_per_celltype()
+            self.assertTrue(compare_dicts(all_states_per_cell_type, expected__all_states_per_cell_type))
