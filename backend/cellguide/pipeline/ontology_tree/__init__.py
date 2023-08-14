36c044238896d1cd95796b5f7495d16a2186cbe2
atarashansky <atarashansky@chanzuckerberg.com>
2023-08-14 16:29:46 -0400

    feat(cellguide): build ontologyTreeStatePerTissue.json (#5488)
    * feat(cellguide): build ontologyTreeStatePerTissue.json
    
    * make line less ugly
    
    * add a couple of comments
    
    * kwargs

--- backend/cellguide/pipeline/ontology_tree/__init__.py
+++ backend/cellguide/pipeline/ontology_tree/__init__.py
@@ -1,4 +1,8 @@
-from backend.cellguide.pipeline.constants import ONTOLOGY_TREE_FILENAME, ONTOLOGY_TREE_STATE_PER_CELLTYPE_FILENAME
+from backend.cellguide.pipeline.constants import (
+    ONTOLOGY_TREE_FILENAME,
+    ONTOLOGY_TREE_STATE_PER_CELLTYPE_FILENAME,
+    ONTOLOGY_TREE_STATE_PER_TISSUE_FILENAME,
+)
 from backend.cellguide.pipeline.ontology_tree.tree_builder import OntologyTreeBuilder
 from backend.cellguide.pipeline.utils import output_json
 from backend.wmg.data.snapshot import WmgSnapshot, load_snapshot
@@ -13,3 +17,6 @@ def run(output_directory):
 
     all_states_per_cell_type = tree_builder.get_ontology_tree_state_per_celltype()
     output_json(all_states_per_cell_type, f"{output_directory}/{ONTOLOGY_TREE_STATE_PER_CELLTYPE_FILENAME}")
+
+    all_states_per_tissue = tree_builder.get_ontology_tree_state_per_tissue()
+    output_json(all_states_per_tissue, f"{output_directory}/{ONTOLOGY_TREE_STATE_PER_TISSUE_FILENAME}")
