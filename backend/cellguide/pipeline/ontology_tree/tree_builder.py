912da3bb96980f3038a13ed473a94dbcdc8134d7
atarashansky <atarashansky@chanzuckerberg.com>
2023-08-14 14:44:03 -0400

    feat(cellguide): build ontologyTreeStatePerCellType.json

--- backend/cellguide/pipeline/ontology_tree/tree_builder.py
+++ backend/cellguide/pipeline/ontology_tree/tree_builder.py
@@ -1,3 +1,4 @@
+import json
 import logging
 from dataclasses import dataclass
 from typing import Any, Dict, Optional
@@ -56,6 +57,8 @@ class OntologyTreeBuilder:
         self.all_unique_nodes = traverse_ontology_result.all_unique_nodes
 
         self._delete_unknown_terms_from_ontology_graph(self.ontology_graph)
+
+        self.all_children, self.all_parents = self._initialize_children_and_parents_per_node()
         self.start_node = f"{root_node}__0"
 
     def _initialize_cell_counts_df_rollup(self, cell_counts_df):
@@ -205,3 +208,332 @@ class OntologyTreeBuilder:
             traverse_node_counter=traverse_node_counter,
             all_unique_nodes=all_unique_nodes,
         )
+
+    ### Get the ontology tree state
+    def get_ontology_tree_state_per_celltype(self):
+        """
+        This function gets the ontology tree state per cell type. The ontology tree state is a mask that determines
+        which nodes are expanded by default and which nodes are not shown when expanded in the ontology.
+
+        The following rules determine which cell types are expanded by default and not shown when expanded:
+         - All instances of the target cell type are shown.
+         - All children of the first instance of the target cell type are shown.
+         - All grandchildren of the first instance of the target cell type are shown.
+         - All siblings of the first instance of the target cell type are shown.
+         - All nodes in the paths to any instance of the target cell type are shown.
+        Nodes that are not shown when expanded will be collapsed into a dummy node that contains the ids of the
+        children that are not shown. Nodes will be collapsed into dummy nodes if they are siblings of nodes that are
+        in a path to any instances of the target cell type.
+
+        Returns
+        -------
+        all_states_per_cell_type: dict
+            A dictionary that maps cell type ontology term ids to their ontology tree states.
+            The ontology tree state is a dictionary with keys
+             - "isExpandedNodes": A list of cell type ontology term ids that are expanded by default.
+             - "notShownWhenExpandedNodes": A dictionary that maps cell type ontology term ids to their hidden children
+        """
+        all_states_per_cell_type = {}
+        for i, end_node in enumerate(self.all_cell_type_ids_in_corpus):
+            if end_node in self.traverse_node_counter:
+                logger.info(
+                    "Getting ontology tree state for cell type %s (%s/%s)",
+                    end_node,
+                    i + 1,
+                    len(self.all_cell_type_ids_in_corpus),
+                )
+
+                all_paths = []
+                siblings = []
+                for i in range(self.traverse_node_counter[end_node]):
+                    end_node_i = end_node + "__" + str(i)
+                    path = self._depth_first_search_pathfinder(end_node_i)
+                    path = path if path else [end_node_i]
+                    all_paths.append(path)
+
+                    siblings.append(
+                        sum([self.all_children.get(parent, []) for parent in self.all_parents.get(end_node_i, [])], [])
+                    )
+
+                visited_nodes_in_paths = list(set(sum(all_paths, [])))  # in a path to target
+
+                children = self.all_children.get(end_node + "__0", [])  # children
+                grandchildren = sum([self.all_children.get(child, []) for child in children], [])  # grandchildren
+                siblings = list(set(sum(siblings, [])))  # siblings
+                valid_nodes = list(set(visited_nodes_in_paths + children + grandchildren + siblings))
+
+                ontology_graph_copy = self._get_deepcopy_of_ontology_graph()
+                self._truncate_graph_first_pass(ontology_graph_copy, valid_nodes)
+                self._truncate_graph_second_pass(ontology_graph_copy, visited_nodes_in_paths)
+
+                isExpandedNodes = list(set(_getExpandedData(ontology_graph_copy)))
+                notShownWhenExpandedNodes = _getShownData(ontology_graph_copy)
+
+                notShownWhenExpanded = {}
+                for i in notShownWhenExpandedNodes:
+                    notShownWhenExpanded.update(i)
+                all_states_per_cell_type[end_node] = {
+                    "isExpandedNodes": isExpandedNodes,
+                    "notShownWhenExpandedNodes": notShownWhenExpanded,
+                }
+
+        return all_states_per_cell_type
+
+    def _depth_first_search_pathfinder(self, path_end_node, node=None, path=None):
+        """
+        This function finds a path backwards from the end node to the start node using depth-first search.
+
+        Arguments
+        ---------
+        path_end_node: str
+            The end node of the path.
+        node: str, optional, default=None
+            The current node in the depth-first search.
+        path: list, optional, default=None
+            The current path in the depth-first search.
+
+        Returns
+        -------
+        path: list
+            The path from the end node to the start node.
+        """
+        if path is None:
+            path = [path_end_node]
+            node = path_end_node
+
+        if node == self.start_node:
+            return path
+
+        for parent in self.all_parents.get(node, []):
+            full_path = self._depth_first_search_pathfinder(path_end_node, node=parent, path=path + [parent])
+            if full_path:
+                return full_path
+
+    def _truncate_graph_first_pass(self, graph, valid_nodes):
+        """
+        This function truncates the ontology graph in-place by removing nodes that are not in the valid nodes list.
+        It also adds a dummy node to the graph if the graph has children that are not in the valid nodes list.
+        The dummy node contains the ids of the invalid children.
+
+        Arguments
+        ---------
+        graph: dict
+            A node of the ontology graph. At the top-level, this should be the root node of the ontology graph.
+        valid_nodes: list
+            A list of valid cell type ontology term ids.
+        """
+        if graph["id"] not in valid_nodes:
+            return False
+
+        children = graph.get("children", [])
+        valid_children = []
+        append_dummy = False
+
+        invalid_children_ids = []
+        for child in children:
+            is_valid = self._truncate_graph_first_pass(child, valid_nodes)
+            if is_valid:
+                valid_children.append(child)
+            elif child["id"] != "":
+                invalid_children_ids.append(child["id"])
+                append_dummy = True
+
+        if append_dummy and len(valid_children) > 0:
+            valid_children.append(
+                {
+                    "id": "",
+                    "name": "",
+                    "n_cells_rollup": 0,
+                    "n_cells": 0,
+                    "invalid_children_ids": invalid_children_ids,
+                    "parent": graph["id"],
+                }
+            )
+        if len(valid_children) > 0:
+            graph["children"] = valid_children
+        else:
+            if "children" in graph:
+                del graph["children"]
+
+        return True
+
+    def _truncate_graph_second_pass(self, graph, visited_nodes_in_paths, nodesWithChildrenFound=None):
+        """
+        We do not want to show cell types multiple times in the ontology tree unless they are nodes that are in one of the
+        paths to the target cell type. In this case, show the node in the path and collapse all its siblings into the
+        dummy node.
+
+        Arguments
+        ---------
+        graph: dict
+            A node of the ontology graph. At the top-level, this should be the root node of the ontology graph.
+        visited_nodes_in_paths: list
+            A list of cell type ontology term ids that are in one of the paths to the target cell type.
+            The target cell type may have multiple instances in the ontology graph (each instance with a unique suffix).
+        nodesWithChildrenFound: set, optional, default=None
+            A set of cell type ontology term ids that have children. This is used to remove children of nodes that
+            have already been encountered during the traversal.
+        """
+        if nodesWithChildrenFound is None:
+            nodesWithChildrenFound = set()
+        if graph["id"].split("__")[0] in nodesWithChildrenFound:
+            if "children" in graph:
+                children = graph["children"]
+                new_children = []
+                invalid_children_ids = []
+                for child in children:
+                    if child["id"] in visited_nodes_in_paths:
+                        new_children.append(child)
+                    elif child["id"] != "":
+                        invalid_children_ids.append(child["id"])
+
+                if len(children) > len(new_children) and len(new_children) > 0:
+                    # append dummy
+                    new_children.append(
+                        {
+                            "id": "",
+                            "name": "",
+                            "n_cells_rollup": 0,
+                            "n_cells": 0,
+                            "invalid_children_ids": invalid_children_ids,
+                            "parent": graph["id"],
+                        }
+                    )
+                if len(new_children) > 0:
+                    graph["children"] = new_children
+                else:
+                    del graph["children"]
+        elif "children" in graph:
+            nodesWithChildrenFound.add(graph["id"].split("__")[0])
+
+        children = graph.get("children", [])
+        for child in children:
+            if child["id"] != "":
+                self._truncate_graph_second_pass(child, visited_nodes_in_paths, nodesWithChildrenFound)
+
+    def _initialize_children_and_parents_per_node(self):
+        """
+        Initializes dictionaries that map cell type ontology term ids to their children and parents.
+        While the cell ontology contains multiple instances of the same node, the ontology graph
+        contains only one instance of each node as each cell type has been suffixed with a unique integer.
+        Therefore, each node only has one parent and a unique set of children.
+
+        Returns
+        -------
+        all_children: dict
+            A dictionary that maps cell type ontology term ids to their children.
+        all_parents: dict
+            A dictionary that maps cell type ontology term ids to their parents.
+        """
+        all_children = self._build_children_per_node(self.ontology_graph)
+        all_parents = self._build_parents_per_node(self.ontology_graph)
+        return all_children, all_parents
+
+    def _build_children_per_node(self, node, all_children=None):
+        """
+        Builds a dictionary that maps cell type ontology term ids to their children.
+
+        Arguments
+        ---------
+        node: dict
+            A node of the ontology graph. At the top-level, this should be the root node of the ontology graph.
+        all_children: dict, optional, default=None
+            A dictionary that maps cell type ontology term ids to their children.
+
+        Returns
+        -------
+        all_children: dict
+            A dictionary that maps cell type ontology term ids to their children.
+        """
+        if all_children is None:
+            all_children = {}
+        children = node.get("children", [])
+        ids = [] if len(children) == 0 else [child["id"] for child in children]
+
+        all_children[node["id"]] = ids
+        for child in children:
+            self._build_children_per_node(child, all_children)
+        return all_children
+
+    def _build_parents_per_node(self, node, all_parents=None):
+        """
+        Builds a dictionary that maps cell type ontology term ids to their parents.
+
+        Arguments
+        ---------
+        node: dict
+            A node of the ontology graph. At the top-level, this should be the root node of the ontology graph.
+        all_parents: dict, optional, default=None
+            A dictionary that maps cell type ontology term ids to their parents.
+
+        Returns
+        -------
+        all_parents: dict
+            A dictionary that maps cell type ontology term ids to their parents.
+        """
+        if all_parents is None:
+            all_parents = {}
+        children = node.get("children", [])
+
+        for child in children:
+            all_parents[child["id"]] = [node["id"]]
+            self._build_parents_per_node(child, all_parents)
+        return all_parents
+
+    def _get_deepcopy_of_ontology_graph(self):
+        """
+        Returns a deepcopy of the ontology graph.
+        """
+        return json.loads(json.dumps(self.ontology_graph))
+
+
+def _getExpandedData(ontology_graph, isExpandedNodes=None):
+    """
+    This function gets the cell type ontology term ids of the nodes that are expanded by default.
+
+    Arguments
+    ---------
+    ontology_graph: dict
+        A node of the ontology graph. At the top-level, this should be the root node of the ontology graph.
+
+    Returns
+    -------
+    isExpandedNodes: list
+        A list of cell type ontology term ids that are expanded by default.
+    """
+    if isExpandedNodes is None:
+        isExpandedNodes = []
+
+    if "children" in ontology_graph:
+        isExpandedNodes.append(ontology_graph["id"])
+        for child in ontology_graph["children"]:
+            _getExpandedData(child, isExpandedNodes)
+
+    return isExpandedNodes
+
+
+def _getShownData(graph, notShownWhenExpandedNodes=None):
+    """
+    This function gets the children nodes that are not shown when expanded for each node in the ontology graph.
+
+    Arguments
+    ---------
+    graph: dict
+        A node of the ontology graph. At the top-level, this should be the root node of the ontology graph.
+
+    Returns
+    -------
+    notShownWhenExpandedNodes: list
+        A list of dictionaries that map cell type ontology term ids to their hidden children.
+    """
+    if notShownWhenExpandedNodes is None:
+        notShownWhenExpandedNodes = []
+
+    if "children" in graph:
+        for child in graph["children"]:
+            if child["id"] == "":
+                if len(child["invalid_children_ids"]) > 0:
+                    notShownWhenExpandedNodes.append({child["parent"]: list(set(child["invalid_children_ids"]))})
+            else:
+                _getShownData(child, notShownWhenExpandedNodes)
+    return notShownWhenExpandedNodes
