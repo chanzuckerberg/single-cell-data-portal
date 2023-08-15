fbe5a7212c907c4c85f48134e9e80045c58d96a2
Joyce Yan <5653616+joyceyan@users.noreply.github.com>
2023-08-15 12:29:26 -0400

    update type

--- backend/cellguide/pipeline/metadata/__init__.py
+++ backend/cellguide/pipeline/metadata/__init__.py
@@ -1,8 +1,7 @@
-from typing import Any
-
 from backend.cellguide.pipeline.metadata.metadata_generator import generate_cellguide_card_metadata
+from backend.cellguide.pipeline.metadata.types import CellMetadata
 from backend.cellguide.pipeline.ontology_tree.tree_builder import OntologyTreeBuilder
 
 
-def run(ontology_tree: OntologyTreeBuilder) -> dict[str, Any]:
+def run(ontology_tree: OntologyTreeBuilder) -> dict[str, CellMetadata]:
     return generate_cellguide_card_metadata(ontology_tree.all_cell_type_ids_in_corpus)
