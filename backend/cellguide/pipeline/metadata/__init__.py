3fc60ee4b66fd517dbd4ce61d9ecfbe333126c53
Joyce Yan <5653616+joyceyan@users.noreply.github.com>
2023-08-15 12:25:04 -0400

    implement pipeline for cellguide metadata

---
+++ backend/cellguide/pipeline/metadata/__init__.py
@@ -0,0 +1,8 @@
+from typing import Any
+
+from backend.cellguide.pipeline.metadata.metadata_generator import generate_cellguide_card_metadata
+from backend.cellguide.pipeline.ontology_tree.tree_builder import OntologyTreeBuilder
+
+
+def run(ontology_tree: OntologyTreeBuilder) -> dict[str, Any]:
+    return generate_cellguide_card_metadata(ontology_tree.all_cell_type_ids_in_corpus)
---
+++ backend/cellguide/pipeline/metadata/metadata_generator.py
@@ -0,0 +1,26 @@
+from pronto import Ontology
+
+from backend.cellguide.pipeline.metadata.types import CellMetadata
+from backend.wmg.data.constants import CL_BASIC_PERMANENT_URL_PRONTO
+
+
+def generate_cellguide_card_metadata(all_cell_type_ids_in_corpus: list[str]) -> dict[str, CellMetadata]:
+    ontology = Ontology(CL_BASIC_PERMANENT_URL_PRONTO)
+
+    cellguide_card_metadata: dict[str, CellMetadata] = {}
+    for id in all_cell_type_ids_in_corpus:
+        cell = ontology[id]
+        if not cell.id.startswith("CL:"):
+            continue
+        if cell.obsolete:
+            continue
+
+        metadata = CellMetadata(
+            name=cell.name,
+            id=cell.id,
+            clDescription=str(cell.definition) if cell.definition is not None else None,
+            synonyms=[s.description for s in cell.synonyms],
+        )
+        cellguide_card_metadata[cell.id] = metadata
+
+    return cellguide_card_metadata
