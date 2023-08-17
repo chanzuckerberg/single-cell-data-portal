2adbfa552c9f44d1150a5c4256a51c315973db27
atarashansky <atarashansky@chanzuckerberg.com>
2023-08-17 13:49:34 -0400

    feat(cellguide): compile all cell type marker genes from ASCTB (#5520)
    * feat(cellguide):  compile all cell type marker genes from ASCTB (#5520)

--- backend/cellguide/pipeline/constants.py
+++ backend/cellguide/pipeline/constants.py
@@ -1,6 +1,12 @@
 ONTOLOGY_TREE_FILENAME = "ontologyTree.json"
 ONTOLOGY_TREE_STATE_PER_CELLTYPE_FILENAME = "ontologyTreeStatePerCellType.json"
 ONTOLOGY_TREE_STATE_PER_TISSUE_FILENAME = "ontologyTreeStatePerTissue.json"
+CANONICAL_MARKER_GENES_FILENAME = "allCellTypeMarkerGenes.json"
 CELL_GUIDE_METADATA_FILENAME = "cellGuideMetadata.json"
 
 UBERON_BASIC_PERMANENT_URL_PRONTO = "http://purl.obolibrary.org/obo/uberon.obo"
+
+ASCTB_MASTER_SHEET_URL = "https://purl.org/ccf/releases/2.2.1/ccf-asctb-all.json"
+ENSEMBL_GENE_ID_TO_DESCRIPTION_FILENAME = "ensembl_gene_ids_to_descriptions.tsv.gz"
+
+HOMO_SAPIENS_ORGANISM_ONTOLOGY_TERM_ID = "NCBITaxon:9606"
