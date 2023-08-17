4deebcdfd7a9a0a591b811c580b5e492372a9fcf
Daniel Hegeman <daniel.hegeman@chanzuckerberg.com>
2023-08-17 15:41:14 -0700

    test: add resurrection script unit test

--- tests/unit/scripts/test_tombstone.py
+++ tests/unit/scripts/test_tombstone.py
@@ -2,7 +2,7 @@ from unittest.mock import MagicMock
 
 from click import Context
 
-from scripts.cxg_admin_scripts.tombstones import tombstone_collection
+from scripts.cxg_admin_scripts.tombstones import resurrect_collection, tombstone_collection
 from tests.unit.backend.layers.common.base_test import BaseTest
 
 
@@ -18,7 +18,23 @@ class TestTombstone(BaseTest):
 
         context = MagicMock(spec=Context)
         context.obj = {"business_logic": self.business_logic}
+        tombstone_collection(context, c_v.collection_id.id)
+
+        c_v = self.business_logic.get_collection_version(c_v.version_id, get_tombstoned=True)
+        self.assertTrue(c_v.canonical_collection.tombstoned)
+
+    def test__resurrect_collection(self):
+        c_v = self.generate_published_collection()
+        self.assertFalse(c_v.canonical_collection.tombstoned)
+        [self.assertFalse(d.canonical_dataset.tombstoned) for d in c_v.datasets]
+
+        context = MagicMock(spec=Context)
+        context.obj = {"business_logic": self.business_logic}
         tombstone_collection(context, c_v.canonical_collection.id.id)
 
         c_v = self.business_logic.get_collection_version(c_v.version_id, get_tombstoned=True)
         self.assertTrue(c_v.canonical_collection.tombstoned)
+
+        resurrect_collection(context, c_v.collection_id.id)
+        c_v = self.business_logic.get_collection_version(c_v.version_id)
+        self.assertFalse(c_v.canonical_collection.tombstoned)
