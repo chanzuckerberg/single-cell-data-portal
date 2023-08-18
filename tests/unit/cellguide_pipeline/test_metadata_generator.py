9fd7468d2d97e18629470f35827570a37539b926
atarashansky <atarashansky@chanzuckerberg.com>
2023-08-18 14:42:54 -0400

    feat(cellguide): add tissue metadata pipeline

--- tests/unit/cellguide_pipeline/test_metadata_generator.py
+++ tests/unit/cellguide_pipeline/test_metadata_generator.py
@@ -1,7 +1,10 @@
 import json
 import unittest
 
-from backend.cellguide.pipeline.metadata.metadata_generator import generate_cellguide_card_metadata
+from backend.cellguide.pipeline.metadata.metadata_generator import (
+    generate_cellguide_card_metadata,
+    generate_cellguide_tissue_card_metadata,
+)
 from backend.cellguide.pipeline.ontology_tree.tree_builder import OntologyTreeBuilder
 from backend.cellguide.pipeline.utils import convert_dataclass_to_dict_and_strip_nones
 from tests.test_utils.dict_compare import compare_dicts
@@ -25,4 +28,18 @@ class TestMetadataGenerator(unittest.TestCase):
             )
             self.assertTrue(
                 compare_dicts(convert_dataclass_to_dict_and_strip_nones(cell_metadata), expected__cell_metadata)
+            )
+
+    def test__tissue_metadata_generator(self):
+        with open("tests/unit/cellguide_pipeline/fixtures/tissue_metadata.json", "r") as f:
+            expected__tissue_metadata = json.load(f)
+        with load_realistic_test_snapshot(TEST_SNAPSHOT) as snapshot:
+            cell_counts_df = snapshot.cell_counts_cube.df[:]
+            tree_builder = OntologyTreeBuilder(cell_counts_df)
+
+            tissue_metadata = generate_cellguide_tissue_card_metadata(
+                all_tissue_ids_in_corpus=tree_builder.all_tissue_ids_in_corpus
+            )
+            self.assertTrue(
+                compare_dicts(convert_dataclass_to_dict_and_strip_nones(tissue_metadata), expected__tissue_metadata)
             )
