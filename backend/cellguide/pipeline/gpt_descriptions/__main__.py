61e1ffcd482158c7a074e49133ac4239467e375c
atarashansky <atarashansky@chanzuckerberg.com>
2023-08-25 15:27:41 -0700

    fix __main__

--- backend/cellguide/pipeline/gpt_descriptions/__main__.py
+++ backend/cellguide/pipeline/gpt_descriptions/__main__.py
@@ -1,8 +1,16 @@
+import logging
+import time
+
 from backend.cellguide.pipeline.gpt_descriptions import run as run_gpt_description_pipeline
+
+logging.basicConfig(level=logging.INFO)
 
 
 def run_pipeline():
-    run_gpt_description_pipeline()
+    output_directory = f"gpt_descriptions__{int(time.time())}"
+
+    # Generate computational marker genes from the CZI corpus
+    run_gpt_description_pipeline(output_directory=output_directory)
 
 
 if __name__ == "__main__":
