f3a7d2d5988b61ce48986a3edad5946966e0d6b9
Daniel Hegeman <daniel.hegeman@chanzuckerberg.com>
2023-08-28 11:51:03 -0700

    refactor context in cxg admin tombstone tests

--- tests/unit/scripts/test_tombstone.py
+++ tests/unit/scripts/test_tombstone.py
@@ -11,12 +11,15 @@ class TestTombstone(BaseTest):
     Test scripting for tombstoning Collections and Datasets
     """
 
+    def get_context(self):
+        context = MagicMock(spec=Context)
+        context.obj = {"business_logic": self.business_logic}
+        return context
+
     def test__tombstone_collection(self):
         c_v = self.generate_published_collection()
 
-        context = MagicMock(spec=Context)
-        context.obj = {"business_logic": self.business_logic}
-        tombstone_collection(context, c_v.collection_id.id)
+        tombstone_collection(self.get_context(), c_v.collection_id.id)
 
         c_v = self.business_logic.get_collection_version(c_v.version_id, get_tombstoned=True)
         self.assertTrue(c_v.canonical_collection.tombstoned)
@@ -24,12 +27,10 @@ class TestTombstone(BaseTest):
     def test__resurrect_collection(self):
         c_v = self.generate_published_collection()
 
-        context = MagicMock(spec=Context)
-        context.obj = {"business_logic": self.business_logic}
+        context = self.get_context()
         tombstone_collection(context, c_v.canonical_collection.id.id)
 
         c_v = self.business_logic.get_collection_version(c_v.version_id, get_tombstoned=True)
-        self.assertTrue(c_v.canonical_collection.tombstoned)
 
         resurrect_collection(context, c_v.collection_id.id)
         c_v = self.business_logic.get_collection_version(c_v.version_id)
