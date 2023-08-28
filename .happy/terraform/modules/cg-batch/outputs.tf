5583b2ff17bc679cafce7660f37f3869a0a0ace2
atarashansky <atarashansky@chanzuckerberg.com>
2023-08-28 13:07:12 -0400

    desc

--- .happy/terraform/modules/cg-batch/outputs.tf
+++ .happy/terraform/modules/cg-batch/outputs.tf
@@ -1,9 +1,9 @@
 output batch_job_definition {
   value       = aws_batch_job_definition.batch_job_def.id
-  description = "ARN for the batch job definition"
+  description = "ARN for the CellGuide batch job definition"
 }
 
 output batch_job_definition_no_revision {
   value       = "arn:aws:batch:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:job-definition/${aws_batch_job_definition.batch_job_def.name}"
-  description = "ARN for the batch job definition"
+  description = "ARN for the CellGuide batch job definition"
 }
