0daf42544133afa318f70853b1df6d01209ecb87
prathap sridharan <psridharan@chanzuckerberg.com>
2023-08-29 16:21:40 -0700

    feat(cellguide): Add global tissue and organism filter dropdown (#5554)
    * feat: Add global tissue filter dropdown
    
    * Filter marker genes tables by selected tissue
    
    * Refactor marker genes table filtering hook
    
    * Fix typescript compilation error
    
    * Fix E2E tests for marker genes filtering in cellguide
    
    * Display marker genes unavailable when filter result set is empty
    
    * Filter tissue list by cell type
    
    * Add global organism filter dropdown for cell card
    
    * Make organism and tissue filter to be right aligned
    
    * Use enriched genes to derive the dropdown list for global tissue filter
    
    * Construct a map of tissue label to tissue id for ontology dag filtering
    
    * Filter cell ontology dag by selected tissue in cell card
    
    * Set NO_TISSUE_ID constant to be the empty string
    
    * Move all tissues map construction into a hook
    
    * Rename implemention of filtering to impart concept of filtering by "organs"
    
    * resolve merge conflicts
    
    * resolve merge conflicts pt2
    
    * Filter marker gene table by organ and its sub-tissues
    
    * Filter source data by global tissue and organism filters
    
    * Make default dropdown selection from tissue dropdown to be robust
    
    * Check if all of the filtered computational marker genes are due to low score
    
    * Fix bug in identifying set of genes failing marker score filter
    
    * Simplify logic for identifying marker genes filtered by low marker score
    
    * Add better comments
    
    * fix: Failed to execute insertBefore on Node issue (#5637)
    
    * Fix code style
    
    * Fix code style
    
    * test: Add tryUntil for CellGuide test
    
    ---------
    
    Co-authored-by: atarashansky <atarashansky@chanzuckerberg.com>
    Co-authored-by: Timmy Huang <tihuan@users.noreply.github.com>
    Co-authored-by: Timmy Huang <thuang@chanzuckerberg.com>

--- frontend/src/views/CellGuide/components/CellGuideCard/constants.ts
+++ frontend/src/views/CellGuide/components/CellGuideCard/constants.ts
@@ -3,3 +3,9 @@ export const CELL_GUIDE_CARD_HEADER_NAME = "cell-guide-card-header-name";
 export const CELL_GUIDE_CARD_HEADER_TAG = "cell-guide-card-header-tag";
 
 export const CELL_GUIDE_CARD_SYNONYMS = "cell-guide-card-synonyms";
+
+export const CELL_GUIDE_CARD_GLOBAL_TISSUE_FILTER_DROPDOWN =
+  "cell-guide-card-global-tissue-filter-dropdown";
+
+export const CELL_GUIDE_CARD_GLOBAL_ORGANISM_FILTER_DROPDOWN =
+  "cell-guide-card-global-organism-filter-dropdown";
