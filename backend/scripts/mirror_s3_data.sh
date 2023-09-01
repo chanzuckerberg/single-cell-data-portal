509659893446a3b20ef39bd2a49b80fe692f05d3
Daniel Hegeman <daniel.hegeman@chanzuckerberg.com>
2023-09-01 14:30:51 -0700

    implement s3 cp for $COLLECTIONS arg

--- backend/scripts/mirror_s3_data.sh
+++ backend/scripts/mirror_s3_data.sh
@@ -28,43 +28,43 @@ if [[ ! -z "$COLLECTIONS" ]]; then
   IFS=$'\n'
   uris=($res)
   IFS=$old_ifs
-#  for uri in "${uris[@]}"; do
-#    bucket=$(sed -E 's/s3:\/\/([^\/]+).*/\1/' <<< $uri)
-#    if [[ ! -z `grep 'hosted-cellxgene' $bucket` ]]; then
-#      rdev_bucket_suffix="cellxgene"
-#    elif [[ ! -z `grep 'corpora-data' $bucket`]]; then
-#      rdev_bucket_suffix="artifacts"
-#    key=$(sed -E 's/s3:\/\/([^\/]+)\/(.*)/\2/' <<< $uri)
-#    if [[ "${key: -1}" == "/" ]]; then
-#      aws s3 cp --recursive $uri s3://env-rdev-${rdev_bucket_suffix}/${STACK}/${key}
-#    else
-#      aws s3 cp $uri s3://env-rdev-${rdev_bucket_suffix}/${STACK}/${key}
-#    fi
-#  done
+  for uri in "${uris[@]}"; do
+    bucket=$(sed -E 's/s3:\/\/([^\/]+).*/\1/' <<< $uri)
+    if [[ ! -z `grep 'hosted-cellxgene' <<< $bucket` ]]; then
+      rdev_bucket_suffix="cellxgene"
+    elif [[ ! -z `grep 'corpora-data' <<< $bucket` ]]; then
+      rdev_bucket_suffix="artifacts"
+    fi
+    key=$(sed -E 's/s3:\/\/([^\/]+)\/(.*)/\2/' <<< $uri)
+    recursive_flag=$([[ ! "${key: -1}" == "/" ]] || echo "--recursive ")
+    echo "copying ${recursive_flag}$uri to s3://env-rdev-${rdev_bucket_suffix}/${STACK}/${key}"
+    aws s3 cp ${recursive_flag}${uri} s3://env-rdev-${rdev_bucket_suffix}/${STACK}/${key}
+  done
+  exit 0
 fi
 
-## TODO: Add --delete once we confirm that is no data in the folders
-## that needs to be kept around (buckets are versioned, so we're not in
-## jeopardy of losing anything permanently). This would make the
-## operation more destructive, of course!
-##
-## Note: "--copy-props metadata-directive" copies s3 object metadata, but not tags.
-## We have not granted s3:GetObjectTagging perm to dev AWS account on prod buckets,
-## so this avoids errors. As none of the s3 objects contain tags, this is acceptable.
-#S3_SYNC_CMD="aws s3 sync --copy-props metadata-directive --no-progress"
+# TODO: Add --delete once we confirm that is no data in the folders
+# that needs to be kept around (buckets are versioned, so we're not in
+# jeopardy of losing anything permanently). This would make the
+# operation more destructive, of course!
 #
-#set -x
-#$S3_SYNC_CMD --exclude '*loom' s3://corpora-data-${SRC_ENV}/ s3://corpora-data-${DEST_ENV}/
-#$S3_SYNC_CMD s3://hosted-cellxgene-${SRC_ENV}/ s3://hosted-cellxgene-${DEST_ENV}/
-## We need to assume the sync-datasets-{dev|staging} role, which has the necessary permissions to perform a s3 sync from
-## {dev|staging|prod} to {dev|staging} **for the public-access datasets bucket**.
-#ACCOUNT_ID=`aws sts get-caller-identity | jq -r '.Arn' | grep -o '[0-9]*'`  # First get AWS account id for dev account
-#SYNC_ROLE_CREDENTIALS=`aws sts assume-role --role-arn arn:aws:iam::${ACCOUNT_ID}:role/sync-datasets-dev \
-#                                           --role-session-name sync-datasets \
-#                       | jq -r '.Credentials'`
-## Execute sync for datasets bucket with sync role credentials
-#AWS_SESSION_TOKEN=`echo $SYNC_ROLE_CREDENTIALS | jq -r '.SessionToken'` \
-#AWS_SECRET_ACCESS_KEY=`echo $SYNC_ROLE_CREDENTIALS | jq -r '.SecretAccessKey'` \
-#AWS_ACCESS_KEY_ID=`echo $SYNC_ROLE_CREDENTIALS | jq -r '.AccessKeyId'` \
-#$S3_SYNC_CMD s3://dataset-assets-public-${SRC_ENV}/ s3://dataset-assets-public-${DEST_ENV}/
-#set +x
+# Note: "--copy-props metadata-directive" copies s3 object metadata, but not tags.
+# We have not granted s3:GetObjectTagging perm to dev AWS account on prod buckets,
+# so this avoids errors. As none of the s3 objects contain tags, this is acceptable.
+S3_SYNC_CMD="aws s3 sync --copy-props metadata-directive --no-progress"
+
+set -x
+$S3_SYNC_CMD --exclude '*loom' s3://corpora-data-${SRC_ENV}/ s3://corpora-data-${DEST_ENV}/
+$S3_SYNC_CMD s3://hosted-cellxgene-${SRC_ENV}/ s3://hosted-cellxgene-${DEST_ENV}/
+# We need to assume the sync-datasets-{dev|staging} role, which has the necessary permissions to perform a s3 sync from
+# {dev|staging|prod} to {dev|staging} **for the public-access datasets bucket**.
+ACCOUNT_ID=`aws sts get-caller-identity | jq -r '.Arn' | grep -o '[0-9]*'`  # First get AWS account id for dev account
+SYNC_ROLE_CREDENTIALS=`aws sts assume-role --role-arn arn:aws:iam::${ACCOUNT_ID}:role/sync-datasets-dev \
+                                           --role-session-name sync-datasets \
+                       | jq -r '.Credentials'`
+# Execute sync for datasets bucket with sync role credentials
+AWS_SESSION_TOKEN=`echo $SYNC_ROLE_CREDENTIALS | jq -r '.SessionToken'` \
+AWS_SECRET_ACCESS_KEY=`echo $SYNC_ROLE_CREDENTIALS | jq -r '.SecretAccessKey'` \
+AWS_ACCESS_KEY_ID=`echo $SYNC_ROLE_CREDENTIALS | jq -r '.AccessKeyId'` \
+$S3_SYNC_CMD s3://dataset-assets-public-${SRC_ENV}/ s3://dataset-assets-public-${DEST_ENV}/
+set +x
