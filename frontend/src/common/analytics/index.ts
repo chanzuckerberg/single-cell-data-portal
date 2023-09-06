0b4a6ba43a86719c6b2390379fb386b8f268aa70
atarashansky <atarashansky@chanzuckerberg.com>
2023-09-06 09:27:21 -0400

    fix(e2e): add try-catch to ignore plausible errors in playwright tests

--- frontend/src/common/analytics/index.ts
+++ frontend/src/common/analytics/index.ts
@@ -20,8 +20,11 @@ const serializeProps = (
 
 export function track(event: EVENTS, props?: Record<string, unknown>): void {
   const options = serializeProps(props);
-  if (process.env.NODE_ENV !== "development") {
+  try {
     window.plausible(event, options);
+  } catch (e) {
+    // adding this to catch errors in tests
+    console.error(e);
   }
 
   if (API_URL !== "https://api.cellxgene.cziscience.com") {
