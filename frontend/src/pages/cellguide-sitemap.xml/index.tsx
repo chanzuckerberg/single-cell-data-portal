ca836eb6780bd392db9a3476faddaf696e20e71b
Seth Feingold <sfeingold1104@gmail.com>
2023-09-08 11:12:06 -0500

    updating cellguide xml sitemap to include tissues, fixing date formatting on lastmod

--- frontend/src/pages/cellguide-sitemap.xml/index.tsx
+++ frontend/src/pages/cellguide-sitemap.xml/index.tsx
@@ -2,6 +2,7 @@ import { GetServerSideProps } from "next";
 import { getServerSideSitemap, ISitemapField } from "next-sitemap";
 import { convertTimestamp } from "src/common/sitemaps/utils";
 import { CELLGUIDE_DATA_URL } from "src/configs/configs";
+import { ROUTES } from "src/common/constants/routes";
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const latestSnapshotRes = await fetch(
@@ -14,19 +15,37 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     `${CELLGUIDE_DATA_URL}/${latestSnapshotId}/celltype_metadata.json`
   );
 
+  const tissueResponse = await fetch(
+    `${CELLGUIDE_DATA_URL}/${latestSnapshotId}/tissue_metadata.json`
+  );
+
   if (!response.ok) {
     throw new Error(`Failed to fetch data. Status: ${response.status}`);
   }
 
+  if (!tissueResponse.ok) {
+    throw new Error(`Failed to fetch data. Status: ${tissueResponse.status}`);
+  }
+
   const cellGuideData = await response.json();
+
+  const tissueData = await tissueResponse.json();
 
   const cellGuideIds = Object.keys(cellGuideData);
 
-  const fields: ISitemapField[] = cellGuideIds.map((id: string) => {
-    const date = new Date().getTime();
+  const tissueDataIds = Object.keys(tissueData);
+
+  const allIds = [...cellGuideIds, ...tissueDataIds];
+
+  const fields: ISitemapField[] = allIds.map((id: string) => {
+    const date = Math.floor(new Date().getTime() / 1000);
+
+    const endpoint = id.includes("UBERON")
+      ? `https://cellxgene.cziscience.com${ROUTES.CELL_GUIDE}/tissues/${id}`
+      : `https://cellxgene.cziscience.com${ROUTES.CELL_GUIDE}/${id}`;
 
     return {
-      loc: `https://cellxgene.cziscience.com/cellguide/${id}`,
+      loc: endpoint,
       lastmod: convertTimestamp(date.toString()),
     };
   });
