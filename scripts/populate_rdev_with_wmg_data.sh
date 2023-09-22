964f21e443d513974c9234a2eea29bba2d680572
atarashansky <atarashansky@chanzuckerberg.com>
2023-09-22 09:30:37 -0700

    chore: add script to seed wmg rdev data (#5803)

--- scripts/populate_rdev_with_wmg_data.sh
+++ scripts/populate_rdev_with_wmg_data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 STACK_NAME=""
 SRC_DEPLOYMENT="staging"
-POPULATE_ONLY_GPT=false
+SNAPSHOT_VERSION="v1"
 
 function usage {
     echo "Usage: $0 [-s|--src_deployment <src_deployment>] [-g|--gpt_only] <stack_name>"
@@ -11,7 +11,7 @@ function usage {
     echo ""
     echo "Options:"
     echo "  -s, --src_deployment    The source deployment to use. Can be either 'staging' or 'dev'. Default is 'staging'."
-    echo "  -g, --gpt_only          Only populate gpt_descriptions and gpt_seo_descriptions. Default is false."
+    echo "  -v, --version           The snapshot schema version to mirror. Default is 'v1'."
     echo ""
     echo "Arguments:"
     echo "  stack_name              The name of the rdev stack to be populated."
@@ -31,9 +31,9 @@ while (( "$#" )); do
       fi
       shift 2
       ;;
-    -g|--gpt_only)
-      POPULATE_ONLY_GPT=true
-      shift
+    -v|--version)
+      SNAPSHOT_VERSION="$2"
+      shift 2
       ;;
     --) # end argument parsing
       shift
@@ -56,16 +56,11 @@ then
   exit 1
 fi
 
-LATEST_SNAPSHOT_IDENTIFIER=$(aws s3 cp s3://cellguide-data-public-${SRC_DEPLOYMENT}/latest_snapshot_identifier -)
+LATEST_SNAPSHOT_IDENTIFIER=$(aws s3 cp s3://cellxgene-wmg-${SRC_DEPLOYMENT}/snapshots/${SNAPSHOT_VERSION}/latest_snapshot_identifier -)
 if [ -n "$LATEST_SNAPSHOT_IDENTIFIER" ]
-then
-  aws s3 sync s3://cellguide-data-public-${SRC_DEPLOYMENT}/gpt_seo_descriptions s3://cellguide-data-public-dev/env-rdev-cellguide/${STACK_NAME}/gpt_seo_descriptions
-  aws s3 sync s3://cellguide-data-public-${SRC_DEPLOYMENT}/gpt_descriptions s3://cellguide-data-public-dev/env-rdev-cellguide/${STACK_NAME}/gpt_descriptions
-  aws s3 sync s3://cellguide-data-public-${SRC_DEPLOYMENT}/validated_descriptions s3://cellguide-data-public-dev/env-rdev-cellguide/${STACK_NAME}/validated_descriptions
-  if [ "$POPULATE_ONLY_GPT" = false ] ; then
-    aws s3 sync s3://cellguide-data-public-${SRC_DEPLOYMENT}/${LATEST_SNAPSHOT_IDENTIFIER} s3://cellguide-data-public-dev/env-rdev-cellguide/${STACK_NAME}/${LATEST_SNAPSHOT_IDENTIFIER}
-    aws s3 cp s3://cellguide-data-public-${SRC_DEPLOYMENT}/latest_snapshot_identifier s3://cellguide-data-public-dev/env-rdev-cellguide/${STACK_NAME}/latest_snapshot_identifier
-  fi
+then  
+  aws s3 sync s3://cellxgene-wmg-${SRC_DEPLOYMENT}/snapshots/${SNAPSHOT_VERSION}/${LATEST_SNAPSHOT_IDENTIFIER} s3://env-rdev-wmg/${STACK_NAME}/${LATEST_SNAPSHOT_IDENTIFIER}
+  aws s3 cp s3://cellxgene-wmg-${SRC_DEPLOYMENT}/snapshots/${SNAPSHOT_VERSION}/latest_snapshot_identifier s3://env-rdev-wmg/${STACK_NAME}/latest_snapshot_identifier
 else
   echo "Warning: LATEST_SNAPSHOT_IDENTIFIER is invalid"
 fi
