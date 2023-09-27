c8abe987bf2e2815b51397af4d5118e38d3f613e
kaloster <rkalo@contractor.chanzuckerberg.com>
2023-09-27 17:54:31 -0400

    feat(citation): added basic event tracking

--- frontend/src/views/WheresMyGeneV2/components/GeneSearchBar/components/CitationButton/connect.ts
+++ frontend/src/views/WheresMyGeneV2/components/GeneSearchBar/components/CitationButton/connect.ts
@@ -1,4 +1,6 @@
 import { useCallback, useContext, useState } from "react";
+import { track } from "src/common/analytics";
+import { EVENTS } from "src/common/analytics/events";
 import { StateContext } from "src/views/WheresMyGene/common/store";
 import { CITATION_TEXT } from "src/views/WheresMyGeneV2/common/constants";
 
@@ -7,6 +9,7 @@ export const useConnect = () => {
   const { selectedGenes } = state;
   const [showCitationNotification, setShowCitationNotification] = useState(0);
   const copyCitation = useCallback(() => {
+    track(EVENTS.WMG_CITATION_CLICKED);
     setShowCitationNotification((prev) => prev + 1);
     navigator.clipboard.writeText(CITATION_TEXT);
   }, []);
