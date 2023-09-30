fe35b97c6f1d0179265f7e97959530db05691411
kaloster <rkalo@contractor.chanzuckerberg.com>
2023-09-29 20:06:24 -0400

    test(wmg): tests for tissue auto expand

--- frontend/tests/features/wheresMyGene/tissueAutoExpand.test.ts
+++ frontend/tests/features/wheresMyGene/tissueAutoExpand.test.ts
@@ -3,10 +3,12 @@ import { toInteger } from "lodash";
 import { collapseTissue, expandTissue } from "tests/utils/helpers";
 import { conditionallyRunTests, goToWMG } from "tests/utils/wmgUtils";
 
-const { describe } = test;
 const TISSUE_NODE_TEST_ID = "tissue-name";
 const TISSUE_FILTER_TEST_ID = "tissue-filter";
 const CELL_TYPE_FILTER = "B cell";
+const FILTERED_TISSUES = ["abdomen", "axilla", "brain"];
+
+const { describe } = test;
 
 describe("WMG tissue auto-expand", () => {
   conditionallyRunTests({ forceRun: true });
@@ -16,11 +18,10 @@ describe("WMG tissue auto-expand", () => {
    * automatically.
    */
   test("Filter tissue auto expansion", async ({ page }) => {
-    const expandedTissues = ["abdomen", "axilla"];
     await loadPageAndTissues(page);
-    await expandTissue(page, expandedTissues[0]);
-    await filterTissues(page, expandedTissues);
-    await checkTissues(page, expandedTissues, expandedTissues);
+    await expandTissue(page, FILTERED_TISSUES[0]);
+    await filterTissues(page, FILTERED_TISSUES);
+    await checkTissues(page, FILTERED_TISSUES, FILTERED_TISSUES);
   });
   /**
    * Filter first two tissues from the left panel, collapse the first
@@ -30,12 +31,11 @@ describe("WMG tissue auto-expand", () => {
   test("Filter tissue auto expansion - respect user collapsed tissue state", async ({
     page,
   }) => {
-    const filteredTissues = ["abdomen", "axilla", "brain"];
     await loadPageAndTissues(page);
-    await filterTissues(page, filteredTissues.slice(0, 2));
-    await collapseTissue(page, filteredTissues[0]);
-    await filterTissues(page, filteredTissues.slice(2, 3));
-    await checkTissues(page, filteredTissues, filteredTissues.slice(1, 3));
+    await filterTissues(page, FILTERED_TISSUES.slice(0, 2));
+    await collapseTissue(page, FILTERED_TISSUES[0]);
+    await filterTissues(page, FILTERED_TISSUES.slice(2, 3));
+    await checkTissues(page, FILTERED_TISSUES, FILTERED_TISSUES.slice(1, 3));
   });
   /**
    * Filter two tissues, expect tissues to be expanded. Remove both tissues
@@ -44,39 +44,59 @@ describe("WMG tissue auto-expand", () => {
   test("Filter tissue auto expansion - exit filter tissues mode", async ({
     page,
   }) => {
-    const filteredTissues = ["abdomen", "axilla"];
     await loadPageAndTissues(page);
-    await filterTissues(page, filteredTissues);
-    await checkTissues(page, filteredTissues, filteredTissues);
-    await filterTissues(page, filteredTissues);
+    await filterTissues(page, FILTERED_TISSUES);
+    await checkTissues(page, FILTERED_TISSUES, FILTERED_TISSUES);
+    await filterTissues(page, FILTERED_TISSUES);
     await checkTissues(page, [], []);
   });
   /**
-   * Expect tissue list to only show tissues that contain "B cell" and
-   * automatically expanded.
+   * Filter two tissues, expect tissues to be expanded. Expect tissue list
+   * to only show tissues that contain "B cell" and automatically expanded.
+   * Check that only 'B Cell' cells are visible under expanded tissues.
+   * When cell type filter is removed, expect all cells to be shown under
+   * expanded tissues.
    */
   test("Filter tissue auto expansion - filter cell type 'B cell'", async ({
     page,
   }) => {
-    const filteredTissues = ["abdomen", "axilla"];
     await loadPageAndTissues(page);
-    await filterTissues(page, filteredTissues);
+    await filterTissues(page, FILTERED_TISSUES);
     await filterCellType(page);
-    await checkTissues(page, filteredTissues, filteredTissues);
+    await checkTissues(page, FILTERED_TISSUES, FILTERED_TISSUES);
     await checkCellTypes(page);
+    await page.getByTestId("CancelIcon").click();
+    await expect(checkCellTypes(page)).rejects.toThrow();
   });
 });
+/**
+ * *******************************************
+ * Helper Functions
+ * *******************************************
+ */
 
+/**
+ * loadPageAndTissues
+ * Load the WMG page and wait for the tissue nodes to be visible
+ */
 async function loadPageAndTissues(page: Page) {
   await goToWMG(page);
   await expect(page.getByTestId(TISSUE_NODE_TEST_ID)).not.toHaveCount(0);
 }
 
+/**
+ * clickIntoFilter
+ * Click into the filter and wait for the tooltip to be visible
+ */
 async function clickIntoFilter(page: Page, filterName: string) {
   await page.getByTestId(filterName).getByRole("button").first().click();
   await page.getByRole("tooltip").waitFor();
 }
 
+/**
+ * filterCellType
+ * Filter cell type 'B cell' and exit filter mode
+ */
 async function filterCellType(page: Page) {
   await page.getByRole("combobox").first().click();
   await page
@@ -85,20 +105,27 @@ async function filterCellType(page: Page) {
   await page.keyboard.press("Escape");
 }
 
-async function filterTissues(page: Page, expandedTissues: string[]) {
+/**
+ * filterTissues
+ * Filter the tissuesfrom the left panel
+ */
+async function filterTissues(page: Page, filteredTissues: string[]) {
   await clickIntoFilter(page, TISSUE_FILTER_TEST_ID);
-  for (const tissue of expandedTissues) {
+  for (const tissue of filteredTissues) {
     await page.getByRole("option", { name: tissue }).click();
   }
   await page.keyboard.press("Escape");
 }
 
+/**
+ * checkTissues
+ * Check that the filtered tissues are visible, and expanded tissues are expanded
+ */
 async function checkTissues(
   page: Page,
   filteredTissues: string[],
   expandedTissues: string[]
 ) {
-  // check that only n tissue nodes are displayed
   if (filteredTissues.length !== 0) {
     await expect(page.getByTestId("tissue-name")).toHaveCount(
       filteredTissues.length
@@ -106,7 +133,6 @@ async function checkTissues(
   }
 
   let i = 0;
-  // check that the filtered tissues are visible and expanded
   for (const tissue of expandedTissues) {
     await expect(page.getByTestId(`cell-type-labels-${tissue}`)).toBeVisible();
     const height = await page
@@ -117,6 +143,10 @@ async function checkTissues(
   await expect(i).toEqual(expandedTissues.length);
 }
 
+/**
+ * checkCellTypes
+ * Check that all cells under expanded tissues are 'B Cell'
+ */
 async function checkCellTypes(page: Page) {
   const cells = await page.getByTestId("cell-type-name").allInnerTexts();
   for (const cell of cells) {
