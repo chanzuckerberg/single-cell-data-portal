aeb5173af4ad6a15a3306c755c5cc3a3b06b212f
Trent Smith <trent.smith@chanzuckerberg.com>
2023-10-03 12:54:42 -0700

    Add concurrency to rdev GHA
    Signed-off-by: Trent Smith <trent.smith@chanzuckerberg.com>

--- backend/layers/processing/h5ad_data_file.py
+++ backend/layers/processing/h5ad_data_file.py
@@ -112,7 +112,7 @@ class H5ADDataFile:
                 * follows ScanPy embedding naming conventions
                 * with all values finite or NaN (no +Inf or -Inf)
             """
-
+            # TODO is this in the cellxgene_cli validator and can this be removed from here?
             is_valid = isinstance(embedding_name, str) and embedding_name.startswith("X_") and len(embedding_name) > 2
             is_valid = is_valid and isinstance(embedding_array, np.ndarray) and embedding_array.dtype.kind in "fiu"
             is_valid = is_valid and embedding_array.shape[0] == adata.n_obs and embedding_array.shape[1] >= 2
