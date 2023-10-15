e30b806e52321e9ff17c6db7ea440845b3c00938
atarashansky <atarashansky@chanzuckerberg.com>
2023-10-15 11:18:22 -0700

    feat: implement primary filter dimensions builder (#5974)

--- backend/wmg/data/schemas/marker_gene_cube_schema.py
+++ backend/wmg/data/schemas/marker_gene_cube_schema.py
@@ -1,10 +1,9 @@
 # This module contains the schema components for the precomputed marker gene cube
 # (marker_genes_cube).
-
 import numpy as np
 import tiledb
 
-filters = [tiledb.ZstdFilter(level=+22)]
+from backend.wmg.data.schemas.tiledb_filters import filters_categorical, filters_numeric
 
 # These are the queryable cube dimensions that will be modeled as
 # TileDB `Dim`s and thus can be used for _efficiently_ querying
@@ -12,19 +11,21 @@ filters = [tiledb.ZstdFilter(level=+22)]
 marker_genes_indexed_dims = [
     "tissue_ontology_term_id",
     "cell_type_ontology_term_id",
+    "organism_ontology_term_id",
 ]
 
 # The cube attributes that comprise the core data stored within the cube.
 # Unlike the other cube schemas, this cube schema does not have any
 # distinction between logical and physical attributes.
 marker_genes_attrs = [
-    tiledb.Attr(name="gene_ontology_term_id", dtype="ascii", var=True, filters=filters),
-    tiledb.Attr(name="marker_score", dtype=np.float32, filters=filters),
+    tiledb.Attr(name="gene_ontology_term_id", dtype="ascii", var=True, filters=filters_categorical),
+    tiledb.Attr(name="marker_score", dtype=np.float32, filters=filters_numeric),
+    tiledb.Attr(name="specificity", dtype=np.float32, filters=filters_numeric),
 ]
 
 marker_genes_domain = tiledb.Domain(
     [
-        tiledb.Dim(name=marker_genes_indexed_dim, domain=None, tile=None, dtype="ascii", filters=filters)
+        tiledb.Dim(name=marker_genes_indexed_dim, domain=None, tile=None, dtype="ascii", filters=filters_categorical)
         for marker_genes_indexed_dim in marker_genes_indexed_dims
     ]
 )
