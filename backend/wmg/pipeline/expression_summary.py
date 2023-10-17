2121a4c78c5d3b7dcd249b08406f11751fe9df21
atarashansky <atarashansky@chanzuckerberg.com>
2023-10-17 16:52:00 -0400

    k

--- backend/wmg/pipeline/expression_summary.py
+++ backend/wmg/pipeline/expression_summary.py
@@ -243,7 +243,12 @@ class ExpressionSummaryCubeBuilder:
             data_filt = raw_array.data >= NORM_EXPR_COUNT_FILTERING_MIN_THRESHOLD
 
             # convert data to log(X+1)
-            raw_array.data[:] = np.log(raw_array.data + 1)
+            raw_array = rankit(raw_array)
+            data = raw_array.data
+            row_indices, col_var = raw_array.row, raw_array.col
+
+            # convert row coordinates to obs coordinates
+            row_obs = obs_soma_joinids_chunk[row_indices]
 
             # filter data
             keep_data = np.logical_and(np.isfinite(data), data_filt)
@@ -356,3 +361,49 @@ def gene_expression_sum_x_cube_dimension(
             sum_into[grp_idx, cidx] += val
             sqsum_into[grp_idx, cidx] += val**2
             nnz_into[grp_idx, cidx] += 1
+
+
+import numba as nb
+from scipy import stats
+
+
+@nb.jit
+def quantiles(max_rank: int, ranks: np.ndarray) -> np.ndarray:
+    """
+    :returns an array of n floats equally spaced from 0 to 1
+    """
+    return np.array([np.round((i - 0.5) / max_rank, 5) for i in ranks])
+
+
+def rankit(Xraw: sparse.spmatrix, offset: float = 3.0) -> sparse.csr_matrix:
+    """
+    Row-wise normalizes values of a matrix using the rankit method. The target distribution is a normal distribution
+    with variance of 1 and mean as set in `offset`
+    https://en.wikipedia.org/wiki/Rankit
+    In statistics, rankits of a set of data are the expected values of the order statistics of
+    a sample from the standard normal distribution the same size as the data
+    Caveat: equal values are ranked in undefined order.
+    param Xraw: query matrix to be normalized
+    param offset: mean for the resulting row-wise values that will follow a normal distribution with variance 1. This
+    helps to shift values to a positive scale.
+    :returns row-wise normalized matrix using rankit
+    """
+    X = Xraw.tocsr(copy=True)  # get Compressed Sparse Row format of raw expression values matrix
+    indptr = X.indptr  # get row count
+    warning_raised = False
+    for row in range(0, indptr.shape[0] - 1):
+        data = X.data[indptr[row] : indptr[row + 1]]
+        if len(data) > 0:
+            # Assign ranks to data, assigning the same value to ties
+            ranks = stats.rankdata(data, method="dense")
+
+            max_rank = max(ranks)
+            prob_level = quantiles(max_rank, ranks)
+
+            normal_quantiles = stats.norm.ppf(prob_level, loc=offset)
+            X.data[indptr[row] : indptr[row + 1]] = normal_quantiles
+        elif not warning_raised:
+            logging.warn("This dataset has at least one row of all zero expressions")
+            warning_raised = True
+
+    return X
