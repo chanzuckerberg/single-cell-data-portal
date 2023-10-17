23787d1958c122f8af6767e2de49e1b9e8b7871a
atarashansky <atarashansky@chanzuckerberg.com>
2023-10-17 15:55:23 -0400

    implement cell type ordering builder  (#5998)

--- backend/wmg/pipeline/__init__.py
+++ backend/wmg/pipeline/__init__.py
@@ -4,7 +4,9 @@ import time
 from typing import Optional
 
 from backend.wmg.data.utils import log_func_runtime
+from backend.wmg.pipeline.cell_type_ordering import create_cell_type_ordering
 from backend.wmg.pipeline.constants import (
+    CELL_TYPE_ORDERING_CREATED_FLAG,
     DATASET_METADATA_CREATED_FLAG,
     EXPRESSION_SUMMARY_AND_CELL_COUNTS_CUBE_CREATED_FLAG,
     EXPRESSION_SUMMARY_DEFAULT_CUBE_CREATED_FLAG,
@@ -32,6 +34,7 @@ PIPELINE_STEPS = [
     {"flag": PRIMARY_FILTER_DIMENSIONS_CREATED_FLAG, "step": create_primary_filter_dimensions},
     {"flag": MARKER_GENES_CUBE_CREATED_FLAG, "step": create_marker_genes_cube},
     {"flag": DATASET_METADATA_CREATED_FLAG, "step": create_dataset_metadata},
+    {"flag": CELL_TYPE_ORDERING_CREATED_FLAG, "step": create_cell_type_ordering},
 ]
 
 
