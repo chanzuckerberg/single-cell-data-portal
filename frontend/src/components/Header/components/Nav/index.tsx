1c1446b503dde6ceffb2057cc2dd425dd097b154
Seve Badajoz <sbadajoz@chanzuckerberg.com>
2023-10-24 11:56:56 -0700

    more robust styling

--- frontend/src/components/Header/components/Nav/index.tsx
+++ frontend/src/components/Header/components/Nav/index.tsx
@@ -6,7 +6,7 @@ import { track } from "src/common/analytics";
 import { EVENTS } from "src/common/analytics/events";
 import NavDivider from "src/components/Header/components/Nav/components/NavDivider";
 import { isRouteActive } from "src/components/Header";
-import { LinkWrapper, Nav as NavWrapper } from "./style";
+import { LinkWrapper, NavSection, Nav as NavWrapper } from "./style";
 import { BetaChip } from "../../style";
 
 export const CENSUS_LINK = "https://chanzuckerberg.github.io/cellxgene-census/";
@@ -17,7 +17,8 @@ interface Props {
 }
 
 export default function Nav({ className, pathname }: Props): JSX.Element {
-  return (
+  const isCensusDirectory = pathname === ROUTES.CENSUS_DIRECTORY;
+  return !isCensusDirectory ? (
     <NavWrapper className={className}>
       <LinkWrapper>
         <Link href={ROUTES.COLLECTIONS} passHref>
@@ -85,6 +86,89 @@ export default function Nav({ className, pathname }: Props): JSX.Element {
           target="_self"
           text="Census"
         />
+      </LinkWrapper>
+    </NavWrapper>
+  ) : (
+    <NavWrapper className={className}>
+      <LinkWrapper>
+        <Link href={ROUTES.COLLECTIONS} passHref>
+          <AnchorButton
+            active={isRouteActive(pathname, ROUTES.COLLECTIONS)}
+            data-testid="collections-link"
+            href="passHref"
+            minimal
+            onClick={() => {
+              track(EVENTS.COLLECTIONS_CLICK_NAV);
+            }}
+            text="Collections"
+          />
+        </Link>
+      </LinkWrapper>
+      <LinkWrapper>
+        <Link href={ROUTES.DATASETS} passHref>
+          <AnchorButton
+            active={isRouteActive(pathname, ROUTES.DATASETS)}
+            href="passHref"
+            minimal
+            onClick={() => {
+              track(EVENTS.DATASETS_CLICK_NAV);
+            }}
+            text="Datasets"
+          />
+        </Link>
+      </LinkWrapper>
+      <LinkWrapper>
+        <Link href={ROUTES.WHERE_IS_MY_GENE} passHref>
+          <AnchorButton
+            active={isRouteActive(pathname, ROUTES.WHERE_IS_MY_GENE)}
+            href="passHref"
+            minimal
+            onClick={() => {
+              track(EVENTS.WMG_CLICK_NAV);
+            }}
+            text="Gene Expression"
+          />
+        </Link>
+      </LinkWrapper>
+      <LinkWrapper>
+        <Link href={ROUTES.CELL_GUIDE} passHref>
+          <AnchorButton
+            active={isRouteActive(pathname, ROUTES.CELL_GUIDE)}
+            href="passHref"
+            minimal
+            onClick={() => {
+              track(EVENTS.CELL_GUIDE_CLICK_NAV);
+            }}
+            text="Cell Guide"
+          />
+        </Link>
+        <BetaChip label="Beta" size="small" />
+      </LinkWrapper>
+      <NavDivider />
+      <LinkWrapper>
+        <AnchorButton
+          href={CENSUS_LINK}
+          minimal
+          onClick={() => {
+            track(EVENTS.CENSUS_CLICK_NAV);
+          }}
+          rel="noopener"
+          target="_self"
+          text="Documentation"
+        />
+      </LinkWrapper>
+      <LinkWrapper>
+        <Link href={ROUTES.CENSUS_DIRECTORY} passHref>
+          <AnchorButton
+            active={isRouteActive(pathname, ROUTES.CENSUS_DIRECTORY)}
+            href="passHref"
+            minimal
+            onClick={() => {
+              // TODO ANALYTICS
+            }}
+            text="Directory"
+          />
+        </Link>
       </LinkWrapper>
     </NavWrapper>
   );
--- frontend/src/views/CensusDirectory/index.tsx
+++ frontend/src/views/CensusDirectory/index.tsx
@@ -1,6 +1,7 @@
 import {
   Button,
   fontBodyS,
+  fontCapsXxxs,
   fontHeaderL,
   fontHeaderM,
   fontHeaderXl,
@@ -8,6 +9,28 @@ import {
 } from "@czi-sds/components";
 import styled from "@emotion/styled";
 import React from "react";
+
+function DetailItem(props: { label: string; children: string }) {
+  const ItemContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+  `;
+  const ItemLabel = styled.div`
+    ${fontCapsXxxs}
+    font-weight: 600;
+    font-feature-settings:
+      "clig" off,
+      "liga" off;
+    color: #959595;
+  `;
+
+  return (
+    <ItemContainer>
+      <ItemLabel>{props.label}</ItemLabel>
+      {props.children}
+    </ItemContainer>
+  );
+}
 
 function CensusDirectory() {
   const Content = styled.div`
@@ -17,38 +40,55 @@ function CensusDirectory() {
   `;
 
   const Header = styled.h1`
+    ${fontHeaderXxl}
     margin-bottom: 8px;
     font-weight: 700;
-    ${fontHeaderXxl}
   `;
 
   const Paragraph = styled.p`
-    font-weight: 400;
     ${fontBodyS}
+    font-weight: 400;
+    margin-bottom: 0;
+  `;
+
+  const DirectoryDescription = styled(Paragraph)`
+    margin-bottom: 80px;
+  `;
+
+  const TierContainer = styled.div`
+    margin-bottom: 120px;
   `;
 
   const TierTitle = styled.h3`
+    ${fontHeaderXl}
     margin-bottom: 8px;
     font-weight: 600;
-    ${fontHeaderXl}
+  `;
+
+  const TierDescription = styled.p`
+    ${fontBodyS}
+    color: #767676;
+    font-weight: 400;
+    margin-bottom: 0;
   `;
 
   const ProjectTitle = styled.h4`
+    ${fontHeaderL}
     font-weight: 600;
     margin-bottom: 8px;
-    ${fontHeaderL}
   `;
 
   const ProjectSubmitter = styled.h4`
+    ${fontBodyS}
     font-weight: 600;
     margin-bottom: 8px;
-    ${fontHeaderM}
   `;
 
   const ProjectContainer = styled.div`
     display: flex;
     flex-direction: row;
     justify-content: space-between;
+    margin-top: 20px;
   `;
   const ProjectButtons = styled.div`
     display: flex;
@@ -56,13 +96,17 @@ function CensusDirectory() {
     gap: 8px;
   `;
   const ProjectDetails = styled.div``;
-  const SubmissionDetails = styled.div``;
-  const DataDetails = styled.div``;
+  const DetailsContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    gap: 24px;
+    margin-top: 16px;
+  `;
 
   return (
     <Content>
       <Header>Census Directory</Header>
-      <Paragraph>
+      <DirectoryDescription>
         Habitant sit tristique pharetra at. Quis ipsum morbi pharetra venenatis
         amet purus aliquam nunc. Mi feugiat elementum nec sagittis enim. Turpis
         purus mi tellus leo in vestibulum enim varius. Ut dictum lobortis in
@@ -70,39 +114,122 @@ function CensusDirectory() {
         Parturient pharetra amet sit facilisis sagittis quis. Dignissim
         fermentum consectetur fames vulputate semper neque est non pharetra.
         Amet et elementum neque turpis hac bibendum ac id ipsum.
-      </Paragraph>
-      <TierTitle>Census Partners</TierTitle>
-      <ProjectContainer>
-        <ProjectDetails>
-          <ProjectTitle> BioAI</ProjectTitle>
-          <ProjectSubmitter>
-            Turing Institute for Biomedical Machine Learning
-          </ProjectSubmitter>
-          <Paragraph>
-            Ligula imperdiet eget et enim id morbi. Pretium diam risus placerat
-            felis vulputate adipiscing sed integer. Mauris commodo risus
-            scelerisque tempus mi venenatis egestas. Sed at scelerisque
-            vulputate egestas vulputate condimentum libero tempus convallis.
-            Nulla id eget fringilla ultrices pellentesque nunc faucibus
-            condimentum. Ornare porta eget porttitor cum arcu id ultricies id.
-            Massa interdum orci risus arcu mattis massa. Amet metus nibh enim
-            nam pellentesque sagittis diam id quam.
-          </Paragraph>
-          <SubmissionDetails></SubmissionDetails>
-          <DataDetails></DataDetails>
-        </ProjectDetails>
-        <ProjectButtons>
-          <Button sdsType="secondary" sdsStyle="square">
-            Embedding
-          </Button>
-
-          <Button sdsType="primary" sdsStyle="square">
-            Model
-          </Button>
-        </ProjectButtons>
-      </ProjectContainer>
-      <TierTitle>Tier 2</TierTitle>
-      <TierTitle>Tier 3</TierTitle>
+      </DirectoryDescription>
+      <TierContainer>
+        <TierTitle>Census Partners</TierTitle>
+        <TierDescription>
+          Ut nisi non lorem adipiscing. Orci tellus quisque quam ac purus vitae.
+          Aliquet quis egestas viverra nulla quis lectus adipiscing.
+        </TierDescription>
+        <ProjectContainer>
+          <ProjectDetails>
+            <ProjectTitle>BioAI</ProjectTitle>
+            <ProjectSubmitter>
+              Turing Institute for Biomedical Machine Learning
+            </ProjectSubmitter>
+            <Paragraph>
+              Ligula imperdiet eget et enim id morbi. Pretium diam risus
+              placerat felis vulputate adipiscing sed integer. Mauris commodo
+              risus scelerisque tempus mi venenatis egestas. Sed at scelerisque
+              vulputate egestas vulputate condimentum libero tempus convallis.
+              Nulla id eget fringilla ultrices pellentesque nunc faucibus
+              condimentum. Ornare porta eget porttitor cum arcu id ultricies id.
+              Massa interdum orci risus arcu mattis massa. Amet metus nibh enim
+              nam pellentesque sagittis diam id quam.
+            </Paragraph>
+            <DetailsContainer>
+              <DetailItem label="contact">Haotian Cui</DetailItem>
+              <DetailItem label="publication">
+                Cul et al. (2023) bioRxiv
+              </DetailItem>
+            </DetailsContainer>
+          </ProjectDetails>
+          <ProjectButtons>
+            <Button sdsType="secondary" sdsStyle="square">
+              Embedding
+            </Button>
+            <Button sdsType="primary" sdsStyle="square">
+              Model
+            </Button>
+          </ProjectButtons>
+        </ProjectContainer>
+      </TierContainer>
+      <TierContainer>
+        <TierTitle>Tier 2</TierTitle>
+        <TierDescription>
+          Ut nisi non lorem adipiscing. Orci tellus quisque quam ac purus vitae.
+          Aliquet quis egestas viverra nulla quis lectus adipiscing.
+        </TierDescription>
+        <ProjectContainer>
+          <ProjectDetails>
+            <ProjectTitle>scGPT</ProjectTitle>
+            <ProjectSubmitter>
+              WangLab at University of Toronto{" "}
+            </ProjectSubmitter>
+            <Paragraph>
+              Ligula imperdiet eget et enim id morbi. Pretium diam risus
+              placerat felis vulputate adipiscing sed integer. Mauris commodo
+              risus scelerisque tempus mi venenatis egestas. Sed at scelerisque
+              vulputate egestas vulputate condimentum libero tempus convallis.
+              Nulla id eget fringilla ultrices pellentesque nunc faucibus
+              condimentum. Ornare porta eget porttitor cum arcu id ultricies id.
+              Massa interdum orci risus arcu mattis massa. Amet metus nibh enim
+              nam pellentesque sagittis diam id quam.
+            </Paragraph>
+            <DetailsContainer>
+              <DetailItem label="contact">Haotian Cui</DetailItem>
+              <DetailItem label="publication">
+                Cul et al. (2023) bioRxiv
+              </DetailItem>
+            </DetailsContainer>
+            <DetailsContainer>
+              <DetailItem label="Census Version">LTS 1.0</DetailItem>
+              <DetailItem label="experiment">homo_sapiens</DetailItem>
+              <DetailItem label="measurement">RNA</DetailItem>
+              <DetailItem label="embedding">obs</DetailItem>
+            </DetailsContainer>
+          </ProjectDetails>
+          <ProjectButtons>
+            <Button sdsType="secondary" sdsStyle="square">
+              Embedding
+            </Button>
+            <Button sdsType="primary" sdsStyle="square">
+              Model
+            </Button>
+          </ProjectButtons>
+        </ProjectContainer>
+      </TierContainer>
+      <TierContainer>
+        <TierTitle>Tier 3</TierTitle>
+        <TierDescription>
+          Ut nisi non lorem adipiscing. Orci tellus quisque quam ac purus vitae.
+          Aliquet quis egestas viverra nulla quis lectus adipiscing.
+        </TierDescription>
+        <ProjectContainer>
+          <ProjectDetails>
+            <ProjectTitle>OpenCell ML</ProjectTitle>
+            <ProjectSubmitter>CZ BioHub </ProjectSubmitter>
+            <Paragraph>
+              Ligula imperdiet eget et enim id morbi. Pretium diam risus
+              placerat felis vulputate adipiscing sed integer. Mauris commodo
+              risus scelerisque tempus mi venenatis egestas. Sed at scelerisque
+              vulputate egestas vulputate condimentum libero tempus convallis.
+              Nulla id eget fringilla ultrices pellentesque nunc faucibus
+              condimentum. Ornare porta eget porttitor cum arcu id ultricies id.
+              Massa interdum orci risus arcu mattis massa. Amet metus nibh enim
+              nam pellentesque sagittis diam id quam.
+            </Paragraph>
+          </ProjectDetails>
+          <ProjectButtons>
+            <Button sdsType="secondary" sdsStyle="square">
+              Embedding
+            </Button>
+            <Button sdsType="primary" sdsStyle="square">
+              Model
+            </Button>
+          </ProjectButtons>
+        </ProjectContainer>
+      </TierContainer>
     </Content>
   );
 }
