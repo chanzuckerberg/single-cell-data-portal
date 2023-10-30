26d0eba23680499ec4653fc8c581bce057b14b17
Daniel Hegeman <daniel.hegeman@chanzuckerberg.com>
2023-10-30 10:57:42 -0700

    do not clean up artifacts if seurat fails

--- backend/layers/processing/upload_success/app.py
+++ backend/layers/processing/upload_success/app.py
@@ -3,7 +3,12 @@ import logging
 from backend.layers.business.business import BusinessLogic
 from backend.layers.common.entities import DatasetProcessingStatus, DatasetStatusKey, DatasetVersionId
 from backend.layers.persistence.persistence import DatabaseProvider
-from backend.layers.processing.upload_failures.app import handle_failure
+from backend.layers.processing.upload_failures.app import (
+    handle_failure,
+    parse_event,
+    trigger_slack_notification,
+    update_dataset_processing_status_to_failed,
+)
 
 database_provider = DatabaseProvider()
 business_logic = BusinessLogic(database_provider, None, None, None, None)
@@ -26,7 +31,20 @@ def success_handler(events: dict, context) -> None:
         handle_failure(cxg_job, context)
     else:
         if seurat_job.get("error"):
-            handle_failure(seurat_job, context)
+            # Same as handle_failure except do not delete artifacts (because cxg succeeded)
+            (
+                dataset_id,
+                collection_version_id,
+                error_step_name,
+                error_job_id,
+                error_aws_regions,
+                error_cause,
+                execution_arn,
+            ) = parse_event(seurat_job)
+            trigger_slack_notification(
+                dataset_id, collection_version_id, error_step_name, error_job_id, error_aws_regions, execution_arn
+            )
+            update_dataset_processing_status_to_failed(dataset_id)
         business_logic.update_dataset_version_status(
             DatasetVersionId(cxg_job["dataset_id"]), DatasetStatusKey.PROCESSING, DatasetProcessingStatus.SUCCESS
         )
