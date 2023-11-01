5c75364d844d7a47a34a9ae75210aa69805104b9
kaloster <rkalo@contractor.chanzuckerberg.com>
2023-11-01 09:42:00 -0400

    chore(wmg): add dd tracing

--- backend/wmg/api/v2.py
+++ backend/wmg/api/v2.py
@@ -298,7 +298,7 @@ def build_expression_summary(
 
     return structured_result
 
-
+@tracer.wrap(name="fill_out_structured_dict_aggregated", service="wmg-api", resource="query", span_type="wmg-api")
 def fill_out_structured_dict_aggregated(gene_expr_grouped_df, structured_result):
     n = gene_expr_grouped_df["nnz"].astype("int").values
     me = (gene_expr_grouped_df["sum"] / gene_expr_grouped_df["nnz"]).values
@@ -316,7 +316,7 @@ def fill_out_structured_dict_aggregated(gene_expr_grouped_df, structured_result)
             tpc=float(tpc[i]),
         )
 
-
+@tracer.wrap(name="fill_out_structured_dict_compare", service="wmg-api", resource="query", span_type="wmg-api")
 def fill_out_structured_dict_compare(rolled_gene_expression_df, structured_result, compare):
     n = rolled_gene_expression_df["nnz"].astype("int").values
     me = (rolled_gene_expression_df["sum"] / rolled_gene_expression_df["nnz"]).values
@@ -346,7 +346,7 @@ def build_gene_id_label_mapping(gene_ontology_term_ids: List[str]) -> List[dict]
 def build_ontology_term_id_label_mapping(ontology_term_ids: Iterable[str]) -> List[dict]:
     return [{ontology_term_id: ontology_term_label(ontology_term_id)} for ontology_term_id in ontology_term_ids]
 
-
+@tracer.wrap(name="fill_out_structured_tissue_agg", service="wmg-api", resource="query", span_type="wmg-api")
 def fill_out_structured_tissue_agg(tissue_agg, structured_result):
     tissues = tissue_agg["tissue_ontology_term_id"].values
     n = tissue_agg["n_cells_cell_type"].values
@@ -359,7 +359,7 @@ def fill_out_structured_tissue_agg(tissue_agg, structured_result):
             "order": -1,
         }
 
-
+@tracer.wrap(name="fill_out_structured_cell_type_agg", service="wmg-api", resource="query", span_type="wmg-api")
 def fill_out_structured_cell_type_agg(cell_type_agg, structured_result, ordering):
     tissues = cell_type_agg["tissue_ontology_term_id"].values
     cell_types = cell_type_agg["cell_type_ontology_term_id"].values
@@ -373,7 +373,7 @@ def fill_out_structured_cell_type_agg(cell_type_agg, structured_result, ordering
             "order": ordering.get((tissues[i], cell_types[i]), -1),
         }
 
-
+@tracer.wrap(name="fill_out_structured_cell_type_compare", service="wmg-api", resource="query", span_type="wmg-api")
 def fill_out_structured_cell_type_compare(cell_type_agg_compare, structured_result, ordering, compare):
     tissues = cell_type_agg_compare["tissue_ontology_term_id"].values
     cell_types = cell_type_agg_compare["cell_type_ontology_term_id"].values
@@ -392,6 +392,7 @@ def fill_out_structured_cell_type_compare(cell_type_agg_compare, structured_resu
 
 
 # getting only cell type metadata, no genes
+@tracer.wrap(name="build_ordered_cell_types_by_tissue", service="wmg-api", resource="query", span_type="wmg-api")   
 def build_ordered_cell_types_by_tissue(
     rolled_cell_counts_cell_type_agg: DataFrame,
     cell_counts_cell_type_agg: DataFrame,
