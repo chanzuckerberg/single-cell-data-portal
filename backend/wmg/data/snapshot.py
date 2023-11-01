9f7d59b79a839f67bf680ea6ef5331c848e44123
kaloster <rkalo@contractor.chanzuckerberg.com>
2023-11-01 10:01:30 -0400

    chore(wmg): add dd tracing

--- backend/wmg/data/snapshot.py
+++ backend/wmg/data/snapshot.py
@@ -6,6 +6,7 @@ from typing import Dict, Optional
 
 import pandas as pd
 import tiledb
+from ddtrace import tracer
 from pandas import DataFrame
 from tiledb import Array
 
@@ -79,7 +80,7 @@ class WmgSnapshot:
 # Cached data
 cached_snapshot: Optional[WmgSnapshot] = None
 
-
+@tracer.wrap(name="load_snapshot", service="wmg-api", resource="query", span_type="wmg-api")
 def load_snapshot(
     *,
     snapshot_schema_version: str,
