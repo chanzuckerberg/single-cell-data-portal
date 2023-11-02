ab837e3a9d7c0fb3823783d1ad91178ca7d7f2d4
Seve Badajoz <sbadajoz@chanzuckerberg.com>
2023-11-02 10:42:19 -0700

    update response type and mock

--- frontend/src/common/queries/censusDirectory.ts
+++ frontend/src/common/queries/censusDirectory.ts
@@ -1,23 +1,81 @@
 import { useQuery } from "react-query";
-import { DEFAULT_FETCH_OPTIONS, JSON_BODY_FETCH_OPTIONS } from "./common";
 import { ENTITIES } from "./entities";
 
-async function fetchProjects() {
-  // TMP LOCALDEV ONLY BE SERVER
-  // TMP LOCALDEV ONLY BE SERVER
-  // TMP LOCALDEV ONLY BE SERVER
-  // TMP LOCALDEV ONLY BE SERVER
-  console.log("fetching projects");
+// assumed tier 2 for now
+export interface Project {
+  census_version: string;
+  experiment_name: string;
+  measurement_name: string;
+  submission_date: string;
+  n_features: number;
+  data_type: string;
+  revised_by?: string;
+  model_link?: string;
+  last_updated?: string;
+  contributor_metadata: {
+    title: string;
+    description: string;
+    contact_name: string;
+    contact_affiliation: string;
+    contact_email: string;
+    DOI?: string;
+  };
+}
 
-  const response = await fetch("http://localhost:5005/census-directory", {
-    ...DEFAULT_FETCH_OPTIONS,
-    ...JSON_BODY_FETCH_OPTIONS,
-  });
+export interface ProjectResponse {
+  [accessionID: string]: Project;
+}
 
-  const result = await response.json();
-  if (!response.ok) throw result;
+async function fetchProjects(): Promise<ProjectResponse> {
+  // const url = API_URL + API.CENSUS_DIRECTORY;
 
-  return result.data;
+  // const response = await fetch(url, {
+  //   ...DEFAULT_FETCH_OPTIONS,
+  //   ...JSON_BODY_FETCH_OPTIONS,
+  // });
+
+  // const result = await response.json();
+  // if (!response.ok) throw result;
+
+  // return result.data;
+  return {
+    accession_ID_1: {
+      census_version: "2099-08-11",
+      experiment_name: "homo_sapiens",
+      measurement_name: "RNA",
+      submission_date: "2023-01-11",
+      n_features: 2,
+      data_type: "obs_embedding",
+      revised_by: "accession_ID_2",
+      contributor_metadata: {
+        DOI: "10.3352/jeehp.2013.10.3",
+        title: "brief title",
+        description: "...",
+        contact_name: "...",
+        contact_email: "...",
+        contact_affiliation: "...",
+      },
+    },
+    accession_ID_2: {
+      census_version: "2099-10-11",
+      submission_date: "2023-01-11",
+      last_updated: "2023-11-02",
+      n_features: 2,
+      data_type: "obs_embedding",
+      measurement_name: "RNA",
+      experiment_name: "homo_sapiens",
+      model_link: "http://example.com",
+      contributor_metadata: {
+        contact_affiliation: "Turing Institute for Biomedical Machine Learning",
+        contact_name: "Haotian Cu",
+        contact_email: "mailto:name@example.com",
+        DOI: "10.3352/jeehp.2013.10.3",
+        title: "BioAI",
+        description:
+          "Ligula imperdiet eget et enim id morbi. Pretium diam risus placerat felis vulputate adipiscing sed integer. Mauris commodo risus scelerisque tempus mi venenatis egestas. Sed at scelerisque vulputate egestas vulputate condimentum libero tempus convallis. Nulla id eget fringilla ultrices pellentesque nunc faucibus condimentum. Ornare porta eget porttitor cum arcu id ultricies id. Massa interdum orci risus arcu mattis massa. Amet metus nibh enim nam pellentesque sagittis diam id quam.",
+      },
+    },
+  };
 }
 
 export const USE_PROJECTS = {
@@ -25,25 +83,6 @@ export const USE_PROJECTS = {
   id: "census-directory-projects",
 };
 
-// assumed tier 2 for now
-export interface CensusDirectoryProject {
-  id: string;
-  obsoleted_by: string;
-  title: string;
-  description: string;
-  submitter: string;
-  contact_name: string;
-  contact_email: string;
-  doi: string;
-  model_link: string;
-  census_release: string;
-  organism_name: string;
-  measurement_name: string;
-  embeddings_link: string;
-  contribution_type: string;
-  notebook_links: string[]; // committed directly
-}
-
 export function useProjects() {
-  return useQuery<CensusDirectoryProject[]>([USE_PROJECTS], fetchProjects);
+  return useQuery<ProjectResponse>([USE_PROJECTS], fetchProjects);
 }
