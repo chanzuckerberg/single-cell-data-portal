8f2eba7e695de6eb60114e644465ee2d3befd059
Trent Smith <1429913+Bento007@users.noreply.github.com>
2023-11-02 23:11:06 -0700

    refactor: URI->URL (#6129)
    ## Reason for Change
    
    The goal of this PR is to simplify the numerous download methods used during ingest.
    
    ## Changes
    
    - refactor URL-> URI
    - add download function to URI

--- tests/unit/backend/layers/utils/test_uri.py
+++ tests/unit/backend/layers/utils/test_uri.py
@@ -1,11 +1,12 @@
 import os
 import unittest
+from tempfile import TemporaryDirectory
 from unittest import TestCase
 
 import boto3
 from moto import mock_s3
 
-from backend.common.utils.dl_sources.url import S3URI, S3URL, DropBoxURL, RegisteredSources, from_url
+from backend.common.utils.dl_sources.uri import S3URI, S3URL, DropBoxURL, RegisteredSources, from_uri
 
 
 class TestRegisteredSources(unittest.TestCase):
@@ -20,16 +21,16 @@ class TestRegisteredSources(unittest.TestCase):
         RegisteredSources.add(S3URL)
         self.assertIn(S3URL, RegisteredSources.get())
 
-    def test_from_url(self):
+    def test_from_uri(self):
         RegisteredSources.add(S3URL)
-        self.assertIsInstance(from_url("https://somebucket.s3.amazonaws.com"), S3URL)
+        self.assertIsInstance(from_uri("https://somebucket.s3.amazonaws.com"), S3URL)
 
         RegisteredSources.add(DropBoxURL)
-        self.assertIsInstance(from_url("https://www.dropbox.com/s/abcd1234/abcd1234?dl=0"), DropBoxURL)
-        self.assertIsNone(from_url("http://example.com"))
+        self.assertIsInstance(from_uri("https://www.dropbox.com/s/abcd1234/abcd1234?dl=0"), DropBoxURL)
+        self.assertIsNone(from_uri("http://example.com"))
 
         RegisteredSources.add(S3URI)
-        self.assertIsInstance(from_url("s3://bucket/key"), S3URI)
+        self.assertIsInstance(from_uri("s3://bucket/key"), S3URI)
 
 
 class TestS3URI(TestCase):
@@ -58,8 +59,32 @@ class TestS3URI(TestCase):
         )
         s3.put_object(Bucket=bucket_name, Key=key, Body=content)
 
-        s3_uri = from_url(f"s3://{bucket_name}{key}")
+        s3_uri = from_uri(f"s3://{bucket_name}{key}")
         info = s3_uri.file_info()
 
         self.assertEqual(key, info["name"])
         self.assertEqual(len(content), info["size"])
+
+    @mock_s3
+    def test_download(self):
+        s3 = boto3.client("s3")
+        bucket_name = "bucket"
+        key = "/key"
+        content = "stuff"
+        s3.create_bucket(
+            Bucket=bucket_name, CreateBucketConfiguration={"LocationConstraint": os.environ["AWS_DEFAULT_REGION"]}
+        )
+        s3.put_object(Bucket=bucket_name, Key=key, Body=content)
+
+        # add a context manager for a temporary directory
+        with TemporaryDirectory() as tmpdir:
+            # create a temporary file
+            tmpfile = os.path.join(tmpdir, "test.txt")
+            # download the file to the temporary directory
+            s3_uri = from_uri(f"s3://{bucket_name}{key}")
+            s3_uri.download(tmpfile)
+            # read the file contents
+            with open(tmpfile, "r") as f:
+                data = f.read()
+            # assert the contents are correct
+            self.assertEqual(data, content)
