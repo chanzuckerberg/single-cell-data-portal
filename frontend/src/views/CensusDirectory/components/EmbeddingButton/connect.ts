3c2f0d96242915072dd84ca44b745aa44d03796b
Severiano Badajoz <sbadajoz@chanzuckerberg.com>
2023-11-06 15:46:29 -0800

    feat(projects): add analytics to census projects (#6157)

--- frontend/src/views/CensusDirectory/components/EmbeddingButton/connect.ts
+++ frontend/src/views/CensusDirectory/components/EmbeddingButton/connect.ts
@@ -1,15 +1,24 @@
 import { useCallback, useState } from "react";
+import { track } from "src/common/analytics";
+import { EVENTS } from "src/common/analytics/events";
+import { EmbeddingButtonProps } from "./types";
+import { getProjectTier } from "../../utils";
 
-export const useConnect = () => {
+export const useConnect = ({ project }: EmbeddingButtonProps) => {
   const [isOpen, setIsOpen] = useState(false);
   const [isCopied, setIsCopied] = useState(false);
   const [language, setLanguage] = useState<string>("python");
 
+  const projectTier = getProjectTier(project);
+
   const handleButtonClick = useCallback(() => {
-    // TODO: Analytics
-    // if (!isOpen) track(EVENTS.WMG_DOWNLOAD_CLICKED);
+    if (!isOpen)
+      track(EVENTS.CENSUS_EMBEDDING_CLICKED, {
+        project: project.title,
+        category: projectTier,
+      });
     setIsOpen(!isOpen);
-  }, [isOpen]);
+  }, [isOpen, projectTier, project.title]);
 
   // These can be derived from the static S3 namespace + the accessor_id or will be a static url provided in json blob
   const pythonCode = "Long-arbitrary-string-here-python";
@@ -18,7 +27,11 @@ export const useConnect = () => {
   const handleCopyClick = () => {
     setIsCopied(true);
     navigator.clipboard.writeText(language === "python" ? pythonCode : rCode);
-    // TODO: analytics
+    track(EVENTS.CENSUS_EMBEDDING_COPIED, {
+      project: project.title,
+      category: projectTier,
+      version: language,
+    });
   };
   const handleCopyMouseEnter = () => setIsCopied(false);
 
