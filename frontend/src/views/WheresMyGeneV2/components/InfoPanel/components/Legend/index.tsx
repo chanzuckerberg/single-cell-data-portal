739a1182079a37e1b587377b1be5cb786606f9e7
kaloster <rkalo@contractor.chanzuckerberg.com>
2023-11-09 14:02:01 -0500

    layout

--- frontend/src/views/WheresMyGeneV2/components/InfoPanel/components/Legend/index.tsx
+++ frontend/src/views/WheresMyGeneV2/components/InfoPanel/components/Legend/index.tsx
@@ -5,7 +5,7 @@ import SaveExport from "src/views/WheresMyGeneV2/components/GeneSearchBar/compon
 import ShareButton from "src/views/WheresMyGeneV2/components/GeneSearchBar/components/ShareButton";
 import ExpressedInCells from "../ExpressedInCells";
 import RelativeGeneExpression from "../RelativeGeneExpression";
-import { LegendWrapper } from "./style";
+import { ActionsWrapper, ColorLegendWrapper, LegendWrapper } from "./style";
 import { EMPTY_ARRAY, EMPTY_OBJECT } from "src/common/constants/utils";
 import SourceDataButton from "src/views/WheresMyGeneV2/components/GeneSearchBar/components/SourceDataButton";
 import { Props } from "./types";
@@ -26,27 +26,31 @@ export default memo(function Legend({
 }: Props): JSX.Element {
   return (
     <LegendWrapper data-testid="legend-wrapper">
-      <SaveExport
-        selectedGenes={selectedGenes}
-        selectedCellTypes={selectedCellTypes}
-        setDownloadStatus={setDownloadStatus}
-        setEchartsRendererMode={setEchartsRendererMode}
-        allChartProps={allChartProps}
-        availableFilters={availableFilters}
-        tissues={tissues || EMPTY_OBJECT}
-        expandedTissueIds={expandedTissueIds ?? EMPTY_ARRAY}
-        filteredCellTypes={filteredCellTypes ?? EMPTY_ARRAY}
-      />
-      <ShareButton />
-      <CitationButton />
-      <SourceDataButton
-        handleRightSidebarButtonClick={handleRightSidebarButtonClick}
-      />
-      <RelativeGeneExpression
-        isScaled={isScaled}
-        maxExpression={maxExpression}
-      />
-      <ExpressedInCells />
+      <ActionsWrapper>
+        <SaveExport
+          selectedGenes={selectedGenes}
+          selectedCellTypes={selectedCellTypes}
+          setDownloadStatus={setDownloadStatus}
+          setEchartsRendererMode={setEchartsRendererMode}
+          allChartProps={allChartProps}
+          availableFilters={availableFilters}
+          tissues={tissues || EMPTY_OBJECT}
+          expandedTissueIds={expandedTissueIds ?? EMPTY_ARRAY}
+          filteredCellTypes={filteredCellTypes ?? EMPTY_ARRAY}
+        />
+        <ShareButton />
+        <CitationButton />
+        <SourceDataButton
+          handleRightSidebarButtonClick={handleRightSidebarButtonClick}
+        />
+      </ActionsWrapper>
+      <ColorLegendWrapper>
+        <RelativeGeneExpression
+          isScaled={isScaled}
+          maxExpression={maxExpression}
+        />
+        <ExpressedInCells />
+      </ColorLegendWrapper>
     </LegendWrapper>
   );
 });
--- frontend/src/views/WheresMyGeneV2/components/InfoPanel/components/Legend/style.ts
+++ frontend/src/views/WheresMyGeneV2/components/InfoPanel/components/Legend/style.ts
@@ -1,10 +1,19 @@
 import styled from "@emotion/styled";
 
-export const LEGEND_HEIGHT_PX = 52;
+export const LEGEND_HEIGHT_PX = 100;
 
 export const LegendWrapper = styled.div`
   display: flex;
-  width: 700px;
-  justify-content: flex-end;
+  width: 100%;
   height: ${LEGEND_HEIGHT_PX}px;
+  justify-content: space-between;
+  height: ${LEGEND_HEIGHT_PX}px;
+`;
+
+export const ActionsWrapper = styled.div`
+  display: flex;
+`;
+
+export const ColorLegendWrapper = styled.div`
+  display: flex;
 `;
