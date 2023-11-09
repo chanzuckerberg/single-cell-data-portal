6742dba9aa678bbd528fe333169d57506785eccb
Trent Smith <trent.smith@chanzuckerberg.com>
2023-11-09 10:52:21 -0800

    fix link

--- backend/common/auth0_manager.py
+++ backend/common/auth0_manager.py
@@ -13,7 +13,7 @@ class Auth0ManagementSession:
     """
     A wrapper around the Auth0 Management API. https://auth0.com/docs/api/management/v2
 
-     Don't skip these tests https://github.com/chanzuckerberg/single-cell-data-portal/blob/655f391d81c89b403422ea81e0d5d69cf3427aa5/tests/functional/backend/corpora/test_api_key.py#L9
+     Don't skip these tests https://github.com/chanzuckerberg/single-cell-data-portal/blob/82ddc2a019bb8e057bc5783e659231d4e2dc9867/tests/functional/backend/corpora/test_api_key.py#L10
      if modifying this code.
     """
 
