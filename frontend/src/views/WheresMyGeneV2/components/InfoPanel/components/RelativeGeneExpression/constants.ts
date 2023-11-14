84ed422759fc2b44ee295100133e9d6861c5ca03
kaloster <rkalo@contractor.chanzuckerberg.com>
2023-11-14 12:18:25 -0500

    now with even more tests passing

--- frontend/src/views/WheresMyGeneV2/components/InfoPanel/components/RelativeGeneExpression/constants.ts
+++ frontend/src/views/WheresMyGeneV2/components/InfoPanel/components/RelativeGeneExpression/constants.ts
@@ -1,4 +1,4 @@
 export const MAX_EXPRESSION_LABEL_TEST_ID = "max-expression-label";
 export const COLOR_LEGEND_TOOLTIP_CONTENT =
-  "After filtering cells with low coverage (less than 500 genes expressed), gene counts are normalized independently for each cell by converting counts to quantiles and obtaining the corresponding values from the standard normal distribution. Then normalized cell vectors are concatenated along the gene axis. The algorithm is summarized in detail in ";
+  "After filtering cells with low coverage (less than 500 genes expressed), gene counts are normalized independently for each cell using ln(TTPM+1). The algorithm is summarized in detail in our  ";
 export const COLOR_LEGEND = "Gene Expression color legend";
--- frontend/tests/features/wheresMyGene/downloadCsv.test.ts
+++ frontend/tests/features/wheresMyGene/downloadCsv.test.ts
@@ -14,8 +14,6 @@ import {
 } from "tests/common/constants";
 import { test } from "tests/common/test";
 
-const TISSUE_NODE_TEST_ID = "tissue-name";
-
 const { describe } = test;
 describe("CSV download tests", () => {
   test(`Should verify CSV metadata and header for lung tissue with no group set`, async ({
@@ -23,7 +21,7 @@ describe("CSV download tests", () => {
   }) => {
     // set app state
     await goToWMG(page, SHARED_LINK_NO_GROUP);
-    await expect(page.getByTestId(TISSUE_NODE_TEST_ID)).not.toHaveCount(0);
+    await expect(page.locator("canvas")).not.toHaveCount(0);
 
     const tissues = ["blood", "lung"];
     const fileTypes = ["csv"];
@@ -47,6 +45,8 @@ describe("CSV download tests", () => {
   }) => {
     // set app state
     await goToWMG(page, SHARED_LINK_FILTER);
+    await expect(page.locator("canvas")).not.toHaveCount(0);
+
     const tissues = ["blood", "lung"];
     const fileTypes = ["csv"];
     const folder = subDirectory();
@@ -69,6 +69,8 @@ describe("CSV download tests", () => {
   }) => {
     // set app state
     await goToWMG(page, SHARED_LINK_NO_FILTER);
+    await expect(page.locator("canvas")).not.toHaveCount(0);
+
     const tissues = ["blood", "lung"];
     const fileTypes = ["csv"];
     const folder = subDirectory();
