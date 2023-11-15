280687bb992d90dcc13735ba893475a015115fa1
Ronen <kaloster@users.noreply.github.com>
2023-11-15 12:20:27 -0500

    feat(wmg): updates to data source action (#6210)

--- frontend/src/views/WheresMyGeneV2/components/InfoPanel/components/RelativeGeneExpression/index.tsx
+++ frontend/src/views/WheresMyGeneV2/components/InfoPanel/components/RelativeGeneExpression/index.tsx
@@ -1,8 +1,21 @@
 import Image from "next/image";
+import { Tooltip } from "@czi-sds/components";
 import { Content, Label, LowHigh } from "../../common/style";
 import plasmaImage from "./plasma.png";
-import { ContentWrapper, Wrapper } from "./style";
+import { ContentWrapper, Wrapper, TooltipLink } from "./style";
 import { MAX_EXPRESSION_LABEL_TEST_ID } from "./constants";
+import {
+  StyledIconImage,
+  StyledTooltip,
+  TooltipContent,
+  TooltipButton,
+} from "src/views/WheresMyGeneV2/components/CellInfoSideBar/style";
+import questionMarkIcon from "src/common/images/question-mark-icon.svg";
+import { COLOR_LEGEND, COLOR_LEGEND_TOOLTIP_CONTENT } from "./constants";
+import { ROUTES } from "src/common/constants/routes";
+import { track } from "src/common/analytics";
+import { EVENTS } from "src/common/analytics/events";
+import { useConnect } from "../Legend/connect";
 
 const CONTENT_WIDTH_PX = 120;
 
@@ -14,9 +27,42 @@ export default function RelativeGeneExpression({
   isScaled,
   maxExpression,
 }: Props): JSX.Element {
+  const { setHoverStartTime, handleLegendHoverEnd } = useConnect();
   return (
     <Wrapper id="relative-gene-expression">
       <Label id="relative-gene-expression-label">Gene Expression</Label>
+      <Tooltip
+        sdsStyle="dark"
+        placement="bottom-end"
+        width="wide"
+        className="fmg-tooltip-icon"
+        arrow
+        onOpen={() => setHoverStartTime(Date.now())}
+        onClose={handleLegendHoverEnd}
+        title={
+          <StyledTooltip>
+            <TooltipContent>
+              {COLOR_LEGEND_TOOLTIP_CONTENT}
+              <TooltipLink
+                href={ROUTES.WMG_DOCS}
+                rel="noopener"
+                target="_blank"
+                onClick={() => {
+                  track(EVENTS.WMG_FMG_DOCUMENTATION_CLICKED, {
+                    label: COLOR_LEGEND,
+                  });
+                }}
+              >
+                our documentation.
+              </TooltipLink>
+            </TooltipContent>
+          </StyledTooltip>
+        }
+      >
+        <TooltipButton sdsStyle="minimal" sdsType="secondary" isAllCaps={false}>
+          <StyledIconImage src={questionMarkIcon} />
+        </TooltipButton>
+      </Tooltip>
       <ContentWrapper>
         <Content>
           <Image
---
+++ frontend/src/views/WheresMyGeneV2/components/InfoPanel/components/SourceData/constants.ts
@@ -0,0 +1,3 @@
+export const SOURCE_DATA_INFO_TEXT =
+  "Gene Expression is powered by primary data submitted to CZ CELLxGENE Discover. See ";
+export const SOURCE_DATA = "Soure Data for Gene Expression";
