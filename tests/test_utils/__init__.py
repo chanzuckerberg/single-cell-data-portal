582d96b607f145c8bc575a5933b82dcba4548888
Trent Smith <trent.smith@chanzuckerberg.com>
2023-11-16 15:23:04 -0800

    behave the same as old code by failing with first mismatch

--- tests/test_utils/__init__.py
+++ tests/test_utils/__init__.py
@@ -1,59 +1,82 @@
-class CompareDictsAddin:
-    _maxDiff = 10
+import contextlib
+from contextlib import contextmanager
 
-    def assert_dicts_equal(self, dict1, dict2):
+
+class CompareDictsAddin:
+    def assert_dicts_equal(self, dict1, dict2, fail_fast=True, max_diff=None):
         """
         This function recursive compares two dictionaries handling cases where the values
         are unordered arrays with elements that could be dictionaries.
+        :param dict1: first dictionary
+        :param dict2: second dictionary
+        :param fail_fast: if True, raise an exception on the first mismatch
+        :param max_diff: maximum number of differences to report
         """
-        mismatch = []
-        path = ["$"]
+        _mismatch = []
+        _path = ["$"]
+
+        class Stop(Exception):
+            pass
+
+        def update_mismatch(message):
+            _mismatch.append(message)
+            if fail_fast:
+                raise Stop()
+
+        @contextmanager
+        def path(key):
+            try:
+                yield path.append(key)
+            finally:
+                path.pop()
 
         def _compare_dict(_dict1, _dict2):
             if len(_dict1) != len(_dict2):
-                mismatch.append("{}: Lengths differ: {} vs {}".format(".".join(path), len(_dict1), len(_dict2)))
+                update_mismatch("{}: Lengths differ: {} vs {}".format(".".join(path), len(_dict1), len(_dict2)))
 
             for key in dict1:
                 if key not in _dict2:
-                    mismatch.append("{}: Key {} not in dict2".format(".".join(path), key))
+                    update_mismatch("{}: Key {} not in dict2".format(".".join(path), key))
                     continue
-                path.append(key)
                 value1 = _dict1[key]
                 value2 = _dict2[key]
 
-                if isinstance(value1, dict) and isinstance(value2, dict):
-                    _compare_dict(value1, value2)
-                elif isinstance(value1, list) and isinstance(value2, list):
-                    if len(value1) != len(value2):
-                        mismatch.append("{}: Lengths differ: {} vs {}".format(".".join(path), len(value1), len(value2)))
-                    # check if the lists contain dictionaries as elements
-                    elif len(value1) > 0 and isinstance(value1[0], dict) and isinstance(value2[0], dict):
-                        for i in range(len(value1)):
-                            path.append(f"[{i}]")
-                            _compare_dict(value1[i], value2[i])
-                            path.pop()
-                    elif sorted(value1) != sorted(value2):
-                        # compare as sets and print the differences
-                        value1_set = set(value1)
-                        value2_set = set(value2)
-                        mismatch.append(
-                            "{}: Values differ: {} vs {}".format(
-                                ".".join(path), value1_set - value2_set, value2_set - value1_set
+                with path(key):
+                    if isinstance(value1, dict) and isinstance(value2, dict):
+                        _compare_dict(value1, value2)
+                    elif isinstance(value1, list) and isinstance(value2, list):
+                        if len(value1) != len(value2):
+                            update_mismatch(
+                                "{}: Lengths differ: {} vs {}".format(".".join(path), len(value1), len(value2))
                             )
-                        )
-                else:
-                    if value1 != value2:
-                        mismatch.append("{}: Values differ: {} vs {}".format(".".join(path), value1, value2))
-                path.pop()
+                        # check if the lists contain dictionaries as elements
+                        elif len(value1) > 0 and isinstance(value1[0], dict) and isinstance(value2[0], dict):
+                            for i in range(len(value1)):
+                                with path(f"[{i}]"):
+                                    _compare_dict(value1[i], value2[i])
+                        elif sorted(value1) != sorted(value2):
+                            # compare as sets and print the differences
+                            value1_set = set(value1)
+                            value2_set = set(value2)
+                            update_mismatch(
+                                "{}: Values differ: {} vs {}".format(
+                                    ".".join(path), value1_set - value2_set, value2_set - value1_set
+                                )
+                            )
+                    else:
+                        if value1 != value2:
+                            update_mismatch("{}: Values differ: {} vs {}".format(".".join(path), value1, value2))
 
-        _compare_dict(dict1, dict2)
-        if mismatch:
-            if len(mismatch) > self._maxDiff:
+        with contextlib.suppress(Stop):
+            _compare_dict(dict1, dict2)
+
+        if _mismatch:
+            if len(_mismatch) > max_diff:
                 raise self.failureException(
-                    f"Mismatched keys: {len[mismatch]}" + "\n\t".join(mismatch[: self._maxDiff]) + "\n\t..."
+                    f"Mismatched keys: {len[_mismatch]}" + "\n\t".join(_mismatch[:max_diff]) + "\n\t..."
                 )
             else:
-                raise self.failureException(f"Mismatched keys: {len[mismatch]}" + "\n\t".join(mismatch))
+                raise self.failureException(f"Mismatched keys: {len[_mismatch]}" + "\n\t".join(_mismatch))
 
 
 def sort_dataframe(df):
