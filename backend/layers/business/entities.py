73de6911385dd518f5ce5fcec91ef3a5cc7180ec
MillenniumFalconMechanic <mim@clevercanary.com>
2023-11-20 11:46:59 -0800

    Linting.

--- backend/layers/business/entities.py
+++ backend/layers/business/entities.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 from typing import List, Optional
 
-from backend.layers.common.entities import DatasetArtifactType, Link
+from backend.layers.common.entities import Link
 
 
 @dataclass
