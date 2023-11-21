de49a5905641f872c79de3d77e504eaf0785d8e9
kaloster <rkalo@contractor.chanzuckerberg.com>
2023-11-21 13:44:48 -0500

    update chevron

--- frontend/src/views/WheresMyGeneV2/components/Filters/components/Compare/style.ts
+++ frontend/src/views/WheresMyGeneV2/components/Filters/components/Compare/style.ts
@@ -1,6 +1,10 @@
 import styled from "@emotion/styled";
 import { spacesXs } from "src/common/theme";
 
+export const CompareWrapper = styled("div")`
+  margin: 0;
+`;
+
 export const LabelWrapper = styled("div")`
   display: flex;
   gap: ${spacesXs}px;
