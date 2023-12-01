25d4e14f91f081d73effc09b88a70fc648eb9c1b
Joyce Yan <5653616+joyceyan@users.noreply.github.com>
2023-12-01 11:08:56 -0500

    feat: updates to marker genes panel (#6260)

--- frontend/src/views/WheresMyGeneV2/components/CellInfoSideBar/constants.ts
+++ frontend/src/views/WheresMyGeneV2/components/CellInfoSideBar/constants.ts
@@ -1,11 +1,7 @@
-export const MARKER_GENE_LABEL = "marker genes";
-export const MARKER_SCORE_LABEL = "marker score";
-export const MARKER_GENES_TOOLTIP_CONTENT =
-  "Marker genes are highly and uniquely expressed in the cell type relative to all other cell types.";
-export const MARKER_SCORE_TOOLTIP_CONTENT =
-  "Marker Score indicates the strength and specificity of agene as a marker. It is the 5th percentile of the effect sizes when comparing the expressions in a cell type of interest to each other cell type in the tissue.";
-export const MARKER_SCORE_TOOLTIP_LINK_TEXT =
-  "Click to read more about the identification method.";
+export const SPECIFICITY_TOOLTIP_CONTENT_FIRST_HALF =
+  "Specificity indicates the percentage of cell types in";
+export const SPECIFICITY_TOOLTIP_CONTENT_SECOND_HALF =
+  "with a lower Effect Size for this gene. Higher Specificity values indicate markers which are more specific for this cell type.";
 export const MARKER_SCORE_CELLGUIDE_LINK_TEXT = "Open in CellGuide";
 export const MARKER_SCORE_DOTPLOT_BUTTON_TEXT = "Add to Dot Plot";
 export const NO_MARKER_GENES_HEADER = "No Marker Genes";
@@ -16,5 +12,5 @@ export const TOO_FEW_CELLS_NO_MARKER_GENES_DESCRIPTION =
 export const NO_MARKER_GENES_FOR_BLOOD_DESCRIPTION =
   "Cell types within Blood are closely related, so we are unable to generate high value marker genes at this time.";
 export const TABLE_HEADER_GENE = "Gene";
-export const TABLE_HEADER_SCORE = "Marker Score";
+export const TABLE_HEADER_SPECIFICITY = "Specificity";
 export const TISSUES_WITHOUT_MARKER_GENES = ["blood"];
