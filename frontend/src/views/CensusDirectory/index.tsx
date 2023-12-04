a2819ec98260d59a2ddd075ac2d4c1ab931fd0d6
Sidney Bell <sidneymbell@gmail.com>
2023-12-04 11:09:50 -0800

    Reformat

--- frontend/src/views/CensusDirectory/index.tsx
+++ frontend/src/views/CensusDirectory/index.tsx
@@ -34,15 +34,13 @@ function CensusDirectory() {
         <p>
           This page features models and integrated embeddings of the Census data
           corpus, organized by CELL×GENE’s level of involvement with their
-          maintenance and availability.
+          maintenance and availability. These models are breaking new ground and
+          will continue to improve. We encourage you to try out these models and
+          provide feedback!
         </p>
         <p>
           {/* TODO: add link to notebooks once available */}
           Please <Link href="">see these tutorials</Link> for usage details.
-        </p>
-        <p>
-          These models are breaking new ground and will continue to improve. We
-          encourage you to try out these models and provide feedback!
         </p>
         <p>
           If you’d like to have your project featured here, please{" "}
@@ -53,19 +51,16 @@ function CensusDirectory() {
         <TierContainer>
           <TierTitle>CELL×GENE Maintained Projects</TierTitle>
           <TierDescription>
-            <p>
-              These models and their output embeddings are maintained and
-              regularly re-trained by CELL×GENE in close collaboration with
-              their creators. Embeddings are accessible via the Census API;
-              corresponding models are available via CELL×GENE-maintained links.
-            </p>
-            <p>
-              Please{" "}
-              <Link href="mailto:cellxgene@chanzuckerberg.com">
-                contact the CELL×GENE team with feedback
-              </Link>
-              .
-            </p>
+            These models and their output embeddings are maintained and
+            regularly re-trained by CELL×GENE in close collaboration with their
+            creators. Embeddings are accessible via the Census API;
+            corresponding models are available via CELL×GENE-maintained links.
+            <br />
+            Please{" "}
+            <Link href="mailto:cellxgene@chanzuckerberg.com">
+              contact the CELL×GENE team with feedback
+            </Link>
+            .
           </TierDescription>
           {maintainedProjects.map((project) => (
             <Project key={project.title} project={project} />
@@ -76,20 +71,17 @@ function CensusDirectory() {
         <TierContainer>
           <TierTitle>CELL×GENE Hosted Projects</TierTitle>
           <TierDescription>
-            <p>
-              CELL×GENE makes these embeddings directly available through the
-              Census API, but does not actively maintain or update them.
-              Corresponding models are accessible via external links (when
-              available).
-            </p>
-            <p>
-              For issues accessing these embeddings, please{" "}
-              <Link href="mailto:cellxgene@chanzuckerberg.com">
-                contact the CELL×GENE team
-              </Link>
-              . For feedback on the embeddings themselves, please contact the
-              creators.
-            </p>
+            CELL×GENE makes these embeddings directly available through the
+            Census API, but does not actively maintain or update them.
+            Corresponding models are accessible via external links (when
+            available).
+            <br />
+            For issues accessing these embeddings, please{" "}
+            <Link href="mailto:cellxgene@chanzuckerberg.com">
+              contact the CELL×GENE team
+            </Link>
+            . For feedback on the embeddings themselves, please contact the
+            creators.
           </TierDescription>
           {hostedProjects.map(([id, project]) => (
             <Project key={id} id={id} project={project} />
@@ -100,12 +92,11 @@ function CensusDirectory() {
         <TierContainer>
           <TierTitle>Community Projects</TierTitle>
           <TierDescription>
-            <p>
-              The community has also developed many wonderful projects using
-              Census data. While CELL×GENE does not directly host or maintain
-              these projects, we’re excited to showcase them here.
-            </p>
-            <p>Please contact their creators with questions or feedback.</p>
+            The community has also developed many wonderful projects using
+            Census data. While CELL×GENE does not directly host or maintain
+            these projects, we’re excited to showcase them here.
+            <br />
+            Please contact their creators with questions or feedback.
           </TierDescription>
           {communityProjects.map((project) => (
             <Project key={project.title} project={project} />
