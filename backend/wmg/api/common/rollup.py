906004d5ff4b63bf05cbf59547888f6e473c3c68
atarashansky <atarashansky@chanzuckerberg.com>
2023-12-06 10:46:47 -0800

    fix

--- backend/wmg/api/common/rollup.py
+++ backend/wmg/api/common/rollup.py
@@ -12,39 +12,36 @@ from pandas import DataFrame
 
 
 @tracer.wrap(name="rollup", service="wmg-api", resource="query", span_type="wmg-api")
-def rollup(gene_expression_df, universal_set_cell_counts_df) -> DataFrame:
+def rollup(gene_expression_df, cell_counts_df) -> DataFrame:
     # if the gene expression dataframe is empty, then there is nothing to roll up
-    if gene_expression_df.shape[0] == 0 or universal_set_cell_counts_df.shape[0] == 0:
-        return gene_expression_df, universal_set_cell_counts_df
+    if gene_expression_df.shape[0] == 0 or cell_counts_df.shape[0] == 0:
+        return gene_expression_df, cell_counts_df
 
     gene_expression_df["cell_type_ontology_term_id_ancestors"] = gene_expression_df.apply(lambda x: x.split(","))
-    universal_set_cell_counts_df["cell_type_ontology_term_id_ancestors"] = universal_set_cell_counts_df.apply(
-        lambda x: x.split(",")
-    )
+    cell_counts_df["cell_type_ontology_term_id_ancestors"] = cell_counts_df.apply(lambda x: x.split(","))
 
     del gene_expression_df["cell_type_ontology_term_id"]
-    del universal_set_cell_counts_df["cell_type_ontology_term_id"]
+    del cell_counts_df["cell_type_ontology_term_id"]
 
     gene_expression_df = gene_expression_df.explode("cell_type_ontology_term_id_ancestors")
-    universal_set_cell_counts_df = universal_set_cell_counts_df.explode("cell_type_ontology_term_id_ancestors")
+    cell_counts_df = cell_counts_df.explode("cell_type_ontology_term_id_ancestors")
 
     gene_expression_df.rename(
         columns={"cell_type_ontology_term_id_ancestors": "cell_type_ontology_term_id"}, inplace=True
     )
-    universal_set_cell_counts_df.rename(
-        columns={"cell_type_ontology_term_id_ancestors": "cell_type_ontology_term_id"}, inplace=True
-    )
+    cell_counts_df.rename(columns={"cell_type_ontology_term_id_ancestors": "cell_type_ontology_term_id"}, inplace=True)
     # get non-numeric column names
     ge_dim_cols = [
         col for col in gene_expression_df.columns if not np.issubtype(gene_expression_df[col].dtype, np.number)
     ]
-    cc_dim_cols = [
-        col
-        for col in universal_set_cell_counts_df.columns
-        if not np.issubtype(universal_set_cell_counts_df[col].dtype, np.number)
-    ]
+    cc_dim_cols = [col for col in cell_counts_df.columns if not np.issubtype(cell_counts_df[col].dtype, np.number)]
 
-    rolled_up_gene_expression_df = gene_expression_df.groupby(ge_dim_cols, as_index=False).sum()
-    rolled_up_universal_set_cell_counts_df = universal_set_cell_counts_df.groupby(cc_dim_cols, as_index=False).sum()
+    agg_dict = {
+        col: "sum" if col != "n_cells_tissue" else "first"
+        for col in gene_expression_df.columns
+        if col not in ge_dim_cols
+    }
+    rolled_up_gene_expression_df = gene_expression_df.groupby(ge_dim_cols).agg(agg_dict)
+    rolled_up_cell_counts_df = cell_counts_df.groupby(cc_dim_cols).sum()
 
-    return rolled_up_gene_expression_df, rolled_up_universal_set_cell_counts_df
+    return rolled_up_gene_expression_df, rolled_up_cell_counts_df
