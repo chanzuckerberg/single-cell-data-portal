a42b3cd3bd562536404a87943c3683ec0e131ad7
MillenniumFalconMechanic <mim@clevercanary.com>
2023-12-19 09:45:41 -0800

    fix: unescaped CrossRef journal

--- backend/layers/thirdparty/crossref_provider.py
+++ backend/layers/thirdparty/crossref_provider.py
@@ -1,3 +1,4 @@
+import html
 import logging
 from datetime import datetime
 from urllib.parse import urlparse
@@ -107,13 +108,15 @@ class CrossrefProvider(CrossrefProviderInterface):
             # Journal
             try:
                 if "short-container-title" in message and message["short-container-title"]:
-                    journal = message["short-container-title"][0]
+                    raw_journal = message["short-container-title"][0]
                 elif "container-title" in message and message["container-title"]:
-                    journal = message["container-title"][0]
+                    raw_journal = message["container-title"][0]
                 elif "institution" in message:
-                    journal = message["institution"][0]["name"]
+                    raw_journal = message["institution"][0]["name"]
             except Exception:
                 raise CrossrefParseException("Journal node missing") from None
+
+            journal = html.unescape(raw_journal)
 
             # Authors
             # Note: make sure that the order is preserved, as it is a relevant information
