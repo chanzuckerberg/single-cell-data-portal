bce0cead75e3baea28afea199b3fe968a245749a
Timmy Huang <thuang@chanzuckerberg.com>
2024-01-03 12:46:25 -0800

    remove @mui/styles

--- frontend/src/components/common/Filter/components/FilterContent/components/FilterRange/style.ts
+++ frontend/src/components/common/Filter/components/FilterContent/components/FilterRange/style.ts
@@ -1,75 +1,81 @@
-import makeStyles from "@mui/styles/makeStyles";
 import {
   GRAY,
   LIGHT_GRAY,
   PRIMARY_BLUE,
   PT_TEXT_COLOR,
 } from "src/components/common/theme";
+import Slider from "@mui/material/Slider";
+import styled from "@emotion/styled";
 
-export const useSliderStyles = makeStyles({
-  root: {
-    color: PRIMARY_BLUE,
-    display: "block",
-    height: 4,
-    margin: 0,
-    marginLeft: 24,
-    marginRight: 24,
-    paddingBottom: 30,
-    paddingTop: 34,
-    width: 220,
-    "& .MuiSlider-mark": {
-      display: "none",
-    },
-    "& .MuiSlider-markLabel": {
-      color: GRAY.A,
-      fontSize: 12,
-      letterSpacing: "normal",
-      lineHeight: "15px",
-      top: 47,
-    },
-    "& .MuiSlider-rail": {
-      borderRadius: 2,
-      color: LIGHT_GRAY.C,
-      height: 4,
-      opacity: 1,
-    },
-    "& .MuiSlider-thumb": {
-      height: 14,
-      marginLeft: -7,
-      width: 14,
-      "&:focus": {
-        outline: "none",
-      },
-      "&:hover": {
-        boxShadow: "none",
-      },
-    },
-    "& .MuiSlider-track": {
-      borderRadius: 2,
-      height: 4,
-    },
-    "& .MuiSlider-valueLabel": {
-      fontSize: 12,
-      fontWeight: 500,
-      left: "50%",
-      letterSpacing: "normal",
-      lineHeight: "15px",
-      top: -13,
-      transform: "scale(1) translateX(-50%) translateY(-10px) !important",
+export const StyledSlider = styled(Slider)`
+  color: ${PRIMARY_BLUE};
+  display: block;
+  height: 4px;
+  margin: 0;
+  margin-left: 24px;
+  margin-right: 24px;
+  padding-bottom: 30px;
+  padding-top: 34px;
+  width: 220px;
 
-      "& > span": {
-        background: LIGHT_GRAY.E,
-        borderRadius: 4,
-        height: "unset",
-        padding: "2px 4px",
-        width: "unset",
-      },
+  & .MuiSlider-mark {
+    display: none;
+  }
 
-      "& *": {
-        color: PT_TEXT_COLOR,
-        transform:
-          "none" /* removes transform rotate from all children of valueLabel */,
-      },
-    },
-  },
-});
+  & .MuiSlider-markLabel {
+    color: ${GRAY.A};
+    font-size: 12px;
+    letter-spacing: normal;
+    line-height: 15px;
+    top: 47px;
+  }
+
+  & .MuiSlider-rail {
+    border-radius: 2px;
+    color: ${LIGHT_GRAY.C};
+    height: 4px;
+    opacity: 1;
+  }
+
+  & .MuiSlider-thumb {
+    height: 14px;
+    margin-left: -7px;
+    width: 14px;
+
+    &:focus {
+      outline: none;
+    }
+
+    &:hover {
+      box-shadow: none;
+    }
+  }
+
+  & .MuiSlider-track {
+    border-radius: 2px;
+    height: 4px;
+  }
+
+  & .MuiSlider-valueLabel {
+    font-size: 12px;
+    font-weight: 500;
+    left: 50%;
+    letter-spacing: normal;
+    line-height: 15px;
+    top: -13px;
+    transform: scale(1) translateX(-50%) translateY(-10px) !important;
+
+    & > span {
+      background: ${LIGHT_GRAY.E};
+      border-radius: 4px;
+      height: unset;
+      padding: 2px 4px;
+      width: unset;
+    }
+
+    & * {
+      color: ${PT_TEXT_COLOR};
+      transform: none; /* removes transform rotate from all children of valueLabel */
+    }
+  }
+`;
