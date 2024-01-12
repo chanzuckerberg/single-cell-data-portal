54fa58496d84cbc20f190fb5aa6d4234c6f1af02
nayib-jose-gloria <ngloria@chanzuckerberg.com>
2024-01-12 16:23:46 -0500

    reuse promotion script in GHA

--- scripts/promote_staging_to_prod.sh
+++ scripts/promote_staging_to_prod.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-checks=$(curl -s  "https://api.github.com/repos/chanzuckerberg/single-cell-data-portal/actions/runs?actor=czibuildbot" | jq -r '.workflow_runs[] | select(.display_title == "stage") | .conclusion' | head -n 1)
+if [ ! -z $1 ]; then
+  checks=$(curl -s -H "Authorization: Bearer $1" "https://api.github.com/repos/chanzuckerberg/single-cell-data-portal/actions/runs?actor=czibuildbot" | jq -r '.workflow_runs[] | select(.display_title == "stage") | .conclusion' | head -n 1)
+else
+  checks=$(curl -s  "https://api.github.com/repos/chanzuckerberg/single-cell-data-portal/actions/runs?actor=czibuildbot" | jq -r '.workflow_runs[] | select(.display_title == "stage") | .conclusion' | head -n 1)
+fi
+
 if [ "$checks" == "success" ]; then
   echo "All checks passed in most recent staging deployment!"
 else
@@ -27,8 +32,17 @@ echo "Latest commit on 'prod' branch is: $(git log -1 --format='%H' prod)"
 echo "Latest commit on 'staging' branch is: $(git log -1 --format='%H' staging)"
 
 echo "About to merge 'staging' branch into 'prod' branch"
-git merge --verbose staging -m "Merging staging branch into prod branch"
-echo "Merge completed"
+if git merge --verbose staging -m "Merging staging branch into prod branch"; then
+  echo "Merge was Successful"
+else
+  echo "Merge has conflicts or other issues. Please resolve and try again."
+  exit 1
+fi
 
 echo "Pushing to Prod"
-git push origin prod
+if git push origin prod; then
+  echo "Successfully Promoted Staging to Prod"
+else
+  echo "Staging push to Prod failed"
+  exit 1
+fi
