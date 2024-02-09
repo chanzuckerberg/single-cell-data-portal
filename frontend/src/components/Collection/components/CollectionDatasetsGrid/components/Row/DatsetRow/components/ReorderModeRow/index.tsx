783150e240a602d4a948be82bae169cf0547576e
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2024-02-09 23:44:19 +1000

    feat: updated drag and drop action (#6483)

--- frontend/src/components/Collection/components/CollectionDatasetsGrid/components/Row/DatsetRow/components/ReorderModeRow/index.tsx
+++ frontend/src/components/Collection/components/CollectionDatasetsGrid/components/Row/DatsetRow/components/ReorderModeRow/index.tsx
@@ -3,6 +3,8 @@ import { Dataset } from "src/common/entities";
 import ReorderCell from "src/components/common/Grid/components/ReorderCell";
 import { Row } from "src/components/Collection/components/CollectionDatasetsGrid/components/Row/DatsetRow/components/ReorderModeRow/style";
 import { ReorderAction } from "src/views/Collection/hooks/useReorderMode";
+import { DragAndDropAction } from "src/views/Collection/hooks/useDropAndDrag/useDropAndDrag";
+import { SerializedStyles } from "@emotion/react";
 
 export enum DRAG_EVENT_TYPE {
   DRAG = "DRAG",
@@ -14,83 +16,108 @@ export type ReorderModeRowProps = Props;
 interface Props {
   children: ReactNode | ReactNode[];
   dataset: Dataset;
+  datasetIndex: number;
+  dragAndDropAction: DragAndDropAction;
+  draggingStyles: SerializedStyles;
   reorderAction: ReorderAction;
 }
 
 export default function ReorderModeRow({
   children,
   dataset,
+  datasetIndex,
+  dragAndDropAction,
+  draggingStyles,
   reorderAction,
 }: Props): JSX.Element {
   const { onReorder } = reorderAction;
+  const {
+    onDraggingOver,
+    onDropping,
+    onEndDragging,
+    onStartDragging,
+    onUpdateDirection,
+  } = dragAndDropAction;
   const [dragEventType, setDragEventTypeState] = useState<DRAG_EVENT_TYPE>();
 
   // Row is being dragged.
-  const onDrag = useCallback((dragEvent: DragEvent<HTMLTableRowElement>) => {
-    dragEvent.preventDefault();
-    // Set drag event type state to DRAG as dragging is in progress;
-    // now that the representation of the original row is generated,
-    // the original row styles are updated to indicate the row is being dragged.
-    setDragEventTypeState(DRAG_EVENT_TYPE.DRAG);
-  }, []);
+  const onDrag = useCallback(
+    (dragEvent: DragEvent<HTMLTableRowElement>) => {
+      dragEvent.preventDefault();
+      // Set drag event type state to DRAG as dragging is in progress;
+      // now that the representation of the original row is generated,
+      // the original row styles are updated to indicate the row is being dragged.
+      setDragEventTypeState(DRAG_EVENT_TYPE.DRAG);
+      // While dragging, update the direction of the drag event.
+      onUpdateDirection(dragEvent);
+    },
+    [onUpdateDirection]
+  );
 
   // Row dragging is successful or cancelled.
-  const onDragEnd = useCallback((dragEvent: DragEvent<HTMLTableRowElement>) => {
-    dragEvent.preventDefault();
-    dragEvent.dataTransfer.clearData();
-    // Set drag event type state to undefined as dragging is now complete; row styles revert to their default setting.
-    setDragEventTypeState(undefined);
-  }, []);
+  const onDragEnd = useCallback(
+    (dragEvent: DragEvent<HTMLTableRowElement>) => {
+      dragEvent.preventDefault();
+      dragEvent.dataTransfer.clearData();
+      // Set drag event type state to undefined as dragging is now complete; row styles revert to their default setting.
+      setDragEventTypeState(undefined);
+      onEndDragging();
+    },
+    [onEndDragging]
+  );
 
   // Row is dragging over a valid drop target.
   const onDragOver = useCallback(
-    (dragEvent: DragEvent<HTMLTableRowElement>) => {
+    (dragEvent: DragEvent<HTMLTableRowElement>, droppingIndex: number) => {
       dragEvent.preventDefault(); // Enables drop container to receive drop events.
       dragEvent.dataTransfer.dropEffect = "move";
+      // While dragging over a valid drop target, update the dropping index.
+      onDraggingOver(droppingIndex);
     },
-    []
+    [onDraggingOver]
   );
 
   // Row dragging has started; row element is referenced for dragging.
-  const onDragStart = (
-    dragEvent: DragEvent<HTMLTableRowElement>,
-    datasetID: string
-  ) => {
-    dragEvent.dataTransfer.setData("text/plain", datasetID);
-    dragEvent.dataTransfer.effectAllowed = "move";
-    // Set drag event type state to DRAG_START as dragging has started; row styles are updated to facilitate
-    // the image capture of the row being dragged. This image is generated from the drag target, and will represent
-    // the draggable row as it is being dragged.
-    setDragEventTypeState(DRAG_EVENT_TYPE.DRAG_START);
-  };
+  const onDragStart = useCallback(
+    (
+      dragEvent: DragEvent<HTMLTableRowElement>,
+      datasetID: string,
+      draggingIndex: number
+    ) => {
+      dragEvent.dataTransfer.setData("text/plain", datasetID);
+      dragEvent.dataTransfer.effectAllowed = "move";
+      // Set drag event type state to DRAG_START as dragging has started; row styles are updated to facilitate
+      // the image capture of the row being dragged. This image is generated from the drag target, and will represent
+      // the draggable row as it is being dragged.
+      setDragEventTypeState(DRAG_EVENT_TYPE.DRAG_START);
+      onStartDragging(dragEvent, draggingIndex);
+    },
+    [onStartDragging]
+  );
 
   // Row is dropped on a valid drop target.
   const onDrop = useCallback(
-    (dragEvent: DragEvent<HTMLTableRowElement>, targetDatasetID: string) => {
+    (dragEvent: DragEvent<HTMLTableRowElement>) => {
       dragEvent.preventDefault();
-      // Calculate the position of the row being dropped relative to the drop target; the position is either
-      // above or below the drop target mid-point.
-      const targetEl = dragEvent.target as HTMLTableRowElement;
-      const domRect = targetEl.getBoundingClientRect();
-      const midPoint = domRect.top + domRect.height / 2;
-      const orderPosition = Math.sign(dragEvent.clientY - midPoint);
       // Get the ID of the row being dragged.
       const datasetID = dragEvent.dataTransfer.getData("text");
+      if (!datasetID) return; // Drop unsuccessful.
       // Update the order of the datasets.
-      onReorder(datasetID, targetDatasetID, orderPosition);
+      onDropping(onReorder);
     },
-    [onReorder]
+    [onDropping, onReorder]
   );
 
   return (
     <Row
       dragEventType={dragEventType}
       draggable
+      draggingStyles={draggingStyles}
       onDrag={onDrag}
       onDragEnd={onDragEnd}
-      onDragOver={onDragOver}
-      onDragStart={(event) => onDragStart(event, dataset.id)}
-      onDrop={(event) => onDrop(event, dataset.id)}
+      onDragOver={(event) => onDragOver(event, datasetIndex)}
+      onDragStart={(event) => onDragStart(event, dataset.id, datasetIndex)}
+      onDrop={onDrop}
     >
       {/* Reordering handle */}
       <td>
