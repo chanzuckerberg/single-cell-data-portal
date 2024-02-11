a6811c092f219d9ce10e7f673060b8019f96c9ba
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2024-02-11 12:11:53 +1000

    feat: minor refactor (#6483)

--- frontend/src/views/Collection/hooks/useDragAndDrop/common/constants.ts
+++ frontend/src/views/Collection/hooks/useDragAndDrop/common/constants.ts
@@ -1,7 +1,6 @@
 import {
   Dragging,
   DRAGGING_DIRECTION,
-  OffsetByIndex,
 } from "src/views/Collection/hooks/useDragAndDrop/common/entities";
 import { css } from "@emotion/react";
 
@@ -11,13 +10,12 @@ export const DEFAULT_DRAGGING: Dragging = {
   draggingDirection: DRAGGING_DIRECTION.DOWN,
   draggingIndex: 0,
   droppingIndex: 0,
+  offsetByIndex: new Map(),
   shadowIndex: 0,
-  size: 0,
 };
 export const DEFAULT_DRAGGING_STYLES = css`
   transform: translateY(0);
   transition: none;
 `;
-export const DEFAULT_OFFSETS: OffsetByIndex = new Map();
 export const SELECTOR_TABLE_BODY = "table tbody";
 export const SELECTOR_TABLE_ROW = "tr";
