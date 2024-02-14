4feae081abdf7ccd6427cbe0e7b33381fb253b74
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2024-02-14 11:42:31 +1000

    feat: updated styles to use theme (#6483)

--- frontend/src/components/common/Grid/components/ReorderCell/style.ts
+++ frontend/src/components/common/Grid/components/ReorderCell/style.ts
@@ -1,6 +1,7 @@
 import styled from "@emotion/styled";
 import { Icon as SDSIcon } from "@czi-sds/components";
+import { spacesXxs } from "src/common/theme";
 
 export const Icon = styled(SDSIcon)`
-  margin: 4px 0;
+  margin: ${spacesXxs}px 0;
 `;
