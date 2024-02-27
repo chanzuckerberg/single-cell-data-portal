c9b3e98087422294e21ee1de8d60ef450a9ee477
MillenniumFalconMechanic <mim@clevercanary.com>
2024-02-27 13:27:40 -0800

    Linting

--- backend/database/versions/06_8bced1b1470b_add_data_submission_policy_version.py
+++ backend/database/versions/06_8bced1b1470b_add_data_submission_policy_version.py
@@ -6,9 +6,8 @@ Create Date: 2024-02-14 15:42:20.549707
 
 """
 
-from alembic import op
 import sqlalchemy as sa
-
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "06_8bced1b1470b"
