d1046428dc686f949eca0bcb288e2c45866cc51c
MillenniumFalconMechanic <mim@clevercanary.com>
2024-02-27 09:57:14 -0800

    Corrected test ID

--- frontend/src/components/Collection/components/CollectionDatasetsGrid/components/Row/DatsetRow/index.tsx
+++ frontend/src/components/Collection/components/CollectionDatasetsGrid/components/Row/DatsetRow/index.tsx
@@ -176,16 +176,16 @@ const DatasetRow: FC<Props> = ({
   const isOverMaxCellCount = checkIsOverMaxCellCount(cell_count);
 
   const Row = reorder.isReorder ? ReorderModeRow : "tr";
-  const rowPropsBase = {
-    testId: `dataset-row-${dataset.id}`,
-  };
+  const testId = `dataset-row-${dataset.id}`;
   const rowProps = reorder.isReorder
     ? {
-        ...rowPropsBase,
         dragAndDrop,
         reorder,
+        testId,
       }
-    : rowPropsBase;
+    : {
+        "data-testid": testId,
+      };
 
   return (
     <Row {...(rowProps as ReorderModeRowProps)}>
