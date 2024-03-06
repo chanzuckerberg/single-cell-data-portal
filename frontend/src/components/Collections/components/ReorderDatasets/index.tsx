a2c27ed1ee6d5f3816e15a573ebcc221d6cea39e
Mim Hastie <mim@clevercanary.com>
2024-03-06 13:18:47 -0800

    feat: Reorder collection datasets (#6483) (#6665)
    Co-authored-by: Fran McDade <franmcdade@Frans-MacBook-Pro.local>

---
+++ frontend/src/components/Collections/components/ReorderDatasets/index.tsx
@@ -0,0 +1,35 @@
+import {
+  MinimalButton,
+  SquareButton,
+} from "src/components/Collections/components/ReorderDatasets/style";
+import { ReorderAction } from "src/views/Collection/hooks/useReorder/useReorder";
+
+interface Props {
+  reorderAction: ReorderAction;
+}
+
+export default function ReorderDatasets({ reorderAction }: Props): JSX.Element {
+  const { onCancelReorder, onSaveReorder } = reorderAction;
+  return (
+    <>
+      <MinimalButton
+        data-testid="datasets-reorder-cancel"
+        isAllCaps={false}
+        onClick={onCancelReorder}
+        sdsStyle="minimal"
+        sdsType="secondary"
+      >
+        Cancel
+      </MinimalButton>
+      <SquareButton
+        data-testid="datasets-reorder-save"
+        color="success"
+        onClick={onSaveReorder}
+        sdsStyle="square"
+        sdsType="primary"
+      >
+        Save Order
+      </SquareButton>
+    </>
+  );
+}
