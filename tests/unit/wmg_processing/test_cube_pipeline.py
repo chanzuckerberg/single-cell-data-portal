d5b41d731ec6aef3bb9b8c6afb974f7d5406bb18
atarashansky <atarashansky@chanzuckerberg.com>
2024-03-20 16:44:24 -0700

    update test

--- tests/unit/wmg_processing/test_cube_pipeline.py
+++ tests/unit/wmg_processing/test_cube_pipeline.py
@@ -7,10 +7,10 @@ from unittest import mock
 from unittest.mock import Mock, patch
 
 from backend.wmg.data.snapshot import _get_wmg_snapshot_schema_dir_rel_path
-from backend.wmg.pipeline import logger, main
 from backend.wmg.pipeline.constants import MAXIMUM_ADMISSIBLE_CENSUS_SCHEMA_MAJOR_VERSION
 from backend.wmg.pipeline.expression_summary_and_cell_counts import create_expression_summary_and_cell_counts_cubes
 from backend.wmg.pipeline.load_cube import _get_wmg_snapshot_s3_fullpath
+from backend.wmg.pipeline.pipeline import logger, main
 
 
 def mock_census_schema_version_unsupported(_census):
