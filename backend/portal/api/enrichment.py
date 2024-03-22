6814662afd571baf5f48beb6e89c0c6777e1bf68
nayib-jose-gloria <ngloria@chanzuckerberg.com>
2024-03-22 13:14:58 -0400

    limit index endpoint ancestor enrichment to terms in the subtree of the curated high level ontology terms

--- backend/portal/api/enrichment.py
+++ backend/portal/api/enrichment.py
@@ -3,9 +3,27 @@ This method enriches the response of a Dataset object with ancestor mappings.
 These responses are specific to the current API layer
 """
 
+from cellxgene_ontology_guide.curated_ontology_term_lists import get_curated_ontology_term_list
+from cellxgene_ontology_guide.entities import CuratedOntologyTermList
 from cellxgene_ontology_guide.ontology_parser import OntologyParser
 
 ONTOLOGY_PARSER = OntologyParser(schema_version="v5.0.0")  # TODO: this should be a constant
+# TODO: clean-up where to set these constants
+ACCEPTED_TISSUE_ANCESTORS = set(
+    ONTOLOGY_PARSER.map_term_descendants(
+        get_curated_ontology_term_list(CuratedOntologyTermList.SYSTEM), include_self=True
+    ).values()
+)
+ACCEPTED_CELL_TYPE_ANCESTORS = set(
+    ONTOLOGY_PARSER.map_term_descendants(
+        get_curated_ontology_term_list(CuratedOntologyTermList.CELL_CLASS), include_self=True
+    ).values()
+)
+ACCEPTED_UBERON_DEVELOPMENT_STAGE_ANCESTORS = set(
+    ONTOLOGY_PARSER.map_term_descendants(
+        get_curated_ontology_term_list(CuratedOntologyTermList.UBERON_DEVELOPMENT_STAGE), include_self=True
+    ).values()
+)
 
 
 def enrich_dataset_with_ancestors(dataset, key):
@@ -18,6 +36,13 @@ def enrich_dataset_with_ancestors(dataset, key):
     unique_ancestors = set()
     for term in dataset[key]:
         ancestors = ONTOLOGY_PARSER.get_term_ancestors(term["ontology_term_id"], include_self=False)
+        # TODO: clean-up this logic
+        if is_tissue:
+            ancestors = list(set(ancestors) & ACCEPTED_TISSUE_ANCESTORS)
+        elif key == "cell_type":
+            ancestors = list(set(ancestors) & ACCEPTED_CELL_TYPE_ANCESTORS)
+        elif key == "development_stage" and term["ontology_term_id"].contains("UBERON"):
+            ancestors = list(set(ancestors) & ACCEPTED_UBERON_DEVELOPMENT_STAGE_ANCESTORS)
         # If the term is a tissue, tag itself with the tissue type in the ancestor list
         self_as_ancestor = generate_tagged_tissue_ontology_id(term) if is_tissue else term["ontology_term_id"]
         ancestors.append(self_as_ancestor)
