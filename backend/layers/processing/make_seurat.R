234b2f6214a0865fb88fef1a2de63b76e9556cd0
Bento007 <trent.smith007@gmail.com>
2024-04-23 12:26:26 -0700

    use schard to convert h5ad to seurat

--- backend/layers/processing/make_seurat.R
+++ backend/layers/processing/make_seurat.R
@@ -1,8 +1,19 @@
+install.packages('devtools')
+if (!require("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+BiocManager::install("rhdf5")
+devtools::install_github("cellgeni/schard")
+
 library(sceasy)
 
 require(devtools)
 
-h5adPath <- commandArgs(trailingOnly = TRUE)[1]
+h5adPath <- '/Users/trentsmith/workspace/single-cell-curation/cellxgene_schema_cli/tests/fixtures/h5ads/example_valid.h5ad'
+
+# load h5ad as Seurat
+converted = schard::h5ad2seurat('/Users/trentsmith/workspace/single-cell-curation/cellxgene_schema_cli/tests/fixtures/h5ads/example_valid.h5ad')
+saveRDS(converted, '/Users/trentsmith/workspace/single-cell-curation/cellxgene_schema_cli/tests/fixtures/h5ads/example_valid.rds')
+
 
 target_uns_keys <- c("schema_version",
                      "title",
@@ -19,3 +30,4 @@ sceasy::convertFormat(h5adPath,
                       outFile = gsub(".h5ad", ".rds", h5adPath),
                       main_layer = "data",
                       target_uns_keys = target_uns_keys)
+
