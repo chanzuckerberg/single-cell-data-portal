1194e30ee13fef7a5010e09f92fdc54316ad114b
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2024-06-04 12:43:53 +1000

    refactor: filter button dropdown

--- frontend/src/components/common/Filter/components/FilterLabel/index.tsx
+++ frontend/src/components/common/Filter/components/FilterLabel/index.tsx
@@ -1,13 +1,11 @@
 import React, { MouseEvent } from "react";
-import { ButtonDropdown } from "@czi-sds/components";
 import FilterLabelTooltip from "src/components/common/Filter/components/FilterLabel/components/FilterLabelTooltip";
-import { CategoryButton } from "./style";
+import { ButtonDropdown } from "./style";
 
 type OnOpenFilterFn = (event: MouseEvent<HTMLElement>) => void;
 
 interface Props {
   isDisabled: boolean;
-  isOpen: boolean;
   label: string;
   onOpenFilter: OnOpenFilterFn;
   tooltip?: string;
@@ -15,23 +13,20 @@ interface Props {
 
 export default function FilterLabel({
   isDisabled,
-  isOpen,
   label,
   onOpenFilter,
   tooltip,
 }: Props): JSX.Element {
   return (
     <FilterLabelTooltip tooltip={tooltip}>
-      <CategoryButton isOpen={isOpen}>
-        <ButtonDropdown
-          disabled={isDisabled}
-          onClick={onOpenFilter}
-          sdsStyle="minimal"
-          sdsType="secondary"
-        >
-          {label}
-        </ButtonDropdown>
-      </CategoryButton>
+      <ButtonDropdown
+        disabled={isDisabled}
+        onClick={onOpenFilter}
+        sdsStyle="minimal"
+        sdsType="secondary"
+      >
+        {label}
+      </ButtonDropdown>
     </FilterLabelTooltip>
   );
 }
--- frontend/src/components/common/Filter/components/FilterLabel/style.ts
+++ frontend/src/components/common/Filter/components/FilterLabel/style.ts
@@ -1,21 +1,15 @@
 import styled from "@emotion/styled";
-import { CommonThemeProps, fontBodyS, getColors } from "@czi-sds/components";
-import { spacesS, spacesXxxs } from "src/common/theme";
+import {
+  ButtonDropdown as SDSButtonDropdown,
+  fontBodyS,
+} from "@czi-sds/components";
+import { grey500, spacesS, spacesXxxs } from "src/common/theme";
 
-interface Props extends CommonThemeProps {
-  isOpen: boolean;
-}
-
-const grey500 = (props: CommonThemeProps) => getColors(props)?.gray[500];
-
-export const CategoryButton = styled("div")<Props>`
+export const ButtonDropdown = styled(SDSButtonDropdown)`
+  ${fontBodyS}
+  color: ${grey500};
+  font-weight: 500;
   margin: ${spacesXxxs}px 0 ${spacesS}px;
-
-  .MuiButton-root {
-    ${fontBodyS}
-    color: ${(props) => (props.isOpen ? `#000000` : grey500(props))};
-    font-weight: 500;
-    padding: 0;
-    text-transform: capitalize;
-  }
+  padding: 0;
+  text-transform: capitalize;
 `;
