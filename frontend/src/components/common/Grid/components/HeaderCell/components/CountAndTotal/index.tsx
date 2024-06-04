efd7871732a55fa0644a6e9fc25de580fe5b6637
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2024-06-04 23:00:13 +1000

    fix: collections column counter

--- frontend/src/components/common/Grid/components/HeaderCell/components/CountAndTotal/index.tsx
+++ frontend/src/components/common/Grid/components/HeaderCell/components/CountAndTotal/index.tsx
@@ -21,7 +21,7 @@ export default function CountAndTotal({
           hover={false}
           label={countAndTotal}
           sdsStyle="square"
-          sdsType="primary"
+          sdsType="secondary"
         />
       )}
     </>
