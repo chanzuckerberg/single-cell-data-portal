74835c44159bd901beaba4e87ff2250250c1d8b8
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2024-06-04 23:25:30 +1000

    fix: create collection dropdown style

--- frontend/src/components/common/Form/Dropdown/style.ts
+++ frontend/src/components/common/Form/Dropdown/style.ts
@@ -1,41 +1,16 @@
 import styled from "@emotion/styled";
 import { CommonThemeProps } from "@czi-sds/components";
-import { gray400, gray500, textPrimary } from "src/common/theme";
+import { gray400 } from "src/common/theme";
 
 interface DropdownFormProps extends CommonThemeProps {
   isSelected?: boolean;
 }
 
 export const DropdownForm = styled("div")<DropdownFormProps>`
+  grid-column: 1 / -1;
+
   .MuiButton-root {
     border-color: ${gray400};
-
-    :hover {
-      border-color: ${gray400};
-    }
-
-    height: 32px;
     width: 100%;
-  }
-
-  .MuiButton-text {
-    > span {
-      letter-spacing: -0.003em;
-
-      ${(props) =>
-        props.isSelected &&
-        `
-          color: ${textPrimary(props)};
-        `}
-    }
-
-    svg {
-      height: 16px;
-      width: 16px;
-
-      path {
-        ${(props) => (!props.isSelected ? `fill: ${gray500};` : "")}
-      }
-    }
   }
 `;
