f64e78ceca115217ecb8e1cdc6989a302a419096
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2024-06-04 18:38:41 +1000

    refactor: removed unused copy button style

--- frontend/src/components/Collections/components/Dataset/components/DownloadDataset/components/Content/components/DownloadLink/components/CopyButton/style.ts
+++
@@ -1,9 +0,0 @@
-import styled from "@emotion/styled";
-import { Button, fontBodyS } from "@czi-sds/components";
-
-export const CopyButton = styled(Button)`
-  ${fontBodyS}
-  font-weight: 500;
-  min-width: unset;
-  text-transform: none;
-`;
