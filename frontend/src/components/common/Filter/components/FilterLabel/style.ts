3f2c0f4a6cc08ad7b158717a9d0c94ae2456f1a8
Fran McDade <franmcdade@Frans-MacBook-Pro.local>
2024-06-04 12:08:48 +1000

    refactor: filter button dropdown style

--- frontend/src/components/common/Filter/components/FilterLabel/style.ts
+++ frontend/src/components/common/Filter/components/FilterLabel/style.ts
@@ -1,6 +1,6 @@
 import styled from "@emotion/styled";
 import { CommonThemeProps, fontBodyS, getColors } from "@czi-sds/components";
-import { spacesS, spacesXxs, spacesXxxs } from "src/common/theme";
+import { spacesS, spacesXxxs } from "src/common/theme";
 
 interface Props extends CommonThemeProps {
   isOpen: boolean;
@@ -15,24 +15,7 @@ export const CategoryButton = styled("div")<Props>`
     ${fontBodyS}
     color: ${(props) => (props.isOpen ? `#000000` : grey500(props))};
     font-weight: 500;
-    gap: ${spacesXxs}px;
-    justify-content: flex-start;
-    letter-spacing: -0.006em;
-    min-width: 0;
     padding: 0;
     text-transform: capitalize;
-
-    &:hover {
-      color: #000000;
-    }
-
-    .MuiButton-endIcon {
-      align-items: center;
-      display: flex;
-      height: 16px;
-      justify-content: center;
-      margin: 0;
-      width: 16px;
-    }
   }
 `;
