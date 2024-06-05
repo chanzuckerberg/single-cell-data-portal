05f4e17c0490b031cd7353eff0af55c505e85762
Bento007 <trent.smith007@gmail.com>
2024-06-05 12:25:44 -0700

    update the cxg_admin script

--- scripts/cxg_admin_scripts/schema_migration.py
+++ scripts/cxg_admin_scripts/schema_migration.py
@@ -20,7 +20,7 @@ def rollback_dataset(ctx, report_path: Path):
 
 def generate_report(ctx, execution_id: str, report_path: str, artifact_bucket: str):
     schema_migration = SchemaMigrate(ctx.obj["business_logic"], None)
-    report = schema_migration.report(execution_id=execution_id, artifact_bucket=artifact_bucket, progress=True)
+    report = schema_migration.report(execution_id=execution_id, artifact_bucket=artifact_bucket, dry_run=True)
     report_file = Path(report_path).joinpath(f"{ctx.obj['deployment']}-{execution_id}.json")
     with open(report_file, "w") as f:
         json.dump(report, f, indent=4, sort_keys=True, cls=CustomJSONEncoder)
