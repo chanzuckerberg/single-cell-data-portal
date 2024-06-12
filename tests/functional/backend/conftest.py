cb9d31a647cb3333926067e97c8aa8b517ee9a5b
Bento007 <trent.smith007@gmail.com>
2024-06-12 16:52:25 -0700

    update test to use pytest fixtures

---
+++ tests/functional/backend/conftest.py
@@ -0,0 +1,196 @@
+import base64
+import json
+import os
+import time
+
+import pytest
+import requests
+from requests.adapters import HTTPAdapter, Retry
+
+from backend.common.corpora_config import CorporaAuthConfig
+
+API_URL = {
+    "prod": "https://api.cellxgene.cziscience.com",
+    "staging": "https://api.cellxgene.staging.single-cell.czi.technology",
+    "dev": "https://api.cellxgene.dev.single-cell.czi.technology",
+    "test": "https://localhost:5000",
+    "rdev": f"https://{os.getenv('STACK_NAME', '')}-backend.rdev.single-cell.czi.technology",
+}
+
+AUDIENCE = {
+    "prod": "api.cellxgene.cziscience.com",
+    "staging": "api.cellxgene.staging.single-cell.czi.technology",
+    "test": "api.cellxgene.dev.single-cell.czi.technology",
+    "dev": "api.cellxgene.dev.single-cell.czi.technology",
+    "rdev": "api.cellxgene.dev.single-cell.czi.technology",
+}
+
+
+@pytest.fixture(scope="session")
+def config():
+    return CorporaAuthConfig()
+
+
+@pytest.fixture(scope="session")
+def deployment_stage():
+    return os.environ["DEPLOYMENT_STAGE"]
+
+
+@pytest.fixture(scope="session")
+def dataset_uri():
+    return (
+        "https://www.dropbox.com/scl/fi/y50umqlcrbz21a6jgu99z/5_0_0_example_valid.h5ad?rlkey"
+        "=s7p6ybyx082hswix26hbl11pm&dl=0"
+    )
+
+
+@pytest.fixture(scope="session")
+def visium_dataset_uri():
+    return (
+        "https://www.dropbox.com/scl/fi/lmhue0va6ihk50ivp26da/visium_small.h5ad?rlkey=n0fo4dyi1ah7ckg9kgzwlhm8s&st"
+        "=p7jzej8j&dl=0"
+    )
+
+
+@pytest.fixture(scope="session")
+def session(deployment_stage, config, tmp_path_factory, worker_id):
+    session = requests.Session()
+    retry_config = Retry(
+        total=7,
+        backoff_factor=2,
+        status_forcelist=[500, 502, 503, 504],
+        allowed_methods={"DELETE", "GET", "HEAD", "PUT", "POST"},
+    )
+    session.mount("https://", HTTPAdapter(max_retries=retry_config))
+    if deployment_stage == "rdev":
+        payload = {
+            "client_id": config.test_app_id,
+            "client_secret": config.test_app_secret,
+            "grant_type": "client_credentials",
+            "audience": "https://api.cellxgene.dev.single-cell.czi.technology/dp/v1/curator",
+        }
+        headers = {"content-type": "application/json"}
+        res = requests.post("https://czi-cellxgene-dev.us.auth0.com/oauth/token", json=payload, headers=headers)
+        res.raise_for_status()
+        access_token = res.json()["access_token"]
+        session.headers.update(Authorization=f"Bearer {access_token}")
+    yield session
+    session.close()
+
+
+@pytest.fixture(scope="session")
+def auth_token(config, session, deployment_stage):
+    username = config.functest_account_username
+    password = config.functest_account_password
+    response = session.post(
+        "https://czi-cellxgene-dev.us.auth0.com/oauth/token",
+        headers={"content-type": "application/x-www-form-urlencoded"},
+        data=dict(
+            grant_type="password",
+            username=username,
+            password=password,
+            audience=AUDIENCE.get(deployment_stage),
+            scope="openid profile email offline",
+            client_id=config.client_id,
+            client_secret=config.client_secret,
+        ),
+    )
+    response.raise_for_status()
+    return {
+        "access_token": response.json()["access_token"],
+        "id_token": response.json()["id_token"],
+    }
+
+
+@pytest.fixture(scope="session")
+def curator_cookie(auth_token):
+    return base64.b64encode(json.dumps(auth_token).encode("utf-8")).decode()
+
+
+@pytest.fixture(scope="session")
+def api_url(deployment_stage):
+    return API_URL.get(deployment_stage)
+
+
+@pytest.fixture(scope="session")
+def curation_api_access_token(session, api_url, config):
+    response = session.post(
+        f"{api_url}/curation/v1/auth/token",
+        headers={"x-api-key": config.super_curator_api_key},
+    )
+    response.raise_for_status()
+    return response.json()["access_token"]
+
+
+def assertStatusCode(actual: int, expected_response: requests.Response):
+    request_id = expected_response.headers.get("X-Request-Id")
+    assert actual == expected_response.status_code, f"{request_id=}"
+
+
+@pytest.fixture(scope="session")
+def upload_and_wait(session, api_url, curator_cookie, deployment_stage, request):
+    def _upload_and_wait(collection_id, dropbox_url, existing_dataset_id=None, cleanup=True):
+        headers = {"Cookie": f"cxguser={curator_cookie}", "Content-Type": "application/json"}
+        body = {"url": dropbox_url}
+
+        if existing_dataset_id is None:
+            res = session.post(
+                f"{api_url}/dp/v1/collections/{collection_id}/upload-links", data=json.dumps(body), headers=headers
+            )
+        else:
+            body["id"] = existing_dataset_id
+            res = session.put(
+                f"{api_url}/dp/v1/collections/{collection_id}/upload-links", data=json.dumps(body), headers=headers
+            )
+
+        res.raise_for_status()
+        dataset_id = json.loads(res.content)["dataset_id"]
+        if cleanup:
+            request.addfinalizer(lambda: session.delete(f"{api_url}/dp/v1/datasets/{dataset_id}", headers=headers))
+        assert res.status_code == requests.codes.accepted
+
+        keep_trying = True
+        expected_upload_statuses = ["WAITING", "UPLOADING", "UPLOADED"]
+        expected_conversion_statuses = ["CONVERTING", "CONVERTED", "FAILED", "UPLOADING", "UPLOADED", "NA", None]
+        timer = time.time()
+        while keep_trying:
+            res = session.get(f"{api_url}/dp/v1/datasets/{dataset_id}/status", headers=headers)
+            res.raise_for_status()
+            data = json.loads(res.content)
+            upload_status = data["upload_status"]
+            if upload_status:
+                assert upload_status in expected_upload_statuses
+
+            if upload_status == "UPLOADED":
+                cxg_status = data.get("cxg_status")
+                rds_status = data.get("rds_status")
+                h5ad_status = data.get("h5ad_status")
+                assert data.get("cxg_status") in expected_conversion_statuses
+                if cxg_status == "FAILED":
+                    pytest.fail(f"CXG CONVERSION FAILED. Status: {data}, Check logs for dataset: {dataset_id}")
+                if rds_status == "FAILED":
+                    pytest.fail(f"RDS CONVERSION FAILED. Status: {data}, Check logs for dataset: {dataset_id}")
+                if h5ad_status == "FAILED":
+                    pytest.fail(f"Anndata CONVERSION FAILED. Status: {data}, Check logs for dataset: {dataset_id}")
+                if cxg_status == rds_status == h5ad_status == "UPLOADED":
+                    keep_trying = False
+            if time.time() >= timer + 1200:
+                raise TimeoutError(
+                    f"Dataset upload or conversion timed out after 10 min. Check logs for dataset: {dataset_id}"
+                )
+            time.sleep(10)
+        return dataset_id
+
+    return _upload_and_wait
+
+
+@pytest.fixture
+def collection_data():
+    return {
+        "contact_email": "lisbon@gmail.com",
+        "contact_name": "Madrid Sparkle",
+        "curator_name": "John Smith",
+        "description": "Well here are some words",
+        "links": [{"link_name": "a link to somewhere", "link_type": "PROTOCOL", "link_url": "https://protocol.com"}],
+        "name": "my2collection",
+    }
--- tests/functional/backend/corpora/test_api.py
+++ tests/functional/backend/corpora/test_api.py
@@ -1,226 +1,200 @@
 import json
 import os
-import unittest
 
+import pytest
 import requests
+from functional.backend.conftest import assertStatusCode
 from requests import HTTPError
 
 from backend.common.constants import DATA_SUBMISSION_POLICY_VERSION
-from tests.functional.backend.common import BaseFunctionalTestCase
 
 
-class TestApi(BaseFunctionalTestCase):
-    @classmethod
-    def setUpClass(cls):
-        super().setUpClass()
+def test_version(session, api_url):
+    res = session.get(f"{api_url}/dp/v1/deployed_version")
+    res.raise_for_status()
+    assert res.status_code == requests.codes.ok
+    assert len(res.json()["Data Portal"]) > 0
 
-    def test_version(self):
-        res = self.session.get(f"{self.api}/dp/v1/deployed_version")
+
+def test_auth(session, api_url, curator_cookie):
+    headers = {"Cookie": f"cxguser={curator_cookie}", "Content-Type": "application/json"}
+    res = session.get(f"{api_url}/dp/v1/userinfo", headers=headers)
+    res.raise_for_status()
+    assert res.status_code == requests.codes.ok
+    data = json.loads(res.content)
+    assert data["email"] == "functest@example.com"
+
+
+def test_root_route(session, api_url):
+    res = session.get(f"{api_url}/")
+    res.raise_for_status()
+    assert res.status_code == requests.codes.ok
+
+
+def test_get_collections(session, api_url):
+    res = session.get(f"{api_url}/dp/v1/collections")
+    res.raise_for_status()
+    assert res.status_code == requests.codes.ok
+    data = json.loads(res.content)
+    for collection in data["collections"]:
+        assert isinstance(collection["id"], str)
+        assert isinstance(collection["created_at"], float)
+
+
+@pytest.mark.skipIf(os.environ["DEPLOYMENT_STAGE"] == "prod", "Do not make test collections public in prod")
+def test_collection_flow(session, api_url, curator_cookie, upload_and_wait, dataset_uri, collection_data):
+    # create collection
+    headers = {"Cookie": f"cxguser={curator_cookie}", "Content-Type": "application/json"}
+    res = session.post(f"{api_url}/dp/v1/collections", data=json.dumps(collection_data), headers=headers)
+    res.raise_for_status()
+    data = json.loads(res.content)
+    collection_id = data["collection_id"]
+    assertStatusCode(requests.codes.created, res)
+    assert "collection_id" in data
+
+    # Test created collection is private
+    res = session.get(f"{api_url}/dp/v1/collections", headers=headers)
+    data = json.loads(res.content)
+    private_collection_ids = []
+    for collection in data["collections"]:
+        if collection["visibility"] == "PRIVATE":
+            private_collection_ids.append(collection["id"])
+    assert collection_id in private_collection_ids
+
+    # Test update collection info
+    updated_data = {
+        "contact_email": "person@random.com",
+        "contact_name": "Doctor Who",
+        "description": "These are different words",
+        "links": [{"link_name": "The Source", "link_type": "DATA_SOURCE", "link_url": "https://datasource.com"}],
+        "name": "lots of cells",
+    }
+    res = session.put(f"{api_url}/dp/v1/collections/{collection_id}", data=json.dumps(updated_data), headers=headers)
+    res.raise_for_status()
+    data = json.loads(res.content)
+    data.pop("access_type")
+    for key in updated_data:
+        assert updated_data[key] == data[key]
+
+    upload_and_wait(collection_id, dataset_uri)
+
+    # make collection public
+    body = {"data_submission_policy_version": DATA_SUBMISSION_POLICY_VERSION}
+    res = session.post(f"{api_url}/dp/v1/collections/{collection_id}/publish", headers=headers, data=json.dumps(body))
+    res.raise_for_status()
+    assertStatusCode(requests.codes.accepted, res)
+
+    # get canonical collection_id
+    res = session.get(f"{api_url}/dp/v1/collections/{collection_id}", headers=headers)
+    data = json.loads(res.content)
+    canonical_collection_id = data["id"]
+
+    # check collection returns as public
+    res = session.get(f"{api_url}/dp/v1/collections", headers=headers)
+    data = json.loads(res.content)
+    public_collection_ids = []
+    for collection in data["collections"]:
+        if collection["visibility"] == "PUBLIC":
+            public_collection_ids.append(collection["id"])
+
+    assert canonical_collection_id in public_collection_ids
+
+    # Test everyone can retrieve a public collection
+    no_auth_headers = {"Content-Type": "application/json"}
+    res = session.get(f"{api_url}/dp/v1/collections", headers=no_auth_headers)
+    data = json.loads(res.content)
+    collection_ids = [x["id"] for x in data["collections"]]
+    assert canonical_collection_id in collection_ids
+
+    # Test a public collection cannot be tombstoned
+    res = session.delete(f"{api_url}/dp/v1/collections/{canonical_collection_id}", headers=headers)
+    assertStatusCode(requests.codes.method_not_allowed, res)
+
+    res = session.get(f"{api_url}/dp/v1/collections/{collection_id}", headers=headers)
+    assertStatusCode(requests.codes.ok, res)
+
+
+@pytest.mark.skipIf(os.environ["DEPLOYMENT_STAGE"] == "prod", "Do not make test collections public in prod")
+def test_delete_private_collection(session, api_url, curator_cookie, collection_data, request):
+    # create collection
+    headers = {"Cookie": f"cxguser={curator_cookie}", "Content-Type": "application/json"}
+    res = session.post(f"{api_url}/dp/v1/collections", data=json.dumps(collection_data), headers=headers)
+    res.raise_for_status()
+    data = json.loads(res.content)
+    collection_id = data["collection_id"]
+    request.addfinalizer(lambda: session.delete(f"{api_url}/dp/v1/collections/{collection_id}", headers=headers))
+    assertStatusCode(requests.codes.created, res)
+    assert "collection_id" in data
+
+    # check created collection returns as private
+    res = session.get(f"{api_url}/dp/v1/collections", headers=headers)
+    data = json.loads(res.content)
+    private_collection_ids = []
+    for collection in data["collections"]:
+        if collection["visibility"] == "PRIVATE":
+            private_collection_ids.append(collection["id"])
+
+    assert collection_id in private_collection_ids
+
+    # delete collection
+    res = session.delete(f"{api_url}/dp/v1/collections/{collection_id}?visibility=PRIVATE", headers=headers)
+    res.raise_for_status()
+    assertStatusCode(requests.codes.no_content, res)
+
+    # check collection gone
+    no_auth_headers = {"Content-Type": "application/json"}
+    res = session.get(f"{api_url}/dp/v1/collections?visibility=PRIVATE", headers=no_auth_headers)
+    data = json.loads(res.content)
+    collection_ids = [x["id"] for x in data["collections"]]
+    assert collection_id in collection_ids
+
+
+@pytest.mark.skipIf(os.environ["DEPLOYMENT_STAGE"] == "prod", "Do not make test collections public in prod")
+def test_dataset_upload_flow_with_visium_dataset(
+    session, curator_cookie, api_url, upload_and_wait, visium_dataset_uri, request
+):
+    headers = {"Cookie": f"cxguser={curator_cookie}", "Content-Type": "application/json"}
+    collection_id = _create_test_collection(
+        headers, request, session, api_url, "test_dataset_upload_flow_with_visium_dataset"
+    )
+    _verify_upload_and_delete_succeeded(collection_id, headers, visium_dataset_uri, session, api_url, upload_and_wait)
+
+
+def _create_test_collection(headers, request, session, api_url, name="my2collection"):
+    body = {
+        "contact_email": "lisbon@gmail.com",
+        "contact_name": "Madrid Sparkle",
+        "curator_name": "John Smith",
+        "description": "Well here are some words",
+        "links": [{"link_name": "a link to somewhere", "link_type": "PROTOCOL", "link_url": "https://protocol.com"}],
+        "name": name,
+    }
+
+    res = session.post(f"{api_url}/dp/v1/collections", data=json.dumps(body), headers=headers)
+    res.raise_for_status()
+    data = json.loads(res.content)
+    collection_id = data["collection_id"]
+    request.addfinalizer(lambda: session.delete(f"{api_url}/dp/v1/collections/{collection_id}", headers=headers))
+    assertStatusCode(requests.codes.created, res)
+    return collection_id
+
+
+def _verify_upload_and_delete_succeeded(collection_id, headers, dataset_uri, session, api_url, upload_and_wait):
+    dataset_id = upload_and_wait(collection_id, dataset_uri)
+    # test non owner cant retrieve status
+    no_auth_headers = {"Content-Type": "application/json"}
+    res = session.get(f"{api_url}/dp/v1/datasets/{dataset_id}/status", headers=no_auth_headers)
+    with pytest.raises(HTTPError):
         res.raise_for_status()
-        self.assertStatusCode(requests.codes.ok, res)
-        self.assertTrue(len(res.json()["Data Portal"]) > 0)
 
-    def test_auth(self):
-        headers = {"Cookie": f"cxguser={self.curator_cookie}", "Content-Type": "application/json"}
-        res = self.session.get(f"{self.api}/dp/v1/userinfo", headers=headers)
-        res.raise_for_status()
-        self.assertStatusCode(requests.codes.ok, res)
-        data = json.loads(res.content)
-        self.assertEqual(data["email"], "functest@example.com")
+    # Test dataset deletion
+    res = session.delete(f"{api_url}/dp/v1/datasets/{dataset_id}", headers=headers)
+    res.raise_for_status()
+    assertStatusCode(requests.codes.accepted, res)
 
-    def test_root_route(self):
-        res = self.session.get(f"{self.api}/")
-
-        res.raise_for_status()
-        self.assertStatusCode(requests.codes.ok, res)
-
-    def test_get_collections(self):
-        res = self.session.get(f"{self.api}/dp/v1/collections")
-
-        res.raise_for_status()
-        self.assertStatusCode(requests.codes.ok, res)
-        data = json.loads(res.content)
-        for collection in data["collections"]:
-            self.assertIsInstance(collection["id"], str)
-            self.assertIsInstance(collection["created_at"], float)
-
-    @unittest.skipIf(os.environ["DEPLOYMENT_STAGE"] == "prod", "Do not make test collections public in prod")
-    def test_collection_flow(self):
-        # create collection
-        data = {
-            "contact_email": "lisbon@gmail.com",
-            "contact_name": "Madrid Sparkle",
-            "curator_name": "John Smith",
-            "description": "Well here are some words",
-            "links": [
-                {"link_name": "a link to somewhere", "link_type": "PROTOCOL", "link_url": "https://protocol.com"}
-            ],
-            "name": "my2collection",
-        }
-
-        headers = {"Cookie": f"cxguser={self.curator_cookie}", "Content-Type": "application/json"}
-        res = self.session.post(f"{self.api}/dp/v1/collections", data=json.dumps(data), headers=headers)
-        res.raise_for_status()
-        data = json.loads(res.content)
-        collection_id = data["collection_id"]
-        self.assertStatusCode(requests.codes.created, res)
-        self.assertIn("collection_id", data)
-
-        with self.subTest("Test created collection is private"):
-            res = self.session.get(f"{self.api}/dp/v1/collections", headers=headers)
-            data = json.loads(res.content)
-            private_collection_ids = []
-            for collection in data["collections"]:
-                if collection["visibility"] == "PRIVATE":
-                    private_collection_ids.append(collection["id"])
-            self.assertIn(collection_id, private_collection_ids)
-
-        with self.subTest("Test update collection info"):
-            updated_data = {
-                "contact_email": "person@random.com",
-                "contact_name": "Doctor Who",
-                "description": "These are different words",
-                "links": [
-                    {"link_name": "The Source", "link_type": "DATA_SOURCE", "link_url": "https://datasource.com"}
-                ],
-                "name": "lots of cells",
-            }
-            res = self.session.put(
-                f"{self.api}/dp/v1/collections/{collection_id}", data=json.dumps(updated_data), headers=headers
-            )
-            res.raise_for_status()
-            data = json.loads(res.content)
-            data.pop("access_type")
-            for key in updated_data:
-                self.assertEqual(updated_data[key], data[key])
-
-        self.upload_and_wait(collection_id, self.test_dataset_uri)
-
-        # make collection public
-        with self.subTest("Test make collection public"):
-            body = {"data_submission_policy_version": DATA_SUBMISSION_POLICY_VERSION}
-            res = self.session.post(
-                f"{self.api}/dp/v1/collections/{collection_id}/publish", headers=headers, data=json.dumps(body)
-            )
-            res.raise_for_status()
-            self.assertStatusCode(requests.codes.accepted, res)
-
-            # get canonical collection_id
-            res = self.session.get(f"{self.api}/dp/v1/collections/{collection_id}", headers=headers)
-            data = json.loads(res.content)
-            canonical_collection_id = data["id"]
-
-            # check collection returns as public
-            res = self.session.get(f"{self.api}/dp/v1/collections", headers=headers)
-            data = json.loads(res.content)
-            public_collection_ids = []
-            for collection in data["collections"]:
-                if collection["visibility"] == "PUBLIC":
-                    public_collection_ids.append(collection["id"])
-
-            self.assertIn(canonical_collection_id, public_collection_ids)
-
-        with self.subTest("Test everyone can retrieve a public collection"):
-            no_auth_headers = {"Content-Type": "application/json"}
-            res = self.session.get(f"{self.api}/dp/v1/collections", headers=no_auth_headers)
-            data = json.loads(res.content)
-            collection_ids = [x["id"] for x in data["collections"]]
-            self.assertIn(canonical_collection_id, collection_ids)
-
-        with self.subTest("Test a public collection cannot be tombstoned"):
-            res = self.session.delete(f"{self.api}/dp/v1/collections/{canonical_collection_id}", headers=headers)
-            self.assertStatusCode(requests.codes.method_not_allowed, res)
-
-            res = self.session.get(f"{self.api}/dp/v1/collections/{collection_id}", headers=headers)
-            self.assertStatusCode(requests.codes.ok, res)
-
-    @unittest.skipIf(os.environ["DEPLOYMENT_STAGE"] == "prod", "Do not make test collections public in prod")
-    def test_delete_private_collection(self):
-        # create collection
-        data = {
-            "contact_email": "lisbon@gmail.com",
-            "contact_name": "Madrid Sparkle",
-            "curator_name": "John Smith",
-            "description": "Well here are some words",
-            "links": [
-                {"link_name": "a link to somewhere", "link_type": "PROTOCOL", "link_url": "https://protocol.com"}
-            ],
-            "name": "my2collection",
-        }
-
-        headers = {"Cookie": f"cxguser={self.curator_cookie}", "Content-Type": "application/json"}
-        res = self.session.post(f"{self.api}/dp/v1/collections", data=json.dumps(data), headers=headers)
-        res.raise_for_status()
-        data = json.loads(res.content)
-        collection_id = data["collection_id"]
-        self.addCleanup(self.session.delete, f"{self.api}/dp/v1/collections/{collection_id}", headers=headers)
-        self.assertStatusCode(requests.codes.created, res)
-        self.assertIn("collection_id", data)
-
-        # check created collection returns as private
-        res = self.session.get(f"{self.api}/dp/v1/collections", headers=headers)
-        data = json.loads(res.content)
-        private_collection_ids = []
-        for collection in data["collections"]:
-            if collection["visibility"] == "PRIVATE":
-                private_collection_ids.append(collection["id"])
-        self.assertIn(collection_id, private_collection_ids)
-
-        # delete collection
-        res = self.session.delete(f"{self.api}/dp/v1/collections/{collection_id}?visibility=PRIVATE", headers=headers)
-        res.raise_for_status()
-        self.assertStatusCode(requests.codes.no_content, res)
-
-        # check collection gone
-        no_auth_headers = {"Content-Type": "application/json"}
-        res = self.session.get(f"{self.api}/dp/v1/collections?visibility=PRIVATE", headers=no_auth_headers)
-        data = json.loads(res.content)
-        collection_ids = [x["id"] for x in data["collections"]]
-        self.assertNotIn(collection_id, collection_ids)
-
-    @unittest.skipIf(os.environ["DEPLOYMENT_STAGE"] == "prod", "Do not make test collections public in prod")
-    def test_dataset_upload_flow_with_visium_dataset(self):
-        headers = {"Cookie": f"cxguser={self.curator_cookie}", "Content-Type": "application/json"}
-        collection_id = self._create_test_collection(headers, "test_dataset_upload_flow_with_visium_dataset")
-        self._verify_upload_and_delete_succeeded(collection_id, headers, self.test_visium_dataset_uri, timeout=1200)
-
-    def _create_test_collection(self, headers, name="my2collection"):
-        body = {
-            "contact_email": "lisbon@gmail.com",
-            "contact_name": "Madrid Sparkle",
-            "curator_name": "John Smith",
-            "description": "Well here are some words",
-            "links": [
-                {"link_name": "a link to somewhere", "link_type": "PROTOCOL", "link_url": "https://protocol.com"}
-            ],
-            "name": name,
-        }
-
-        res = self.session.post(f"{self.api}/dp/v1/collections", data=json.dumps(body), headers=headers)
-        res.raise_for_status()
-        data = json.loads(res.content)
-        collection_id = data["collection_id"]
-        self.addCleanup(self.session.delete, f"{self.api}/dp/v1/collections/{collection_id}", headers=headers)
-        self.assertStatusCode(requests.codes.created, res)
-        self.assertIn("collection_id", data)
-        return collection_id
-
-    def _verify_upload_and_delete_succeeded(self, collection_id, headers, dataset_uri):
-        dataset_id = self.upload_and_wait(collection_id, dataset_uri)
-        with self.subTest("test non owner cant retrieve status"):
-            no_auth_headers = {"Content-Type": "application/json"}
-            res = self.session.get(f"{self.api}/dp/v1/datasets/{dataset_id}/status", headers=no_auth_headers)
-            with self.assertRaises(HTTPError):
-                res.raise_for_status()
-
-        with self.subTest("Test dataset deletion"):
-            res = self.session.delete(f"{self.api}/dp/v1/datasets/{dataset_id}", headers=headers)
-            res.raise_for_status()
-            self.assertStatusCode(requests.codes.accepted, res)
-
-            # Check that the dataset is gone from collection version
-            res = self.session.get(f"{self.api}/dp/v1/collections/{collection_id}", headers=headers)
-            data = json.loads(res.content)
-            datasets = data["datasets"]
-            dataset_ids = [dataset.get("id") for dataset in datasets]
-            self.assertNotIn(dataset_id, dataset_ids)
+    # Check that the dataset is gone from collection version
+    res = session.get(f"{api_url}/dp/v1/collections/{collection_id}", headers=headers)
+    data = json.loads(res.content)
+    datasets = data["datasets"]
+    dataset_ids = [dataset.get("id") for dataset in datasets]
+    assert dataset_id not in dataset_ids
