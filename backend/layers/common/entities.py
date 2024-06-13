9c8b2f5f63082e581118912dc4ea263393994f2b
nayib-jose-gloria <ngloria@chanzuckerberg.com>
2024-06-13 13:55:27 -0400

    make is_auto_version optional since its nullable

--- backend/layers/common/entities.py
+++ backend/layers/common/entities.py
@@ -297,7 +297,7 @@ class CollectionVersionBase:
     schema_version: str
     canonical_collection: CanonicalCollection
     has_custom_dataset_order: bool
-    is_auto_version: bool
+    is_auto_version: Optional[bool]
     data_submission_policy_version: str
 
     def is_published(self) -> bool:
