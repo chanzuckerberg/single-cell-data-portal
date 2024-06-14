f22b8c524309a8f3decc948143209eec71e7421b
Bento007 <trent.smith007@gmail.com>
2024-06-14 01:16:28 -0700

    fix errors

--- backend/layers/persistence/persistence.py
+++ backend/layers/persistence/persistence.py
@@ -1024,16 +1024,14 @@ class DatabaseProvider(DatabaseProviderInterface):
             # Confirm collection version datasets length matches given dataset version IDs length.
             if len(collection_version.datasets) != len(dataset_version_ids):
                 raise ValueError(
-                    f"Dataset Version IDs length does not match Collection Version {collection_version_id} Datasets "
-                    f"length"
+                    f"Dataset Version IDs length does not match Collection Version {collection_version_id} Datasets length"
                 )
 
             # Confirm all given dataset version IDs belong to collection version.
             if {dv_id.id for dv_id in dataset_version_ids} != {str(d) for d in collection_version.datasets}:
                 raise ValueError("Dataset Version IDs do not match saved Collection Version Dataset IDs")
 
-            # Replace collection version datasets with given, ordered dataset version IDs and update custom ordered
-            # flag.
+            # Replace collection version datasets with given, ordered dataset version IDs and update custom ordered flag.
             updated_datasets = [uuid.UUID(dv_id.id) for dv_id in dataset_version_ids]
             collection_version.datasets = updated_datasets
             collection_version.has_custom_dataset_order = True
