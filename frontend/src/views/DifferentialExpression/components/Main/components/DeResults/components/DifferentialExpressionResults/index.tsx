11026d81bfcfc2f40f7766da449d36dd63ff3f86
atarashansky <atarashansky@chanzuckerberg.com>
2024-06-25 13:01:57 -0700

    chore(de): truncate genes longer than 8 characters and display tooltip (#7204)

--- frontend/src/views/DifferentialExpression/components/Main/components/DeResults/components/DifferentialExpressionResults/index.tsx
+++ frontend/src/views/DifferentialExpression/components/Main/components/DeResults/components/DifferentialExpressionResults/index.tsx
@@ -262,6 +262,9 @@ const DifferentialExpressionResults = ({
           )}
           hoverable={false}
           columnIdToName={columnIdToName}
+          columnIdToNumCharactersTruncateThreshold={{
+            name: 8,
+          }}
         />
 
         <Pagination count={pageCount} page={page} onChange={handlePageChange} />
