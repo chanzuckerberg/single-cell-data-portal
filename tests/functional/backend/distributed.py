ea41a602af86e0a2f1250c77a2659d9bbc66d1e2
Bento007 <trent.smith007@gmail.com>
2024-07-03 16:15:48 -0700

    add distributed file

---
+++ tests/functional/backend/distributed.py
@@ -0,0 +1,26 @@
+import json
+from typing import Callable
+
+from filelock import FileLock
+
+
+def distributed_singleton(tmp_path_factory, worker_id: str, func: Callable) -> dict:
+    """
+    This function wraps a pytest fixture so it is only instantiated once and shared across all workers in a distributed
+    test run.
+    """
+    if worker_id != "master":
+        # not executing with multiple workers, just produce the data and let
+        # pytest's fixture caching do its job
+        return func()
+    # get the temp directory shared by all workers
+    root_tmp_dir = tmp_path_factory.getbasetemp().parent
+
+    fn = root_tmp_dir.joinpath(func.__name__ + ".json")
+    with FileLock(str(fn) + ".lock"):
+        if fn.is_file():
+            data = json.loads(fn.read_text())
+        else:
+            data = func()
+            fn.write_text(json.dumps(data))
+    return data
