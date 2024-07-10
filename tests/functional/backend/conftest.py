422d9db6138cf96fd3c86754b9f79a30f68d4fac
Trent Smith <1429913+Bento007@users.noreply.github.com>
2024-07-10 14:32:46 -0700

    chore(functional test): run in parallel (#7236)

---
+++ tests/functional/backend/conftest.py
@@ -0,0 +1,103 @@
+import os
+
+import pytest
+
+from backend.common.corpora_config import CorporaAuthConfig
+from tests.functional.backend.constants import API_URL
+from tests.functional.backend.distributed import distributed_singleton
+from tests.functional.backend.utils import (
+    get_auth_token,
+    make_cookie,
+    make_proxy_auth_token,
+    make_session,
+    upload_and_wait,
+)
+
+
+@pytest.fixture(scope="session")
+def config():
+    return CorporaAuthConfig()
+
+
+@pytest.fixture(scope="session")
+def deployment_stage():
+    return os.environ["DEPLOYMENT_STAGE"]
+
+
+@pytest.fixture(scope="session")
+def proxy_auth_token(config, deployment_stage, tmp_path_factory, worker_id) -> dict:
+    """
+    Generate a proxy token for rdev. If running in parallel mode this will be shared across workers to avoid rate
+    limiting
+    """
+
+    def _proxy_auth_token() -> dict:
+        return make_proxy_auth_token(config, deployment_stage)
+
+    return distributed_singleton(tmp_path_factory, worker_id, _proxy_auth_token)
+
+
+@pytest.fixture(scope="session")
+def session(proxy_auth_token):
+    session = make_session(proxy_auth_token)
+    yield session
+    session.close()
+
+
+@pytest.fixture(scope="session")
+def functest_auth_token(config, session, deployment_stage, tmp_path_factory, worker_id):
+    def _functest_auth_token() -> dict[str, str]:
+        username = config.functest_account_username
+        password = config.functest_account_password
+        return get_auth_token(username, password, session, config, deployment_stage)
+
+    return distributed_singleton(tmp_path_factory, worker_id, _functest_auth_token)
+
+
+@pytest.fixture(scope="session")
+def curator_cookie(functest_auth_token):
+    return make_cookie(functest_auth_token)
+
+
+@pytest.fixture(scope="session")
+def api_url(deployment_stage):
+    return API_URL.get(deployment_stage)
+
+
+@pytest.fixture(scope="session")
+def curation_api_access_token(session, api_url, config, tmp_path_factory, worker_id):
+    def _curation_api_access_token() -> str:
+        response = session.post(
+            f"{api_url}/curation/v1/auth/token",
+            headers={"x-api-key": config.super_curator_api_key},
+        )
+        response.raise_for_status()
+        return response.json()["access_token"]
+
+    return distributed_singleton(tmp_path_factory, worker_id, _curation_api_access_token)
+
+
+@pytest.fixture(scope="session")
+def upload_dataset(session, api_url, curator_cookie, request):
+    def _upload_dataset(collection_id, dropbox_url, existing_dataset_id=None):
+        result = upload_and_wait(session, api_url, curator_cookie, collection_id, dropbox_url, existing_dataset_id)
+        dataset_id = result["dataset_id"]
+        headers = {"Cookie": f"cxguser={curator_cookie}", "Content-Type": "application/json"}
+        request.addfinalizer(lambda: session.delete(f"{api_url}/dp/v1/datasets/{dataset_id}", headers=headers))
+        if result["errors"]:
+            raise pytest.fail(str(result["errors"]))
+        return dataset_id
+
+    return _upload_dataset
+
+
+@pytest.fixture()
+def collection_data(request):
+    return {
+        "contact_email": "lisbon@gmail.com",
+        "contact_name": "Madrid Sparkle",
+        "curator_name": "John Smith",
+        "description": "Well here are some words",
+        "links": [{"link_name": "a link to somewhere", "link_type": "PROTOCOL", "link_url": "https://protocol.com"}],
+        "name": request.function.__name__,
+    }
---
+++ tests/functional/backend/utils.py
@@ -0,0 +1,154 @@
+import base64
+import json
+import time
+from typing import Optional
+
+import requests
+from requests import Session
+from requests.adapters import HTTPAdapter, Retry
+
+from backend.common.corpora_config import CorporaAuthConfig
+from tests.functional.backend.constants import AUDIENCE
+
+
+def get_auth_token(
+    username: str,
+    password: str,
+    session: Session,
+    config: CorporaAuthConfig,
+    deployment_stage: str,
+    additional_claims: Optional[list] = None,
+) -> dict[str, str]:
+    standard_claims = "openid profile email offline"
+    if additional_claims:
+        additional_claims.append(standard_claims)
+        claims = " ".join(additional_claims)
+    else:
+        claims = standard_claims
+    response = session.post(
+        "https://czi-cellxgene-dev.us.auth0.com/oauth/token",  # hardcoded becasue this is only needed for dev and rdev
+        headers={"content-type": "application/x-www-form-urlencoded"},
+        data=dict(
+            grant_type="password",
+            username=username,
+            password=password,
+            audience=AUDIENCE.get(deployment_stage),
+            scope=claims,
+            client_id=config.client_id,
+            client_secret=config.client_secret,
+        ),
+    )
+    response.raise_for_status()
+    access_token = response.json()["access_token"]
+    id_token = response.json()["id_token"]
+    token = {"access_token": access_token, "id_token": id_token}
+    return token
+
+
+def make_cookie(auth_token: dict) -> str:
+    return base64.b64encode(json.dumps(auth_token).encode("utf-8")).decode()
+
+
+def assertStatusCode(actual: int, expected_response: requests.Response):
+    request_id = expected_response.headers.get("X-Request-Id")
+    assert actual == expected_response.status_code, f"{request_id=}"
+
+
+def create_test_collection(headers, request, session, api_url, body):
+    res = session.post(f"{api_url}/dp/v1/collections", data=json.dumps(body), headers=headers)
+    res.raise_for_status()
+    data = json.loads(res.content)
+    collection_id = data["collection_id"]
+    request.addfinalizer(lambda: session.delete(f"{api_url}/dp/v1/collections/{collection_id}", headers=headers))
+    assertStatusCode(requests.codes.created, res)
+    return collection_id
+
+
+def create_explorer_url(dataset_id: str, deployment_stage: str) -> str:
+    return f"https://cellxgene.{deployment_stage}.single-cell.czi.technology/e/{dataset_id}.cxg/"
+
+
+def upload_and_wait(session, api_url, curator_cookie, collection_id, dropbox_url, existing_dataset_id=None):
+    headers = {"Cookie": f"cxguser={curator_cookie}", "Content-Type": "application/json"}
+    body = {"url": dropbox_url}
+    errors = []
+    if existing_dataset_id is None:
+        res = session.post(
+            f"{api_url}/dp/v1/collections/{collection_id}/upload-links", data=json.dumps(body), headers=headers
+        )
+    else:
+        body["id"] = existing_dataset_id
+        res = session.put(
+            f"{api_url}/dp/v1/collections/{collection_id}/upload-links", data=json.dumps(body), headers=headers
+        )
+
+    res.raise_for_status()
+    dataset_id = json.loads(res.content)["dataset_id"]
+    assert res.status_code == requests.codes.accepted
+
+    keep_trying = True
+    expected_upload_statuses = ["WAITING", "UPLOADING", "UPLOADED"]
+    expected_conversion_statuses = ["CONVERTING", "CONVERTED", "FAILED", "UPLOADING", "UPLOADED", "NA", None]
+    timer = time.time()
+    while keep_trying:
+        res = session.get(f"{api_url}/dp/v1/datasets/{dataset_id}/status", headers=headers)
+        res.raise_for_status()
+        data = json.loads(res.content)
+        upload_status = data["upload_status"]
+        if upload_status:
+            assert upload_status in expected_upload_statuses
+
+        if upload_status == "UPLOADED":
+            cxg_status = data.get("cxg_status")
+            rds_status = data.get("rds_status")
+            h5ad_status = data.get("h5ad_status")
+            assert data.get("cxg_status") in expected_conversion_statuses
+            if cxg_status == "FAILED":
+                errors.append(f"CXG CONVERSION FAILED. Status: {data}, Check logs for dataset: {dataset_id}")
+            if rds_status == "FAILED":
+                errors.append(f"RDS CONVERSION FAILED. Status: {data}, Check logs for dataset: {dataset_id}")
+            if h5ad_status == "FAILED":
+                errors.append(f"Anndata CONVERSION FAILED. Status: {data}, Check logs for dataset: {dataset_id}")
+            if cxg_status == rds_status == h5ad_status == "UPLOADED" or errors:
+                keep_trying = False
+        if time.time() >= timer + 1200:
+            raise TimeoutError(
+                f"Dataset upload or conversion timed out after 10 min. Check logs for dataset: {dataset_id}"
+            )
+        time.sleep(10)
+    return {"dataset_id": dataset_id, "errors": errors}
+
+
+def make_session(proxy_auth_token):
+    retry_strategy = Retry(
+        total=7,
+        backoff_factor=2,
+        status_forcelist=[500, 502, 503, 504],
+        allowed_methods={"DELETE", "GET", "HEAD", "PUT", "POST"},
+    )
+    http_adapter = HTTPAdapter(max_retries=retry_strategy)
+    session = requests.Session()
+    session.mount("https://", http_adapter)
+    session.headers.update(**proxy_auth_token)
+    return session
+
+
+def make_proxy_auth_token(config, deployment_stage) -> dict:
+    """
+    Generate a proxy token for rdev. If running in parallel mode this will be shared across workers to avoid rate
+    limiting
+    """
+
+    if deployment_stage == "rdev":
+        payload = {
+            "client_id": config.test_app_id,
+            "client_secret": config.test_app_secret,
+            "grant_type": "client_credentials",
+            "audience": "https://api.cellxgene.dev.single-cell.czi.technology/dp/v1/curator",
+        }
+        headers = {"content-type": "application/json"}
+        res = requests.post("https://czi-cellxgene-dev.us.auth0.com/oauth/token", json=payload, headers=headers)
+        res.raise_for_status()
+        access_token = res.json()["access_token"]
+        return {"Authorization": f"Bearer {access_token}"}
+    return {}
