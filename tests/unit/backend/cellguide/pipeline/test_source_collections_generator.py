42fa07394ab50649baea632de82bfa00b962b47c
atarashansky <atarashansky@chanzuckerberg.com>
2024-07-16 09:53:16 -0700

    update test

--- tests/unit/backend/cellguide/pipeline/test_source_collections_generator.py
+++ tests/unit/backend/cellguide/pipeline/test_source_collections_generator.py
@@ -22,7 +22,7 @@ class TestSourceCollectionsGenerator(unittest.TestCase):
             expected__source_collections = json.load(f)
         with load_realistic_test_snapshot(TEST_SNAPSHOT) as snapshot:
             all_cell_type_ids_in_corpus = get_all_cell_type_ids_in_corpus(snapshot)
-            source_collections = generate_source_collections_data(all_cell_type_ids_in_corpus)
+            source_collections = generate_source_collections_data(all_cell_type_ids_in_corpus, snapshot.cell_counts_df)
             self.assertTrue(
                 compare_dicts(
                     convert_dataclass_to_dict_and_strip_nones(source_collections), expected__source_collections
