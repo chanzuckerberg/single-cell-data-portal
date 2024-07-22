a9b73538b1bfca997d6ef0aaf5f2456e6b477e90
nayib-jose-gloria <ngloria@chanzuckerberg.com>
2024-07-22 15:27:51 -0400

    rollback migration test changes

--- backend/common/census_cube/utils.py
+++ backend/common/census_cube/utils.py
@@ -15,7 +15,7 @@ from backend.common.census_cube.data.snapshot import CensusCubeSnapshot
 from backend.common.constants import DEPLOYMENT_STAGE_TO_API_URL
 
 # exported and used by all modules related to the census cube
-ontology_parser = OntologyParser(schema_version="v5.1.0")
+ontology_parser = OntologyParser()
 
 
 def find_all_dim_option_values(snapshot, organism: str, dimension: str) -> list:
