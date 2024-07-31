ef126c87f0be94e34eccf9f6047614a5317bcdac
Suzette McCanny <smccanny@deloitte.ca>
2024-07-31 11:44:24 -0400

    add survey bottom banner without hubspot

--- frontend/src/components/BottomBanner/constants.ts
+++ frontend/src/components/BottomBanner/constants.ts
@@ -1,11 +1,10 @@
-export const FORM_CONTAINER_ID = "hubspot-form-container";
-export const FORM_CONTAINER_ID_QUERY = `#${FORM_CONTAINER_ID}`;
-export const FAILED_EMAIL_VALIDATION_STRING =
-  "Please provide a valid email address.";
-export const HIDDEN_NEWSLETTER_SUCCESS_MESSAGE =
-  "Thank you for joining our newsletter.";
 export const BOTTOM_BANNER_EXPIRATION_TIME_MS = 30 * 24 * 60 * 60 * 1000; // 30 days
 export const BOTTOM_BANNER_LAST_CLOSED_TIME_KEY = "bottomBannerLastClosedTime";
+export const BOTTOM_BANNER_SURVEY_LINK_TEXT = "quick survey.";
+export const BOTTOM_BANNER_SURVEY_TEXT = "Send us feedback with this";
+
+//(smcanny) 07/24 - Newsletter constants not currently in use
+// leaving it here incase a new newsletter modal is established
 export const NEWSLETTER_SIGNUP_TITLE = "Join Our Newsletter";
 export const NEWSLETTER_SIGNUP_MESSAGE = "Thanks for subscribing!";
 export const NEWSLETTER_SIGNUP_SUCCESS_MESSAGE =
@@ -13,10 +12,10 @@ export const NEWSLETTER_SIGNUP_SUCCESS_MESSAGE =
 export const NEWSLETTER_SIGNUP_UNSUBSCRIBE_MESSAGE_UPON_SIGNUP =
   'To unsubscribe, click on the "Unsubscribe" link at the bottom of the newsletter.';
 export const NEWSLETTER_SIGNUP_UNSUBSCRIBE_MESSAGE = "Unsubscribe at any time.";
-export const HUBSPOT_URL = "https://js.hsforms.net/forms/v2.js";
 export const NEWSLETTER_SIGNUP_BANNER_SUBSCRIBE_BUTTON_TEXT = "Subscribe";
 export const NEWSLETTER_SIGNUP_BANNER_SUBSCRIBE_TEXT =
   "to our newsletter to receive updates about new features.";
-export const NEWSLETTER_SIGNUP_BANNER_SURVEY_LINK_TEXT = "quick survey.";
-export const NEWSLETTER_SIGNUP_BANNER_SURVEY_TEXT =
-  "Send us feedback with this";
+export const FAILED_EMAIL_VALIDATION_STRING =
+  "Please provide a valid email address.";
+export const HIDDEN_NEWSLETTER_SUCCESS_MESSAGE =
+  "Thank you for joining our newsletter.";
