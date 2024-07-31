2ca7562d483f2f565a7024fbd7f15766133f7325
atarashansky <atarashansky@chanzuckerberg.com>
2024-07-31 10:29:46 -0700

    chore(wmg+cellguide): remove calls to datasets and collections in discover api (#7281)

--- backend/wmg/pipeline/dataset_metadata.py
+++ backend/wmg/pipeline/dataset_metadata.py
@@ -1,9 +1,17 @@
 import json
 import logging
+import os
 
-from backend.common.census_cube.data.snapshot import DATASET_METADATA_FILENAME
-from backend.common.census_cube.utils import get_datasets_from_discover_api
-from backend.wmg.pipeline.constants import DATASET_METADATA_CREATED_FLAG
+import cellxgene_census
+import tiledb
+
+from backend.common.census_cube.data.snapshot import CELL_COUNTS_CUBE_NAME, DATASET_METADATA_FILENAME
+from backend.wmg.pipeline.constants import (
+    DATASET_METADATA_CREATED_FLAG,
+    EXPRESSION_SUMMARY_AND_CELL_COUNTS_CUBE_CREATED_FLAG,
+    CensusParameters,
+)
+from backend.wmg.pipeline.errors import PipelineStepMissing
 from backend.wmg.pipeline.utils import load_pipeline_state, log_func_runtime, write_pipeline_state
 
 logger = logging.getLogger(__name__)
@@ -18,16 +26,31 @@ def create_dataset_metadata(corpus_path: str) -> None:
     """
     logger.info("Generating dataset metadata file")
     pipeline_state = load_pipeline_state(corpus_path)
-    datasets = get_datasets_from_discover_api()
+
+    if not pipeline_state.get(EXPRESSION_SUMMARY_AND_CELL_COUNTS_CUBE_CREATED_FLAG):
+        raise PipelineStepMissing("cell_counts")
+
+    with cellxgene_census.open_soma(census_version=CensusParameters.census_version) as census:
+        dataset_metadata = census["census_info"]["datasets"].read().concat().to_pandas()
+
+    # read in the cell counts df and only keep the dataset_ids that are in the cube
+    with tiledb.open(os.path.join(corpus_path, CELL_COUNTS_CUBE_NAME)) as cc_cube:
+        cell_counts_df = cc_cube.df[:]
+        unique_dataset_ids = cell_counts_df["dataset_id"].unique()
+
+    dataset_metadata = dataset_metadata[dataset_metadata["dataset_id"].isin(unique_dataset_ids)]
+
+    datasets = dataset_metadata.to_dict(orient="records")
+
     dataset_dict = {}
     for dataset in datasets:
-        dataset_id = dataset["dataset_id"]
-        dataset_dict[dataset_id] = dict(
-            id=dataset_id,
-            label=dataset["title"],
+        dataset_dict[dataset["dataset_id"]] = dict(
+            id=dataset["dataset_id"],
+            label=dataset["dataset_title"],
             collection_id=dataset["collection_id"],
             collection_label=dataset["collection_name"],
         )
+
     logger.info("Writing dataset metadata file")
     with open(f"{corpus_path}/{DATASET_METADATA_FILENAME}", "w") as f:
         json.dump(dataset_dict, f)
