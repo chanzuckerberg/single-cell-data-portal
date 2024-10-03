f02db4d7e3a61f4b1378d81118c8e670fc5e44d6
Nayib Gloria <55710092+nayib-jose-gloria@users.noreply.github.com>
2024-10-03 11:30:07 -0400

    fix: anndata 0.10 matrix slicing bug (#7351)

--- backend/layers/processing/process_validate.py
+++ backend/layers/processing/process_validate.py
@@ -147,7 +147,7 @@ class ProcessValidate(ProcessingLogic):
             range(stride, layer_for_mean_genes_per_cell.shape[0] + stride, stride),
             strict=False,
         ):
-            chunk = layer_for_mean_genes_per_cell[bounds[0] : bounds[1], filter_gene_vars]
+            chunk = layer_for_mean_genes_per_cell[bounds[0] : bounds[1], :][:, filter_gene_vars]
             numerator += chunk.nnz if hasattr(chunk, "nnz") else numpy.count_nonzero(chunk)
             denominator += chunk.shape[0]
 
