a0503197ea058c32f6d937420644b82c7255cdc1
Bento007 <trent.smith007@gmail.com>
2025-02-09 10:20:11 -0800

    fix docstring

--- backend/layers/common/ingestion_manifest.py
+++ backend/layers/common/ingestion_manifest.py
@@ -1,6 +1,29 @@
-from typing import Optional
+import re
+from typing import Optional, Union
 
-from pydantic import BaseModel, HttpUrl
+from pydantic import BaseModel, Field, HttpUrl
+
+
+class S3Path(BaseModel):
+    """Pydantic Model for S3 URLs
+
+    Copied from https://gist.github.com/rajivnarayan/c38f01b89de852b3e7d459cfde067f3f
+    # TODO consolidate with backend/common/utils/dl_sources/uri.py
+    """
+
+    # https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html
+    _s3_pattern = re.compile(
+        r"^s3://"
+        r"(?=[a-z0-9])"  # Bucket name must start with a letter or digit
+        r"(?!(^xn--|sthree-|sthree-configurator|.+-s3alias$))"  # Bucket name must not start with xn--, sthree-, sthree-configurator or end with -s3alias
+        r"(?!.*\.\.)"  # Bucket name must not contain two adjacent periods
+        r"[a-z0-9][a-z0-9.-]{1,61}[a-z0-9]"  # Bucket naming constraints
+        r"(?<!\.-$)"  # Bucket name must not end with a period followed by a hyphen
+        r"(?<!\.$)"  # Bucket name must not end with a period
+        r"(?<!-$)"  # Bucket name must not end with a hyphen
+        r"(/([a-zA-Z0-9._-]+/?)*)?$"  # key naming constraints
+    )
+    url: str = Field(..., strip_whitespace=True, pattern=_s3_pattern, min_length=8, max_length=1023)
 
 
 class IngestionManifest(BaseModel):
@@ -12,5 +35,5 @@ class IngestionManifest(BaseModel):
     json_output = data_obj.model_dump_json(indent=2)
     """
 
-    anndata: HttpUrl
-    atac_seq_fragment: Optional[HttpUrl] = None  # Optional field
+    anndata: Union[HttpUrl, S3Path]
+    atac_seq_fragment: Optional[Union[HttpUrl, S3Path]] = None  # Optional field
