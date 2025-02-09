e21cfad1303443f51465899577a254eeacbde4c5
Bento007 <trent.smith007@gmail.com>
2025-02-09 09:18:08 -0800

    fix tests

--- tests/unit/backend/common/test_manifest.py
+++ tests/unit/backend/common/test_manifest.py
@@ -7,10 +7,10 @@ from backend.layers.common.ingestion_manifest import IngestionManifest
 @pytest.mark.parametrize(
     "manifest",
     [
-        {"anndata": "https://example.com/dataset.h5ad"},
-        {"anndata": "s3://bucket/dataset.h5ad"},
-        {"anndata": "https://example.com/dataset.h5ad", "atac_seq_fragments": "https://example.com/fragments.tsv.gz"},
-        {"anndata": "https://example.com/dataset.h5ad", "atac_seq_fragments": "s3://bucket/fragments.tsv.gz"},
+        """{"anndata": "https://example.com/dataset.h5ad"}""",
+        """{"anndata": "s3://bucket/dataset.h5ad"}""",
+        """{"anndata": "https://example.com/dataset.h5ad", "atac_seq_fragments": "https://example.com/fragments.tsv.gz"}""",
+        """{"anndata": "https://example.com/dataset.h5ad", "atac_seq_fragments": "s3://bucket/fragments.tsv.gz"}""",
     ],
 )
 def test_anndata_validation_success(manifest):
@@ -19,7 +19,7 @@ def test_anndata_validation_success(manifest):
 
 @pytest.mark.parametrize(
     "manifest",
-    [{"atac_seq_fragments": "https://example.com/fragments.tsv.gz"}, {"anndata": 1234}],
+    ["""{"atac_seq_fragments": "https://example.com/fragments.tsv.gz"}""", """{"anndata": 1234}"""],
 )
 def test_anndata_validation_failure(manifest):
     with pytest.raises(pydantic.ValidationError):
