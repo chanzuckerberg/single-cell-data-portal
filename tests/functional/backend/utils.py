93f1ace035ddf7ee95786d49e5b0db45a20df2e4
Joyce Yan <5653616+joyceyan@users.noreply.github.com>
2025-02-28 11:03:32 -0800

    fix: visium test in schema 5.3 branch (#7462)

--- tests/functional/backend/utils.py
+++ tests/functional/backend/utils.py
@@ -123,7 +123,8 @@ def upload_and_wait(session, api_url, curator_cookie, collection_id, dropbox_url
                 keep_trying = False
         if time.time() >= timer + 1200:
             raise TimeoutError(
-                f"Dataset upload or conversion timed out after 20 min. Check logs for dataset: {dataset_id}"
+                f"Dataset upload or conversion timed out after 10 min. Check logs for dataset: {dataset_id}"
+                f"upload_status: {upload_status} cxg_status: {cxg_status}, rds_status: {rds_status}, h5ad_status: {h5ad_status}"
             )
         time.sleep(10)
     return {"dataset_id": dataset_id, "errors": errors}
