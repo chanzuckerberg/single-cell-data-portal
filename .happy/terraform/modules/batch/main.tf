a754791e003bae5eb1bd577ccd2ae2dd49397d89
Bento007 <trent.smith007@gmail.com>
2025-03-27 09:12:35 -0700

    give a little memory back to os

--- .happy/terraform/modules/batch/main.tf
+++ .happy/terraform/modules/batch/main.tf
@@ -107,7 +107,7 @@ resource aws_batch_job_definition atac_job_def {
   container_properties = jsonencode({
   "jobRoleArn": "${var.batch_role_arn}",
   "image": "${var.image}",
-  "memory": 64000,
+  "memory": 62000,
   "environment": [
     {
       "name": "ARTIFACT_BUCKET",
