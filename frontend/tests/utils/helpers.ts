997602da553291e5167f5dfdf43bafe6fd21f828
kaloster <rkalo@contractor.chanzuckerberg.com>
2025-04-14 17:06:58 -0400

    Increase timeout

--- frontend/tests/utils/helpers.ts
+++ frontend/tests/utils/helpers.ts
@@ -53,7 +53,7 @@ export const shouldUseRdevToken =
   process.env.RDEV_TOKEN === "true" || TEST_ENV === "rdev";
 
 export const TIMEOUT_MS = 5 * 1000;
-export const WAIT_FOR_TIMEOUT_MS = 6 * 1000;
+export const WAIT_FOR_TIMEOUT_MS = 7 * 1000;
 
 // (seve): We use TEST_ENV to describe the environment that playwright is running against. Sometimes the FE tests are run against a local instance of the app which points at a deployed instance of the backend.
 
@@ -149,7 +149,7 @@ export async function tryUntil(
     silent = false,
   }: TryUntilConfigs
 ): Promise<void> {
-  const WAIT_FOR_MS = 1000;
+  const WAIT_FOR_MS = 2000;
 
   const startTime = Date.now();
 
