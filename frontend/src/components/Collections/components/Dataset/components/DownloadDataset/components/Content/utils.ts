4cd772d683420e0694ffc7cdab999c17e02ee41f
Suzette McCanny <smccanny@deloitte.ca>
2025-04-18 17:06:13 -0400

    fix: patch Multiome Download - front end fixes (#7539)

--- frontend/src/components/Collections/components/Dataset/components/DownloadDataset/components/Content/utils.ts
+++ frontend/src/components/Collections/components/Dataset/components/DownloadDataset/components/Content/utils.ts
@@ -5,17 +5,22 @@ import { apiTemplateToUrl } from "src/common/utils/apiTemplateToUrl";
 import { API_URL } from "src/configs/configs";
 import { API } from "src/common/API";
 import { DatasetAsset } from "src/common/entities";
+import { getNotAvailableText } from "./components/DataFormat/constants";
 
 export const downloadMultipleFiles = (
   formatsToDownload: DATASET_ASSET_FORMAT[],
   downloadLinks: DownloadLinkType[]
 ) => {
-  downloadLinks.forEach((download) => {
-    if (formatsToDownload.includes(download.filetype) && download.downloadURL) {
+  formatsToDownload.forEach((format) => {
+    const downloadLink = downloadLinks.find((link) => link.filetype === format);
+    if (
+      downloadLink &&
+      downloadLink.downloadURL &&
+      downloadLink.downloadURL !== getNotAvailableText(downloadLink.filetype)
+    ) {
       const a = document.createElement("a");
-      a.href =
-        "https://datasets.cellxgene.staging.single-cell.czi.technology/542d0a93-9f13-4a77-8d3f-65172615bce8.h5ad";
-      a.download = download.downloadURL.split("/").pop() || "";
+      a.href = downloadLink.downloadURL;
+      a.download = downloadLink.downloadURL.split("/").pop() || "";
       document.body.appendChild(a);
       a.click();
       document.body.removeChild(a);
@@ -36,7 +41,6 @@ export const getDownloadLink = async (
       ...DEFAULT_FETCH_OPTIONS,
       method: "GET",
     }).then((res) => res.json());
-    console.log("result", result);
     return {
       filetype: asset.filetype,
       fileSize: result.file_size,
