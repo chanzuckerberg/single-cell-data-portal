3adfce5c648eb5ab6246af975df6e9f73df9e466
Joyce Yan <5653616+joyceyan@users.noreply.github.com>
2025-05-13 13:41:04 -0400

    read organism terms from uns

--- backend/layers/processing/process_add_labels.py
+++ backend/layers/processing/process_add_labels.py
@@ -155,9 +155,15 @@ class ProcessAddLabels(ProcessingLogic):
             else:
                 return None
 
+        def _get_organism_terms() -> List[OntologyTermId]:
+            if "organism" in adata.uns and "organism_ontology_term_id" in adata.uns:
+                return [adata.uns["organism"], adata.uns["organism_ontology_term_id"]]
+            else:
+                return _get_term_pairs("organism")
+
         return DatasetMetadata(
             name=adata.uns["title"],
-            organism=_get_term_pairs("organism"),
+            organism=_get_organism_terms(),
             tissue=_get_tissue_terms(),
             assay=_get_term_pairs("assay"),
             disease=_get_term_pairs("disease"),
