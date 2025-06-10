29431829b83b1c23f948e23eee0bd0d4af2fe1ac
Ronen <kaloster@users.noreply.github.com>
2025-06-10 11:05:11 -0400

    fix: Up CG batch memory limit (#7604)

--- .happy/terraform/envs/prod/main.tf
+++ .happy/terraform/envs/prod/main.tf
@@ -17,7 +17,7 @@ module stack {
   wmg_batch_container_memory_limit = 248000
   wmg_desired_vcpus                = 128
   cg_desired_vcpus                 = 128
-  cg_batch_container_memory_limit  = 248000
+  cg_batch_container_memory_limit  = 496000
   backend_memory               = 6 * 1024
   frontend_memory              = 4 * 1024
   backend_instance_count       = 2
