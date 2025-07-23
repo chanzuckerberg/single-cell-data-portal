2050eb39bdcd75e1b4926f97ef5947f2dd52c31f
Ronen <kaloster@users.noreply.github.com>
2025-07-23 12:06:19 -0400

    chore: Update resources for CXG sfn worker (#7622)

--- backend/layers/processing/h5ad_data_file.py
+++ backend/layers/processing/h5ad_data_file.py
@@ -127,7 +127,7 @@ class H5ADDataFile:
             {
                 "num_workers": 1,  # a single worker with as many threads as vCPUs is more memory efficient
                 "threads_per_worker": 2,  # match the number of vCPUs
-                "distributed.worker.memory.limit": "14GB",
+                "distributed.worker.memory.limit": "28GB",
                 "scheduler": "threads",
             }
         ):
