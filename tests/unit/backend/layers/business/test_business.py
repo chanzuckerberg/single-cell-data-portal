1d05b00f84c333e601045610d8b7ea5be44d24d5
Bento007 <trent.smith007@gmail.com>
2025-09-05 17:47:16 -0700

    fix a test

--- tests/unit/backend/layers/business/test_business.py
+++ tests/unit/backend/layers/business/test_business.py
@@ -1043,9 +1043,8 @@ class TestUpdateCollectionDatasets(BaseBusinessLogicTestCase):
 
         self.uri_provider.validate = Mock(return_value=False)
 
-        with self.assertRaises(DatasetIngestException) as ex:
+        with self.assertRaises(InvalidURIException):
             self.business_logic.ingest_dataset(version.version_id, url, None, None)
-        self.assertEqual(str(ex.exception), "Trying to upload invalid URI: http://bad.url/")
 
         self.step_function_provider.start_step_function.assert_not_called()
 
