d28dbb5b4561bb6664b7dcb2be2a962cef122fe6
kaloster <rkalo@contractor.chanzuckerberg.com>
2025-09-11 17:28:22 -0400

    feat: Benchmarks banner

--- frontend/src/components/Layout/index.tsx
+++ frontend/src/components/Layout/index.tsx
@@ -13,10 +13,35 @@ interface Props {
 const Layout = ({ children }: Props) => {
   const { pathname } = useRouter();
 
+  const showBanner = () => {
+    const now = new Date();
+    const startDate = new Date("2025-08-25");
+    const endDate = new Date("2026-09-25");
+    return now >= startDate && now <= endDate;
+  };
+
   if (pathname === ROUTES.HOMEPAGE || pathname === ROUTES.SITEMAP) {
     return (
       <>
-        <LandingHeader data-testid="landing-footer" banner={false} />
+        <LandingHeader
+          data-testid="landing-footer"
+          banner={showBanner()}
+          bannerOptions={{
+            content: (
+              <p>
+                New! Explore our{" "}
+                <a
+                  href="https://virtualcellmodels.cziscience.com/benchmarks"
+                  target="_blank"
+                >
+                  benchmarking
+                </a>{" "}
+                suite to evaluate single-cell foundation model performance on
+                tasks like cell clustering and cell type classification.
+              </p>
+            ),
+          }}
+        />
         {children}
         <LandingFooter />
       </>
