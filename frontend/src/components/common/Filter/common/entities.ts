248b52c2152a5df649bc8e38d04c8f8636e3db3e
kaloster <rkalo@contractor.chanzuckerberg.com>
2025-09-18 11:55:44 -0400

    fix: Update date + enum

--- frontend/src/components/common/Filter/common/entities.ts
+++ frontend/src/components/common/Filter/common/entities.ts
@@ -786,6 +786,7 @@ export interface Testable {
  * Set of tissue types.
  */
 export enum TISSUE_TYPE {
+  CELL_CULTURE = "cell culture", // TODO: remove after schema 7.0 migration
   PRIMARY_CELL_CULTURE = "primary cell culture",
   ORGANOID = "organoid",
   TISSUE = "tissue",
