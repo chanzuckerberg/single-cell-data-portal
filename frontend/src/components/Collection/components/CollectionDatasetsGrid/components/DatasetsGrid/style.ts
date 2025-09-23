571d4fb8de47f39b9f777b790ba7a4ed618fe493
Erin Hoops <erin.hoops+gh@chanzuckerberg.com>
2025-09-23 11:56:57 -0700

    linting

--- frontend/src/components/Collection/components/CollectionDatasetsGrid/components/DatasetsGrid/style.ts
+++ frontend/src/components/Collection/components/CollectionDatasetsGrid/components/DatasetsGrid/style.ts
@@ -8,11 +8,11 @@ interface DatasetsGridProps extends CommonThemeProps {
 }
 
 export const DatasetsGrid = styled(Grid)<DatasetsGridProps>`
+  ${({ dragAndDropStyles }) =>
     dragAndDropStyles &&
     css`
       tbody tr {
         ${dragAndDropStyles}
       }
     `}
-
 `;
