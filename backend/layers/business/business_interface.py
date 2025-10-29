4a839c2f9a33e11b99e61d4b6406b1e6ff61f927
Trent Smith <1429913+Bento007@users.noreply.github.com>
2025-10-29 14:31:01 -0700

    fix: cleanup delete logic (#7675)

--- backend/layers/business/business_interface.py
+++ backend/layers/business/business_interface.py
@@ -77,10 +77,10 @@ class BusinessLogicInterface:
         """
         pass
 
-    def delete_artifacts(self, artifacts: List[DatasetArtifact]) -> None:
+    def delete_artifacts_from_private_bucket(self, artifacts: List[DatasetArtifact]) -> None:
         pass
 
-    def delete_dataset_versions_from_public_bucket(self, dataset_versions: List[DatasetVersion]) -> List[str]:
+    def delete_artifacts_from_public_bucket(self, dataset_versions: List[DatasetVersion]) -> List[str]:
         pass
 
     def delete_all_dataset_versions_from_public_bucket_for_collection(self, collection_id: CollectionId) -> List[str]:
