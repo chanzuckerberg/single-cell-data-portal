5a70b78d085dde8544e22f0a63e68c5046e7898c
Mike Lin <mlin@contractor.chanzuckerberg.com>
2026-01-23 10:09:31 -1000

    Revert "chore: VC-4962 update Authlib & urllib (#7703)"
    This reverts commit b5479404f846858e37852f7813996686b638bf7d.
    
    Major authlib upgrade from 5-year-old version broke staging, further dev/debugging needed.

--- backend/portal/api/app/v1/authentication.py
+++ backend/portal/api/app/v1/authentication.py
@@ -7,7 +7,8 @@ from typing import Optional
 from urllib.parse import urlencode
 
 import requests
-from authlib.integrations.flask_client import FlaskOAuth2App, OAuth
+from authlib.integrations.flask_client import OAuth
+from authlib.integrations.flask_client.remote_app import FlaskRemoteApp
 from flask import Response, after_this_request, current_app, g, jsonify, make_response, redirect, request, session
 
 from backend.common.authorizer import assert_authorized_token, get_userinfo_from_auth0
@@ -16,10 +17,10 @@ from backend.common.corpora_config import CorporaAuthConfig
 # global oauth client
 from backend.common.utils.http_exceptions import ExpiredCredentialsError, UnauthorizedError
 
-oauth_client: Optional[FlaskOAuth2App] = None
+oauth_client = None
 
 
-def get_oauth_client(config: CorporaAuthConfig) -> FlaskOAuth2App:
+def get_oauth_client(config: CorporaAuthConfig) -> FlaskRemoteApp:
     """Create an oauth client on the first invocation, then return oauth client for subsequent calls.
 
     :param config:  An object containing the auth configuration.
